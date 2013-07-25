65b8ff6e67e76e7334f77f6fa7579253fa9065a9
Patrick Quinn <patrick@element84.com>
2013-07-25 16:47:08 -0400

    Add HTML5 boilderplate

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
+//= require modernizr-2.6.2-respond-1.1.0.min
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
