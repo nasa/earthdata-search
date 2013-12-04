ce0b3b36bd9c28fcb2fc21aab66b40d9e23bba99
Patrick Quinn <patrick@element84.com>
2013-12-04 15:53:13 -0500

    Implement projection switcher

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,8 +4,6 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
 
-require 'helpers/shared_browser_session'
-
 require 'capybara-screenshot/rspec'
 require 'capybara-webkit'
 
@@ -19,7 +17,7 @@ Capybara.default_driver = :webkit
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
-Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+Dir[Rails.root.join("spec/helpers/**/*.rb")].each { |f| require f }
 
 # Checks for pending migrations before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
