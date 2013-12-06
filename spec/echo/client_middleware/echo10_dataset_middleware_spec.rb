4cf5f4e1b78cdde0641fa1a33fa2bf1def20fc9e
Patrick Quinn <patrick@element84.com>
2013-12-06 09:29:20 -0500

    Update Echo10DatasetMiddleware spec with notes on how to make the test more complete

--- spec/echo/client_middleware/echo10_dataset_middleware_spec.rb
+++ spec/echo/client_middleware/echo10_dataset_middleware_spec.rb
@@ -5,6 +5,8 @@ describe Echo::ClientMiddleware::Echo10DatasetMiddleware do
 
   context 'when given parsed Echo10 dataset data' do
     let(:env) do
+      # TODO Provide a more complete example so you can test transformations
+      #      of science keywords, online access urls, etc
       { body: {
           'Collection' => {
             'ShortName' => '',
@@ -29,13 +31,20 @@ describe Echo::ClientMiddleware::Echo10DatasetMiddleware do
       }
     end
 
+    let(:expected_body) do
+      {
+       'collection' => {
+         # TODO Create a JSON representation of the parsed dataset here
+       }
+      }
+    end
+
     it 'updates the parsed body to contain a list of datasets' do
-      original_body = env[:body]
       middleware.process_response(env)
-      datasets = env[:body]
+      datasets = env[:body].dup
       expect(datasets).to be_instance_of(Array)
       expect(datasets.size).to eq(1)
-      expect(datasets[0]).to eq(original_body["Collection"])
+      expect(datasets[0].as_json).to eq(expected_body['collection'])
     end
 
   end
