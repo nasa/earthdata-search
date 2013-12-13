febca72683272e5bc4785321de33998145def06e
Matthew Crouch <matthew@element84.com>
2013-12-13 13:55:16 -0500

    EDSC-37, added a button to clear all search filters

--- spec/features/map/spatial_selection_spec.rb
+++ spec/features/map/spatial_selection_spec.rb
@@ -4,37 +4,6 @@
 require "spec_helper"
 
 describe "Spatial" do
-
-  # Helpers
-  def choose_tool_from_site_toolbar(name)
-    # CSS mouseovers in capybara are screwy, so this is a bit of a hack
-    # And selenium freaks out if we try to use jQuery to do this
-    # We resort to dealing directly with the click handler
-    script = "edsc.models.searchModel.ui.spatialType.select#{name}()"
-    page.evaluate_script(script)
-  end
-
-  def choose_tool_from_map_toolbar(name)
-    within '#map' do
-      click_link "Search by spatial #{name.downcase}"
-    end
-  end
-
-  def create_point(lat=0, lon=0)
-    script = "edsc.models.searchModel.query.spatial('point:#{lon},#{lat}')"
-    page.evaluate_script(script)
-  end
-
-  def create_bounding_box(lat0=0, lon0=0, lat1=10, lon1=10)
-    script = "edsc.models.searchModel.query.spatial('bounding_box:#{lon0},#{lat0}:#{lon1},#{lat1}')"
-    page.evaluate_script(script)
-  end
-
-  def clear_spatial
-    script = "edsc.models.searchModel.query.spatial(null)"
-    page.evaluate_script(script)
-  end
-
   before do
     visit "/"
 
