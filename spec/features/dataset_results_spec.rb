cead52a9280bbe6e49b5ff4d9ed5697042460914
Patrick Quinn <patrick@element84.com>
2014-01-06 11:29:50 -0500

    Load the top 20 results by default, ensuring that tall screens can still scroll to see more results

--- spec/features/dataset_results_spec.rb
+++ spec/features/dataset_results_spec.rb
@@ -14,21 +14,19 @@ describe "Dataset results" do
 
   shared_browser_session do
     it "displays the first 10 datasets when first visiting the page" do
-      expect(page).to have_css('#dataset-results .panel-list-item', count: 10)
+      expect(page).to have_css('#dataset-results .panel-list-item', count: 20)
     end
 
     it "loads more results when the user scrolls to the bottom of the current list" do
       page.execute_script "$('#dataset-results .master-overlay-content')[0].scrollTop = 10000"
-      expect(page).to have_css('#dataset-results .panel-list-item', count: 20)
+      expect(page).to have_css('#dataset-results .panel-list-item', count: 40)
     end
 
     it "does not load additional results after all results have been loaded" do
       fill_in "keywords", with: "AST"
-      expect(page).to have_css('#dataset-results .panel-list-item', count: 10)
-      page.execute_script "$('#dataset-results .master-overlay-content')[0].scrollTop = 10000"
       expect(page).to have_css('#dataset-results .panel-list-item', count: 20)
       page.execute_script "$('#dataset-results .master-overlay-content')[0].scrollTop = 10000"
-      expect(page).to have_css('#dataset-results .panel-list-item', count: 22)
+      expect(page).to have_css('#dataset-results .panel-list-item', count: 23)
       expect(page).to have_no_content('Loading datasets...')
     end
 
