7f31593afb950c1347e9f88cbccff4a4b970b387
Matthew Crouch <matthew@element84.com>
2014-01-14 11:40:40 -0500

    took the numbers out of the specs, they aren't constant

--- spec/features/dataset_facets/dataset_facets_spec.rb
+++ spec/features/dataset_facets/dataset_facets_spec.rb
@@ -10,55 +10,55 @@ describe "Dataset Facets" do
 
   context "facet listings" do
     it "shows the first Campaign facet" do
-      expect(page).to have_content("Campaigns EOSDIS (1552)")
+      expect(page).to have_content("Campaigns EOSDIS")
     end
     
     it "shows the first Platforms facet" do
-      expect(page).to have_content("Platforms FIELD INVESTIGATION (486)")
+      expect(page).to have_content("Platforms FIELD INVESTIGATION")
     end
     
     it "shows the first Instruments facet" do
-      expect(page).to have_content("Instruments ANALYSIS (250)")
+      expect(page).to have_content("Instruments ANALYSIS")
     end
     
     it "shows the first Sensors facet" do
-      expect(page).to have_content("Sensors ANALYSIS (250)")
+      expect(page).to have_content("Sensors ANALYSIS")
     end
     
     it "shows the first 2D Coordinate Name facet" do
-      expect(page).to have_content("2D Coordinate Name MODIS Tile SIN (65)")
+      expect(page).to have_content("2D Coordinate Name MODIS Tile SIN")
     end
     
     it "shows the first Category Keyword facet" do
-      expect(page).to have_content("Category Keyword EARTH SCIENCE (12230)")
+      expect(page).to have_content("Category Keyword EARTH SCIENCE")
     end
     
     it "shows the first Topic Keyword facet" do
-      expect(page).to have_content("Topic Keyword ATMOSPHERE (6099)")
+      expect(page).to have_content("Topic Keyword ATMOSPHERE")
     end
     
     it "shows the first Term Keyword facet" do
-      expect(page).to have_content("Term Keyword VEGETATION (904)")
+      expect(page).to have_content("Term Keyword VEGETATION")
     end
     
     it "shows the first Variable Level 1 Keyword facet" do
-      expect(page).to have_content("Variable Level 1 Keyword AIR TEMPERATURE (354)")
+      expect(page).to have_content("Variable Level 1 Keyword AIR TEMPERATURE")
     end
     
     it "shows the first Variable Level 2 Keyword facet" do
-      expect(page).to have_content("Variable Level 2 Keyword OZONE (143)")
+      expect(page).to have_content("Variable Level 2 Keyword OZONE")
     end
     
     it "shows the first Variable Level 3 Keyword facet" do
-      expect(page).to have_content("Variable Level 3 Keyword TOOTHED WHALES (3)")
+      expect(page).to have_content("Variable Level 3 Keyword TOOTHED WHALES")
     end
     
     it "shows the first Detailed Variable Keyword facet" do
-      expect(page).to have_content("Detailed Variable Keyword TOTAL OZONE (29)")
+      expect(page).to have_content("Detailed Variable Keyword TOTAL OZONE")
     end
     
     it "shows the first Processing Level facet" do
-      expect(page).to have_content("Processing Level 3 (1541)")
+      expect(page).to have_content("Processing Level 3")
     end
   end
 end
