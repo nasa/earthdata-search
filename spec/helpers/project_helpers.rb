a66e61f7c7b6f5d7601b44819a4b6e6fd7dc6d6c
Patrick Quinn <patrick@element84.com>
2014-02-14 13:36:03 -0500

    EDSC-122: Add specs for data direct download

--- spec/helpers/project_helpers.rb
+++ spec/helpers/project_helpers.rb
@@ -1,6 +1,13 @@
 module Helpers
   module ProjectHelpers
 
+    def add_dataset_to_project(id, name)
+      fill_in "keywords", with: id
+      expect(page).to have_content(name)
+      expect(page).to have_css('#dataset-results .panel-list-item', count: 1)
+      first_dataset_result.click_link "Add dataset to the current project"
+    end
+
     def reset_project
       page.evaluate_script('edsc.models.page.current.project.datasets([])')
       page.evaluate_script('edsc.models.page.current.project.datasets()') # Read it back out to ensure it was applied
