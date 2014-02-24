8c3c8d3708db6287698a0fb6554703a0eea3dfa6
Patrick Quinn <patrick@element84.com>
2014-02-24 13:30:16 -0500

    EDSC-96: Show browse imagery at selected point

--- spec/features/map/map_granule_info_spec.rb
+++ spec/features/map/map_granule_info_spec.rb
@@ -1,6 +1,9 @@
 # EDSC-93: As a user, I want to see an indication of granule counts under my
 #          mouse cursor so I may find more information about granules under
 #          that point
+# EDSC-95: As a user, I want to see basic granule information such as start
+#          and end time at a point I select so I may find the most desirable
+#          granules at a particular location
 
 require "spec_helper"
 
