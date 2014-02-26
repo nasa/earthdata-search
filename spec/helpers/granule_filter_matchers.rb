73af8b68fe50256aad297b1a7ebaeb4b00b939c1
Matthew Crouch <matthew@element84.com>
2014-02-26 09:52:40 -0500

    EDSC-65, added indication of granule filters applied

--- spec/helpers/granule_filter_matchers.rb
+++ spec/helpers/granule_filter_matchers.rb
@@ -2,6 +2,7 @@ RSpec::Matchers.define :filter_granules_from do |expected|
   match do |selector|
     click_button "granule-filters-submit"
     expect(selector).to have_no_content(expected.to_s + ' Granules')
+    expect(selector).to have_css(".granule-filters-applied")
 
     number_granules = expect(selector.text).to match_regex /\d+ Granule/
     after_granule_count = number_granules.to_s.split(" ")[0].to_i
