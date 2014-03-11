df78a688f89002f3603e8a108b6691c71a2df074
Patrick Quinn <patrick@element84.com>
2014-03-11 13:57:43 -0400

    EDSC-186: Specs for granule z-ordering

--- spec/features/map/granule_footprint_visualizations_spec.rb
+++ spec/features/map/granule_footprint_visualizations_spec.rb
@@ -158,6 +158,24 @@ describe "Granule footprint visualizations", reset: false, wait: 30 do
   end
 
   context "granule selection", pq: true do
+    is_temporal_ordered_script = """
+      var layers = $('#map').data('map').map._layers, key, layer, result;
+      for (key in layers) {
+        if (layers[key]._getBackTile) {
+          layer = layers[key];
+          break;
+        }
+      }
+      if (!layer) {
+        result = true;
+      }
+      else {
+        result = layer._results[0].getTemporal() > layer._results[1].getTemporal()
+      }
+      result;
+    """
+
+
     before :all do
       create_bounding_box(0, 0, 15, 15)
     end
@@ -209,6 +227,10 @@ describe "Granule footprint visualizations", reset: false, wait: 30 do
         end
       end
 
+      it "displays the granule above all other granules" do
+        expect(page.evaluate_script(is_temporal_ordered_script)).to be_false
+      end
+
       context "and clicking on it again" do
         before :all do
           nth_granule_list_item(10).click
@@ -241,6 +263,10 @@ describe "Granule footprint visualizations", reset: false, wait: 30 do
             expect(page.evaluate_script("$('#map').data('map').map.getCenter().lat")).to eql 0
             expect(page.evaluate_script("$('#map').data('map').map.getCenter().lng")).to eql 0
           end
+        end
+
+        it "returns the granule ordering to its original state" do
+          expect(page.evaluate_script(is_temporal_ordered_script)).to be_true
         end
       end
     end
@@ -288,6 +314,10 @@ describe "Granule footprint visualizations", reset: false, wait: 30 do
         end
       end
 
+      it "displays the granule above all other granules" do
+        expect(page.evaluate_script(is_temporal_ordered_script)).to be_false
+      end
+
       context "and clicking on it again" do
         before :all do
           map_mouseclick
@@ -320,6 +350,10 @@ describe "Granule footprint visualizations", reset: false, wait: 30 do
             expect(page.evaluate_script("$('#map').data('map').map.getCenter().lat")).to eql 0
             expect(page.evaluate_script("$('#map').data('map').map.getCenter().lng")).to eql 0
           end
+        end
+
+        it "returns the granule ordering to its original state" do
+          expect(page.evaluate_script(is_temporal_ordered_script)).to be_true
         end
       end
     end
