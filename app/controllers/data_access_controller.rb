5614650aff4ba305ce2f5b8bc9ad433df35e90bc
Patrick Quinn <patrick@element84.com>
2014-03-31 16:51:50 -0400

    EDSC-133: Implement data download script

--- app/controllers/data_access_controller.rb
+++ app/controllers/data_access_controller.rb
@@ -8,4 +8,11 @@ class DataAccessController < ApplicationController
   def retrieve
     @project = JSON.parse(params[:project])
   end
+
+  def data_download
+    @ip = request.remote_ip
+    @user_id = get_user_id
+    # TODO: This could be dangerous
+    @query = request.env['QUERY_STRING']
+  end
 end
