e071c99c09d7c911272bc2f887a619cb1ac0f81f
Patrick Quinn <patrick@element84.com>
2014-04-01 14:55:42 -0400

    EDSC-133: Specs for download shell script

--- spec/features/data_access/data_download_spec.rb
+++ spec/features/data_access/data_download_spec.rb
@@ -48,7 +48,11 @@ describe "Data download page", reset: false do
     end
 
     it "displays a link to access a page containing direct download urls for datasets chosen for direct download" do
-      expect(page).to have_content(downloadable_dataset_title)
+      expect(page).to have_link('View Download Links')
+    end
+
+    it "displays a link to access a page containing direct download urls for datasets chosen for direct download" do
+      expect(page).to have_link('Download Access Script')
     end
 
     it "displays no links for direct downloads for datasets that were not chosen for direct download" do
@@ -63,6 +67,19 @@ describe "Data download page", reset: false do
       it "displays a page containing direct download hyperlinks for the dataset's granules in a new window" do
         within_window('Earthdata Search - Downloads') do
           expect(page).to have_link("http://daac.ornl.gov/data/fife/data/hydrolgy/strm_15m/y1984/43601715.s15")
+        end
+      end
+    end
+
+    context "upon clicking on a direct download link" do
+      before :all do
+        click_link "Download Access Script"
+      end
+
+      it "downloads a shell script which performs the user's query" do
+        within_window(page.driver.browser.get_window_handles.last) do
+          expect(page).to have_content('#!/bin/sh')
+          expect(page).to have_content('echo_collection_id%5B%5D=C179003030-ORNL_DAAC')
         end
       end
     end
