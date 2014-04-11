c4486458191d2cf24f4580b00062691d2ffb6538
Matthew Crouch <matthew@element84.com>
2014-04-11 13:02:41 -0400

    EDSC-105, EDSC-138: added specs for showing/hiding tour

---
+++ spec/features/site_preferences_spec.rb
@@ -0,0 +1,57 @@
+# EDSC-105: As a user, I want to save my site-viewing preferences 
+#           so that I may customize my experience with the site
+# EDSC-138: As a user, I want the system to remember when I dismiss 
+#           the introductory tour so that it does not interfere with 
+#           subsequent visits
+
+require 'spec_helper'
+
+describe 'Site Preferences' do
+  before :each do
+    visit '/'    
+  end
+
+  context "when user is logged in" do
+    before :each do
+      login
+    end
+
+    it "shows the tour" do
+      expect(page).to have_css '.tour'
+    end
+
+    it "sets site preferences when closing the tour" do
+      click_button 'Close'
+      expect(page).to have_no_css '.tour'
+
+      script = 'edsc.page.preferences.showTour()'
+      response = evaluate_script(script)
+      expect(response).to eq(false)
+    end
+
+    it "shows Take a Tour link after closing the tour" do
+      click_button 'Close'
+      expect(page).to have_content 'Take a Tour'
+    end
+  end
+
+  context "when user is a guest" do
+    it "shows the tour" do
+      expect(page).to have_css '.tour'
+    end
+
+    it "sets site preferences when closing the tour" do
+      click_button 'Close'
+      expect(page).to have_no_css '.tour'
+
+      script = 'edsc.page.preferences.showTour()'
+      response = evaluate_script(script)
+      expect(response).to eq(false)
+    end
+
+    it "shows Take a Tour link after closing the tour" do
+      click_button 'Close'
+      expect(page).to have_content 'Take a Tour'
+    end
+  end
+end
