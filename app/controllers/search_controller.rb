f38323da423bf96df8277b60b9c3928b9a325562
Matthew Crouch <matthew@element84.com>
2014-04-30 13:39:48 -0400

    EDSC-278: fixed test failures

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,7 @@
 class SearchController < ApplicationController
 
   def index
-    if request.referrer == nil && !token.nil?
+    if request.referrer == nil && token && !token.empty?
       response = Echo::Client.token_expires_soon(token)
       if response.body["token_expires_soon"]["expires_soon"] == true
         session[:token] = nil
