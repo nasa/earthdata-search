9111df16000ab23223e1d15f45ed376ec13be0ad
Patrick Quinn <patrick@element84.com>
2014-06-27 16:04:59 -0400

    EDSC-367: Fix specs broken by renamed button

--- spec/features/site_preferences_spec.rb
+++ spec/features/site_preferences_spec.rb
@@ -21,7 +21,7 @@ describe 'Site Preferences', reset: true do
     end
 
     it "sets site preferences when closing the tour" do
-      click_button 'Close'
+      click_button 'End Tour'
       expect(page).to have_no_css '.tour'
       wait_for_xhr
 
@@ -30,7 +30,7 @@ describe 'Site Preferences', reset: true do
     end
 
     it "shows Take a Tour link after closing the tour" do
-      click_button 'Close'
+      click_button 'End Tour'
       expect(page).to have_content 'Take a Tour'
     end
   end
@@ -41,7 +41,7 @@ describe 'Site Preferences', reset: true do
     end
 
     it "sets site preferences when closing the tour" do
-      click_button 'Close'
+      click_button 'End Tour'
       expect(page).to have_no_css '.tour'
       wait_for_xhr
 
@@ -50,7 +50,7 @@ describe 'Site Preferences', reset: true do
     end
 
     it "shows Take a Tour link after closing the tour" do
-      click_button 'Close'
+      click_button 'End Tour'
       expect(page).to have_content 'Take a Tour'
     end
   end
