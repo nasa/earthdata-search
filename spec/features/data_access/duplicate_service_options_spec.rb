5e843e3b261d148cefb3a1d2c09cec4b199bdaf7
Patrick Quinn <patrick@element84.com>
2014-07-02 08:58:59 -0400

    Revert "EDSC-135: Add additional wait in problematic spec"
    This reverts commit f95ba48aabd7ef8b7d8ee138b0f8e6339be63397.

--- spec/features/data_access/duplicate_service_options_spec.rb
+++ spec/features/data_access/duplicate_service_options_spec.rb
@@ -78,8 +78,6 @@ describe 'Duplicate Service Options', reset: false do
         choose 'Download'
       end
       click_button 'Continue'
-      wait_for_xhr
-
       click_button 'Submit'
       wait_for_xhr
     end
