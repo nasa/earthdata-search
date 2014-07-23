3503e66fc1f45e930d676c521c282ce482d11c85
Matthew Crouch <matthew@element84.com>
2014-07-23 12:08:29 -0400

    fixed failing specs for viewing projects

--- spec/features/projects/viewing_projects_spec.rb
+++ spec/features/projects/viewing_projects_spec.rb
@@ -5,6 +5,7 @@ describe "Viewing Projects", reset: false do
     path = '/search/datasets?p=!C179003030-ORNL_DAAC!C179001887-SEDAC'
 
     before :all do
+      Capybara.reset_sessions!
       visit '/'
       login
 
@@ -13,7 +14,7 @@ describe "Viewing Projects", reset: false do
       click_on 'End Tour'
       wait_for_xhr
 
-      user = User.first
+      user = User.order("updated_at desc").first
 
       project = Project.new
       project.path = path
