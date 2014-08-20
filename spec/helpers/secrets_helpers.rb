2af08c2d046a46b2ea2e9682091a6419de19a405
Patrick Quinn <patrick@element84.com>
2014-08-20 16:31:46 -0400

    EDSC-284: Fix build configuration using committed ERB and limited secrets

--- spec/helpers/secrets_helpers.rb
+++ spec/helpers/secrets_helpers.rb
@@ -1,8 +1,8 @@
 module Helpers
   module SecretsHelpers
     def urs_tokens
-      $_spec_config ||= YAML.load_file(Rails.root.join('spec/config.yml'))
-      JSON.parse($_spec_config['urs_tokens'])
+      $_spec_config ||= YAML.load(ERB.new(File.read(Rails.root.join('spec/config.yml'))).result)
+      $_spec_config['urs_tokens']
     end
   end
 end
