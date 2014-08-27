40e8ba52514b68fe1ad9a20015d13464c531e6b8
Patrick Quinn <patrick@element84.com>
2014-08-27 16:14:30 -0400

    EDSC-444: Use Faraday for URS calls

--- app/models/oauth_token.rb
+++ app/models/oauth_token.rb
@@ -4,43 +4,29 @@ require 'net/https'
 
 class OauthToken
   def self.get_oauth_tokens(auth_code)
-    uri = URI.parse("#{ ENV['urs_root'] }oauth/token?grant_type=authorization_code&code=#{ auth_code }&redirect_uri=#{ENV['urs_callback_url']}")
-
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = uri.scheme == "https"
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
-    request = Net::HTTP::Post.new(uri.request_uri)
-    request.basic_auth(ENV['urs_username'],ENV['urs_password'])
-    request.set_form_data({})
-
-    response = http.request(request)
-
-    if response.kind_of? Net::HTTPSuccess
-      JSON.parse(response.body)
-    else
-      nil
-    end
+    urs_post "/oauth/token?grant_type=authorization_code&code=#{auth_code}&redirect_uri=#{ENV['urs_callback_url']}"
   end
 
   def self.refresh_token(refresh_token)
-    uri = URI.parse("#{ ENV['urs_root'] }oauth/token?grant_type=refresh_token&refresh_token=#{ refresh_token }")
-
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = uri.scheme == "https"
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
-    request = Net::HTTP::Post.new(uri.request_uri)
-    request.basic_auth(ENV['urs_username'],ENV['urs_password'])
-    request.set_form_data({})
-
-    response = http.request(request)
-
-    if response.kind_of? Net::HTTPSuccess
-      JSON.parse(response.body)
-    else
-      nil
-    end
+    urs_post "/oauth/token?grant_type=refresh_token&refresh_token=#{refresh_token}"
   end
 
+  private
+
+  def self.urs_post(path)
+    response = connection.post path
+    response.body if response.success?
+  end
+
+  def self.connection
+    Thread.current[:edsc_urs_connection] ||= self.build_connection
+  end
+
+  def self.build_connection
+    connection = Faraday.new(ENV['urs_root'].chomp('/'))
+    connection.basic_auth(ENV['urs_username'], ENV['urs_password'])
+    connection.request :json
+    connection.response :json, :content_type => /\bjson$/
+    connection
+  end
 end
