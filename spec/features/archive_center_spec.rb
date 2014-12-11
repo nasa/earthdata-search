c8142868ceb656afde3399141f78fee4852257ef
Matthew Crouch <matthew@element84.com>
2014-12-11 15:21:04 -0500

    fixed archive center specs by using cmr

--- spec/features/archive_center_spec.rb
+++ spec/features/archive_center_spec.rb
@@ -2,11 +2,13 @@ require 'spec_helper'
 
 describe 'Archive Center Query Param', reset: false do
   before :all do
-    visit '/search?ac=ORNL_DAAC'
+    visit '/search?use_cmr=true&ac=ORNL_DAAC'
   end
 
   it "returns datasets from the provided archive center" do
-    expect(page).to have_content("ORNL_DAAC", count: 20)
+    within '#dataset-results .master-overlay-content' do
+      expect(page).to have_content("- ORNL_DAAC", count: 20)
+    end
   end
 
   it "does not return datasets from other archive centers" do
