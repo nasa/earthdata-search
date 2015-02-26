007c31314fe133f0f4554c0a64891fd15d1b0bb3
Matthew Crouch <matthew@element84.com>
2015-02-26 17:10:29 -0500

    EDSC-359 fixed failing spec

--- spec/features/datasets/dataset_apis_spec.rb
+++ spec/features/datasets/dataset_apis_spec.rb
@@ -6,7 +6,7 @@ describe 'Dataset API Endpoints', reset: false do
       visit '/search'
       wait_for_xhr
       click_link "Temporal"
-      fill_in "Start", with: "1985-12-01 00:00:00\t"
+      fill_in "Start", with: "1985-12-01 00:00:00\t\t"
       # close_datetimepicker
       js_click_apply ".temporal-dropdown"
       wait_for_xhr
