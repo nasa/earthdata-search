51dc3b9582bea0debd52091da911d743a4f25fee
Bo Sun <bo.sun@nasa.gov>
2015-08-28 12:12:23 -0400

    Set client-id value in each of the environments config files.

--- config/application.rb
+++ config/application.rb
@@ -92,7 +92,6 @@ module EarthdataSearchClient
     config.services = YAML.load_file(Rails.root.join('config/services.yml'))
     config.gibs = JSON.parse(IO.read(Rails.root.join('config/gibs.json')))
     config.echo_env = 'ops'
-    config.cmr_client_id = ENV['cmr_client_id'] || config.services['cmr'][Rails.env.to_s]['cmr_client_id']
     services = config.services
     config.urs_client_id = services['urs'][Rails.env.to_s][services['earthdata'][config.echo_env]['urs_root']]
   end
