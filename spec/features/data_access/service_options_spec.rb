e372fae8b75b9caf51963415945578285aafb385
Bo Sun <bo.sun@nasa.gov>
2015-09-17 12:01:52 -0400

    Fix service_options_spec.rb

--- spec/features/data_access/service_options_spec.rb
+++ spec/features/data_access/service_options_spec.rb
@@ -1,8 +1,10 @@
 require 'spec_helper'
 
 describe 'Service Options', reset: false do
-  downloadable_dataset_id = 'C179003030-ORNL_DAAC'
-  downloadable_dataset_title = '15 Minute Stream Flow Data: USGS (FIFE)'
+  downloadable_dataset_id = 'C90762182-LAADS'
+  downloadable_dataset_title = 'MODIS/Aqua Calibrated Radiances 5-Min L1B Swath 250m V005'
+
+  dataset_with_intermittent_timeline_id = 'C179003030-ORNL_DAAC'
 
   before :all do
     load_page :search, overlay: false
@@ -24,17 +26,17 @@ describe 'Service Options', reset: false do
 
       context 'choosing to set an order option' do
         before :all do
-          choose 'Ftp_Pull'
+          choose 'FtpPushPull'
         end
 
         it 'shows the form for the associated order option' do
-          expect(page).to have_text('Media Options')
+          expect(page).to have_text('Distribution Options')
         end
 
         it 'hides and shows portions of the form based on form selections' do
-          expect(page).to have_no_field('Offered Media Format for FTPPULL')
-          select 'FTP Pull', from: 'Offered Media Delivery Types'
-          expect(page).to have_field('Offered Media Format for FTPPULL')
+          expect(page).to have_no_text('Ftp Push Information')
+          select 'FtpPush', from: 'Distribution Options'
+          expect(page).to have_text('Ftp Push Information')
         end
       end
     end
@@ -42,14 +44,14 @@ describe 'Service Options', reset: false do
 
   context 'for datasets without granule results' do
     before :all do
-      load_page :search, project: [downloadable_dataset_id], view: :project, temporal: [DateTime.new(2014, 2, 10, 12, 30, 0, '+0')]
+      load_page :search, project: [dataset_with_intermittent_timeline_id], view: :project, temporal: ['2014-07-10T00:00:00Z', '2014-07-10T03:59:59Z']
       wait_for_xhr
       click_link "Retrieve project data"
       wait_for_xhr
     end
 
     it 'displays no service options' do
-      expect(page).to have_no_field('Ftp_Pull')
+      expect(page).to have_no_field('FtpPushPull')
     end
 
     it 'displays a message indicating that there are no matching granules' do
