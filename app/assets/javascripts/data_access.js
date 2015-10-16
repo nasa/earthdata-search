bfe97503ec4ea7b9ae27539d30461600bb86d3bb
Patrick Quinn <patrick@element84.com>
2015-10-16 16:09:07 -0400

    EDSC-731: Include ECHO forms as a submodule

--- app/assets/javascripts/data_access.js
+++ app/assets/javascripts/data_access.js
@@ -7,9 +7,7 @@
 //= require toastr
 //= require history.js-1.8.6/jquery.history.js
 //= require dropzone-3.8.3
-//= require echoforms/wgxpath.install.patched
-//= require echoforms/jstree.min
-//= require echoforms/jquery.echoforms.min
+//= require echoforms/dist/jquery.echoforms-full.min
 //= require namespaces
 //= require config
 //= require util/index
