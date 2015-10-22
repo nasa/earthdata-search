e03a50e9b4b0a451e55bdc019455bfcb5cbe2be7
Patrick Quinn <patrick@element84.com>
2015-10-22 16:51:41 -0400

    Automatically start node processes as needed

---
+++ config/webpack.config.js
@@ -0,0 +1,48 @@
+var webpack = require("webpack"),
+    fs = require('fs'),
+    pkg = JSON.parse(fs.readFileSync('./package.json')),
+    license;
+
+license = fs.readFileSync('./LICENSE')
+  .toString()
+  .split(/\s+---\s+/, 1)[0];
+
+module.exports = {
+  context: __dirname + '/..',
+  entry: {
+    search: "./edsc/search.js"
+  },
+  output: {
+    path: "./edsc/dist",
+    filename: "edsc-[name].min.js"
+  },
+  module: {
+    loaders: [
+      { test: /\.css$/, loader: "style!css" },
+      { test: /\.less$/, loader: "style!css!less" },
+      { test: /\.coffee$/, loader: "coffee" },
+      { test: /\.(coffee\.md|litcoffee)$/, loader: "coffee?literate" },
+      { test: /\.(gif|png)$/, loader: "url-loader?limit=100000" },
+      { test: /\.hbs$/, loader: "handlebars-loader" },
+      { test: /\.jpg$/, loader: "file-loader" }
+    ]
+  },
+  devtool: '#sourcemap',
+  externals: {
+    "jquery": "jQuery",
+    "window": "window"
+  },
+  resolve: {
+    extensions: ['', '.js', '.json', '.coffee'],
+    modulesDirectories: ['edsc', 'node_modules']
+  },
+  plugins: [
+    new webpack.BannerPlugin(license),
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+      "window.jQuery": "jquery",
+      "root.jQuery": "jquery"
+    })
+  ]
+};
