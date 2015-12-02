f3e3efc3772e0151d61ba3b62fc639537ccb90b2
Bo Sun <bo.sun@nasa.gov>
2015-12-01 21:54:26 -0500

    EDSC-883 Add a test.

--- spec/features/collection_facets/collection_facets_spec.rb
+++ spec/features/collection_facets/collection_facets_spec.rb
@@ -246,6 +246,26 @@ describe "Collection Facets", reset: false do
     end
   end
 
+  context "when applied science keyword facets and search terms filter the collections list to no results" do
+    before(:all) do
+      find(".facets-item", text: "BIOSPHERE").click
+      fill_in :keywords, with: "somestringthatmatchesnocollections"
+      wait_for_xhr
+    end
+
+    after(:all) do
+      reset_search
+      find(".facets-item", text: 'BIOSPHERE').click
+      wait_for_xhr
+    end
+
+    it "continues to display applied facets" do
+      within(:css, '#collapse1 .panel-body.facets') do
+        expect(page).to have_content("BIOSPHERE")
+      end
+    end
+  end
+
   context "when applied facets and search terms filter the collections list to no results" do
     before(:all) do
       find("h3.facet-title", text: 'Project').click
