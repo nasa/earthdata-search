4b39d38a27d7672d9525588b4c1a4da007ccd9b5
Bo Sun <bo.sun@nasa.gov>
2016-01-25 11:19:42 -0500

    EDSC-813: Double the page size with every fetch to prevent multiple scroll events from firing.

--- lib/echo/cwic_client.rb
+++ lib/echo/cwic_client.rb
@@ -1,7 +1,7 @@
 module Echo
   class CwicClient < BaseClient
-    def get_cwic_granules(short_name, startPage=1, count=10)
-      get("/opensearch/granules.atom?datasetId=#{short_name}&clientId=#{Rails.configuration.cmr_client_id}&startPage=#{startPage}&count=#{count}")
+    def get_cwic_granules(short_name, start_page=1, count=10)
+      get("/opensearch/granules.atom?datasetId=#{short_name}&clientId=#{Rails.configuration.cmr_client_id}&startPage=#{start_page}&count=#{count}")
     end
   end
 end
