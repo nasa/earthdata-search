699d6e2715f678a0f1623a6d5e4fa3b830c05a60
Bo Sun <bo.sun@nasa.gov>
2016-02-16 11:35:30 -0500

    EDSC-797: Add more tests and tag existing tests with 'acceptance: true' flag.

--- spec/features/cwic/granules/cwic_granule_filtering_spec.rb
+++ spec/features/cwic/granules/cwic_granule_filtering_spec.rb
@@ -24,6 +24,28 @@ describe "CWIC Granule list", reset: false do
         collection_details.click_link('Back to Granules')
       end
 
+      it "does not display the search by granule id fields", acceptance: true do
+        expect(page).to have_no_content("Granule ID")
+      end
+
+      it "displays the collection-specific temporal fields", acceptance: true do
+        expect(page).to have_css("#temporal_start_button")
+        expect(page).to have_css("#temporal_end_button")
+      end
+
+      it "does not display the search by day/night flag field", acceptance: true do
+        expect(page).to have_no_content("Day / Night Flag")
+      end
+
+      it "does not display the search by data access fields", acceptance: true do
+        expect(page).to have_no_content("Find only granules that have browse images.")
+        expect(page).to have_no_content("Find only granules that are available online.")
+      end
+
+      it "does not display the search by cloud cover percentage fields", acceptance: true do
+        expect(page).to have_no_content("Cloud Cover")
+      end
+
       context "and selecting temporal range" do
         before :all do
           number_granules = granule_list.text.match /\d+ matching granules/
@@ -35,13 +57,13 @@ describe "CWIC Granule list", reset: false do
           wait_for_xhr
         end
 
-        it "filters granules" do
+        it "filters granules", acceptance: true do
           number_granules = granule_list.text.match /\d+ matching granules/
           after_granule_count = number_granules.to_s.split(" ")[0].to_i
           expect(after_granule_count < before_granule_count).to be_true
         end
 
-        it "has query param 'pg[0][qt]' in the url" do
+        it "has query param 'pg[0][qt]' in the url", acceptance: true do
           project_id = current_url.match(/projectId=\d+/).to_s.split("=")[1]
           expect(Project.find(project_id).path).to have_text("pg[0][qt]=2010-02-02T00%3A00%3A00.000Z%2C2010-02-02T23%3A59%3A59.000Z")
         end
