cc168d49b048dd0b0292d55ca5724db9555e63d5
Patrick Quinn <patrick@element84.com>
2016-02-19 10:21:42 -0500

    EDSC-803, EDSC-804, EDSC-805: Fetch and link browse thumbnails and images

--- edsc/plugins/datasource/cwic/src/js/Granule.jsx
+++ edsc/plugins/datasource/cwic/src/js/Granule.jsx
@@ -8,14 +8,29 @@ let CwicGranule = (function() {
   }
 
   CwicGranule.prototype.edsc_browse_url = function(w, h) {
-    // arguments w, h are not currently used.
     for (var i = 0; i < this.links.length; i ++) {
-      if (this.links[i].title == 'Browser') {
+      if (this.links[i].rel == 'icon') {
         return this.links[i].href;
       }
     }
     return null;
   };
+
+  CwicGranule.prototype.edsc_full_browse_url = function() {
+    // This is kind of a hack because CWIC does not have a standard link relation
+    // for large-sized browse, so we look for URLs with extensions of jpg, jpeg,
+    // gif, and png, and return the first one which is not marked as the icon.
+    // If there's no such relation, we return nothing, which results in there being
+    // no link to the full size image.
+    for (var i = 0; i < this.links.length; i ++) {
+      var href = this.links[i].href;
+      if (this.links[i].rel != 'icon' && href.match(/\.(?:gif|jpg|jpeg|png)(?:\?|#|$)/)) {
+        return href;
+      }
+    }
+    return null;
+  };
+
 
   CwicGranule.prototype.getTemporal = function () {
     if (typeof this.date !== "undefined" && this.date !== null) {
