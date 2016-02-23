bbc125c8bf0d2fa2415b5ae5e548a3abb0f29c84
Patrick Quinn <patrick@element84.com>
2016-02-23 09:04:56 -0500

    EDSC-801: (WIP) Indicate recurring temporal

--- edsc/plugins/renderer/cwic/src/js/edsc-plugin.renderer.cwic.jsx
+++ edsc/plugins/renderer/cwic/src/js/edsc-plugin.renderer.cwic.jsx
@@ -1,18 +1,42 @@
-// import style from '../css/cwic.useable.less';
+var style = require("../css/cwic.useable.less");
+
+console.log(style);
+
+function toggleClass(el, classname, set) {
+  let classes = el.className.split(/\s+/);
+  let index = classes.indexOf(classname);
+  if (index !== -1 && !set) {
+    classes.splice(index, 1);
+    this.setClasses(el, classes);
+  }
+  if (index === -1 && set) {
+    classes.push(classname);
+  }
+  el.className = classes.join(' ');
+}
 
 export default class CwicRendererPlugin {
-  constructor(edsc) {
-    //style.use();
+  constructor(edsc, collection) {
     console.log('Loaded cwic renderer plugin');
+    this.collection = collection;
   }
   destroy(edsc) {
-    //style.unuse();
     console.log('Unloaded cwic renderer plugin');
   }
   startSearchFocus() {
     console.log('Start search focus');
+    console.log(style);
+    style.use();
+    let flag = this._recurringQueryFlag();
+    this._onRecurringChange(flag());
+    this._recurringSubscription = flag.subscribe(this._onRecurringChange, this);
   }
   endSearchFocus() {
+    if (this._recurringSubscription) {
+      this._recurringSubscription.dispose();
+    }
+    this._onRecurringChange(false);
+    style.unuse();
     console.log('End search focus');
   }
   startSearchView() {
@@ -26,6 +50,17 @@ export default class CwicRendererPlugin {
   }
   endAccessPreview() {
     console.log('End access preview');
+  }
+
+  _onRecurringChange(isRecurring) {
+    let el = document.getElementById('temporal-query');
+    toggleClass(el, 'feature-missing', isRecurring);
+    console.log('recurring changed', isRecurring);
+  }
+
+  _recurringQueryFlag() {
+    // Demeter :(. This should probably end up in the facade somehow
+    return this.collection.query.temporal.applied.isRecurring;
   }
 };
 
