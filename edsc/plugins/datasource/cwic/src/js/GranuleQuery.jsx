54e5ab4dc55aa87e30d31aa9545b6843ed152a38
Patrick Quinn <patrick@element84.com>
2016-02-26 15:08:34 -0500

    EDSC-801: Fix temporal filtering after query updates

--- edsc/plugins/datasource/cwic/src/js/GranuleQuery.jsx
+++ edsc/plugins/datasource/cwic/src/js/GranuleQuery.jsx
@@ -1,5 +1,39 @@
 import extend from './extend.jsx';
 
+let Temporal = window.edsc.models.ui.Temporal;
+
+class GranuleQuery {
+  constructor(parentQuery) {
+    this.parentQuery = parentQuery;
+    this.temporal = new Temporal();
+    this.singleGranuleId = ko.observable(null);
+    this.params = ko.computed({
+      read: this.serialize,
+      write: this.fromJson,
+      deferEvaluation: true,
+      owner: this});
+  }
+
+  fromJson(query) {
+    this._temporalQuery(query.temporal);
+  }
+
+  serialize() {
+    let result = {
+      temporal: this._temporalQuery()
+    };
+    return result;
+  }
+
+  clearFilters() {
+    this.fromJson({});
+  }
+
+  _temporalQuery() {
+    return this.temporal.applied.queryCondition;
+  }
+}
+
 let CwicGranuleQuery = (function() {
   extend(CwicGranuleQuery, window.edsc.models.data.query.GranuleQuery);
 
