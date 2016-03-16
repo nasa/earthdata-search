b52204d604c2071893cf435a8a501354e750a918
Bo Sun <bo.sun@nasa.gov>
2016-03-16 14:05:06 -0400

    EDSC-984 Tag the background_job portion of the log with version/git head info.

--- app/models/retrieval.rb
+++ app/models/retrieval.rb
@@ -28,48 +28,50 @@ class Retrieval < ActiveRecord::Base
 
   # Delayed Jobs calls this method to excute an order creation
   def self.process(id, token, env, base_url, access_token)
-    if Rails.env.test?
-      normalizer = VCR::HeaderNormalizer.new('Echo-Token', token + ':' + Rails.configuration.urs_client_id, 'edsc')
-      VCR::EDSCConfigurer.register_normalizer(normalizer)
-    end
-    retrieval = Retrieval.find_by_id(id)
-    project = retrieval.jsondata
-    user_id = retrieval.user.echo_id
-    client = Echo::Client.client_for_environment(env, Rails.configuration.services)
+    logger.tagged("delayed_job version: #{Rails.configuration.version.strip}") do
+      if Rails.env.test?
+        normalizer = VCR::HeaderNormalizer.new('Echo-Token', token + ':' + Rails.configuration.urs_client_id, 'edsc')
+        VCR::EDSCConfigurer.register_normalizer(normalizer)
+      end
+      retrieval = Retrieval.find_by_id(id)
+      project = retrieval.jsondata
+      user_id = retrieval.user.echo_id
+      client = Echo::Client.client_for_environment(env, Rails.configuration.services)
 
-    retrieval.collections.each do |collection|
-      params = Rack::Utils.parse_nested_query(collection['params'])
-      params.reject!{|p| ['datasource', 'short_name'].include? p}
-      params.merge!(page_size: 2000, page_num: 1)
+      retrieval.collections.each do |collection|
+        params = Rack::Utils.parse_nested_query(collection['params'])
+        params.reject! { |p| ['datasource', 'short_name'].include? p }
+        params.merge!(page_size: 2000, page_num: 1)
 
-      access_methods = collection['serviceOptions']['accessMethod']
-      access_methods.each do |method|
-        if method['type'] == 'order'
-          order_response = client.create_order(params,
-                                                method['id'],
-                                                method['method'],
-                                                method['model'],
-                                                user_id,
-                                                token,
-                                                client,
-                                                access_token)
-          method[:order_id] = order_response[:order_id]
-          method[:dropped_granules] = order_response[:dropped_granules]
-          Rails.logger.info "Granules dropped from the order: #{order_response[:dropped_granules].map {|dg| dg[:id]}}"
-        elsif method['type'] == 'service'
-          request_url = "#{base_url}/data/retrieve/#{retrieval.to_param}"
+        access_methods = collection['serviceOptions']['accessMethod']
+        access_methods.each do |method|
+          if method['type'] == 'order'
+            order_response = client.create_order(params,
+                                                 method['id'],
+                                                 method['method'],
+                                                 method['model'],
+                                                 user_id,
+                                                 token,
+                                                 client,
+                                                 access_token)
+            method[:order_id] = order_response[:order_id]
+            method[:dropped_granules] = order_response[:dropped_granules]
+            Rails.logger.info "Granules dropped from the order: #{order_response[:dropped_granules].map { |dg| dg[:id] }}"
+          elsif method['type'] == 'service'
+            request_url = "#{base_url}/data/retrieve/#{retrieval.to_param}"
 
-          method[:collection_id] = collection['id']
-          service_response = MultiXml.parse(ESIClient.submit_esi_request(collection['id'], params, method, request_url, client, token).body)
-          method[:order_id] = service_response['agentResponse'].nil? ? nil : service_response['agentResponse']['order']['orderId']
-          method[:error_code] = service_response['Exception'].nil? ? nil : service_response['Exception']['Code']
-          method[:error_message] = service_response['Exception'].nil? ? nil : service_response['Exception']['Message']
+            method[:collection_id] = collection['id']
+            service_response = MultiXml.parse(ESIClient.submit_esi_request(collection['id'], params, method, request_url, client, token).body)
+            method[:order_id] = service_response['agentResponse'].nil? ? nil : service_response['agentResponse']['order']['orderId']
+            method[:error_code] = service_response['Exception'].nil? ? nil : service_response['Exception']['Code']
+            method[:error_message] = service_response['Exception'].nil? ? nil : service_response['Exception']['Message']
+          end
         end
       end
-    end
 
-    retrieval.jsondata = project
-    retrieval.save!
+      retrieval.jsondata = project
+      retrieval.save!
+    end
   end
 
   def collections
