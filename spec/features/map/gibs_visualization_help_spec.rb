37f5426ff03819f0820a1c1f4da222dfb9587356
Bo Sun <bo.sun@nasa.gov>
2016-03-26 21:58:47 -0400

    EDSC-985: Fix gibs_visualization_help_spec.

--- spec/features/map/gibs_visualization_help_spec.rb
+++ spec/features/map/gibs_visualization_help_spec.rb
@@ -6,18 +6,18 @@ require "spec_helper"
 
 describe "Collection GIBS help", reset: true do
 
-  gibs_collection_id = 'C1000000019-LANCEMODIS'
+  gibs_collection_id = 'C119124186-NSIDC_ECS'
   gibs_tile_layer = '.leaflet-tile-pane .leaflet-layer:nth-child(2)'
 
   before :each do
     load_page :search
     fill_in "keywords", with: gibs_collection_id
-    expect(page).to have_content('MOD04_L2')
+    expect(page).to have_content('AE_Rain')
   end
 
   context "when visualizing a GIBS-enabled collection" do
     before :each do
-      first_featured_collection.click_link "View collection"
+      first_collection_result.click_link "View collection"
     end
 
     it "displays information on the source and accuracy of GIBS browse" do
@@ -30,14 +30,14 @@ describe "Collection GIBS help", reset: true do
 
   context "when visualizing a GIBS-enabled collection a second time" do
     before :each do
-      first_featured_collection.click_link "View collection"
+      first_collection_result.click_link "View collection"
       within '.popover-navigation' do
         click_on 'Close'
       end
       page.should have_css(gibs_tile_layer)
-      first_featured_collection.click_link "Hide collection"
+      first_collection_result.click_link "Hide collection"
       page.should have_no_css(gibs_tile_layer)
-      first_featured_collection.click_link "View collection"
+      first_collection_result.click_link "View collection"
       page.should have_css(gibs_tile_layer)
     end
 
