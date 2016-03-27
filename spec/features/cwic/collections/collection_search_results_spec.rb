a88caea3b5cf52f409ddb1f3cf5084a4e87a3c15
Bo Sun <bo.sun@nasa.gov>
2016-03-26 23:05:06 -0400

    EDSC-985: Fix collection_search_results_spec.

--- spec/features/cwic/collections/collection_search_results_spec.rb
+++ spec/features/cwic/collections/collection_search_results_spec.rb
@@ -5,8 +5,7 @@ describe "CWIC-capable collection search results", reset: false do
 
   before :all do
     Capybara.reset_sessions!
-    load_page :search, env: :sit, facets: true
-    wait_for_xhr
+    load_page :search, env: :uat, facets: true
   end
 
   after :all do
@@ -14,14 +13,10 @@ describe "CWIC-capable collection search results", reset: false do
   end
 
   context "in the collection results list" do
-    before :all do
-      login
-      wait_for_xhr
-    end
 
     context "when CWIC feature is not selected" do
       it "does not show CIWC-tagged items", acceptance: true do
-        fill_in "keywords", with: 'C1000003579-GCMDTEST'
+        fill_in "keywords", with: 'C1204449891-GCMDTEST'
         wait_for_xhr
         expect(page).to have_content('0 Matching Collections')
       end
@@ -31,17 +26,16 @@ describe "CWIC-capable collection search results", reset: false do
       before :all do
         cwic_feature_facet.click
         wait_for_xhr
-        fill_in "keywords", with: 'C1000003579-GCMDTEST'
+        fill_in "keywords", with: 'C1204449891-GCMDTEST'
         wait_for_xhr
       end
 
       after :all do
-        load_page :search, env: :sit, facets: true
-        wait_for_xhr
+        load_page :search, env: :uat, facets: true
       end
 
       it "shows the CWIC tagged collection", acceptance: true do
-        expect(first_collection_result).to have_content('3DIMG_L2P_IRW')
+        expect(first_collection_result).to have_content('EO-1 (Earth Observing-1) Advanced Land Imager (ALI) Instrument Level 1R, Level 1Gs, Level 1Gst Data')
       end
 
       it "does not show a granule count", acceptance: true do
@@ -54,7 +48,7 @@ describe "CWIC-capable collection search results", reset: false do
     end
 
     context "a non-CWIC-tagged item" do
-      use_collection 'C3878-LPDAAC_ECS', 'MCD43A4'
+      use_collection 'C43947-GSFCS4PA', 'AIRIBRAD_NRT'
 
       it "shows a granule count", acceptance: true do
         expect(first_collection_result).to have_text('Granules')
@@ -71,14 +65,14 @@ describe "CWIC-capable collection search results", reset: false do
       end
 
       after :all do
-        load_page :search, env: :sit, facets: true
+        load_page :search, env: :uat, facets: true
       end
 
       context "a CWIC-tagged item" do
         before :all do
           cwic_feature_facet.click
           wait_for_xhr
-          fill_in "keywords", with: 'C1000003579-GCMDTEST'
+          fill_in "keywords", with: 'C1204449891-GCMDTEST'
           wait_for_xhr
           first_collapsed_collection.click_link('Toggle details')
         end
@@ -97,7 +91,7 @@ describe "CWIC-capable collection search results", reset: false do
       end
 
       context "a non-CWIC-tagged item" do
-        use_collection 'C3878-LPDAAC_ECS', 'MCD43A4'
+        use_collection 'C43947-GSFCS4PA', 'AIRIBRAD_NRT'
 
         before :all do
           first_collapsed_collection.click_link('Toggle details')
