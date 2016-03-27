a52b5565a58f1bb0845b79aad427055b41e1a062
Bo Sun <bo.sun@nasa.gov>
2016-03-26 23:49:36 -0400

    EDSC-985: Add missing fixtures for passing tests and fix other failed ones. (for build #4)

--- spec/features/collections/collection_details_spec.rb
+++ spec/features/collections/collection_details_spec.rb
@@ -46,7 +46,7 @@ describe 'Collection details', reset: false do
         expect(page).to have_content('Processing Center: JPL')
         expect(page).to have_content('Short Name: ASTGTM')
         expect(page).to have_content('VERSION 002')
-        expect(page).to have_content('Contacts: LP DAAC User Services 605-594-6116 (phone) 605-594-6963 (fax) lpdaac@eos.nasa.gov')
+        expect(page).to have_content('Contacts: LP DAAC User Services 605-594-6116 (phone) 605-594-6963 (fax) lpdaac@usgs.gov')
         expect(page).to have_content('Spatial Coordinates: Bounding Rectangle: (90.0°, -180.0°, -90.0°, 180.0°)')
         expect(page).to have_content('Metadata Formats: Native | ATOM | ECHO10 | ISO19115 | DIF')
         expect(page).to have_content('Temporal Extent: 1999-12-18 to 2011-02-28')
