1fbcb02045f6651a32117398189e2cc009cbcaa3
Bo Sun <bo.sun@nasa.gov>
2016-04-25 14:18:30 -0400

    EDSC-984: Send hash to client and validate on the server side. Also add a spec for changed form data.

--- app/models/access_configuration.rb
+++ app/models/access_configuration.rb
@@ -1,17 +1,20 @@
 require 'digest/sha1'
+require 'json'
 
 class AccessConfiguration < ActiveRecord::Base
   belongs_to :user
   store :service_options, coder: JSON
 
   def self.get_default_access_config(user, cmr_concept_id)
-    self.find_by(user: user, dataset_id: cmr_concept_id)
+    access_config = self.find_by(user: user, dataset_id: cmr_concept_id)
+    access_config.echoform_digest = JSON.parse(access_config.echoform_digest) if access_config.present?
+    access_config
   end
 
-  def self.set_default_access_config(user, cmr_concept_id, options, echoform)
+  def self.set_default_access_config(user, cmr_concept_id, options, form_hashes)
     access_config = self.find_or_initialize_by(user: user, dataset_id: cmr_concept_id)
     access_config.service_options = options
-    access_config.echoform_digest = echoform.nil? ? nil : Digest::SHA1.hexdigest(echoform.join("\n"))
+    access_config.echoform_digest = form_hashes.nil? ? nil : form_hashes.to_json
     access_config.save!
   end
 end
