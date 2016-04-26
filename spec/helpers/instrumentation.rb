d73d54c4ec70c92eca1b51d1afc7c66be3f790c6
Patrick Quinn <patrick@element84.com>
2016-04-26 14:41:30 -0400

    EDSC-1046: Add controller instrumentation

--- spec/helpers/instrumentation.rb
+++ spec/helpers/instrumentation.rb
@@ -20,7 +20,7 @@ module Helpers
         duration_s = @duration / 1000.0
         average = duration_s / @count.to_f
         last = @last / 1000.0
-        "| %9.3fs | %7.3fs | %7.3fs | %5d | %-30s |" % [duration_s, average, last, @count, @key]
+        "| %9.3fs | %7.3fs | %7.3fs | %5d | %-45s |" % [duration_s, average, last, @count, @key]
       end
 
       def <=>(other)
@@ -40,8 +40,8 @@ module Helpers
       timings = @events.map(&:second).sort.reverse
       puts "\nInstrumented performance:\n"
 
-      puts "| Total time |  Average |   Last   | Count | Activity                       |"
-      puts "|------------+----------+----------+-------+--------------------------------|"
+      puts "| Total time |  Average |   Last   | Count | Activity                                      |"
+      puts "|------------+----------+----------+-------+-----------------------------------------------|"
 
       puts timings.join("\n")
     end
@@ -57,5 +57,16 @@ end
 ActiveSupport::Notifications.subscribe /^edsc\.performance/ do |*args|
   event = ActiveSupport::Notifications::Event.new(*args)
   key = event.payload && event.payload[:activity] || event.name.gsub(/^edsc.performance\./, '')
+  Helpers::Instrumentation.record_performance(key, event.duration)
+end
+
+ActiveSupport::Notifications.subscribe /process_action.action_controller/ do |*args|
+  event = ActiveSupport::Notifications::Event.new(*args)
+  controller = event.payload[:controller]
+  action = event.payload[:action]
+  format = event.payload[:format] || "all"
+  format = "all" if format == "*/*"
+  status = event.payload[:status]
+  key = "#{controller}##{action}.#{format}"
   Helpers::Instrumentation.record_performance(key, event.duration)
 end
