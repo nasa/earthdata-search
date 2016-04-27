e93a57799c670d1e49ec3eade0f2cc7d2ecca5dd
Patrick Quinn <patrick@element84.com>
2016-04-27 13:17:44 -0400

    EDSC-1034, EDSC-1034: Specs for portals

--- spec/features/portals/portal_collection_filtering_spec.rb
+++ spec/features/portals/portal_collection_filtering_spec.rb
@@ -1,4 +1,9 @@
 require "spec_helper"
 
-describe "Portal", reset: false do
+describe "Portal collection filtering", reset: false do
+  it "Visiting an Earthdata Search portal restricts visible collections to those matching its configured filter", acceptance: true do
+    load_page :search, portal: 'simple'
+    expect(page.status_code).to eq(200)
+    expect(page).to have_text("1 Matching Collection")
+  end
 end
