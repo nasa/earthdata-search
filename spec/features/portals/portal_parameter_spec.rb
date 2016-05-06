e6b540afae0d2ed7f79f62c15ff98849b5d8e36e
Patrick Quinn <patrick@element84.com>
2016-05-06 11:57:18 -0400

    EDSC-987: Fix specs broken by duplicate link titles

--- spec/features/portals/portal_parameter_spec.rb
+++ spec/features/portals/portal_parameter_spec.rb
@@ -24,7 +24,9 @@ describe "Portal parameters", reset: true do
     expect(page).to have_text("1 Matching Collection")
     expect(page).to have_path_prefix("/portal/simple/")
 
-    click_link "Simple"
+    within '#main-toolbar' do
+      click_link "Simple"
+    end
     expect(page).to have_path('/portal/simple/')
 
   end
@@ -89,7 +91,9 @@ describe "Portal parameters", reset: true do
 
     context "and clicking the home page link" do
       before :each do
-        click_link 'Simple'
+        within '#main-toolbar' do
+          click_link 'Simple'
+        end
       end
       it "carries the portal parameter to the next page" do
         expect(page).to have_path_prefix("/portal/simple/")
