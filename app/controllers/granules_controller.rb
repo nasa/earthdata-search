071be01e438998d3e40d9c31526331e0642aba75
Bo Sun <bo.sun@nasa.gov>
2016-07-07 01:50:16 -0400

    EDSC-1124 Remove class GranuleUrlStreamer and refactoring.

--- app/controllers/granules_controller.rb
+++ app/controllers/granules_controller.rb
@@ -34,67 +34,6 @@ class GranulesController < ApplicationController
     render json: catalog_response.body, status: catalog_response.status
   end
 
-  class GranuleUrlStreamer
-    def initialize(params, token, url_mapper, echo_client, url_type=:download)
-      params.reject!{|p| ['datasource', 'short_name'].include? p}
-      @params = params
-      @token = token
-      @url_mapper = url_mapper
-      @echo_client = echo_client
-      @url_type = url_type
-    end
-
-    def first
-      catalog_response = @echo_client.get_granules(@params.merge(page_num: 1), @token)
-      if catalog_response.success?
-        granules = catalog_response.body['feed']['entry']
-
-        granule = granules.first
-        first_info = @url_mapper.info_urls_for(granule).first
-        return first_info if first_info
-
-        @url_mapper.send("#{@url_type}_urls_for", granule).first
-      else
-        @errors = catalog_response.body
-        nil
-      end
-    end
-
-    def each
-      yielded_info = false
-      at_end = false
-      page = 1
-      page_size = @params["page_size"]
-      until at_end
-        catalog_response = @echo_client.get_granules(@params.merge(page_num: page), @token)
-        hits = catalog_response.headers['CMR-Hits'].to_i
-
-        if catalog_response.success?
-          granules = catalog_response.body['feed']['entry']
-          at_end = page_size * page >= hits
-
-          granules.each do |granule|
-            unless yielded_info
-              @url_mapper.info_urls_for(granule).each do |url|
-                yield url
-                yielded_info = true
-              end
-            end
-
-            @url_mapper.send("#{@url_type}_urls_for", granule).each do |url|
-              yield url
-            end
-          end
-
-          page += 1
-        else
-          @errors = catalog_response.body
-          return
-        end
-      end
-    end
-  end
-
   def fetch_links
     retrieval = Retrieval.find(request[:project])
     collection_id = params[:collection]
@@ -179,10 +118,28 @@ class GranulesController < ApplicationController
         url_mapper.apply_subsetting(method['subset'])
       end
 
-      @urls = GranuleUrlStreamer.new(query.merge('page_size' => 2000), token, url_mapper, echo_client, url_type)
+      @first_link = first_link(Rack::Utils.parse_nested_query(collection['params']), echo_client, token, url_mapper, url_type)
 
       @user = earthdata_username
       render 'prepare_script.html.erb', layout: false
+    end
+  end
+
+  private
+
+  def first_link(param, echo_client, token, url_mapper, url_type)
+    catalog_response = echo_client.get_granules(param.merge(page_num: 1), token)
+    if catalog_response.success?
+      granules = catalog_response.body['feed']['entry']
+
+      granule = granules.first
+      first_info = url_mapper.info_urls_for(granule).first
+      return first_info if first_info
+
+      url_mapper.send("#{url_type}_urls_for", granule).first
+    else
+      @errors = catalog_response.body
+      nil
     end
   end
 end
