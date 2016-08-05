df62620b17632d15f6217ed13e55a786d8a62a08
Bo Sun <bo.sun@nasa.gov>
2016-08-05 09:20:45 -0400

    EDSC-1144 Fix a typo.

--- config/environments/sit.rb
+++ config/environments/sit.rb
@@ -88,4 +88,4 @@ EarthdataSearchClient::Application.configure do
   config.cmr_client_id = ENV['cmr_client_id'] || 'edsc-prod'
 end
 
-p "------- [sit.rb] config.cmr_env: #{Rails.application.cmr_env}"
+p "------- [sit.rb] config.cmr_env: #{Rails.configuration.cmr_env}"
