806773bcce48dba97389b2359f2ff936096be9a2
Bo Sun <bo.sun@nasa.gov>
2016-08-16 12:16:15 -0400

    EDSC-1153: Store digest as String in db. Need to put in a test before merging.

--- app/models/access_configuration.rb
+++ app/models/access_configuration.rb
@@ -8,14 +8,14 @@ class AccessConfiguration < ActiveRecord::Base
 
   def self.get_default_access_config(user, cmr_concept_id)
     access_config = self.find_by(user: user, dataset_id: cmr_concept_id)
-    access_config.echoform_digest = JSON.parse(access_config.echoform_digest) if access_config.present? && access_config.echoform_digest.present?
+    access_config.echoform_digest = JSON.parse(access_config.echoform_digest) if access_config.present? && access_config.echoform_digest.present? && (access_config.echoform_digest.is_a? String)
     access_config
   end
 
   def self.set_default_access_config(user, cmr_concept_id, options, form_hashes)
     access_config = self.find_or_initialize_by(user: user, dataset_id: cmr_concept_id)
     access_config.service_options = options
-    access_config.echoform_digest = form_hashes.nil? ? nil : form_hashes.to_json
+    access_config.echoform_digest = form_hashes.nil? ? nil : form_hashes
     access_config.save!
   end
 end
