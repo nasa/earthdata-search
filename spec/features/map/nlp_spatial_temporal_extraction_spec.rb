e605935d160b6a127a765d39ae9de0f02cf71c57
Bo Sun <bo.sun@nasa.gov>
2016-08-31 15:28:15 -0400

    EDSC-1113, EDSC-1114 Add EDSC-NLP to README. Some of the new tests.

--- spec/features/map/nlp_spatial_temporal_extraction_spec.rb
+++ spec/features/map/nlp_spatial_temporal_extraction_spec.rb
@@ -6,6 +6,7 @@ describe "Spatial and temporal extraction", reset: false do
     before :all do
       load_page :search
       fill_in 'keywords', with: 'Texas'
+      wait_for_xhr
     end
 
     after :all do
@@ -18,12 +19,18 @@ describe "Spatial and temporal extraction", reset: false do
 
     it "is set in the query" do
       project_id = URI.parse(current_url).query[/^projectId=(\d+)$/, 1].to_i
-      expect(Project.find(project_id).path).to eql('m=31.168933999999993!-100.0768425!5!1!0!0%2C2&ok=Texas&sb=-106.645646%2C25.837163999999998%2C-93.508039%2C36.500704')
+      expect(Project.find(project_id).path).to eql('/search/collections?m=31.168933999999993!-104.5768425!5!1!0!0%2C2&ok=Texas&sb=-106.645646%2C25.837163999999998%2C-93.508039%2C36.500704')
     end
 
     it "is set on the map" do
       expect(page).to have_css('.leaflet-overlay-pane path')
       expect(page).to have_spatial_constraint('bounding_box:-106.645646,25.837163999999998:-93.508039,36.500704')
+    end
+  end
+
+  context "extracted temporal information" do
+    before :all do
+
     end
   end
 end
