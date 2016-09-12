cbce8394967e0a958b9405211230ff9cb45f64ce
Matthew Crouch <matthew@element84.com>
2016-09-12 16:47:33 -0400

    EDSC-990 added loggin to data:load methods

--- app/models/collection_extra.rb
+++ app/models/collection_extra.rb
@@ -9,6 +9,7 @@ class CollectionExtra < ActiveRecord::Base
   store :orbit, coder: JSON
 
   def self.build_echo_client(env=(@cmr_env || Rails.configuration.cmr_env))
+    puts "Building echo client for env: #{env || 'env not working'}"
     Echo::Client.client_for_environment(env, Rails.configuration.services)
   end
 
