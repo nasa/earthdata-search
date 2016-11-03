34688a33898441ed6fbdf6aab6c7ab4273069c8b
Matthew Crouch <matthew@element84.com>
2016-11-03 14:41:21 -0400

    EDSC-1179 fix tests

--- spec/models/gibs_configuration_spec.rb
+++ spec/models/gibs_configuration_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe GibsConfiguration do
   context 'load_configs' do
     it 'merges custom configs into worldview configs' do
-      mopit_config = {:collections=>{"condition"=>{"concept_id"=>"C6011924-LARC_ASDC"}}, :config=>{:match=>{"time_start"=>">=2000-03-01"}, :product=>"CERES_Terra_TOA_Window_Region_Flux_All_Sky_Monthly", :maxNativeZoom=>5, :title=>"TOA Window-Region Flux (Monthly, All-Sky)", :source=>"Terra / CERES", :format=>"png", :resolution=>"2km", :geo=>true, :arctic=>false, :antarctic=>false}}
+      mopit_config = {:collections=>{"condition"=>{"concept_id"=>"C84942916-LARC"}}, :config=>{:match=>{"time_start"=>">=2000-02-01"}, :product=>"MISR_Cloud_Stereo_Height_Histogram_Bin_1.5-2.0km_Monthly", :maxNativeZoom=>5, :title=>"Cloud Stereo Height (No Wind Correction, 1.5 - 2.0 km, Monthly)", :source=>"Terra / MISR", :format=>"png", :resolution=>"2km", :geo=>true, :arctic=>false, :antarctic=>false}}
 
       worldview_url = 'https://worldview.earthdata.nasa.gov/config/wv.json'
       gibs_configuration = GibsConfiguration.new
