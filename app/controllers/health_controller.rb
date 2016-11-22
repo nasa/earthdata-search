b948480f7a4c9571b7053d34b0f7fcf1cbd6f580
John Boukes <jboukes@raytheon.com>
2016-11-21 16:43:31 -0800

    EDSC-1200: Removed unnecessary XHR branch of logic

--- app/controllers/health_controller.rb
+++ app/controllers/health_controller.rb
@@ -25,30 +25,26 @@ class HealthController < ApplicationController
   end
 
   def redoHealthCheck
-    if request.xhr?
-      render json: {errors: {error: 'SSL Certificate Failed'}}, status: 504
-    else
-      health = Health.new
-      response = {
-          edsc: nil,
-          background_jobs: {
-              delayed_job: health.delayed_job_status,
-              data_load_tags: health.data_load_tags_status,
-              data_load_echo10: health.data_load_echo10_status,
-              data_load_granules: health.data_load_granules_status,
-              colormaps_load: health.colormap_load_status,
-          },
-          dependencies: {
-              echo: health.echo_status(echo_client),
-              cmr: health.cmr_status(echo_client),
-              cmr_search: health.cmr_search_status(echo_client),
-              opensearch: health.opensearch_status(echo_client),
-              browse_scaler: health.browse_scaler_status(echo_client),
-              urs: {'ok?' => false, 'error' => 'Faraday::Error::ConnectionFailed (SSL Certificate Failed)'}
-          }
-      }
-      response[:edsc] = health.edsc_status
-      respond_with(response, status: :ok)
-    end 
+    health = Health.new
+    response = {
+        edsc: nil,
+        background_jobs: {
+            delayed_job: health.delayed_job_status,
+            data_load_tags: health.data_load_tags_status,
+            data_load_echo10: health.data_load_echo10_status,
+            data_load_granules: health.data_load_granules_status,
+            colormaps_load: health.colormap_load_status,
+        },
+        dependencies: {
+            echo: health.echo_status(echo_client),
+            cmr: health.cmr_status(echo_client),
+            cmr_search: health.cmr_search_status(echo_client),
+            opensearch: health.opensearch_status(echo_client),
+            browse_scaler: health.browse_scaler_status(echo_client),
+            urs: {'ok?' => false, 'error' => 'Faraday::Error::ConnectionFailed (SSL Certificate Failed)'}
+        }
+    }
+    response[:edsc] = health.edsc_status
+    respond_with(response, status: :ok)
   end
 end
