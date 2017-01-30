825208019c2e21abd790e6e7d0c0f2fb0073344c
Bo Sun <bo.sun@nasa.gov>
2017-01-30 13:39:18 -0500

    Health check. Check for running crons.

--- app/models/health.rb
+++ app/models/health.rb
@@ -97,7 +97,7 @@ class Health
       task2 = tasks.select {|task| task.host != task1.host}.last
       if task2.nil?
         log_text = "Cron job '#{task_name}' hasn't been run in the past #{(3 * interval).to_i / 3600.0} hours."
-        Rails.logger.info "Health failure: #{log_text} on the other production host (other than #{task1.host}). #{status1[:error]}"
+        Rails.logger.info "Health check results: FAILED: #{log_text} on the other production host (other than #{task1.host}). #{status1[:error]}"
         @ok = false
         {ok?: false, error: log_text}
       else
@@ -119,20 +119,24 @@ class Health
     if task.status == 'succeeded'
       if task.last_run < Time.now - interval && task.last_run > Time.now - 3 * interval
         log_text = "Suspend cron job checks for #{interval.to_i / 3600.0} hours after a new deployment. Last task execution was #{task.status} at #{task.last_run}"
-        Rails.logger.info "Health failure: #{log_text} on host #{task.host}."
+        Rails.logger.info "Health check results: PENDING: #{log_text} on host #{task.host}."
         return {ok?: true, info: log_text}
       elsif task.last_run < Time.now - 3 * interval
         @ok = false
         log_text = "Cron job '#{task_name}' hasn't been run since #{task.last_run}"
-        Rails.logger.info "Health failure: #{log_text} on host #{task.host}."
+        Rails.logger.info "Health check results: FAILED: #{log_text} on host #{task.host}."
         return {ok?: false, error: log_text}
       else
         return {ok?: true}
       end
+    elsif task.status == 'running'
+      log_text = "Cron job #{task_name} is still running."
+      Rails.logger.info "Health check results: PENDING: #{log_text} on host #{task.host}"
+      return {ok?: true, info: log_text}
     else
       @ok = false
         log_text = "Cron job '#{task_name}' failed in last run at #{task.last_run} with message '#{task.message}'"
-        Rails.logger.info "Health failure: #{log_text} on host #{task.host}."
+        Rails.logger.info "Health check results: FAILED: #{log_text} on host #{task.host}."
       {ok?: false, error: log_text}
     end
   end
