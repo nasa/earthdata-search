70b2068d08b1288e8360d07a285f74b7f9bed826
Bo Sun <bosun58@gmail.com>
2017-03-06 13:33:16 -0500

    EDSC-1257 Select multiple facets. Fix failed specs and update fixtures. (#345)
    * EDSC-1257 Select multiple facets. Fix failed specs and update fixtures.
    
    * EDSC-1257 Fix failed specs.
    
    * EDSC-1257 Add new tests and fixtures.
    
    * EDSC-1257 Multi-select for science keywords.
    
    * EDSC-1257 Set 'or' flag for science keywords.

--- spec/features/collection_facets/collection_facets_spec.rb
+++ spec/features/collection_facets/collection_facets_spec.rb
@@ -129,31 +129,30 @@ describe "Collection Facets", reset: false do
       Capybara.reset_sessions!
       load_page :search, facets: true, env: :sit
       # select a project
-      find("h3.panel-title", text: 'Keywords').click
       find("h3.panel-title", text: 'Project').click
-      find("label.facets-item", text: "EOSDIS").click
+      find("label.facets-item", text: "ESSP").click
       wait_for_xhr
       within(:css, '#collapse5 .panel-body.facets') do
-        expect(page).to have_content("EOSDIS")
+        expect(page).to have_content("ESSP")
         expect(page).to have_css(".facets-item.selected")
       end
       expect(page).to have_css("label.facets-item.selected")
 
       # select a platform
       find("h3.panel-title", text: 'Platform').click
-      find(".facets-item", text: "FIELD INVESTIGATION").click
+      find(".facets-item", text: "Aqua").click
       wait_for_xhr
       within(:css, '.platforms') do
-        expect(page).to have_content("FIELD INVESTIGATION")
+        expect(page).to have_content("Aqua")
         expect(page).to have_css(".facets-item.selected")
       end
       expect(page).to have_css("label.facets-item.selected")
 
       # select a second project
-      find(".facets-item", text: "ESIP").click
+      find(".facets-item", text: "CWIC").click
       wait_for_xhr
       within(:css, '.projects') do
-        expect(page).to have_content("EOSDIS")
+        expect(page).to have_content("ESSP")
         expect(page).to have_css(".facets-item.selected")
       end
       expect(page).to have_css("label.facets-item.selected")
@@ -168,7 +167,7 @@ describe "Collection Facets", reset: false do
       end
 
       before :each do
-        find(".facets-item", text: "ESIP").click
+        find(".facets-item", text: "ESSP").click
         wait_for_xhr
       end
 
@@ -178,17 +177,17 @@ describe "Collection Facets", reset: false do
 
       it "clicks remove from selected facets" do
         within(:css, '.projects') do
-          expect(page).to have_content("ESIP")
+          expect(page).to have_content("ESSP")
           expect(page).to have_css(".facets-item.selected")
 
-          find(".facets-item", text: "ESIP").click
+          find(".facets-item", text: "ESSP").click
         end
 
         expect(page).to have_no_css(".facets-item.selected")
       end
 
       it "clicks remove from facet lists" do
-        find("label.facets-item", text: "ESIP").click
+        find("label.facets-item", text: "ESSP").click
         expect(page).to have_no_css(".facets-item.selected")
       end
     end
@@ -205,12 +204,12 @@ describe "Collection Facets", reset: false do
     end
 
     it "updates the collection results" do
-      expect(page).to have_no_content("AIRX3STD")
+      expect(page).to have_no_content("CloudSat 1B-CPR product version 008")
 
       find("h3.panel-title", text: 'Project').click
-      find(".panel.projects label.facets-item", text: "ESIP").click
+      find(".panel.projects label.facets-item", text: "ESSP").click
 
-      expect(page).to have_content("AIRX3STD")
+      expect(page).to have_content("CloudSat 1B-CPR product version 008")
 
       find("h3.panel-title", text: 'Project').click
     end
@@ -218,12 +217,17 @@ describe "Collection Facets", reset: false do
     it "updates facet lists" do
       find("h3.panel-title", text: 'Project').click
       within(:css, ".projects") do
-        expect(page).to have_content("ESSP")
-
-        find("label.facets-item", text: "ESIP").click
-
-        expect(page).to have_no_content("ESSP")
+        expect(page).to have_content("AMBS")
       end
+
+      find("h3.panel-title", text: 'Platforms').click
+      find("label.facets-item", text: "Aqua").click
+      find("h3.panel-title", text: 'Platforms').click
+
+      within(:css, ".projects") do
+        expect(page).to have_no_content("AMBS")
+      end
+
       find("h3.panel-title", text: 'Project').click
     end
 
@@ -470,26 +474,57 @@ describe "Collection Facets", reset: false do
   #  end
   #end
 
-  context "selecting multiple organizations and clear filter" do
+  context "selecting multiple facets from different categories" do
     before :all do
       find("h3.panel-title", text: 'Organizations').click
       within(:css, '.organizations') do
-        find(".facets-item", text: "LaRC", match: :prefer_exact).click
+        find(".facets-item", text: "DOE/ORNL/ESD/CDIAC", match: :prefer_exact).click
       end
       wait_for_xhr
-      within(:css, '.organizations') do
-        find(".facets-item", text: "GSFC", match: :prefer_exact).click
-      end
-      wait_for_xhr
-      expect(page).to have_no_content("Keywords")
-      expect(page).to have_no_content("Platforms")
 
-      click_link "Clear Filters"
+      find("h3.panel-title", text: 'Platforms').click
+      within(:css, '.platforms') do
+        find(".facets-item", text: "BUOYS", match: :prefer_exact).click
+      end
       wait_for_xhr
+      expect(page).to have_no_content("Instruments")
+      expect(page).to have_no_content("Projects")
+      expect(page).to have_no_content("Processing levels")
+
     end
 
-    it "maintains the original facet category order" do
-      expect(page).to have_content("Subsetting Services Keywords Agriculture")
+    it "'AND's the results of different categories and 'OR's the results of the same category" do
+      expect(page).to have_content('1 Matching Collections')
+
+      within(:css, '.platforms') do
+        find(".facets-item", text: "DC-8", match: :prefer_exact).click
+      end
+
+      expect(page).to have_content('2 Matching Collections')
+    end
+
+    context "and clear filters" do
+      before :all do
+        click_link "Clear Filters"
+        wait_for_xhr
+      end
+
+      it "maintains the original facet category order" do
+        expect(page).to have_content("WEATHER STATIONS 149 Instruments")
+        find("h3.panel-title", text: 'Platforms').click
+      end
+    end
+  end
+
+  context "selecting multiple facets from same category" do
+    before :all do
+      find(".facets-item", text: "Agriculture", match: :prefer_exact).click
+      expect(page).to have_content('1628 Matching Collections')
+    end
+
+    it "'OR's the results of the selected facets" do
+      find(".facets-item", text: "Atmosphere", match: :prefer_exact).click
+      expect(page).to have_content('5607 Matching Collections')
     end
   end
 end
