f339af8e6c97aaea20ecfe9a7473ba788bcee3a9
Matthew Crouch <matthew@element84.com>
2017-03-08 11:20:13 -0500

    EDSC-1382 record fixtures for umm 1.9, fix tests, and rubocop fixes

--- spec/features/collections/collection_details_spec.rb
+++ spec/features/collections/collection_details_spec.rb
@@ -23,13 +23,13 @@ describe 'Collection details', reset: false do
     end
   end
 
-  context "when selecting a collection that is only viewable after logging in" do
+  context 'when selecting a collection that is only viewable after logging in' do
     before :all do
       Capybara.reset_sessions!
       load_page :search, env: :uat
       login
       wait_for_xhr
-      fill_in 'keywords', with: 'C1216386350-NSIDC_TS1'
+      fill_in 'keywords', with: 'C1216393716-EDF_OPS'
       wait_for_xhr
     end
 
@@ -37,7 +37,7 @@ describe 'Collection details', reset: false do
       Capybara.reset_sessions!
     end
 
-    it "displays the collection details" do
+    it 'displays the collection details' do
       first_collection_result.click_link('View collection details')
       wait_for_xhr
       within('#collection-details') do
@@ -47,14 +47,14 @@ describe 'Collection details', reset: false do
         expect(page).to have_content('VERSION 001')
         expect(page).to have_content('Spatial Coordinates: Bounding Rectangle: (85.0445°, -180.0°, -85.0445°, 180.0°)')
         expect(page).to have_content('Metadata Formats: HTML | Native | ATOM | ECHO10 | ISO19115 | DIF')
-        expect(page).to have_content('Temporal Extent: 2015-03-31 ongoing')
+        expect(page).to have_content('Temporal Extent: 2015-03-31 to 2020-12-31')
         expect(page).to have_content('API Endpoints: CMR OSDD')
         expect(page).to have_content('Science Keywords: Earth ScienceLand SurfaceSoils')
       end
     end
   end
 
-  context "when selecting a collection without contacts in the xml" do
+  context 'when selecting a collection without contacts in the xml' do
     before :all do
       load_page :search, q: 'Aqua_AMSR-E_L3_TB_23.8GHz-H'
       first_collection_result.click_link('View collection details')
@@ -65,7 +65,7 @@ describe 'Collection details', reset: false do
     end
   end
 
-  context "when selecting a collection with point spatial" do
+  context 'when selecting a collection with point spatial' do
     before :all do
       load_page :search, q: 'C179003030-ORNL_DAAC'
       expect(page).to have_content('15 Minute Stream Flow Data: USGS (FIFE)')
@@ -77,7 +77,7 @@ describe 'Collection details', reset: false do
     end
   end
 
-  context "when selecting a collection with bounding box spatial" do
+  context 'when selecting a collection with bounding box spatial' do
     before :all do
       load_page :search
       fill_in 'keywords', with: 'C179002945-ORNL_DAAC'
@@ -90,7 +90,7 @@ describe 'Collection details', reset: false do
     end
   end
 
-  context "when selecting a collection with polygon spatial" do
+  context 'when selecting a collection with polygon spatial' do
     before :all do
       load_page :search
       fill_in 'keywords', with: 'C1220111370-NSIDCV0'
@@ -104,20 +104,20 @@ describe 'Collection details', reset: false do
   end
 
   # FIXME: This collection no longer has line spatial
-  #context "when selecting a collection with line spatial" do
-  #  before :all do
-  #    load_page :search
-  #    fill_in 'keywords', with: 'NSIDC-0239'
-  #    expect(page).to have_content('SMEX02 Atmospheric Aerosol Optical Properties Data')
-  #    first_collection_result.click_link('View collection details')
-  #  end
+  # context 'when selecting a collection with line spatial' do
+  #   before :all do
+  #     load_page :search
+  #     fill_in 'keywords', with: 'NSIDC-0239'
+  #     expect(page).to have_content('SMEX02 Atmospheric Aerosol Optical Properties Data')
+  #     first_collection_result.click_link('View collection details')
+  #   end
   #
-  #  it "displays the collection's spatial bounds on the map" do
-  #    expect(page).to have_css('#map .leaflet-overlay-pane svg.leaflet-zoom-animated path')
-  #  end
-  #end
+  #   it "displays the collection's spatial bounds on the map" do
+  #     expect(page).to have_css('#map .leaflet-overlay-pane svg.leaflet-zoom-animated path')
+  #   end
+  # end
 
-  context "when selecting a collection with multiple temporal" do
+  context 'when selecting a collection with multiple temporal' do
     before :all do
       load_page '/search/collection-details', env: :uat, focus: 'C1204482909-GCMDTEST'
       expect(page).to have_content('CALIPSO Lidar Level 2 5km aerosol profile data V3-01')
@@ -129,7 +129,7 @@ describe 'Collection details', reset: false do
     end
   end
 
-  context "when selecting a collection with multiple temporal fields but some of which have only BeginningDateTime" do
+  context 'when selecting a collection with multiple temporal fields but some of which have only BeginningDateTime' do
     before :all do
       load_page '/search/collection-details', env: :uat, focus: 'C1204424196-GCMDTEST'
       expect(page).to have_content('JAXA/EORC Tropical Cyclones database')
@@ -141,33 +141,33 @@ describe 'Collection details', reset: false do
     end
   end
 
-  context "when selecting a collection with multiple lines of description" do
+  context 'when selecting a collection with multiple lines of description' do
     before :all do
       load_page '/search/collection-details', focus: 'C197265171-LPDAAC_ECS'
     end
 
-    it "displays carriage returns in the description" do
+    it 'displays carriage returns in the description' do
       expect(page).to have_content(" (METI).\n\nASTER is capable of collecting in-")
     end
   end
 
-  context "when selecting a collection with multiple spatial values" do
+  context 'when selecting a collection with multiple spatial values' do
     before :all do
       load_page '/search/collection-details', focus: 'C1214560151-JAXA'
     end
 
-    it "displays all spatial content" do
-      expect(page).to have_content("Bounding Rectangle: (22.0°, -96.0°, -20.0°, -48.0°) Bounding Rectangle: (10.0°, -14.0°, -9.0°, 34.0°) Bounding Rectangle: (27.0°, 92.0°, -20.0°, 151.0°)")
+    it 'displays all spatial content' do
+      expect(page).to have_content('Bounding Rectangle: (22.0°, -96.0°, -20.0°, -48.0°) Bounding Rectangle: (10.0°, -14.0°, -9.0°, 34.0°) Bounding Rectangle: (27.0°, 92.0°, -20.0°, 151.0°)')
     end
   end
 
-  context "when selecting a collection with multiple contacts values from DataCenters and ContactPersons" do
+  context 'when selecting a collection with multiple contacts values from DataCenters and ContactPersons' do
     before :all do
       load_page '/search/collection-details', focus: 'C1220111401-NSIDCV0'
     end
 
-    it "displays all spatial content" do
-      expect(page).to have_content("Contacts: NSIDC DAAC USER SERVICES nsidc@nsidc.org +1 (303) 492-2468 (Fax) +1 (303) 492-6199 (Telephone) Julienne Stroeve stroeve@nsidc.org Walt Meier walter.n.meier@nasa.gov Donald Cavalieri Donald.J.Cavalieri@nasa.gov 1-301-614-5644 (Fax) 1-301-614-5901 (Telephone) NSIDC USER SERVICES nsidc@nsidc.org 1 (303) 492-2468 (Fax) 1 (303) 492-6199 (Telephone) Download Page")
+    it 'displays all spatial content' do
+      expect(page).to have_content('Contacts: NSIDC DAAC USER SERVICES nsidc@nsidc.org +1 (303) 492-2468 (Fax) +1 (303) 492-6199 (Telephone) Julienne Stroeve stroeve@nsidc.org Walt Meier walter.n.meier@nasa.gov Donald Cavalieri Donald.J.Cavalieri@nasa.gov 1-301-614-5644 (Fax) 1-301-614-5901 (Telephone) NSIDC USER SERVICES nsidc@nsidc.org 1 (303) 492-2468 (Fax) 1 (303) 492-6199 (Telephone) Download Page')
     end
   end
 
@@ -176,8 +176,8 @@ describe 'Collection details', reset: false do
       load_page '/search/collection-details', focus: 'C203234517-LAADS'
     end
 
-    it "displays the temporal correctly" do
-      expect(page).to have_content("1999-12-18 ongoing")
+    it 'displays the temporal correctly' do
+      expect(page).to have_content('1999-12-18 ongoing')
     end
   end
 end
