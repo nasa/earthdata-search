4b8dd96f69c2355e5720cf28a5dedffdaec52763
Jeff Siarto <jsiarto@gmail.com>
2017-03-10 12:13:13 -0500

    Granule filter test fixes

--- spec/features/granules/granule_list_spec.rb
+++ spec/features/granules/granule_list_spec.rb
@@ -51,6 +51,7 @@ describe "Granule list", reset: false do
 
     context "clicking on the download button" do
       before :all do
+        find('#granule-search').click_link('close')
         granule_list.find('.master-overlay-global-actions').click_link('Download collection data')
       end
 
@@ -106,6 +107,7 @@ describe "Granule list", reset: false do
       end
 
       it "removes the selected granule from the list" do
+        page.save_screenshot('tmp/screenshots/105.png')
         expect(page).to have_no_content('FIFE_STRM_15M.80611715.s15')
         expect(page).to have_css('#granule-list .panel-list-item', count: 19)
       end
@@ -116,6 +118,8 @@ describe "Granule list", reset: false do
 
       context "until all granules on current page are excluded" do
         before :all do
+          find('#granule-search').click_link('close')
+
           num_of_clicks = 19
           while num_of_clicks > 0
             first_granule_list_item.click_link "Exclude this granule"
