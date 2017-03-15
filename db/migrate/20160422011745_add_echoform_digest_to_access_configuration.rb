65e242c81b47fb4924731aa822e9ce05d8082961
John Boukes <jboukes@raytheon.com>
2017-03-15 14:51:04 -0400

    EDSC-1352: Updated migration file to convert value from string to text in order to prevent DB exception

--- db/migrate/20160422011745_add_echoform_digest_to_access_configuration.rb
+++ db/migrate/20160422011745_add_echoform_digest_to_access_configuration.rb
@@ -1,5 +1,5 @@
 class AddEchoformDigestToAccessConfiguration < ActiveRecord::Migration
   def change
-    add_column :access_configurations, :echoform_digest, :string
+    add_column :access_configurations, :echoform_digest, :text
   end
 end
