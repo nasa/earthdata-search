bfaa44f55876acdf7fd61672c7c88cf6ff5e3eab
John Boukes <jboukes@raytheon.com>
2017-04-08 17:19:29 -0400

    EDSC-1394: Updated flaky test

--- spec/features/map/search_by_collection_location_spec.rb
+++ spec/features/map/search_by_collection_location_spec.rb
@@ -36,7 +36,7 @@ describe "Spatial search by collection location" do
       it "centers the map over the selected granule" do
         script = "$('#map').data('map').map.getCenter().toString()"
         result = page.evaluate_script script
-        expect(result).to eq("LatLng(39.1, -100.25845)")
+        expect(result).to eq("LatLng(39.1, -100.25625)")
       end
 
       it "zooms the map to the selected granule" do
