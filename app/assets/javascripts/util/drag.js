d3de9489eceaedff8957b73bbb0e0296087a5623
Bo Sun <bo.sun@nasa.gov>
2017-04-19 13:24:11 -0400

    EDSC-1363 Set draggable area for collection list.

--- app/assets/javascripts/util/drag.js
+++ app/assets/javascripts/util/drag.js
@@ -1,17 +1,16 @@
-interact('.master-overlay-main')
+interact(".master-overlay-main")
   .resizable({
     restrict: {
-      restriction: {
-        x: 0,
-        y: 250,
-        width: 0,
-        height: 825
-      }
+      restriction: function() {
+        var toolbarOffset = $("#main-toolbar").position().top + $("#main-toolbar").outerHeight();
+        return {x: 0, y: toolbarOffset, height: $(window).height() - toolbarOffset - $("footer").outerHeight() - 50};
+        },
+      endOnly: false
     },
     preserveAspectRatio: false,
     edges: { left: false, right: false, bottom: false, top: true },
   })
-  .on('resizemove', function (event) {
+  .on("resizemove", function (event) {
     var target = event.target;
     target.style.top = 100 - (((event.rect.height / $(window).height())) * 100) + '%';
     target.style.height = (((event.rect.height / $(window).height())) * 100) + '%';
