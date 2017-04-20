3d7525969feb7e8553842abefd86244d9c2a4b4e
Jeff Siarto <jsiarto@gmail.com>
2017-04-20 16:43:56 -0400

    Test fixes

--- spec/features/map/search_by_collection_location_spec.rb
+++ spec/features/map/search_by_collection_location_spec.rb
@@ -36,7 +36,7 @@ describe "Spatial search by collection location" do
       it "centers the map over the selected granule" do
         script = "$('#map').data('map').map.getCenter().toString()"
         result = page.evaluate_script script
-        expect(result).to eq("LatLng(39.1, -100.25625)")
+        expect(result).to eq("LatLng(39.09814, -100.87207)")
       end
 
       it "zooms the map to the selected granule" do
