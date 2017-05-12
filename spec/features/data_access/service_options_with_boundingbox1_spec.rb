dd6dfb485bc7a2449b9871e51de3ddb19f5c974d
Bo Sun <bosun58@gmail.com>
2017-05-12 08:24:40 -0400

    EDSC-1420 Add map to echoform on spatial subsetting. (#418)
    * EDSC-1420 WIP.
    
    * EDSC-1420 code changes.
    
    * EDSC-1420 Add tests.
    
    * EDSC-1420 Fix map load issue.
    
    * EDSC-1420 Use larger mini map. Show map one second retrieval (used to be hidden).

--- spec/features/data_access/service_options_with_boundingbox1_spec.rb
+++ spec/features/data_access/service_options_with_boundingbox1_spec.rb
@@ -8,7 +8,7 @@ require 'rake'
 describe 'Service options order with boundingbox1 field', reset: false do
   collection_id = 'C1000000445-NSIDC_ECS'
 
-  context 'when placing a service order with a collection with a boundingbox1 field' do
+  context 'when filling in spatial subsetting coordinates on data configure page' do
     before :all do
       Delayed::Worker.delay_jobs = true
 
@@ -29,30 +29,39 @@ describe 'Service options order with boundingbox1 field', reset: false do
       fill_in 'East', with: '-100'
       fill_in 'South', with: '-76'
       wait_for_xhr
-
-      within '.access-item-actions' do
-        # sleep 2
-        click_on 'Continue'
-      end
-      click_on 'Submit'
-      wait_for_xhr
     end
 
     after :all do
       Delayed::Worker.delay_jobs = false
     end
 
-    it 'initially shows the order in the "Creating" state' do
-      expect(page).to have_text('Creating')
+    it 'shows a map with the entered bounding box on it' do
+      expect(page).to have_selector('#image-map')
+      expect(page).to have_selector(".leaflet-overlay-pane path[stroke='#54F7A3']")
     end
 
-    context 'after the order processes' do
+    context 'placing a service order with a collection with a boundingbox1 field' do
       before :all do
-        Delayed::Worker.new.work_off
+        within '.access-item-actions' do
+          # sleep 2
+          click_on 'Continue'
+        end
+        click_on 'Submit'
+        wait_for_xhr
       end
 
-      it 'shows the order in the "Complete" state' do
-        expect(page).to have_text('Complete')
+      it 'initially shows the order in the "Creating" state' do
+        expect(page).to have_text('Creating')
+      end
+
+      context 'after the order processes' do
+        before :all do
+          Delayed::Worker.new.work_off
+        end
+
+        it 'shows the order in the "Complete" state' do
+          expect(page).to have_text('Complete')
+        end
       end
     end
   end
