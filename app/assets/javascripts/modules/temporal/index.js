d717bef4e19ef69ffe7327b0db01c4d294d0992e
John Boukes <jboukes@raytheon.com>
2017-08-17 14:23:49 -0400

    EDSC-1418: Removed reference to deleted file

--- app/assets/javascripts/modules/temporal/index.js
+++ app/assets/javascripts/modules/temporal/index.js
@@ -1,4 +1,3 @@
 //= require bootstrap-datepicker/bootstrap-datepicker
 //= require modules/temporal/temporal_range_selection
-//= require modules/temporal/temporal_datepicker
 //= require bootstrap-slider/bootstrap-slider
