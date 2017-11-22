214203eb47b1e97c47470870803b0ae8339ad424
John Boukes <jboukes@raytheon.com>
2017-11-22 12:23:43 -0500

    EDSC-1683: Created stub for setting x-frame-options

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,8 @@ module EarthdataSearchClient
     config.autoload_paths += Dir["#{config.root}/app/services", "#{config.root}/app/services/**/"]
     config.eager_load_paths += Dir["#{config.root}/app/services", "#{config.root}/app/services/**/"]
 
+    config.action_dispatch.default_headers['X-Frame-Options'] = "ALLOW-FROM https://[scriptdomain]"
+
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
