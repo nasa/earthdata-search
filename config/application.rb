59b750246020fd6c8350eada7d573ab1d9cd11b9
John Boukes <jboukes@raytheon.com>
2017-11-22 12:34:45 -0500

    EDSC-1683: Changed x-frame-options to 'deny'

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ module EarthdataSearchClient
     config.autoload_paths += Dir["#{config.root}/app/services", "#{config.root}/app/services/**/"]
     config.eager_load_paths += Dir["#{config.root}/app/services", "#{config.root}/app/services/**/"]
 
-    config.action_dispatch.default_headers['X-Frame-Options'] = "ALLOW-FROM https://[scriptdomain]"
+    config.action_dispatch.default_headers['X-Frame-Options'] = "DENY"
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
