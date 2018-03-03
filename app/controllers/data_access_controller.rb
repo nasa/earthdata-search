f350c6cd52b8b7c57857679a8ce53aa265429a0c
John Boukes <jboukes@raytheon.com>
2018-03-03 13:08:43 -0500

    EDSC-1534: Removed debug code

--- app/controllers/data_access_controller.rb
+++ app/controllers/data_access_controller.rb
@@ -66,15 +66,7 @@ class DataAccessController < ApplicationController
       end
     end
     new_job = Retrieval.delay(:queue => queue).process(retrieval.id, token, cmr_env, edsc_path(request.base_url + '/'), session[:access_token])
-    if new_job
-      if new_job.queue
-        Rails.logger.info("Delayed job " + new_job.id.to_s + " has been sent into queue " + new_job.queue.to_s + " with:" + params[:project]) 
-      end
-      Rails.logger.info("There was an issue reading the queue from the newest job.")
-    else
-      Rails.logger.info("Retrieval returned an unexpected object when attempting to create a new job.")
-    end
-
+    Rails.logger.info("Delayed job " + new_job.id.to_s + " has been sent into queue " + new_job.queue.to_s + " with:" + params[:project]) 
     redirect_to edsc_path("/data/retrieve/#{retrieval.to_param}")
   end
 
