04953cffc3fff65ffdc73378683376a645690816
John Boukes <jtboukes@gmail.com>
2018-04-12 11:32:53 -0700

    EDSC-1828: Updated facets feature 'Subsetting Services' to 'Customizable' (#601)
    * EDSC-1828: Updated facets feature 'Subsetting Services' to 'Customizable'
    
    * EDSC-1828: Updated tooltip
    
    * EDSC-1828: Updated subsetting badge to 'Customizable'
    
    * EDSC-1828: Updated tests

--- spec/features/collections/collection_results_badges_spec.rb
+++ spec/features/collections/collection_results_badges_spec.rb
@@ -8,6 +8,6 @@ describe "Collection results", reset: false, pending_updates: true do
   it "displays a badge for OPeNDAP-enabled collections" do
     fill_in "keywords", with: "C1211793450-PODAAC"
     wait_for_xhr
-    expect(page).to have_css('.badge-subsetting')
+    expect(page).to have_css('.badge-customizable')
   end
 end
--- spec/features/collections/collection_subsetting_filtering_spec.rb
+++ spec/features/collections/collection_subsetting_filtering_spec.rb
@@ -1,59 +1,59 @@
 require 'spec_helper'
 
-describe 'Collection "Subsetting Services" Filtering', reset: false do
+describe 'Collection "Customizable" Filtering', reset: false do
   before :all do
     Capybara.reset_sessions!
   end
 
-  context 'when a collection search result has ESI subsetting' do
+  context 'when a collection search result has ESI customizability' do
     before :all do
       load_page :search, q: 'C179014697-NSIDC_ECS'
     end
 
-    it 'displays the subsetting badge on the collection' do
-      expect(first_collection_result).to have_css('.badge-subsetting', count: 1)
+    it 'displays the customizable badge on the collection' do
+      expect(first_collection_result).to have_css('.badge-customizable', count: 1)
     end
   end
 
-  context 'when a collection search result has OPeNDAP subsetting' do
+  context 'when a collection search result has OPeNDAP customizability' do
     before :all do
       load_page :search, q: 'C179014688-NSIDC_ECS'
     end
 
-    it 'displays the subsetting badge on the collection' do
-      expect(first_collection_result).to have_css('.badge-subsetting', count: 1)
+    it 'displays the customizable badge on the collection' do
+      expect(first_collection_result).to have_css('.badge-customizable', count: 1)
     end
   end
 
-  context 'when a collection search result has no subsetting' do
+  context 'when a collection search result has no customizability' do
     before :all do
       load_page :search, q: 'C179002726-ORNL_DAAC'
     end
 
-    it 'displays the subsetting badge on the collection' do
-      expect(first_collection_result).to have_no_css('.badge-subsetting')
+    it 'displays the customizable badge on the collection' do
+      expect(first_collection_result).to have_no_css('.badge-customizable')
     end
   end
 
-  context 'when selecting the "Subsetting Services" filter' do
+  context 'when selecting the "Customizable" filter' do
     before :all do
       load_page :search, facets: true
-      find('.facets-item', text: 'Subsetting Services').click
+      find('.facets-item', text: 'Customizable').click
       wait_for_xhr
     end
 
-    it 'shows only subsetting-enabled collections' do
-      expect(page).to have_css('.badge-subsetting', count: 20)
+    it 'shows only customizable-enabled collections' do
+      expect(page).to have_css('.badge-customizable', count: 20)
     end
 
-    context 'when un-selecting the "Subsetting Services" filter' do
+    context 'when un-selecting the "Customizable" filter' do
       before :all do
-        find('.facets-item', text: 'Subsetting Services').click
+        find('.facets-item', text: 'Customizable').click
         wait_for_xhr
       end
 
       it 'shows all collections' do
-        expect(page).to have_css('.circle-badge-subsetting', count: 1)
+        expect(page).to have_css('.circle-badge-customizable', count: 1)
       end
     end
   end
