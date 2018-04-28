19aa3e6c68ff2c7a0f3456974c104665bc1ef96a
Ryan Abbott <ryan@element84.com>
2018-04-28 15:02:45 -0400

    EDSC-1729: Ensure that the opendap urls come back with proper subsetting

--- app/controllers/concerns/granule_utils.rb
+++ app/controllers/concerns/granule_utils.rb
@@ -37,4 +37,55 @@ module GranuleUtils
       #TODO other 'translations' here
     end
   end
+
+  # Provided a project and specific collection from the project this method will
+  # ping OUS to retrieve links to the granules from the appropriate OPeNDAP server
+  def fetch_ous_response(options)
+    project = Retrieval.find(options[:project])
+    project_params = Rack::Utils.parse_nested_query(project.jsondata['query'])
+
+    # Get the location of the collection in the collections list
+    collection_index = project_params['p'].split('!').index(options[:collection])
+
+    # Retrieve the variable concept ids, if any exist
+    collection_variables = project_params.fetch('pg', {}).fetch(collection_index.to_s, {})['variables'] if collection_index
+
+    # OUS expects Variable names, so we'll retrieve them from CMR
+    variable_names = variable_names({ cmr_format: 'umm_json', concept_id: collection_variables.split('!') }, token).join(',') if collection_variables
+
+    # Filter out granules that have been excluded by supported opendap subsetting
+    granules_params = {
+      'echo_collection_id' => options[:collection]
+    }.merge(project_params.select { |param| %w(temporal bounding_box).include?(param) })
+
+    granules_response = echo_client.get_granules(granules_params, token)
+
+    return [] unless granules_response.success?
+
+    granule_ids = granules_response.body.fetch('feed', {}).fetch('entry', {}).map { |g| g['id'] }
+
+    # Now that we have the granules that we're looking for, we'll construct the
+    # payload to send to OUS
+    ous_params = {
+      'coverage' => granule_ids.join(',')
+    }
+
+    # Spatial subsetting is used to subset variables, so we'll only concern ourselves
+    # with spatial subsetting if variables are provided. Additionally OPeNDAP only
+    # supports bounding boxes for spatial subsetting
+    if collection_variables
+      ous_params['RangeSubset'] = variable_names
+
+      if granules_params.key?('bounding_box') && granules_params['bounding_box']
+        bb = granules_params['bounding_box'].split(',')
+
+        # WCS requires that this key be the same so we're adding it as an array
+        # and within the OUS Client we've configured Faraday to use `FlatParamsEncoder`
+        # that will prevent the addition of `[]` at the end of the keys in the url
+        ous_params['subset'] = ["lat(#{bb[1]},#{bb[3]})", "lon(#{bb[0]},#{bb[2]})"]
+      end
+    end
+
+    ous_client.get_coverage(ous_params).body
+  end
 end
