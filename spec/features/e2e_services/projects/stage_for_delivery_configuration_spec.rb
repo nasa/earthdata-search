a90e15d0d56edd63ea348ba9b0331d3c883e6fc5
Ryan Abbott <ryan@element84.com>
2018-06-27 18:36:34 -0400

    EDSC-1897: Adds ability to stage data through new project flow

---
+++ spec/features/e2e_services/projects/stage_for_delivery_configuration_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+
+describe 'When viewing the project page with an EGI supported collection', reset: false do
+  before :all do
+    load_page :search, project: ['C1000000739-DEV08'], env: :sit
+
+    login
+    wait_for_xhr
+
+    click_link('My Project')
+    wait_for_xhr
+  end
+
+  context 'When choosing to customize the collection' do
+    before :all do
+      collection_card = find('.collection-card', match: :first)
+
+      collection_card.find('.customize').click
+    end
+
+    it 'displays the customization modal' do
+      within '.echo-forms .modal-header' do
+        expect(page).to have_content('Customization Options')
+      end
+    end
+
+    it 'displays the variable selection option within the modal' do
+      within '.echo-forms .modal-body' do
+        expect(page).to have_css('.access-form')
+      end
+    end
+
+    it 'displays the Order Distribution Options form' do
+      within '.echo-forms .modal-body' do
+        expect(page).to have_css(:h1, text: 'Order Distribution Options')
+      end
+    end
+  end
+end
