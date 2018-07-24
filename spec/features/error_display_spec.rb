8f9d7a35a623f8adb6c8cad334961869557af830
Matthew Crouch <matthew@element84.com>
2018-07-24 16:26:41 -0400

    EDSC-1922 fix tests and rubocop changes

--- spec/features/error_display_spec.rb
+++ spec/features/error_display_spec.rb
@@ -1,8 +1,8 @@
 # EDSC-247: Allow retrying collection and granule searches after timeout errors
 
-require "spec_helper"
+require 'spec_helper'
 
-describe "Displaying system errors", reset: false do
+describe 'Displaying system errors', reset: false do
   context 'when a system error occurs' do
     before :all do
       load_page :search
@@ -10,21 +10,21 @@ describe "Displaying system errors", reset: false do
       wait_for_xhr
     end
 
-    it "displays an error message containing the type of request that caused the error" do
+    it 'displays an error message containing the type of request that caused the error' do
       expect(page.find('.banner-error')).to have_text('Error retrieving collections')
     end
 
-    it "displays the readable message from the server, if available" do
+    it 'displays the readable message from the server, if available' do
       expect(page).to have_text('Some error string')
     end
 
-    it "displays a link to retry requests" do
+    it 'displays a link to retry requests' do
       expect(page).to have_link('Retry')
     end
 
     context 'and the user performs a subsequent successful request' do
       before :all do
-        fill_in 'keywords', with: ' '
+        click_on 'Clear Filters'
         wait_for_xhr
       end
 
