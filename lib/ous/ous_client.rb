6f3e6299d4467148758033e6ef43bf5cf324e7a0
Matthew Crouch <matthew@element84.com>
2018-08-13 15:20:30 -0400

    EDSC-1939: Added configuration for OUS API version

--- lib/ous/ous_client.rb
+++ lib/ous/ous_client.rb
@@ -11,5 +11,11 @@ module Ous
 
       get("ous/collection/#{collection_id}", default_params.merge(params), token_header(token))
     end
+
+    protected
+
+    def default_headers
+      { 'cmr-media-type' => "cmr-opendap.#{Rails.configuration.ous_version}; format=json" }
+    end
   end
 end
