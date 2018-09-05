21f1041fc0ef4ac4fd13695f889e30626b8ac6bd
Matthew Crouch <matthew@element84.com>
2018-09-05 12:00:32 -0400

    EDSC-1693: Remove Reverb redirect functionality

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,4 @@
 class SearchController < ApplicationController
-
   before_filter :set_env_session
 
   respond_to :json
@@ -7,24 +6,7 @@ class SearchController < ApplicationController
   def extract_filters
     is_immediate_reentered = (params.delete :rerun) == 'true'
     previous_keyword = params.delete :previous_q
-    metrics_event('immediate-reenter', {keyword: params[:q], previous_keyword: previous_keyword}) if is_immediate_reentered
+    metrics_event('immediate-reenter', keyword: params[:q], previous_keyword: previous_keyword) if is_immediate_reentered
     respond_with TextSearchClient.parse_text(params[:q])
-  end
-
-  def log_metrics_event
-    if params[:type] && params[:data] then
-      if params[:type] == 'reverb_redirect'
-        if params[:data] == 'back_to_reverb' || params[:data] == 'stay_in_edsc' then
-          if params[:other_data] && params[:other_data] != "" then
-            if params[:other_data] == "{source: 'modal link'}" || params[:other_data] == "{source: 'toolbar link'}"
-              metrics_event(params[:type], params[:data], eval(params[:other_data])) 
-            end
-          else 
-            metrics_event(params[:type], params[:data])
-          end
-        end
-      end
-    end
-    render nothing: true, status: :ok, content_type: "text/html"
   end
 end
