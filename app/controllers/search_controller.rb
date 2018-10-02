882d9cbdb6504149d3533af10c719ee45b0f8655
Matthew Crouch <matthew@element84.com>
2018-10-02 15:58:16 -0400

    EDSC-1927: If nlp_parser_url doesn't exist, don't use NLP

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,9 +4,21 @@ class SearchController < ApplicationController
   respond_to :json
 
   def extract_filters
+    return respond_with empty_nlp_response if ENV['nlp_parser_url'].nil?
+
     is_immediate_reentered = (params.delete :rerun) == 'true'
     previous_keyword = params.delete :previous_q
     metrics_event('immediate-reenter', keyword: params[:q], previous_keyword: previous_keyword) if is_immediate_reentered
     respond_with TextSearchClient.parse_text(params[:q])
+  end
+
+  private
+
+  def empty_nlp_response
+    {
+      'edscSpatial': nil,
+      'edscTemporal': nil,
+      'keyword': params[:q]
+    }
   end
 end
