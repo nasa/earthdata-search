b9edd428ee8a5fa8aaed30c821ebed8cc0850669
Trevor Lang <trevorlang@ymail.com>
2018-11-09 02:13:08 -0700

    EDSC-1985: Implementing sidebar panel

--- app/assets/javascripts/components/index.js
+++ app/assets/javascripts/components/index.js
@@ -1,1 +1,4 @@
-//= require components/collection-list-item/collection-list-item
+//= require components/collection_list_item/collection_list_item
+//= require components/master_overlay_panel/master_overlay_panel
+//= require components/master_overlay_panel/master_overlay_panel_group
+//= require components/master_overlay_panel/master_overlay_panel_item
