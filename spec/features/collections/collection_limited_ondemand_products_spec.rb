500ddcde0b53fbcd2dcc47f03055de199813b1ce
Matthew Crouch <matthew@element84.com>
2019-01-03 16:01:24 -0500

    EDSC-2024: Adding in more pending tests

--- spec/features/collections/collection_limited_ondemand_products_spec.rb
+++ spec/features/collections/collection_limited_ondemand_products_spec.rb
@@ -1,36 +1,31 @@
 require 'rails_helper'
 
-describe "Limited Collections" do
+describe 'Limited Collections' do
   extend Helpers::CollectionHelpers
 
-
-  before :all do
-    Capybara.reset_sessions!
-    load_page :search, q: 'C14758250-LPDAAC_ECS', close_banner: true
-  end
-
-  pending 'when the number of granules exceeds the collection limit (ASTER for example) in a project' do
+  context 'when the number of granules exceeds the collection limit (ASTER for example) in a project' do
     before :all do
-      target_collection_result('ASTER L1A Reconstructed Unprocessed Instrument Data V003').click_link 'Add collection to the current project'
+      load_page :search, project: 'C14758250-LPDAAC_ECS', authenticate: 'edsc'
       find('div[class="toolbar-secondary"]').click_link 'My Project'
+      wait_for_xhr
+
+      collection_card = find('.project-list-item', match: :first)
+      collection_card.find('.project-list-item-action-edit-options').click
+      wait_for_xhr
+
+      click_on 'Edit Delivery Method'
     end
 
-    after :all do
-      first_project_collection.click_link 'Remove collection from the current project'
-      click_link 'Back to Collection Search'
-    end
-
-    it 'disables the retrieve button' do
-      expect(page).to have_css('button.button-disabled')
-    end
-
-    it 'displays tooltip on mouseover' do
-      page.evaluate_script "$('.button-disabled').trigger('mouseover')"
-      expect(page).to have_content('Due to significant processing times, orders for this collection are limited to 100 granules. Please narrow your search before downloading. Contact the data provider with questions. You can find contact information by clicking on the information icon.')
+    it 'displays a warning to the user' do
+      expect(page).to have_content('This particular data product requires additional processing. Due to the current demand, the turnaround time for producing these products has increased. As a result, orders are limited to 2000 granules per order. To improve the efficiency of the additional processing, all orders up to 2000 granules will be divided into separate requests of 100 granules each with a unique download link for each set of 100 (or less) granules. ')
     end
   end
 
   context 'when the number of granules exceeds the collection limit (ASTER for example) on granule list page' do
+    before :all do
+      load_page :search, q: 'C14758250-LPDAAC_ECS', close_banner: true
+    end
+
     hook_granule_results('ASTER L1A Reconstructed Unprocessed Instrument Data V003')
 
     it 'disables the retrieve button' do
