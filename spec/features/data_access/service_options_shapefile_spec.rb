e69e65e2bb59debdc98dea28a821d3f24854386d
Matthew Crouch <matthew@element84.com>
2019-03-21 17:28:03 -0400

    EDSC-2084: Fix fixtures

--- spec/features/data_access/service_options_shapefile_spec.rb
+++ spec/features/data_access/service_options_shapefile_spec.rb
@@ -1,37 +1,21 @@
-# EDSC-2084
-# Disable the use-shapefile checkbox if there was no upload during search
-
 require 'rails_helper'
-require 'rake'
 
 describe 'Service options order with use shapefile field' do
-  collection_id = 'C1200343735-DEV07'
-
   context 'when viewing service order form' do
     before :all do
-      Delayed::Worker.delay_jobs = true
+      load_page :projects_page, project: ['C1200343735-DEV07'], authenticate: 'edsc', env: :sit
 
-      load_page :search, q: collection_id, authenticate: 'edsc', env: :sit
+      collection_card = find('.project-list-item', match: :first)
+      collection_card.find('.project-list-item-action-edit-options').click
       wait_for_xhr
 
-      first_collection_result.click
+      click_on 'Edit Delivery Method'
+      choose('Customize & Download')
       wait_for_xhr
-
-      find('.retrieve').click
-
-      find('.project-list-item-action-edit-options').click
-      find('button', text: 'Edit Delivery Method').click
-      find('#access-method-' + collection_id + '-02').click
-      wait_for_xhr
-    end
-
-    after :all do
-      Delayed::Worker.delay_jobs = false
     end
 
     it 'contains a disabled checkbox for using shapefile from search' do
-      expect(page).to have_field('ICESAT2-use-shapefile-element', :type => 'checkbox', :disabled => true)
+      expect(page).to have_field('ICESAT2-use-shapefile-element', type: 'checkbox', disabled: true)
     end
-
   end
 end
