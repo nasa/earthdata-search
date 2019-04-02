5183f8a70abf9ea5a3365fd2e01d6bd4773ff8a3
Trevor Lang <trevorlang@ymail.com>
2019-04-02 13:55:54 -0600

    EDSC-2139: Improving collection panel

--- static/src/js/containers/CollectionResultsContainer/CollectionResultsContainer.js
+++ static/src/js/containers/CollectionResultsContainer/CollectionResultsContainer.js
@@ -127,37 +127,63 @@ class CollectionResultsContainer extends Component {
         <header className="container-fluid p-3 coll-res__header">
           <div className="row">
             <div className="col">
-              <form action="/">
-                <span>
-                  Sort by:
-                  <select id="input__sort-relevance">
-                    <option value="">Relevance</option>
-                    <option value="">Usage</option>
-                    <option value="">End Date</option>
-                  </select>
-                </span>
-                <label
-                  className="m-0"
-                  htmlFor="input__only-granules"
-                >
-                  <input
-                    id="input__only-granules"
-                    type="checkbox"
-                    defaultChecked
-                  />
-                   Only include collections with granules
-                </label>
-                <label
-                  className="m-0"
-                  htmlFor="input__non-eosdis"
-                >
-                  <input
-                    id="input__non-eosdis"
-                    type="checkbox"
-                    defaultChecked
-                  />
-                   Only include collections with granules
-                </label>
+              <form className="form-inline mb-1" action="/">
+                <div className="form-row align-items-center">
+                  <div className="col-auto">
+                    <div className="form-group">
+                      <label
+                        className="mr-1"
+                        htmlFor="input__sort-relevance"
+                      >
+                        Sort by:
+                      </label>
+                      <select
+                        id="input__sort-relevance"
+                        className="form-control"
+                      >
+                        <option value="relevance">Relevance</option>
+                        <option value="usage">Usage</option>
+                        <option value="end_data">End Date</option>
+                      </select>
+                    </div>
+                  </div>
+                  <div className="col-auto">
+                    <div className="form-group">
+                      <div className="form-check">
+                        <input
+                          id="input__only-granules"
+                          className="form-check-input"
+                          type="checkbox"
+                          defaultChecked
+                        />
+                        <label
+                          className="form-check-label"
+                          htmlFor="input__only-granules"
+                        >
+                           Only include collections with granules
+                        </label>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-auto">
+                    <div className="form-group">
+                      <div className="form-check">
+                        <input
+                          id="input__non-eosdis"
+                          className="form-check-input"
+                          type="checkbox"
+                          defaultChecked
+                        />
+                        <label
+                          className="form-check-label"
+                          htmlFor="input__non-eosdis"
+                        >
+                          Include non-EOSDIS collections
+                        </label>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </form>
             </div>
           </div>
