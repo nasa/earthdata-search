13c3d09778418ac37124034a3d7ddb6a94faf14f
Matthew Crouch <matthew@element84.com>
2019-05-19 13:48:31 -0400

    EDSC-2209: Pull duplicated code into util functions and add tests

--- serverless/src/__tests__/util.test.js
+++ serverless/src/__tests__/util.test.js
@@ -1,4 +1,19 @@
-import { pick } from '../util'
+import moxios from 'moxios'
+import jwt from 'jsonwebtoken'
+import fs from 'fs'
+
+import {
+  pick,
+  buildURL,
+  doSearchRequest,
+  prepareExposeHeaders
+} from '../util'
+
+const config = JSON.parse(fs.readFileSync('config.json'))
+
+beforeEach(() => {
+  jest.clearAllMocks()
+})
 
 describe('util#pick', () => {
   test('correctly returns when null is provided', () => {
@@ -25,5 +40,74 @@ describe('util#pick', () => {
     const data = pick(object, desiredKeys)
 
     expect(Object.keys(data).sort()).toEqual(['array', 'b'])
+  })
+})
+
+describe('util#buildURL', () => {
+  test('correctly builds a search URL', () => {
+    process.env.cmrHost = 'http://example.com'
+
+    const body = '{"params":{"param1":123,"param2":"abc","param3":["987"]}}'
+    const permittedCmrKeys = [
+      'param1',
+      'param2',
+      'param3'
+    ]
+    const nonIndexedKeys = [
+      'param3'
+    ]
+
+    const params = {
+      body,
+      nonIndexedKeys,
+      path: '/search/path',
+      permittedCmrKeys
+    }
+    expect(buildURL(params)).toEqual('http://example.com/search/path?param1=123&param2=abc&param3%5B%5D=987')
+  })
+})
+
+describe('util#prepareExposeHeaders', () => {
+  test('appends jwt-token to expose headers when they exist', () => {
+    expect(prepareExposeHeaders({ 'access-control-expose-headers': 'header-1' })).toEqual('header-1, jwt-token')
+  })
+
+  test('returns jwt-token when no expose headers exist', () => {
+    expect(prepareExposeHeaders({})).toEqual('jwt-token')
+  })
+})
+
+xdescribe('util#doSearchRequest', () => {
+  beforeEach(() => {
+    moxios.install()
+
+    jest.clearAllMocks()
+  })
+
+  afterEach(() => {
+    moxios.uninstall()
+  })
+
+  test('correctly returns the search response', async () => {
+    moxios.stubRequest(/example.*/, {
+      status: 200,
+      response: {}
+    })
+
+    const token = {
+      token: {
+        access_token: '123'
+      }
+    }
+
+    jest.spyOn(jwt, 'verify').mockImplementation(() => token)
+    config.oauth.client.id = 'clientId'
+    const jwtToken = '123.456.789'
+    const url = 'http://example.com/search/path?param1=123&param2=abc&param3%5B%5D=987'
+
+    // await doSearchRequest(jwtToken, url).then((data) => {
+    //   expect(data).toEqual('data?')
+    // })
+    await expect(doSearchRequest(jwtToken, url)).resolves.toEqual('stuff?')
   })
 })
--- serverless/src/collectionSearch.js
+++ serverless/src/collectionSearch.js
@@ -1,12 +1,10 @@
-import request from 'request-promise'
-import jwt from 'jsonwebtoken'
-import fs from 'fs'
-import { pick, cmrStringify } from './util'
+import { buildURL, doSearchRequest } from './util'
 
-const config = JSON.parse(fs.readFileSync('config.json'))
 
 export default async function collectionSearch(event) {
-  const { jwtToken } = event.requestContext.authorizer
+  const { body, requestContext } = event
+  const { jwtToken } = requestContext.authorizer
+
   // Whitelist parameters supplied by the request
   const permittedCmrKeys = [
     'bounding_box',
@@ -48,61 +46,10 @@ export default async function collectionSearch(event) {
     'tag_key'
   ]
 
-  const { params = {} } = JSON.parse(event.body)
-
-  console.log(`Parameters received: ${Object.keys(params)}`)
-
-  const obj = pick(params, permittedCmrKeys)
-
-  console.log(`Filtered parameters: ${Object.keys(obj)}`)
-
-  // Transform the query string hash to an encoded url string
-  const queryParams = cmrStringify(obj, nonIndexedKeys)
-
-  const collectionUrl = `${process.env.cmrHost}`
-      + `/search/collections.json?${queryParams}`
-
-  console.log(`CMR Query: ${collectionUrl}`)
-
-  const token = jwt.verify(jwtToken, config.secret)
-  const { id: clientId } = config.oauth.client
-  try {
-    const response = await request.get({
-      uri: collectionUrl,
-      resolveWithFullResponse: true,
-      headers: {
-        'Echo-Token': `${token.token.access_token}:${clientId}`
-      }
-    })
-
-    const { body, headers } = response
-
-    const { 'access-control-expose-headers': exposeHeaders = '' } = headers
-    const exposeHeadersList = exposeHeaders.split(',').filter(header => header !== '')
-    exposeHeadersList.push('jwt-token')
-    const updatedExposeHeaders = exposeHeadersList.join(', ')
-
-    return {
-      statusCode: response.statusCode,
-      headers: {
-        ...headers,
-        'access-control-expose-headers': updatedExposeHeaders,
-        'jwt-token': jwtToken
-      },
-      body
-    }
-  } catch (e) {
-    console.log('error', e)
-    if (e.response) {
-      return {
-        statusCode: e.statusCode,
-        body: e.response.body
-      }
-    }
-
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: 'Oh No!' })
-    }
-  }
+  return doSearchRequest(jwtToken, buildURL({
+    body,
+    nonIndexedKeys,
+    path: '/search/collections.json',
+    permittedCmrKeys
+  }))
 }
