d5473d8da3c4856adb07e3f1aac366337dbc22bf
Matthew Crouch <matthew@element84.com>
2019-06-18 10:52:21 -0400

    EDSC-2209: Improved login experience

--- static/src/js/containers/AuthRequiredContainer/AuthRequiredContainer.js
+++ static/src/js/containers/AuthRequiredContainer/AuthRequiredContainer.js
@@ -1,27 +1,46 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { get } from 'tiny-cookie'
 
 import { getEarthdataConfig } from '../../../../../sharedUtils/config'
 
-export const AuthRequiredContainer = (props) => {
-  const {
-    children
-  } = props
-  const token = get('authToken')
+export class AuthRequiredContainer extends Component {
+  constructor(props) {
+    super(props)
 
-  const returnPath = window.location.href
-
-  if (token === null) {
-    window.location.href = `${getEarthdataConfig('prod').apiHost}/login?cmr_env=${'prod'}&state=${encodeURIComponent(returnPath)}`
-    return null
+    this.state = {
+      isLoggedIn: false
+    }
   }
 
-  return (
-    <>
-      { children }
-    </>
-  )
+  componentWillMount() {
+    const token = get('authToken')
+
+    const returnPath = window.location.href
+
+    if (token === null) {
+      window.location.href = `${getEarthdataConfig('prod').apiHost}/login?cmr_env=${'prod'}&state=${encodeURIComponent(returnPath)}`
+    } else {
+      this.setState({ isLoggedIn: true })
+    }
+  }
+
+  render() {
+    const { isLoggedIn } = this.state
+
+    if (isLoggedIn) {
+      const { children } = this.props
+      return (
+        <>
+          { children }
+        </>
+      )
+    }
+
+    return (
+      <div className="route-wrapper" />
+    )
+  }
 }
 
 AuthRequiredContainer.propTypes = {
