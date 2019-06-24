65170c9cb527eacc0197054a59a4edfe86c87598
Ryan Abbott <ryan@element84.com>
2019-06-23 21:52:45 -0400

    EDSC-2195: Expect order submission to be wrapped in params key

--- serverless/src/submitOrder/__test__/mocks.js
+++ serverless/src/submitOrder/__test__/mocks.js
@@ -1,19 +1,21 @@
 export const orderPayload = {
-  body: {
-    collections: [
-      {
-        id: 'C10000005-EDSC',
-        access_method: {
-          type: 'download'
-        },
-        granule_count: 139,
-        granule_params: {
-          bounding_box: '23.607421875,5.381262277997806,27.7965087890625,14.973184553280502'
+  body: JSON.stringify({
+    params: {
+      collections: [
+        {
+          id: 'C10000005-EDSC',
+          access_method: {
+            type: 'download'
+          },
+          granule_count: 139,
+          granule_params: {
+            bounding_box: '23.607421875,5.381262277997806,27.7965087890625,14.973184553280502'
+          }
         }
-      }
-    ],
-    environment: 'prod'
-  },
+      ],
+      environment: 'prod'
+    }
+  }),
   requestContext: {
     authorizer: {
       jwtToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbiI6eyJhY2Nlc3NfdG9rZW4iOiIyZThlOTk1ZTc1MTFjMmM2NjIwMzM2Nzk3YiIsInRva2VuX3R5cGUiOiJCZWFyZXIiLCJleHBpcmVzX2luIjozNjAwLCJyZWZyZXNoX3Rva2VuIjoiNWExMDg2NWVlYjNmNDRhODBhYjk1IiwiZW5kcG9pbnQiOiIvYXBpL3VzZXJzL2Vkc2MiLCJleHBpcmVzX2F0IjoiMjAxOS0wNi0xNlQwMTowMTo0OS41NDVaIn0sImlhdCI6MTU2MDY0MzMwOX0.Xn0SuAMdEcU8amhZM0YunyRQN-e3cVzjoo7qJBr-EwI'
@@ -22,20 +24,22 @@ export const orderPayload = {
 }
 
 export const badOrderPayload = {
-  body: {
-    collections: [
-      {
-        id: 'C10000005-EDSC',
-        access_method: {
-          type: 'download'
-        },
-        granule_count: 139,
-        granule_params: {
-          bounding_box: '23.607421875,5.381262277997806,27.7965087890625,14.973184553280502'
+  body: JSON.stringify({
+    params: {
+      collections: [
+        {
+          id: 'C10000005-EDSC',
+          access_method: {
+            type: 'download'
+          },
+          granule_count: 139,
+          granule_params: {
+            bounding_box: '23.607421875,5.381262277997806,27.7965087890625,14.973184553280502'
+          }
         }
-      }
-    ]
-  },
+      ]
+    }
+  }),
   requestContext: {
     authorizer: {
       jwtToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbiI6eyJhY2Nlc3NfdG9rZW4iOiIyZThlOTk1ZTc1MTFjMmM2NjIwMzM2Nzk3YiIsInRva2VuX3R5cGUiOiJCZWFyZXIiLCJleHBpcmVzX2luIjozNjAwLCJyZWZyZXNoX3Rva2VuIjoiNWExMDg2NWVlYjNmNDRhODBhYjk1IiwiZW5kcG9pbnQiOiIvYXBpL3VzZXJzL2Vkc2MiLCJleHBpcmVzX2F0IjoiMjAxOS0wNi0xNlQwMTowMTo0OS41NDVaIn0sImlhdCI6MTU2MDY0MzMwOX0.Xn0SuAMdEcU8amhZM0YunyRQN-e3cVzjoo7qJBr-EwI'
