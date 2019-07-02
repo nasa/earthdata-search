5b3813083685144e2b4547888359dcae6a98a794
Trevor Lang <trevorlang@ymail.com>
2019-07-02 14:56:54 -0600

    EDSC-2279: Adding tests form collection results

---
+++ static/src/js/components/CollectionResults/__tests__/CollectionResultsBody.test.js
@@ -0,0 +1,65 @@
+import React from 'react'
+import Enzyme, { shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import CollectionResultsBody from '../CollectionResultsBody'
+import CollectionResultsList from '../CollectionResultsList'
+
+// TODO: Write more tests
+
+Enzyme.configure({ adapter: new Adapter() })
+
+function setup() {
+  const props = {
+    collections: {
+      allIds: ['collectionId1', 'collectionId2'],
+      byId: {
+        collectionId1: {
+          id: 'collectionId1'
+        },
+        collectionId2: {
+          id: 'collectionId2'
+        }
+      },
+      hits: '181',
+      isLoaded: true,
+      isLoading: false,
+      loadTime: 1150,
+      timerStart: null
+    },
+    location: {
+      pathname: '/test'
+    },
+    onViewCollectionGranules: jest.fn(),
+    onViewCollectionDetails: jest.fn(),
+    waypointEnter: jest.fn()
+  }
+
+  const enzymeWrapper = shallow(<CollectionResultsBody {...props} />)
+
+  return {
+    enzymeWrapper,
+    props
+  }
+}
+
+describe('CollectionResultsBody component', () => {
+  test('renders itself correctly', () => {
+    const { enzymeWrapper } = setup()
+    expect(enzymeWrapper.type()).toEqual('div')
+    expect(enzymeWrapper.props().className).toEqual('collection-results-body')
+    expect(enzymeWrapper.find(CollectionResultsList).length).toEqual(1)
+  })
+
+  test('passes the correct props to CollectionResultsList', () => {
+    const { enzymeWrapper, props } = setup()
+    const collectionResultsList = enzymeWrapper.find(CollectionResultsList)
+    expect(collectionResultsList.props().collections)
+      .toEqual(props.collections)
+    expect(collectionResultsList.props().onViewCollectionGranules)
+      .toEqual(props.onViewCollectionGranules)
+    expect(collectionResultsList.props().onViewCollectionDetails)
+      .toEqual(props.onViewCollectionDetails)
+    expect(collectionResultsList.props().waypointEnter)
+      .toEqual(props.waypointEnter)
+  })
+})
---
+++ static/src/js/components/CollectionResults/__tests__/CollectionResultsList.test.js
@@ -0,0 +1,109 @@
+import React from 'react'
+import Enzyme, { shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import CollectionResultsList from '../CollectionResultsList'
+import CollectionResultsItem from '../CollectionResultsItem'
+
+// TODO: Write more tests
+
+Enzyme.configure({ adapter: new Adapter() })
+
+function setup(propsOverride = {}) {
+  const props = {
+    collections: {
+      allIds: ['collectionId1', 'collectionId2'],
+      byId: {
+        collectionId1: {
+          id: 'collectionId1'
+        },
+        collectionId2: {
+          id: 'collectionId2'
+        }
+      },
+      hits: '181',
+      isLoaded: true,
+      isLoading: false,
+      loadTime: 1150,
+      timerStart: null
+    },
+    location: {
+      pathname: '/test'
+    },
+    onViewCollectionGranules: jest.fn(),
+    onViewCollectionDetails: jest.fn(),
+    waypointEnter: jest.fn(),
+    ...propsOverride
+  }
+
+  const enzymeWrapper = shallow(<CollectionResultsList {...props} />)
+
+  return {
+    enzymeWrapper,
+    props
+  }
+}
+
+describe('CollectionResultsList component', () => {
+  test('renders itself correctly', () => {
+    const { enzymeWrapper } = setup()
+    expect(enzymeWrapper.type()).toEqual('ul')
+    expect(enzymeWrapper.props().className).toEqual('collection-results-list')
+  })
+
+  test('renders its list correctly', () => {
+    const { enzymeWrapper } = setup()
+    expect(enzymeWrapper.find(CollectionResultsItem).length).toEqual(2)
+  })
+
+  test('sets the is last prop on the last list item', () => {
+    const { enzymeWrapper } = setup()
+    expect(enzymeWrapper.find(CollectionResultsItem).at(1).props().isLast).toEqual(true)
+  })
+
+  describe('loading list item', () => {
+    test('shows on first load', () => {
+      const { enzymeWrapper } = setup({
+        collections: {
+          allIds: [],
+          byId: {},
+          hits: null,
+          isLoaded: false,
+          isLoading: true,
+          loadTime: 0,
+          timerStart: null
+        }
+      })
+
+      expect(enzymeWrapper.find('.collection-results-list__loading').length).toEqual(1)
+    })
+
+    test('shows when additional items are being loaded', () => {
+      const { enzymeWrapper } = setup({
+        collections: {
+          allIds: ['collectionId1', 'collectionId2'],
+          byId: {
+            collectionId1: {
+              id: 'collectionId1'
+            },
+            collectionId2: {
+              id: 'collectionId2'
+            }
+          },
+          hits: '181',
+          isLoaded: true,
+          isLoading: true,
+          loadTime: 1150,
+          timerStart: null
+        }
+      })
+
+      expect(enzymeWrapper.find(CollectionResultsItem).length).toEqual(2)
+      expect(enzymeWrapper.find('.collection-results-list__loading').length).toEqual(1)
+    })
+
+    test('does not show the loading item when items are loaded', () => {
+      const { enzymeWrapper } = setup()
+      expect(enzymeWrapper.find('.collection-results-list__loading').length).toEqual(0)
+    })
+  })
+})
