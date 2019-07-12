746325dcf49d4a90c8ce983f373a157820a27246
Matthew Crouch <matthew@element84.com>
2019-07-12 13:33:51 -0400

    EDSC-2197: Add missing headers in lambda response

--- serverless/src/getAccessMethods/handler.js
+++ serverless/src/getAccessMethods/handler.js
@@ -62,6 +62,10 @@ const getAccessMethods = async (event) => {
 
   return {
     statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true
+    },
     body: JSON.stringify({ accessMethods })
   }
 }
