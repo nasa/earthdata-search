d4804bfa782f7d8febb1c597a9cbad69b2c981ae
Matthew Crouch <matthew@element84.com>
2019-09-09 11:38:42 -0400

    EDSC-2310: Update cmrEnv to use static config

--- sharedUtils/cmrEnv.js
+++ sharedUtils/cmrEnv.js
@@ -1,6 +1,14 @@
+import { getApplicationConfig } from './config'
+
 /**
  * Return the CMR environment to use
  */
-export const cmrEnv = () => 'sit'
+export const cmrEnv = () => {
+  const { env } = getApplicationConfig()
+
+  if (env === 'dev') return 'prod'
+
+  return env
+}
 
 export default cmrEnv
