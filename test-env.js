126940dd311e09d978e4a7d7a438c9a5d4ddd6d5
Trevor Lang <trevorlang@ymail.com>
2019-09-11 16:41:44 -0600

    EDSC-2326: Adding jest console error failure

--- test-env.js
+++ test-env.js
@@ -1,4 +1,27 @@
+/**
+ * Set up globals
+ */
+// jQuery
 import $ from 'jquery'
 
 global.$ = $
 global.jQuery = $
+
+// Google Tag Manager dataLayer
+global.dataLayer = {
+  push: jest.fn()
+}
+
+
+/**
+ * Set up console.error override
+ */
+const { error } = console
+
+const consoleError = function errorOverride(message) {
+  // eslint-disable-next-line prefer-rest-params
+  error.apply(console, arguments) // keep default behaviour
+  throw (message instanceof Error ? message : new Error(message))
+}
+
+console.error = consoleError
