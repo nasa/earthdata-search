3a1abaabab67430c6266250be2216fbe11c76bfb
John Cromartie <john@element84.com>
2019-09-17 16:50:04 -0400

    EDSC-2310 Converts collection tagging to step function workflow

--- serverless/src/generateCollectionCapabilityTags/handler.js
+++ serverless/src/generateCollectionCapabilityTags/handler.js
@@ -2,42 +2,59 @@ import 'array-foreach-async'
 import AWS from 'aws-sdk'
 import request from 'request-promise'
 import { stringify } from 'qs'
-import { chunkArray } from '../util/chunkArray'
 import { getSingleGranule } from '../util/cmr/getSingleGranule'
 import { readCmrResults } from '../util/cmr/readCmrResults'
 import { getEarthdataConfig, getClientId } from '../../../sharedUtils/config'
-import { invokeLambda } from '../util/aws/invokeLambda'
 import { cmrEnv } from '../../../sharedUtils/cmrEnv'
 import { getSystemToken } from '../util/urs/getSystemToken'
 
 let cmrToken
-let lambda
-let sqs
+const sqs = new AWS.SQS({ apiVersion: '2012-11-05' })
+
+const pageSize = 100
 
 const tagName = 'edsc.extra.serverless.collection_capabilities'
 
 /**
+ * Returns tags for a collection based on a single granule sample
+ */
+const collectionTags = async (cmrToken, collection) => {
+  const {
+    id
+  } = collection
+
+  const singleGranule = await getSingleGranule(cmrToken, id)
+  const {
+    cloud_cover: cloudCover = false,
+    day_night_flag: dayNightFlag,
+    online_access_flag: onlineAccessFlag = false,
+    orbit_calculated_spatial_domains: orbitCalculatedSpatialDomains = []
+  } = singleGranule
+
+  return {
+    cloud_cover: cloudCover !== undefined,
+    day_night_flag: dayNightFlag && ['DAY', 'NIGHT', 'BOTH'].includes(dayNightFlag.toUpperCase()),
+    granule_online_access_flag: onlineAccessFlag,
+    orbit_calculated_spatial_domains: orbitCalculatedSpatialDomains.length > 0
+  }
+}
+
+/**
  * Handler to process subsetting information from UMM S associations on collections
  */
-const generateCollectionCapabilityTags = async (event) => {
-  if (lambda === undefined) {
-    lambda = new AWS.Lambda({ apiVersion: '2015-03-31' })
-  }
+const generateCollectionCapabilityTags = async (input) => {
+  // CMR uses 1-based indexing for pages, default to page 1
+  const { pageNumber = 1 } = input
 
-  if (sqs === undefined) {
-    sqs = new AWS.SQS({ apiVersion: '2012-11-05' })
-  }
-
-  const { pageNumber = 1, pageSize = 500 } = event
-
+  // lazily initialize system token
   cmrToken = await getSystemToken(cmrToken)
 
   const cmrParams = {
-    has_granules_or_cwic: true,
+    has_granules: true,
     page_num: pageNumber,
     page_size: pageSize,
     include_granule_counts: true,
-    include_tags: 'edsc.extra.serverless.*,org.ceos.wgiss.cwic.granules.prod'
+    include_tags: 'edsc.extra.serverless.*'
   }
 
   const { cmrHost } = getEarthdataConfig(cmrEnv())
@@ -45,107 +62,55 @@ const generateCollectionCapabilityTags = async (event) => {
 
   console.log(`Requesting collections from ${collectionSearchUrl}`)
 
-  let cmrCollectionResponse
-  try {
-    cmrCollectionResponse = await request.post({
-      uri: collectionSearchUrl,
-      form: stringify(cmrParams, { indices: false, arrayFormat: 'brackets' }),
-      headers: {
-        'Client-Id': getClientId().background,
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Echo-Token': cmrToken
-      },
-      json: true,
-      resolveWithFullResponse: true
-    })
-  } catch (e) {
-    console.log(e)
-  }
+  const cmrCollectionResponse = await request.post({
+    uri: collectionSearchUrl,
+    form: stringify(cmrParams, { indices: false, arrayFormat: 'brackets' }),
+    headers: {
+      'Client-Id': getClientId().background,
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Echo-Token': cmrToken
+    },
+    json: true,
+    resolveWithFullResponse: true
+  })
 
   const { 'cmr-hits': cmrHits = 0 } = cmrCollectionResponse.headers
 
-  console.log(`CMR returned ${cmrHits} collections. Current page number is ${pageNumber}, iterating through ${pageSize} at a time.`)
+  console.log(`CMR returned ${cmrHits} collections. Current page number is ${pageNumber}, tagging ${pageSize} collections.`)
 
-  const responseBody = readCmrResults(collectionSearchUrl, cmrCollectionResponse)
+  const collections = readCmrResults(collectionSearchUrl, cmrCollectionResponse)
 
-  // Chunk the results into 100 collections to minimize the size of the payloads we send to the tagging endpoint
-  const chunkedCollections = chunkArray(responseBody, 100)
+  const collectionsWithGranules = collections.filter(collection => collection.granule_count > 0)
 
-  // Each chunk of 100 collections
-  await chunkedCollections.forEachAsync(async (collectionList) => {
-    const associationPayload = []
+  console.log(`number of collections with granules: ${collectionsWithGranules.length}`)
 
-    // Construct the tag data for each collection in this chunk
-    await collectionList.forEachAsync(async (collection) => {
-      const {
-        id,
-        granule_count: granuleCount = 0,
-        tags = {}
-      } = collection
-
-      try {
-        if (Object.keys(tags).includes('org.ceos.wgiss.cwic.granules.prod') && granuleCount === 0) {
-          // Create an SQS Queue for adding CWIC specific data
-        } else {
-          const singleGranule = await getSingleGranule(cmrToken, id)
-
-          if (singleGranule) {
-            const {
-              cloud_cover: cloudCover = false,
-              day_night_flag: dayNightFlag,
-              online_access_flag: onlineAccessFlag = false,
-              orbit_calculated_spatial_domains: orbitCalculatedSpatialDomains = []
-            } = singleGranule
-
-            const tagData = {
-              cloud_cover: cloudCover !== undefined,
-              day_night_flag: dayNightFlag && ['DAY', 'NIGHT', 'BOTH'].includes(dayNightFlag.toUpperCase()),
-              granule_online_access_flag: onlineAccessFlag,
-              orbit_calculated_spatial_domains: orbitCalculatedSpatialDomains.length > 0
-            }
-
-            associationPayload.push({
-              'concept-id': id,
-              data: tagData
-            })
-          } else {
-            console.log(`${id} doesn't have any granules`)
-          }
-        }
-      } catch (e) {
-        console.log(e)
-      }
+  // build a list of associations to create
+  const associationPayload = []
+  await collectionsWithGranules.forEachAsync(async (collection) => {
+    const { id } = collection
+    const tagData = await collectionTags(cmrToken, collection)
+    associationPayload.push({
+      'concept-id': id,
+      data: tagData
     })
-
-    if (associationPayload.length) {
-      await sqs.sendMessage({
-        QueueUrl: process.env.tagQueueUrl,
-        MessageBody: JSON.stringify({
-          tagName,
-          action: 'ADD',
-          tagData: associationPayload
-        })
-      }).promise()
-    }
   })
 
-  if (cmrHits > pageNumber * pageSize) {
-    const lambdaParams = {
-      ...event,
-      pageNumber: pageNumber + 1
-    }
+  console.log(`associationPayload = ${JSON.stringify(associationPayload, null, 4)}`)
 
-    try {
-      await invokeLambda(process.env.collectionCapabilitiesLambda, lambdaParams)
-    } catch (e) {
-      console.log(e)
-    }
+  if (associationPayload.length > 0) {
+    await sqs.sendMessage({
+      QueueUrl: process.env.tagQueueUrl,
+      MessageBody: JSON.stringify({
+        tagName,
+        action: 'ADD',
+        tagData: associationPayload
+      })
+    }).promise()
   }
 
   return {
-    isBase64Encoded: false,
-    statusCode: 200,
-    body: JSON.stringify({ message: `Processing ${chunkedCollections.length} chunks of 100 collections.` })
+    hasMoreCollections: pageNumber * pageSize < cmrHits,
+    pageNumber: pageNumber + 1
   }
 }
 
