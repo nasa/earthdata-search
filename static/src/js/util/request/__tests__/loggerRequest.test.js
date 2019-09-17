7b64b266a1e4b5cb2f255488d71fcc4f9ea2df07
Matthew Crouch <matthew@element84.com>
2019-09-17 12:53:54 -0400

    EDSC-2360: Error pages

---
+++ static/src/js/util/request/__tests__/loggerRequest.test.js
@@ -0,0 +1,40 @@
+import LoggerRequest from '../loggerRequest'
+import Request from '../request'
+
+beforeEach(() => {
+  jest.restoreAllMocks()
+  jest.clearAllMocks()
+})
+
+describe('LoggerRequest#constructor', () => {
+  test('sets the default values when authenticated', () => {
+    const request = new LoggerRequest()
+
+    expect(request.lambda).toBeTruthy()
+    expect(request.baseUrl).toEqual('http://localhost:3000')
+  })
+})
+
+describe('LoggerRequest#permittedCmrKeys', () => {
+  test('returns an array of timeline CMR keys', () => {
+    const request = new LoggerRequest()
+
+    expect(request.permittedCmrKeys()).toEqual([
+      'error'
+    ])
+  })
+})
+
+describe('LoggerRequest#log', () => {
+  test('calls Request#post', () => {
+    const request = new LoggerRequest()
+
+    const postMock = jest.spyOn(Request.prototype, 'post').mockImplementation()
+
+    const params = { error: { mock: 'error' } }
+    request.log(params)
+
+    expect(postMock).toBeCalledTimes(1)
+    expect(postMock).toBeCalledWith('error_logger', params)
+  })
+})
