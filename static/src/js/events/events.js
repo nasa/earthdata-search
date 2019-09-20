e7666b568ce3f3f9013409f8228f946054a067ea
Trevor Lang <trevorlang@ymail.com>
2019-09-20 10:24:00 -0600

    EDSC-2360: Tweaking error page css

--- static/src/js/events/events.js
+++ static/src/js/events/events.js
@@ -1,4 +1,16 @@
 import EventEmitter from 'events'
+import $ from 'jquery'
+
+const emitter = new EventEmitter()
+
+emitter.on('error.global', (error = true) => {
+  const { body } = document
+  if (error) {
+    $(body).addClass('body--has-error')
+  } else {
+    $(body).removeClass('body--has-error')
+  }
+})
 
 // eslint-disable-next-line import/prefer-default-export
-export const eventEmitter = new EventEmitter()
+export const eventEmitter = emitter
