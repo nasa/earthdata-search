3e39f6eaca4bc221046c9f3fe7a8bf84cab10c8e
Trevor Lang <trevorlang@ymail.com>
2019-10-04 16:45:11 -0600

    EDSC-2348: PR Feedback

---
+++ static/src/js/util/__tests__/focusedCollection.test.js
@@ -0,0 +1,84 @@
+import {
+  getFocusedCollectionObject,
+  getFocusedCollectionMetadata
+} from '../focusedCollection'
+
+describe('getFocusedCollectionObject', () => {
+  describe('when no collections are provided', () => {
+    test('returns undefined', () => {
+      expect(getFocusedCollectionObject('TEST')).toEqual(undefined)
+    })
+  })
+
+  describe('when collections is an empty object', () => {
+    test('returns undefined', () => {
+      expect(getFocusedCollectionObject('TEST', {})).toEqual(undefined)
+    })
+  })
+
+  describe('when passed a non-matching collection', () => {
+    test('returns an empty object', () => {
+      const collections = {
+        byId: {
+          'TEST2': {
+            test: 'test'
+          }
+        }
+      }
+      expect(getFocusedCollectionObject('TEST', collections)).toEqual({})
+    })
+  })
+
+  describe('when passed a matching collection', () => {
+    test('returns an empty object', () => {
+      const obj = { test: 'test' }
+      const collections = {
+        byId: {
+          'TEST': obj
+        }
+      }
+      expect(getFocusedCollectionObject('TEST', collections)).toEqual(obj)
+    })
+  })
+})
+
+describe('getFocusedCollectionMetadata', () => {
+  describe('when no collections are provided', () => {
+    test('returns undefined', () => {
+      expect(getFocusedCollectionMetadata('TEST')).toEqual(undefined)
+    })
+  })
+
+  describe('when collections is an empty object', () => {
+    test('returns undefined', () => {
+      expect(getFocusedCollectionMetadata('TEST', {})).toEqual(undefined)
+    })
+  })
+
+  describe('when passed a non-matching collection', () => {
+    test('returns an empty object', () => {
+      const collections = {
+        byId: {
+          'TEST2': {
+            test: 'test'
+          }
+        }
+      }
+      expect(getFocusedCollectionMetadata('TEST', collections)).toEqual(undefined)
+    })
+  })
+
+  describe('when passed a matching collection', () => {
+    test('returns an empty object', () => {
+      const metadata = { test: 'test' }
+      const collections = {
+        byId: {
+          'TEST': {
+            metadata
+          }
+        }
+      }
+      expect(getFocusedCollectionMetadata('TEST', collections)).toEqual(metadata)
+    })
+  })
+})
---
+++ static/src/js/util/__tests__/focusedGranule.test.js
@@ -0,0 +1,43 @@
+import {
+  getFocusedGranuleObject
+} from '../focusedGranule'
+
+describe('getFocusedGranuleObject', () => {
+  describe('when no granules are provided', () => {
+    test('returns undefined', () => {
+      expect(getFocusedGranuleObject('TEST')).toEqual(undefined)
+    })
+  })
+
+  describe('when granules is an empty object', () => {
+    test('returns undefined', () => {
+      expect(getFocusedGranuleObject('TEST', {})).toEqual(undefined)
+    })
+  })
+
+  describe('when passed a non-matching granule', () => {
+    test('returns an empty object', () => {
+      const granules = {
+        byId: {
+          'TEST2': {
+            test: 'test'
+          }
+        }
+      }
+      expect(getFocusedGranuleObject('TEST', granules)).toEqual({})
+    })
+  })
+
+  describe('when passed a matching granule', () => {
+    test('returns an empty object', () => {
+      const obj = { test: 'test' }
+      const granules = {
+        byId: {
+          'TEST': obj
+        }
+      }
+      expect(getFocusedGranuleObject('TEST', granules)).toEqual(obj)
+    })
+  })
+})
+
