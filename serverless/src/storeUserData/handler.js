f12d6d3aebf06e15002e4d4674db07da127d704c
Ryan Abbott <ryan@element84.com>
2019-11-13 10:07:35 -0500

    EDSC-2434: FIxes cron syntax and storeUserData lambda

--- serverless/src/storeUserData/handler.js
+++ serverless/src/storeUserData/handler.js
@@ -1,8 +1,8 @@
+import 'array-foreach-async'
 import { getUrsUserData } from './getUrsUserData'
 import { getEchoProfileData } from './getEchoProfileData'
 import { getEchoPreferencesData } from './getEchoPreferencesData'
 import { getDbConnection } from '../util/database/getDbConnection'
-import { isWarmUp } from '../util/isWarmup'
 
 /**
  * Log an error from the lambda specific to the calling method
@@ -27,73 +27,79 @@ const storeUserData = async (event, context) => {
   // eslint-disable-next-line no-param-reassign
   context.callbackWaitsForEmptyEventLoop = false
 
-  // Prevent execution if the event source is the warmer
-  if (await isWarmUp(event, context)) return false
-
-  const { environment, userId, username } = event
-
   // Retrieve a connection to the database
   const dbConnection = await getDbConnection()
 
-  // Retrieve the authenticated users' access tokens from the database
-  const existingUserTokens = await dbConnection('user_tokens')
-    .select([
-      'id',
-      'access_token',
-      'refresh_token',
-      'expires_at'
-    ])
-    .where({ user_id: userId, environment })
-    .orderBy('created_at', 'DESC')
+  const { Records: sqsRecords = [] } = event
 
-  let id // ECHO guid
-  let user // ECHO User Profile
-  let echoPreferencesData // ECHO Preferences Data
-  let ursUserData // URS user Profile
+  console.log(`Processing ${sqsRecords.length} user(s)`)
 
-  if (existingUserTokens.length > 0) {
-    const [tokenRow] = existingUserTokens
-    const { access_token: token } = tokenRow
+  await sqsRecords.forEachAsync(async (sqsRecord) => {
+    const { body } = sqsRecord
 
-    try {
-      ursUserData = await getUrsUserData(username, token, environment)
-    } catch (e) {
-      logResponseError(e, 'URS Profile')
+    // Destruct the payload from SQS
+    const {
+      environment, userId, username
+    } = JSON.parse(body)
+
+    // Retrieve the authenticated users' access tokens from the database
+    const existingUserTokens = await dbConnection('user_tokens')
+      .select([
+        'id',
+        'access_token',
+        'refresh_token',
+        'expires_at'
+      ])
+      .where({ user_id: userId, environment })
+      .orderBy('created_at', 'DESC')
+
+    let id // ECHO guid
+    let user // ECHO User Profile
+    let echoPreferencesData // ECHO Preferences Data
+    let ursUserData // URS user Profile
+
+    if (existingUserTokens.length > 0) {
+      const [tokenRow] = existingUserTokens
+      const { access_token: token } = tokenRow
+
+      try {
+        ursUserData = await getUrsUserData(username, token, environment)
+      } catch (e) {
+        logResponseError(e, 'URS Profile')
+      }
+
+      let echoProfileData
+      try {
+        echoProfileData = await getEchoProfileData(token, environment)
+      } catch (e) {
+        logResponseError(e, 'Echo Profile')
+      }
+
+      // Update the previously defined value for this variable
+      const { user: echoProfileUser } = echoProfileData
+      user = echoProfileUser
+
+      // The user GUID will be used to get the preference data as well as stored separately in the database
+      const { id } = user
+
+      try {
+        echoPreferencesData = await getEchoPreferencesData(id, token, environment)
+      } catch (e) {
+        logResponseError(e, 'Echo Preferences')
+      }
     }
 
-    let echoProfileData
-    try {
-      echoProfileData = await getEchoProfileData(token, environment)
-    } catch (e) {
-      logResponseError(e, 'Echo Profile')
+    const userPayload = {
+      echo_id: id,
+      echo_profile: user,
+      echo_preferences: echoPreferencesData,
+      environment,
+      urs_id: username,
+      urs_profile: ursUserData
     }
 
-    // Update the previously defined value for this variable
-    const { user: echoProfileUser } = echoProfileData
-    user = echoProfileUser
-
-    // The user GUID will be used to get the preference data as well as stored separately in the database
-    const { id } = user
-
-    try {
-      echoPreferencesData = await getEchoPreferencesData(id, token, environment)
-    } catch (e) {
-      logResponseError(e, 'Echo Preferences')
-    }
-  }
-
-  const userPayload = {
-    echo_id: id,
-    echo_profile: user,
-    echo_preferences: echoPreferencesData,
-    environment,
-    urs_id: username,
-    urs_profile: ursUserData
-  }
-
-  const updateResponse = await dbConnection('users').update({ ...userPayload }).where({ urs_id: username, environment })
-
-  return updateResponse
+    await dbConnection('users').update({ ...userPayload }).where({ urs_id: username, environment })
+  })
 }
 
 export default storeUserData
