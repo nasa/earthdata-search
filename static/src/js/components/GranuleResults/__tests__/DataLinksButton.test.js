e62a4ac81b5cfc0f9f8c4219788702591165e653
Ryan Abbott <ryan@element84.com>
2019-12-11 15:48:59 -0500

    EDSC-2477: Prevent errors when no granule links exist

--- static/src/js/components/GranuleResults/__tests__/DataLinksButton.test.js
+++ static/src/js/components/GranuleResults/__tests__/DataLinksButton.test.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import Enzyme, { shallow } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
+import { Dropdown } from 'react-bootstrap'
+
 import { DataLinksButton } from '../GranuleResultsItem'
+import Button from '../../Button/Button'
 
 Enzyme.configure({ adapter: new Adapter() })
 
-function setup() {
+function setup(overrideProps) {
   const props = {
     collectionId: 'TEST_ID',
     dataLinks: [
@@ -15,7 +18,8 @@ function setup() {
         href: 'http://linkhref'
       }
     ],
-    onMetricsDataAccess: jest.fn()
+    onMetricsDataAccess: jest.fn(),
+    ...overrideProps
   }
 
   const enzymeWrapper = shallow(<DataLinksButton {...props} />)
@@ -32,14 +36,25 @@ beforeEach(() => {
 
 describe('DataLinksButton component', () => {
   test('renders itself correctly', () => {
-    const { enzymeWrapper } = setup('cmr')
+    const { enzymeWrapper } = setup()
 
-    expect(enzymeWrapper.type()).toBe('a')
+    expect(enzymeWrapper.type()).toBe(Button)
   })
 
-  describe('download button', () => {
+  describe('with no granule links', () => {
+    test('renders a disabled download button', () => {
+      const { enzymeWrapper } = setup({
+        dataLinks: []
+      })
+
+      expect(enzymeWrapper.find(Button).props().disabled).toBe(true)
+      expect(enzymeWrapper.type()).toBe(Button)
+    })
+  })
+
+  describe('with a single granule link', () => {
     test('calls callback with the correct data on click', () => {
-      const { enzymeWrapper, props } = setup('cmr')
+      const { enzymeWrapper, props } = setup()
 
       enzymeWrapper.simulate('click')
 
@@ -50,6 +65,31 @@ describe('DataLinksButton component', () => {
         ],
         type: 'single_granule_download'
       })
+    })
+
+    test('renders the correct element', () => {
+      const { enzymeWrapper } = setup()
+
+      expect(enzymeWrapper.type()).toBe(Button)
+    })
+  })
+
+  describe('with multiple granule links', () => {
+    test('renders the correct element', () => {
+      const { enzymeWrapper } = setup({
+        dataLinks: [
+          {
+            rel: 'http://linkrel/data#',
+            title: 'linktitle',
+            href: 'http://linkhref'
+          }, {
+            rel: 'http://linkrel2/data#',
+            title: 'linktitle2',
+            href: 'http://linkhref2'
+          }
+        ]
+      })
+      expect(enzymeWrapper.type()).toBe(Dropdown)
     })
   })
 })
