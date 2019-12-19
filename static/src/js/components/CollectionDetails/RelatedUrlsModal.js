243ec3c3a290709d26388d57c68869e786793ad3
Trevor Lang <trevorlang@ymail.com>
2019-12-18 23:52:59 -0700

    EDSC-2503: Converting RelatedUrlsModal

--- static/src/js/components/CollectionDetails/RelatedUrlsModal.js
+++ static/src/js/components/CollectionDetails/RelatedUrlsModal.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import { Modal, Button } from 'react-bootstrap'
 
+import EDSCModal from '../EDSCModal/EDSCModal'
 import ArrowTags from '../ArrowTags/ArrowTags'
 
 import { pluralize } from '../../util/pluralize'
@@ -28,65 +28,46 @@ export class RelatedUrlsModal extends Component {
     const { formattedMetadata = {} } = focusedCollectionObject
     const { relatedUrls = [] } = formattedMetadata
 
+    const body = (
+      <>
+        {
+          relatedUrls.map((category, i) => {
+            if (category.urls.length) {
+              const key = `modal_related_url_${i}`
+              return (
+                <div key={key} className="related-urls-modal__group">
+                  <h4 className="related-urls-modal__group-title">{pluralize(category.label, category.urls)}</h4>
+                  {
+                    category.urls.map((url, j) => {
+                      const key = `modal_related_url_${i}-${j}`
+                      return (
+                        <ul key={key} className="related-urls-modal__url">
+                          <ArrowTags tags={[url.Type, url.Subtype]} />
+                          {/* eslint-disable-next-line react/jsx-no-target-blank */}
+                          <a className="related-urls-modal__link" href={url.URL} target="_blank">{url.URL}</a>
+                        </ul>
+                      )
+                    })
+                  }
+                </div>
+              )
+            }
+            return null
+          })
+        }
+      </>
+    )
+
     return (
-      <Modal
-        dialogClassName="related-urls-modal modal--full modal--has-inner-header"
-        show={isOpen}
-        onHide={this.onModalClose}
-        centered
+      <EDSCModal
+        className="related-urls"
+        title="Related URLs"
+        isOpen={isOpen}
+        id="related-urls"
         size="lg"
-        aria-labelledby="modal__related-urls-modal"
-      >
-        <Modal.Header
-          className="related-urls-modal__header"
-          closeButton
-        >
-          <Modal.Title
-            id="modal__related-urls-modal"
-            className="related-urls-modal__title"
-          >
-            Related URLs
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body className="related-urls-modal__body">
-          {
-            relatedUrls.map((category, i) => {
-              if (category.urls.length) {
-                const key = `modal_related_url_${i}`
-                return (
-                  <div key={key} className="related-urls-modal__group">
-                    <h4 className="related-urls-modal__group-title">{pluralize(category.label, category.urls)}</h4>
-                    {
-                      category.urls.map((url, j) => {
-                        const key = `modal_related_url_${i}-${j}`
-                        return (
-                          <ul key={key} className="related-urls-modal__url">
-                            <ArrowTags tags={[url.Type, url.Subtype]} />
-                            {/* eslint-disable-next-line react/jsx-no-target-blank */}
-                            <a className="related-urls-modal__link" href={url.URL} target="_blank">{url.URL}</a>
-                          </ul>
-                        )
-                      })
-                    }
-                  </div>
-                )
-              }
-              return null
-            })
-          }
-        </Modal.Body>
-        <Modal.Footer className="related-urls-modal__footer">
-          <div>
-            <Button
-              className="related-urls-modal__action related-urls-modal__action--close"
-              variant="primary"
-              onClick={this.onModalClose}
-            >
-              Close
-            </Button>
-          </div>
-        </Modal.Footer>
-      </Modal>
+        onClose={this.onModalClose}
+        body={body}
+      />
     )
   }
 }
