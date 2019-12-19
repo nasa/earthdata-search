b156caf82abc7ad77a1d0b49fd6ed1577fb8a171
Trevor Lang <trevorlang@ymail.com>
2019-12-19 00:06:16 -0700

    EDSC-2503: Converting TooManyPointsModal

--- static/src/js/components/TooManyPointsModal/tests__/TooManyPointsModal.test.js
+++ static/src/js/components/TooManyPointsModal/tests__/TooManyPointsModal.test.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import Enzyme, { shallow } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
-import { Modal } from 'react-bootstrap'
+
+import EDSCModal from '../../EDSCModal/EDSCModal'
 import TooManyPointsModal from '../TooManyPointsModal'
 
 Enzyme.configure({ adapter: new Adapter() })
@@ -24,26 +25,28 @@ describe('TooManyPointsModal component', () => {
   test('should render a Modal', () => {
     const { enzymeWrapper } = setup()
 
-    expect(enzymeWrapper.find(Modal).length).toEqual(1)
+    expect(enzymeWrapper.find(EDSCModal).length).toEqual(1)
   })
 
   test('should render a title', () => {
     const { enzymeWrapper } = setup()
 
-    expect(enzymeWrapper.find(Modal.Title).text()).toEqual('Shape file has too many points')
+    expect(enzymeWrapper.find(EDSCModal).prop('title')).toEqual('Shape file has too many points')
   })
 
-  test('should render instructions', () => {
+  test('should render a message', () => {
     const { enzymeWrapper } = setup()
 
-    expect(enzymeWrapper.find(Modal.Body).find('p').at(0).text()).toEqual('To improve search performance, your shapefile has been simplified. Your original shapefile will be used for spatial subsetting if you choose to enable that setting during download.')
+    const message = enzymeWrapper.find(EDSCModal).prop('body').props.children
+
+    expect(message).toEqual('To improve search performance, your shapefile has been simplified. Your original shapefile will be used for spatial subsetting if you choose to enable that setting during download.')
   })
 
   describe('modal actions', () => {
     test('Close button should trigger onToggleTooManyPointsModal', () => {
       const { enzymeWrapper, props } = setup()
 
-      enzymeWrapper.find('.too-many-points-modal__action--secondary').simulate('click')
+      enzymeWrapper.find(EDSCModal).prop('onClose')()
 
       expect(props.onToggleTooManyPointsModal).toHaveBeenCalledTimes(1)
       expect(props.onToggleTooManyPointsModal).toHaveBeenCalledWith(false)
