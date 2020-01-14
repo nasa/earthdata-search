ebb23fad862216f5d258cc467db250e9c1820aa8
Matthew Crouch <matthew@element84.com>
2020-01-14 15:39:05 -0500

    EDSC-2517: Fix per-order granule limit modal

--- static/src/js/actions/__tests__/accessMethods.test.js
+++ static/src/js/actions/__tests__/accessMethods.test.js
@@ -146,4 +146,160 @@ describe('fetchAccessMethods', () => {
       })
     })
   })
+
+  describe('order chunking', () => {
+    test('it chunks the order if more than 2000 granules exist', async () => {
+      const echoOrder0 = {
+        id: 'service_id',
+        type: 'ECHO ORDERS',
+        url: 'mock url',
+        option_definition: {
+          id: 'option_definition_guid',
+          name: 'Delivery Option'
+        },
+        form: 'mock form here'
+      }
+      nock(/localhost/)
+        .post(/access_methods/)
+        .reply(200, {
+          accessMethods: {
+            echoOrder0
+          },
+          selectedAccessMethod: 'echoOrder0'
+        })
+
+      const collectionId = 'collectionId'
+      const store = mockStore({
+        authToken: '123',
+        metadata: {
+          collections: {
+            allIds: [collectionId],
+            byId: {
+              collectionId: {
+                granules: {
+                  hits: 5000
+                },
+                metadata: {
+                  tags: {
+                    'edsc.extra.serverless.subset_service.echo_orders': {
+                      data: {
+                        option_definitions: [
+                          {
+                            id: 'option_definition_guid',
+                            name: 'Delivery Option'
+                          }
+                        ]
+                      }
+                    },
+                    'edsc.extra.serverless.collection_capabilities': {
+                      data: {
+                        granule_online_access_flag: true
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        },
+        project: {
+          collectionIds: [collectionId]
+        },
+        providers: []
+      })
+
+      // call the dispatch
+      await store.dispatch(fetchAccessMethods([collectionId])).then(() => {
+        const storeActions = store.getActions()
+        expect(storeActions[0]).toEqual({
+          type: ADD_ACCESS_METHODS,
+          payload: {
+            collectionId,
+            methods: {
+              echoOrder0
+            },
+            orderCount: 3,
+            selectedAccessMethod: 'echoOrder0'
+          }
+        })
+      })
+    })
+
+    test('it does not chunk the order if less than 2000 granules exist', async () => {
+      const echoOrder0 = {
+        id: 'service_id',
+        type: 'ECHO ORDERS',
+        url: 'mock url',
+        option_definition: {
+          id: 'option_definition_guid',
+          name: 'Delivery Option'
+        },
+        form: 'mock form here'
+      }
+      nock(/localhost/)
+        .post(/access_methods/)
+        .reply(200, {
+          accessMethods: {
+            echoOrder0
+          },
+          selectedAccessMethod: 'echoOrder0'
+        })
+
+      const collectionId = 'collectionId'
+      const store = mockStore({
+        authToken: '123',
+        metadata: {
+          collections: {
+            allIds: [collectionId],
+            byId: {
+              collectionId: {
+                granules: {
+                  hits: 25
+                },
+                metadata: {
+                  tags: {
+                    'edsc.extra.serverless.subset_service.echo_orders': {
+                      data: {
+                        option_definitions: [
+                          {
+                            id: 'option_definition_guid',
+                            name: 'Delivery Option'
+                          }
+                        ]
+                      }
+                    },
+                    'edsc.extra.serverless.collection_capabilities': {
+                      data: {
+                        granule_online_access_flag: true
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        },
+        project: {
+          collectionIds: [collectionId]
+        },
+        providers: []
+      })
+
+      // call the dispatch
+      await store.dispatch(fetchAccessMethods([collectionId])).then(() => {
+        const storeActions = store.getActions()
+        expect(storeActions[0]).toEqual({
+          type: ADD_ACCESS_METHODS,
+          payload: {
+            collectionId,
+            methods: {
+              echoOrder0
+            },
+            orderCount: 1,
+            selectedAccessMethod: 'echoOrder0'
+          }
+        })
+      })
+    })
+  })
 })
