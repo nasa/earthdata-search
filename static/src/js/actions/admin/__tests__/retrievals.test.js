bdea5a8a000cd37088c69e198a86b7df1de7a3e6
Matthew Crouch <matthew@element84.com>
2020-01-15 16:46:42 -0500

    EDSC-2351: Added tests for actions and reducers

---
+++ static/src/js/actions/admin/__tests__/retrievals.test.js
@@ -0,0 +1,307 @@
+import configureMockStore from 'redux-mock-store'
+import thunk from 'redux-thunk'
+import nock from 'nock'
+
+import actions from '../../index'
+import {
+  setAdminRetrieval,
+  setAdminRetrievals,
+  setAdminRetrievalLoading,
+  setAdminRetrievalLoaded,
+  setAdminRetrievalsLoading,
+  setAdminRetrievalsLoaded,
+  setAdminRetrievalsPagination,
+  fetchAdminRetrieval,
+  fetchAdminRetrievals,
+  adminViewRetrieval,
+  updateAdminRetrievalsSortKey,
+  updateAdminRetrievalsPageNum
+} from '../retrievals'
+import {
+  SET_ADMIN_RETRIEVAL,
+  SET_ADMIN_RETRIEVALS,
+  SET_ADMIN_RETRIEVAL_LOADING,
+  SET_ADMIN_RETRIEVAL_LOADED,
+  SET_ADMIN_RETRIEVALS_LOADING,
+  SET_ADMIN_RETRIEVALS_LOADED,
+  SET_ADMIN_RETRIEVALS_PAGINATION,
+  UPDATE_ADMIN_RETRIEVALS_SORT_KEY,
+  UPDATE_ADMIN_RETRIEVALS_PAGE_NUM
+} from '../../../constants/actionTypes'
+
+const mockStore = configureMockStore([thunk])
+
+beforeEach(() => {
+  jest.clearAllMocks()
+  jest.restoreAllMocks()
+})
+
+describe('setAdminRetrieval', () => {
+  test('should create an action to set the admin retrieval data', () => {
+    const payload = {
+      mock: 'data'
+    }
+
+    const expectedAction = {
+      type: SET_ADMIN_RETRIEVAL,
+      payload
+    }
+
+    expect(setAdminRetrieval(payload)).toEqual(expectedAction)
+  })
+})
+
+describe('setAdminRetrievals', () => {
+  test('should create an action to update the admin retrievals list', () => {
+    const payload = [
+      {
+        mock: 'data'
+      },
+      {
+        mock: 'more data'
+      }
+    ]
+
+    const expectedAction = {
+      type: SET_ADMIN_RETRIEVALS,
+      payload
+    }
+
+    expect(setAdminRetrievals(payload)).toEqual(expectedAction)
+  })
+})
+
+describe('setAdminRetrievalsLoading', () => {
+  test('should create an action to update the admin retrievals loading state', () => {
+    const expectedAction = {
+      type: SET_ADMIN_RETRIEVALS_LOADING
+    }
+
+    expect(setAdminRetrievalsLoading()).toEqual(expectedAction)
+  })
+})
+
+describe('setAdminRetrievalsLoaded', () => {
+  test('should create an action to update the admin retrievals loaded state', () => {
+    const expectedAction = {
+      type: SET_ADMIN_RETRIEVALS_LOADED
+    }
+
+    expect(setAdminRetrievalsLoaded()).toEqual(expectedAction)
+  })
+})
+
+describe('setAdminRetrievalLoading', () => {
+  test('should create an action to update an admin retrieval loading state', () => {
+    const payload = 123
+
+    const expectedAction = {
+      type: SET_ADMIN_RETRIEVAL_LOADING,
+      payload
+    }
+
+    expect(setAdminRetrievalLoading(123)).toEqual(expectedAction)
+  })
+})
+
+describe('setAdminRetrievalLoaded', () => {
+  test('should create an action to update an admin retrieval loaded state', () => {
+    const payload = 123
+
+    const expectedAction = {
+      type: SET_ADMIN_RETRIEVAL_LOADED,
+      payload
+    }
+
+    expect(setAdminRetrievalLoaded(123)).toEqual(expectedAction)
+  })
+})
+
+describe('setAdminRetrievalsPagination', () => {
+  test('should create an action to update the admin retrievals pagination state', () => {
+    const payload = {
+      mock: 'data'
+    }
+
+    const expectedAction = {
+      type: SET_ADMIN_RETRIEVALS_PAGINATION,
+      payload
+    }
+
+    expect(setAdminRetrievalsPagination(payload)).toEqual(expectedAction)
+  })
+})
+
+describe('fetchAdminRetrieval', () => {
+  test('fetches a single admin retrieval', async () => {
+    const data = {
+      mock: 'data'
+    }
+
+    nock(/localhost/)
+      .get(/admin\/retrieval/)
+      .reply(200, data)
+
+    const id = 123
+
+    const store = mockStore({
+      authToken: 'mockToken',
+      admin: {
+        isAuthorized: true
+      }
+    })
+
+    await store.dispatch(fetchAdminRetrieval(id)).then(() => {
+      const storeActions = store.getActions()
+      expect(storeActions[0]).toEqual({
+        type: SET_ADMIN_RETRIEVAL_LOADING,
+        payload: id
+      })
+      expect(storeActions[1]).toEqual({
+        type: SET_ADMIN_RETRIEVAL_LOADED,
+        payload: id
+      })
+      expect(storeActions[2]).toEqual({
+        type: SET_ADMIN_RETRIEVAL,
+        payload: data
+      })
+    })
+  })
+})
+
+describe('fetchAdminRetrievals', () => {
+  test('fetches the list of admin retrievals', async () => {
+    const data = {
+      pagination: {
+        pageNum: 1,
+        pageSize: 20,
+        pageCount: 1,
+        totalResults: 1
+      },
+      results: [{ mock: 'data' }]
+    }
+
+    nock(/localhost/)
+      .get(/admin\/retrieval/)
+      .reply(200, data)
+
+    const store = mockStore({
+      authToken: 'mockToken',
+      admin: {
+        isAuthorized: true,
+        retrievals: {
+          sortKey: '-created_at',
+          pagination: {
+            pageNum: 1,
+            pageSize: 20
+          }
+        }
+      }
+    })
+
+    await store.dispatch(fetchAdminRetrievals()).then(() => {
+      const storeActions = store.getActions()
+      expect(storeActions[0]).toEqual({
+        type: SET_ADMIN_RETRIEVALS_LOADING
+      })
+      expect(storeActions[1]).toEqual({
+        type: SET_ADMIN_RETRIEVALS_LOADED
+      })
+      expect(storeActions[2]).toEqual({
+        type: SET_ADMIN_RETRIEVALS_PAGINATION,
+        payload: data.pagination
+      })
+      expect(storeActions[3]).toEqual({
+        type: SET_ADMIN_RETRIEVALS,
+        payload: data.results
+      })
+    })
+  })
+})
+
+describe('adminViewRetrieval', () => {
+  test('should create an action to change the URL', () => {
+    const store = mockStore({
+      router: {
+        location: {
+          pathname: '/admin'
+        }
+      }
+    })
+
+    store.dispatch(adminViewRetrieval(123))
+
+    const storeActions = store.getActions()
+    expect(storeActions[0]).toEqual({
+      payload: {
+        args: [
+          {
+            pathname: '/admin/retrievals/123'
+          }
+        ],
+        method: 'push'
+      },
+      type: '@@router/CALL_HISTORY_METHOD'
+    })
+  })
+})
+
+describe('updateAdminRetrievalsSortKey', () => {
+  test('should create an action to update the sort key and call fetchAdminRetrievals', () => {
+    const sortKey = '+username'
+
+    const fetchAdminRetrievalsMock = jest.spyOn(actions, 'fetchAdminRetrievals')
+    fetchAdminRetrievalsMock.mockImplementation(() => jest.fn())
+
+    const store = mockStore({
+      admin: {
+        retrievals: {
+          sortKey: '-created_at'
+        }
+      }
+    })
+
+    // call the dispatch
+    store.dispatch(updateAdminRetrievalsSortKey(sortKey))
+
+    // Is updateFeatureFacet called with the right payload
+    const storeActions = store.getActions()
+    expect(storeActions[0]).toEqual({
+      type: UPDATE_ADMIN_RETRIEVALS_SORT_KEY,
+      payload: sortKey
+    })
+
+    expect(fetchAdminRetrievalsMock).toHaveBeenCalledTimes(1)
+  })
+})
+
+describe('updateAdminRetrievalsPageNum', () => {
+  test('should create an action to update the page num and call fetchAdminRetrievals', () => {
+    const pageNum = 2
+
+    const fetchAdminRetrievalsMock = jest.spyOn(actions, 'fetchAdminRetrievals')
+    fetchAdminRetrievalsMock.mockImplementation(() => jest.fn())
+
+    const store = mockStore({
+      admin: {
+        retrievals: {
+          pagination: {
+            pageNum: 1
+          }
+        }
+      }
+    })
+
+    // call the dispatch
+    store.dispatch(updateAdminRetrievalsPageNum(pageNum))
+
+    // Is updateFeatureFacet called with the right payload
+    const storeActions = store.getActions()
+    expect(storeActions[0]).toEqual({
+      type: UPDATE_ADMIN_RETRIEVALS_PAGE_NUM,
+      payload: pageNum
+    })
+
+    expect(fetchAdminRetrievalsMock).toHaveBeenCalledTimes(1)
+  })
+})
---
+++ static/src/js/reducers/admin/isAuthorized.js
@@ -0,0 +1,15 @@
+import { SET_ADMIN_IS_AUTHORIZED } from '../../constants/actionTypes'
+
+const initialState = false
+
+const adminIsAuthorizedReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case SET_ADMIN_IS_AUTHORIZED: {
+      return action.payload
+    }
+    default:
+      return state
+  }
+}
+
+export default adminIsAuthorizedReducer
