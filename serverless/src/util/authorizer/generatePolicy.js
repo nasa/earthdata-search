4ee2dd1ac368559a1a4d18e989708db763a6e54d
Matthew Crouch <matthew@element84.com>
2020-01-27 16:42:02 -0500

    EDSC-2351: Create admin authorizer

---
+++ serverless/src/util/authorizer/generatePolicy.js
@@ -0,0 +1,30 @@
+/**
+ * Generate AuthPolicy for the Authorizer, and attach the JWT
+ * @param {String} username username of authenticated uset
+ * @param {Object} jwtToken JWT containing EDL token
+ * @param {String} effect
+ * @param {Object} resource
+ */
+export const generatePolicy = (username, jwtToken, effect, resource) => {
+  const authResponse = {}
+  authResponse.principalId = username
+
+  if (jwtToken) {
+    authResponse.context = { jwtToken }
+  }
+
+  if (effect && resource) {
+    const policyDocument = {}
+    policyDocument.Version = '2012-10-17'
+    policyDocument.Statement = []
+    const statementOne = {}
+    statementOne.Action = 'execute-api:Invoke'
+    statementOne.Effect = effect
+    statementOne.Resource = resource
+    policyDocument.Statement[0] = statementOne
+
+    authResponse.policyDocument = policyDocument
+  }
+
+  return authResponse
+}
