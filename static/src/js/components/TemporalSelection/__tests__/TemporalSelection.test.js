034a9404c152bccf3674f47664848a2edb20949f
Ryan Abbott <ryan@element84.com>
2020-02-03 21:50:10 -0500

    EDSC-2561: Refactor DatePicker component

--- static/src/js/components/TemporalSelection/__tests__/TemporalSelection.test.js
+++ static/src/js/components/TemporalSelection/__tests__/TemporalSelection.test.js
@@ -6,7 +6,7 @@ import moment from 'moment'
 import Alert from 'react-bootstrap/Alert'
 
 import TemporalSelection from '../TemporalSelection'
-import Datepicker from '../../Datepicker/Datepicker'
+import DatepickerContainer from '../../../containers/DatepickerContainer/DatepickerContainer'
 
 Enzyme.configure({ adapter: new Adapter() })
 
@@ -19,7 +19,8 @@ function setup() {
     onSubmitStart: jest.fn(),
     onSubmitEnd: jest.fn(),
     onInvalid: jest.fn(),
-    onValid: jest.fn()
+    onValid: jest.fn(),
+    viewMode: 'years'
   }
 
   const enzymeWrapper = shallow(<TemporalSelection {...props} />)
@@ -31,35 +32,37 @@ function setup() {
 }
 
 describe('TemporalSelection component', () => {
-  test('when passed a start date renders DatePicker component correctly', () => {
+  test('when passed a start date renders DatePickerContainer component correctly', () => {
     const { enzymeWrapper } = setup()
     enzymeWrapper.setProps({
       temporal: {
-        startDate: '2019-03-30T00:00:00.000Z'
+        startDate: '2019-03-30T00:00:00.000Z',
+        endDate: ''
       }
     })
 
-    expect(enzymeWrapper.find(Datepicker).at(0).prop('type')).toBe('start')
-    expect(enzymeWrapper.find(Datepicker).at(0).prop('value')).toBe('2019-03-30T00:00:00.000Z')
-    expect(enzymeWrapper.find(Datepicker).at(1).prop('type')).toBe('end')
-    expect(enzymeWrapper.find(Datepicker).at(1).prop('value')).toBe('')
+    expect(enzymeWrapper.find(DatepickerContainer).at(0).prop('type')).toBe('start')
+    expect(enzymeWrapper.find(DatepickerContainer).at(0).prop('value')).toBe('2019-03-30T00:00:00.000Z')
+    expect(enzymeWrapper.find(DatepickerContainer).at(1).prop('type')).toBe('end')
+    expect(enzymeWrapper.find(DatepickerContainer).at(1).prop('value')).toBe('')
   })
 
-  test('when passed a end date renders DatePicker component correctly', () => {
+  test('when passed a end date renders DatePickerContainer component correctly', () => {
     const { enzymeWrapper } = setup()
     enzymeWrapper.setProps({
       temporal: {
+        startDate: '',
         endDate: '2019-03-30T00:00:00.000Z'
       }
     })
 
-    expect(enzymeWrapper.find(Datepicker).at(0).prop('type')).toBe('start')
-    expect(enzymeWrapper.find(Datepicker).at(0).prop('value')).toBe('')
-    expect(enzymeWrapper.find(Datepicker).at(1).prop('type')).toBe('end')
-    expect(enzymeWrapper.find(Datepicker).at(1).prop('value')).toBe('2019-03-30T00:00:00.000Z')
+    expect(enzymeWrapper.find(DatepickerContainer).at(0).prop('type')).toBe('start')
+    expect(enzymeWrapper.find(DatepickerContainer).at(0).prop('value')).toBe('')
+    expect(enzymeWrapper.find(DatepickerContainer).at(1).prop('type')).toBe('end')
+    expect(enzymeWrapper.find(DatepickerContainer).at(1).prop('value')).toBe('2019-03-30T00:00:00.000Z')
   })
 
-  test('when passed a both start and end dates renders DatePicker components correctly', () => {
+  test('when passed a both start and end dates renders DatePickerContainer components correctly', () => {
     const { enzymeWrapper } = setup()
     enzymeWrapper.setProps({
       temporal: {
@@ -68,13 +71,13 @@ describe('TemporalSelection component', () => {
       }
     })
 
-    expect(enzymeWrapper.find(Datepicker).at(0).prop('type')).toBe('start')
-    expect(enzymeWrapper.find(Datepicker).at(0).prop('value')).toBe('2019-03-29T00:00:00.000Z')
-    expect(enzymeWrapper.find(Datepicker).at(1).prop('type')).toBe('end')
-    expect(enzymeWrapper.find(Datepicker).at(1).prop('value')).toBe('2019-03-30T00:00:00.000Z')
+    expect(enzymeWrapper.find(DatepickerContainer).at(0).prop('type')).toBe('start')
+    expect(enzymeWrapper.find(DatepickerContainer).at(0).prop('value')).toBe('2019-03-29T00:00:00.000Z')
+    expect(enzymeWrapper.find(DatepickerContainer).at(1).prop('type')).toBe('end')
+    expect(enzymeWrapper.find(DatepickerContainer).at(1).prop('value')).toBe('2019-03-30T00:00:00.000Z')
   })
 
-  test('when passed a start date after the end date renders DatePicker components correctly', () => {
+  test('when passed a start date after the end date renders DatePickerContainer components correctly', () => {
     const { enzymeWrapper } = setup()
     enzymeWrapper.setProps({
       temporal: {
@@ -83,10 +86,10 @@ describe('TemporalSelection component', () => {
       }
     })
 
-    expect(enzymeWrapper.find(Datepicker).at(0).prop('type')).toBe('start')
-    expect(enzymeWrapper.find(Datepicker).at(0).prop('value')).toBe('2019-03-30T00:00:00.000Z')
-    expect(enzymeWrapper.find(Datepicker).at(1).prop('type')).toBe('end')
-    expect(enzymeWrapper.find(Datepicker).at(1).prop('value')).toBe('2019-03-29T00:00:00.000Z')
+    expect(enzymeWrapper.find(DatepickerContainer).at(0).prop('type')).toBe('start')
+    expect(enzymeWrapper.find(DatepickerContainer).at(0).prop('value')).toBe('2019-03-30T00:00:00.000Z')
+    expect(enzymeWrapper.find(DatepickerContainer).at(1).prop('type')).toBe('end')
+    expect(enzymeWrapper.find(DatepickerContainer).at(1).prop('value')).toBe('2019-03-29T00:00:00.000Z')
   })
 
   test('when passed a start date after the end date fires isInvalid', () => {
@@ -130,7 +133,7 @@ describe('TemporalSelection component', () => {
 
   test('sets the start date correctly when an invalid date is passed', () => {
     const { enzymeWrapper, props } = setup()
-    enzymeWrapper.find(Datepicker).at(0).props().onSubmit('2012-01-efss 12:00:00')
+    enzymeWrapper.find(DatepickerContainer).at(0).props().onSubmit('2012-01-efss 12:00:00')
     expect(props.onSubmitStart).toBeCalledTimes(1)
     expect(props.onSubmitStart).toHaveBeenCalledWith('2012-01-efss 12:00:00')
   })
@@ -139,7 +142,7 @@ describe('TemporalSelection component', () => {
     const { enzymeWrapper, props } = setup()
     const testObj = moment('2012-01-efss 12:00:00', 'YYYY-MM-DD HH:mm:ss', true)
 
-    enzymeWrapper.find(Datepicker).at(1).props().onSubmit(testObj)
+    enzymeWrapper.find(DatepickerContainer).at(1).props().onSubmit(testObj)
     expect(props.onSubmitEnd).toBeCalledTimes(1)
   })
 })
