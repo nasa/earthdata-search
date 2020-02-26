f342f228e4d2c24e0b51eb64d15f727c23053d25
Ryan Abbott <ryan@element84.com>
2020-02-26 17:19:16 -0500

    EDSC-2552: Implements autocomplete lambda

---
+++ serverless/src/autocomplete/__tests__/handler.test.js
@@ -0,0 +1,90 @@
+import nock from 'nock'
+import * as getJwtToken from '../../util/getJwtToken'
+import * as getEchoToken from '../../util/urs/getEchoToken'
+import * as doSearchRequest from '../../util/cmr/doSearchRequest'
+import autocomplete from '../handler'
+
+beforeEach(() => {
+  jest.clearAllMocks()
+
+  jest.spyOn(getJwtToken, 'getJwtToken').mockImplementation(() => 'mockJwt')
+  jest.spyOn(getEchoToken, 'getEchoToken').mockImplementation(() => '1234-abcd-5678-efgh')
+})
+
+describe('autocomplete', () => {
+  test('calls doSearchRequest', async () => {
+    const mock = jest.spyOn(doSearchRequest, 'doSearchRequest').mockImplementationOnce(() => jest.fn())
+
+    const event = {
+      body: JSON.stringify({
+        params: {
+          q: 'ICE'
+        },
+        requestId: 'asdf-1234-qwer-5678'
+      })
+    }
+
+    await autocomplete(event, {})
+
+    expect(mock).toBeCalledTimes(1)
+    expect(mock).toBeCalledWith({
+      jwtToken: 'mockJwt',
+      method: 'get',
+      path: '/search/collections/autocomplete',
+      params: 'q=ICE',
+      requestId: 'asdf-1234-qwer-5678'
+    })
+  })
+
+  test('responds correctly on http error', async () => {
+    nock(/cmr/)
+      .get(/autocomplete/)
+      .reply(500, {
+        errors: [
+          'Test error message'
+        ]
+      })
+
+    const event = {
+      body: JSON.stringify({
+        params: {
+          q: 'ICE'
+        },
+        requestId: 'asdf-1234-qwer-5678'
+      })
+    }
+
+    const response = await autocomplete(event, {})
+
+    expect(response.statusCode).toEqual(500)
+
+    const { body } = response
+    const parsedBody = JSON.parse(body)
+    const { errors } = parsedBody
+    const [errorMessage] = errors
+
+    expect(errorMessage).toEqual('Test error message')
+  })
+
+  test('responds correctly when an exception is thrown', async () => {
+    jest.spyOn(doSearchRequest, 'doSearchRequest').mockImplementationOnce(() => { throw new Error('Code Exception Occurred') })
+
+    const event = {
+      body: JSON.stringify({
+        params: {
+          q: 'ICE'
+        },
+        requestId: 'asdf-1234-qwer-5678'
+      })
+    }
+
+    const response = await autocomplete(event, {})
+
+    const { body } = response
+    const parsedBody = JSON.parse(body)
+    const { errors } = parsedBody
+    const [errorMessage] = errors
+
+    expect(errorMessage).toEqual('Error: Code Exception Occurred')
+  })
+})
---
+++ serverless/src/autocomplete/handler.js
@@ -0,0 +1,51 @@
+import { doSearchRequest } from '../util/cmr/doSearchRequest'
+import { buildParams } from '../util/cmr/buildParams'
+import { getApplicationConfig } from '../../../sharedUtils/config'
+import { getJwtToken } from '../util/getJwtToken'
+import { parseError } from '../util/parseError'
+
+/**
+ * Perform a search across all endpoints for autocompletion
+ * @param {Object} event Details about the HTTP request that it received
+ */
+const autocomplete = async (event) => {
+  const { body } = event
+
+  const { params, requestId } = JSON.parse(body)
+  const { q } = params
+
+  const { defaultResponseHeaders } = getApplicationConfig()
+
+  const permittedCmrKeys = [
+    'q',
+    'type'
+  ]
+
+  const nonIndexedKeys = [
+    'type'
+  ]
+
+  console.log(`Searching for ${q}`)
+
+  try {
+    return doSearchRequest({
+      jwtToken: getJwtToken(event),
+      method: 'get',
+      path: '/search/collections/autocomplete',
+      params: buildParams({
+        body,
+        permittedCmrKeys,
+        nonIndexedKeys
+      }),
+      requestId
+    })
+  } catch (e) {
+    return {
+      isBase64Encoded: false,
+      headers: defaultResponseHeaders,
+      ...parseError(e)
+    }
+  }
+}
+
+export default autocomplete
