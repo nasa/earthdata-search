37fbefb7a598b298bbe6bdcfdd7b0e616ef04aab
Ryan Abbott <ryan@element84.com>
2020-02-26 17:19:16 -0500

    EDSC-2552: Implements autocomplete lambda

---
+++ static/src/js/util/request/autocompleteRequest.js
@@ -0,0 +1,35 @@
+import Request from './request'
+import { getEnvironmentConfig } from '../../../../../sharedUtils/config'
+
+/**
+ * Request object for autocomplete requests
+ */
+export default class AutocompleteRequest extends Request {
+  constructor(authToken) {
+    super(getEnvironmentConfig().apiHost)
+    this.authenticated = true
+    this.authToken = authToken
+    this.searchPath = 'autocomplete'
+  }
+
+  /**
+ * Defines the default array keys that should exclude their index when stringified.
+ * @return {Array} An empty array
+ */
+  nonIndexedKeys() {
+    return [
+      'type'
+    ]
+  }
+
+  /**
+   * Defines the default keys that our API endpoints allow.
+   * @return {Array} An empty array
+   */
+  permittedCmrKeys() {
+    return [
+      'type',
+      'q'
+    ]
+  }
+}
