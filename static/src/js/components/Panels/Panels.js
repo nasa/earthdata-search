dc9b5edeb8cb803a200e35c56936ef9b38dfdd4e
Trevor Lang <trevorlang@ymail.com>
2020-03-06 16:16:31 -0700

    EDSC-2592: Implementing basic panel dragging behavior

--- static/src/js/components/Panels/Panels.js
+++ static/src/js/components/Panels/Panels.js
@@ -1,17 +1,64 @@
-import React, { Component, Children } from 'react'
+import React, { PureComponent, Children } from 'react'
 import { PropTypes } from 'prop-types'
 import classNames from 'classnames'
 
 import PanelSection from './PanelSection'
 import PanelGroup from './PanelGroup'
 
+import history from '../../util/history'
+
 import './Panels.scss'
 
-export class Panels extends Component {
+export class Panels extends PureComponent {
   constructor(props) {
     super(props)
+
+    this.panelWidth = 600
+
+    this.maxWidth = undefined
+    this.minWidth = undefined
+    this.clickStartWidth = undefined
+    this.clickStartX = undefined
+    this.dragging = false
+    this.width = this.panelWidth
+    this.previousWidth = 0
+
+
     this.onChangePanel = this.onChangePanel.bind(this)
     this.onPanelsClose = this.onPanelsClose.bind(this)
+    this.onPanelsWidthChange = this.onPanelsWidthChange.bind(this)
+    this.onPanelDragStart = this.onPanelDragStart.bind(this)
+    this.onPanelDragEnd = this.onPanelDragEnd.bind(this)
+    // this.onPanelToggle = this.onPanelToggle.bind(this)
+    this.onMouseMove = this.onMouseMove.bind(this)
+    this.onMouseUp = this.onMouseUp.bind(this)
+    this.onMouseDown = this.onMouseDown.bind(this)
+    this.onWindowResize = this.onWindowResize.bind(this)
+    this.onUpdate = this.onUpdate.bind(this)
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this.onWindowResize)
+    this.browserHistoryUnlisten = history.listen(this.onWindowResize)
+
+    const maxWidth = this.calculateMaxWidth()
+
+    this.maxWidth = maxWidth
+  }
+
+  componentWillUnmount() {
+    this.onPanelToggle()
+    if (this.browserHistoryUnlisten) this.browserHistoryUnlisten()
+    window.addEventListener('resize', this.onWindowResize)
+  }
+
+  onWindowResize() {
+    setTimeout(() => {
+      const maxWidth = this.calculateMaxWidth()
+      this.maxWidth = maxWidth
+
+      if (this.width > maxWidth) this.onPanelsWidthChange(maxWidth)
+    }, 0)
   }
 
   onPanelsClose() {
@@ -22,6 +69,97 @@ export class Panels extends Component {
   onChangePanel(panelId) {
     const { onChangePanel } = this.props
     onChangePanel(panelId)
+  }
+
+  onMouseDown(e) {
+    e.stopPropagation()
+    e.preventDefault()
+
+    if (e.button !== 0) return
+
+    document.addEventListener('mousemove', this.onMouseMove)
+    document.addEventListener('mouseup', this.onMouseUp)
+
+    this.onPanelDragStart(this.width, e.pageX)
+  }
+
+  onMouseUp(e) {
+    e.stopPropagation()
+    e.preventDefault()
+
+    document.removeEventListener('mousemove', this.throttledMove)
+    document.removeEventListener('mouseup', this.onMouseUp)
+
+    this.onPanelDragEnd()
+  }
+
+  onMouseMove(e) {
+    this.pageX = e.pageX
+
+    e.stopPropagation()
+    e.preventDefault()
+
+    if (!this.dragging) return
+
+    window.requestAnimationFrame(this.onUpdate)
+  }
+
+  onUpdate() {
+    const {
+      pageX,
+      clickStartX,
+      maxWidth,
+      minWidth,
+      clickStartWidth
+    } = this
+
+    const distanceScrolled = pageX - clickStartX
+    const newWidth = distanceScrolled + clickStartWidth
+
+    if (newWidth > maxWidth) {
+      this.onPanelsWidthChange(maxWidth)
+      return
+    }
+    if (newWidth < minWidth) {
+      this.onPanelsWidthChange(minWidth)
+      return
+    }
+
+    this.onPanelsWidthChange(newWidth)
+  }
+
+  onPanelsWidthChange(newWidth) {
+    this.container.style.width = `${newWidth}px`
+
+    this.width = newWidth
+    this.previousWidth = newWidth
+  }
+
+  onPanelDragStart(clickStartWidth, clickStartX) {
+    this.container.classList.add('panels--is-dragging')
+    document.body.classList.add('is-panels-dragging')
+
+    this.clickStartWidth = clickStartWidth
+    this.clickStartX = clickStartX
+    this.dragging = true
+  }
+
+  onPanelDragEnd() {
+    this.container.classList.remove('panels--is-dragging')
+    document.body.classList.remove('is-panels-dragging')
+
+    this.clickStartWidth = undefined
+    this.clickStartX = undefined
+    this.dragging = false
+  }
+
+  calculateMaxWidth() {
+    // const headerHeight = $('.route-wrapper__header').outerHeight()
+    // const tabHeight = $('.master-overlay-panel__tab').width()
+    // const routeWrapperHeight = $('.route-wrapper').width()
+    // const maxWidth = routeWrapperHeight - (headerHeight + tabHeight)
+    return 1000
+    // return maxWidth
   }
 
   renderPanelGroup(child, index) {
@@ -62,7 +200,11 @@ export class Panels extends Component {
   }
 
   render() {
-    const { children, show } = this.props
+    const {
+      children,
+      draggable,
+      show
+    } = this.props
 
     const panelGroups = Children.map(children,
       (child, index) => this.renderPanelGroup(child, index))
@@ -74,7 +216,26 @@ export class Panels extends Component {
       }
     ])
     return (
-      <section className={className}>
+      <section
+        className={className}
+        style={{ width: '600px' }}
+        ref={(node) => {
+          this.container = node
+        }}
+      >
+        {
+          draggable && (
+            <div
+              className="panels__handle"
+              role="button"
+              tabIndex="0"
+              ref={(node) => {
+                this.node = node
+              }}
+              onMouseDown={this.onMouseDown}
+            />
+          )
+        }
         {panelGroups}
       </section>
     )
@@ -83,6 +244,7 @@ export class Panels extends Component {
 
 Panels.defaultProps = {
   activePanel: '0.0.0',
+  draggable: false,
   show: false,
   onPanelClose: null,
   onChangePanel: null
@@ -94,6 +256,7 @@ Panels.propTypes = {
     PropTypes.arrayOf(PropTypes.node),
     PropTypes.node
   ]).isRequired,
+  draggable: PropTypes.bool,
   show: PropTypes.bool,
   onPanelClose: PropTypes.func,
   onChangePanel: PropTypes.func
