6bfc448a399c12b73dac869bb17d70e8324b3243
Ryan Abbott <ryan@element84.com>
2020-03-09 14:12:31 -0400

    EDSC-2552: Badgless handoff

--- serverless/src/edlOptionalAuthorizer/handler.js
+++ serverless/src/edlOptionalAuthorizer/handler.js
@@ -6,11 +6,32 @@ import { validateToken } from '../util/authorizer/validateToken'
  * @param {Object} event Details about the HTTP request that it received
  * @param {Object} context Methods and properties that provide information about the invocation, function, and execution environment
  */
-const edlAuthorizer = async (event) => {
+const edlOptionalAuthorizer = async (event) => {
   const {
     authorizationToken,
-    methodArn
+    methodArn,
+    requestContext = {}
   } = event
+  const { resourcePath } = requestContext
+
+  console.log('event', JSON.stringify(event, null, 4))
+  console.log('authorizationToken', authorizationToken)
+
+  if (!authorizationToken) {
+    const authOptionalPaths = [
+      '/autocomplete',
+      '/cwic/granules'
+    ]
+
+    // Allow for optional authentication
+    if (authOptionalPaths.includes(resourcePath)) {
+      return generatePolicy('anonymous', undefined, 'Allow', methodArn)
+    }
+
+    console.log(`${resourcePath} does not support optional authentication.`)
+
+    throw new Error('Route Requires Authorization')
+  }
 
   // authorizationToken comes in as `Bearer: asdf.qwer.hjkl` but we only need the actual token
   const tokenParts = authorizationToken.split(' ')
@@ -25,4 +46,4 @@ const edlAuthorizer = async (event) => {
   throw new Error('Unauthorized')
 }
 
-export default edlAuthorizer
+export default edlOptionalAuthorizer
