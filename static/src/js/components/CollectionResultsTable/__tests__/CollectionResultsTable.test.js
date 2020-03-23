72c009b2d5f2427e64013f8a733909d9fbf1cacc
Ryan Abbott <ryan@element84.com>
2020-03-23 15:04:12 -0400

    EDSC-2590: Placeholder test

--- static/src/js/components/CollectionResultsTable/__tests__/CollectionResultsTable.test.js
+++ static/src/js/components/CollectionResultsTable/__tests__/CollectionResultsTable.test.js
@@ -1,19 +1,25 @@
 import React from 'react'
 import Enzyme, { shallow } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
-import { Alert } from 'react-bootstrap'
 
-import CollectionResultsTable.js from '../CollectionResultsTable.js'
+import CollectionResultsTable from '../CollectionResultsTable'
 
 Enzyme.configure({ adapter: new Adapter() })
 
 function setup(overrideProps) {
   const props = {
     bootstrapVariant: 'primary',
+    collections: [],
+    collectionHits: 0,
+    onAddProjectCollection: jest.fn(),
+    onRemoveCollectionFromProject: jest.fn(),
+    onViewCollectionGranules: jest.fn(),
+    onViewCollectionDetails: jest.fn(),
+    waypointEnter: jest.fn(),
     ...overrideProps
   }
 
-  const enzymeWrapper = shallow(<CollectionResultsTable.js {...props} />)
+  const enzymeWrapper = shallow(<CollectionResultsTable {...props} />)
 
   return {
     enzymeWrapper,
@@ -21,41 +27,10 @@ function setup(overrideProps) {
   }
 }
 
-describe('CollectionResultsTable.js component', () => {
-  const { enzymeWrapper } = setup()
+describe('CollectionResultsTable component', () => {
+  test('placeholder', () => {
+    setup()
 
-  test('should render the alert', () => {
-    expect(enzymeWrapper.type()).toEqual(Alert)
-    expect(enzymeWrapper.prop('className')).toContain('edsc-alert')
-  })
-
-  describe('when a variant is declared', () => {
-    const { enzymeWrapper } = setup({
-      variant: 'test-variant'
-    })
-
-    test('should add the variant class name', () => {
-      expect(enzymeWrapper.prop('className')).toContain('edsc-alert--test-variant')
-    })
-  })
-
-  describe('when children are provided', () => {
-    const { enzymeWrapper } = setup({
-      children: <div className="test-child">Test</div>
-    })
-
-    test('should render the children', () => {
-      expect(enzymeWrapper.find('.test-child').length).toEqual(1)
-    })
-  })
-
-  describe('when an icon is provided', () => {
-    const { enzymeWrapper } = setup({
-      icon: 'question-circle'
-    })
-
-    test('should render the icon', () => {
-      expect(enzymeWrapper.find('.fa-question-circle').length).toEqual(1)
-    })
+    expect(true).toBe(true)
   })
 })
