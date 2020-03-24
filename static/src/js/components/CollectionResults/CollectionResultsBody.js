2f1861d12dd18ff6766aff8d4c41ec8562ab0179
Matthew Crouch <matthew@element84.com>
2020-03-24 15:13:52 -0400

    EDSC-2590: Adds more tests

--- static/src/js/components/CollectionResults/CollectionResultsBody.js
+++ static/src/js/components/CollectionResults/CollectionResultsBody.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 
 import CollectionResultsList from './CollectionResultsList'
 import CollectionResultsTable from '../CollectionResultsTable/CollectionResultsTable'
+import { formatCollectionList } from '../../util/formatCollectionList'
 
 
 import './CollectionResultsBody.scss'
@@ -29,98 +30,8 @@ const CollectionResultsBody = ({
 }) => {
   const { allIds: collectionIds, hits: collectionHits, isLoading } = collections
 
-  const collectionList = useMemo(() => collectionIds.map((collectionId, index) => {
-    const collection = collections.byId[collectionId]
-
-    const {
-      dataset_id: datasetId = null,
-      granule_count: granuleCount = 0,
-      has_formats: hasFormats = false,
-      has_spatial_subsetting: hasSpatialSubsetting = false,
-      has_temporal_subsetting: hasTemporalSubsetting = false,
-      has_transforms: hasTransforms = false,
-      has_variables: hasVariables = false,
-      has_map_imagery: hasMapImagery = false,
-      is_cwic: isCwic = false,
-      is_nrt: isNrt = false,
-      organizations = [],
-      short_name: shortName,
-      summary = '',
-      thumbnail = null,
-      time_end: timeEnd = null,
-      time_start: timeStart = null,
-      version_id: versionId
-    } = collection
-
-    const {
-      name: browserName
-    } = browser
-
-    let displayOrganization = ''
-
-    if (organizations && organizations.length) {
-      [displayOrganization] = organizations
-    }
-
-    let temporalRange = ''
-    let temporalStart = ''
-    let temporalEnd = ''
-
-    if (timeStart || timeEnd) {
-      if (timeStart) {
-        const dateStart = new Date(timeStart).toISOString().split('T')[0]
-
-        temporalRange = `${dateStart} ongoing`
-        temporalStart = dateStart
-        temporalEnd = 'ongoing'
-      }
-
-      if (timeEnd) {
-        const dateEnd = new Date(timeEnd).toISOString().split('T')[0]
-
-        temporalRange = `Up to ${dateEnd}`
-        temporalEnd = dateEnd
-      }
-
-      if (timeStart && timeEnd) {
-        const dateStart = new Date(timeStart).toISOString().split('T')[0]
-        const dateEnd = new Date(timeEnd).toISOString().split('T')[0]
-
-        temporalRange = `${dateStart} to ${dateEnd}`
-      }
-    }
-
-    let description = summary
-    if (browserName === 'ie') description = `${description.substring(0, 280)}...`
-
-    const isCollectionInProject = projectIds.indexOf(collectionId) !== -1
-
-    const isLast = index === collectionIds.length - 1
-
-    return {
-      collectionId,
-      datasetId,
-      description,
-      displayOrganization,
-      granuleCount,
-      hasFormats,
-      hasSpatialSubsetting,
-      hasTemporalSubsetting,
-      hasTransforms,
-      hasVariables,
-      hasMapImagery,
-      isCwic,
-      isNrt,
-      shortName,
-      temporalEnd,
-      temporalRange,
-      temporalStart,
-      thumbnail,
-      versionId,
-      isCollectionInProject,
-      isLast
-    }
-  }), [collectionIds, projectIds])
+  const collectionList = useMemo(() => formatCollectionList(collections, projectIds, browser),
+    [collectionIds, projectIds])
 
   return (
     <div className="collection-results-body">
--- static/src/js/components/CollectionResults/__tests__/mocks.js
+++ static/src/js/components/CollectionResults/__tests__/mocks.js
@@ -32,3 +32,27 @@ export const collectionListItemProps = {
 }
 
 export const longSummary = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+
+export const collectionResultsBodyData = {
+  collectionId: 'collectionId',
+  datasetId: 'test dataset id',
+  description: 'test summary',
+  displayOrganization: 'test/org',
+  granuleCount: 42,
+  hasFormats: false,
+  hasMapImagery: false,
+  hasSpatialSubsetting: false,
+  hasTemporalSubsetting: false,
+  hasTransforms: false,
+  hasVariables: false,
+  isCollectionInProject: false,
+  isCwic: false,
+  isLast: true,
+  isNrt: false,
+  shortName: 'test_short_name',
+  temporalEnd: '2019-01-15',
+  temporalRange: '2019-01-14 to 2019-01-15',
+  temporalStart: '2019-01-14',
+  thumbnail: 'http://some.test.com/thumbnail/url.jpg',
+  versionId: 2
+}
