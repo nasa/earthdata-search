8621507e144aed86b6c1e6c0a34c77bc2d19ef53
Trevor Lang <trevorlang@ymail.com>
2020-04-01 17:32:15 -0600

    EDSC-2555: Styling autocomplete results and applied filters

--- static/src/js/components/AutocompleteDisplay/AutocompleteDisplay.js
+++ static/src/js/components/AutocompleteDisplay/AutocompleteDisplay.js
@@ -1,44 +1,74 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Badge } from 'react-bootstrap'
 import { startCase } from 'lodash'
+import { facetCategoryAbbreviationsMap } from '../../util/facetCategoryAbbreviationsMap'
 
-import { Button } from '../Button/Button'
+import Button from '../Button/Button'
+import FilterStackItem from '../FilterStack/FilterStackItem'
+import FilterStackContents from '../FilterStack/FilterStackContents'
 
 import './AutocompleteDisplay.scss'
 
 const AutocompleteDisplay = ({ selected, onRemoveAutocompleteValue }) => {
   if (!selected.length) return null
 
-  return (
-    <li className="filter-stack-item">
+  const filterStackBody = (
+    <div className="autocomplete-display">
       {
         selected.map((item) => {
-          const { type, value } = item
+          const { type, value, fields = '' } = item
+
+          const label = facetCategoryAbbreviationsMap[type]
+
+          let parents = []
+          let titleHierarchy = ''
+
+          if (fields.indexOf(':')) {
+            parents = fields.split(':')
+            parents.pop()
+          }
+
+          parents.forEach((parent) => {
+            titleHierarchy += ` ${parent} >`
+          })
+
+          const title = `${startCase(type)}:${titleHierarchy} ${value}`
+
           return (
-            <Badge
-              className="autocomplete-display__badge"
-              pill
-              variant="primary"
+            <span
+              className="autocomplete-display__item"
               key={`${type}-${value}`}
+              title={title}
             >
-              <span className="autocomplete-display__type">
-                {startCase(type)}
-              </span>
-              <span className="autocomplete-display__value">
-                {value}
+              <span className="autocomplete-display__item-body">
+                <span className="autocomplete-display__type">
+                  {label}
+                </span>
+                <span className="autocomplete-display__value">
+                  {value}
+                </span>
               </span>
               <Button
                 className="autocomplete-display__button"
-                label={`Remove ${value.toLowerCase()} filter`}
+                label={`Remove ${value}`}
                 icon="times"
                 onClick={() => { onRemoveAutocompleteValue(item) }}
               />
-            </Badge>
+            </span>
           )
         })
       }
-    </li>
+    </div>
+  )
+
+  return (
+    <FilterStackItem variant="naked" title="Keyword filters">
+      <FilterStackContents
+        key="filter__autocomplete"
+        body={filterStackBody}
+        title="Autocomplete"
+      />
+    </FilterStackItem>
   )
 }
 
---
+++ static/src/js/util/facetCategoryAbbreviationsMap.js
@@ -0,0 +1,12 @@
+/**
+ * Maps the CMR Autocomplete types to their abbriviations
+ */
+export const facetCategoryAbbreviationsMap = {
+  organization: 'Org.',
+  instrument: 'Inst.',
+  granule_data_format: 'Gran. Fmt.',
+  platform: 'Plat.',
+  processing_level_id: 'Proc. Lvl.',
+  project: 'Proj.',
+  science_keywords: 'Kwd.'
+}
