3eef8822387e64c02eb139c5c326e895dc16d031
Trevor Lang <trevorlang@ymail.com>
2020-06-01 11:36:58 -0600

    EDSC-1434: Cleaning up prepareGranuleAccessParams

--- sharedUtils/prepareGranuleAccessParams.js
+++ sharedUtils/prepareGranuleAccessParams.js
@@ -4,20 +4,16 @@
  * @returns {Object} The granule params
  */
 export const prepareGranuleAccessParams = (params = {}) => {
-  let addedGranules = []
   let granuleParams = params
   const { concept_id: conceptIdsFromParams } = granuleParams
 
-  // If the concept_id value is truthy, we know granules have been manually added.
-  if (conceptIdsFromParams) {
-    addedGranules = [...conceptIdsFromParams]
-  }
-
-  // If there are any added granules, ignore all other granule parameters.
-  if (addedGranules.length) {
+  // If there are any added granules, overwrite and ignore all
+  // other granule parameters.
+  if (conceptIdsFromParams.length) {
     granuleParams = {
-      concept_id: addedGranules
+      concept_id: conceptIdsFromParams
     }
   }
+
   return granuleParams
 }
