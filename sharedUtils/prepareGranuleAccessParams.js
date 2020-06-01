c3b6eae6513e8c361ad161480ddfc23a6009eb29
Trevor Lang <trevorlang@ymail.com>
2020-06-01 12:13:21 -0600

    EDSC-1434: Improving prepareGranuleAccessParams

--- sharedUtils/prepareGranuleAccessParams.js
+++ sharedUtils/prepareGranuleAccessParams.js
@@ -1,19 +1,18 @@
 /**
- * Takes granule params an returns the params to be used in a CMR request
+ * Takes granule params and returns the params to be used in a CMR request
  * @param {Object} params Queue messages from SQS
  * @returns {Object} The granule params
  */
 export const prepareGranuleAccessParams = (params = {}) => {
-  let granuleParams = params
-  const { concept_id: conceptIdsFromParams = [] } = granuleParams
+  const { concept_id: conceptIdsFromParams = [] } = params
 
-  // If there are any added granules, overwrite and ignore all
-  // other granule parameters.
+  // If there are added granules, return only the added granules. Otherwise, send
+  // all of the granules params.
   if (conceptIdsFromParams.length) {
-    granuleParams = {
+    return {
       concept_id: conceptIdsFromParams
     }
   }
 
-  return granuleParams
+  return params
 }
