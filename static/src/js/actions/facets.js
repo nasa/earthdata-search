28ccf2087d105bc264f3f2ea7d3be1816a7645c0
Matthew Crouch <matthew@element84.com>
2020-06-05 12:52:31 -0400

    EDSC-2670: Remove full autocomplete created science keyword facet when one part is removed

--- static/src/js/actions/facets.js
+++ static/src/js/actions/facets.js
@@ -1,3 +1,5 @@
+import { find, isEqual, remove } from 'lodash'
+
 import {
   UPDATE_SELECTED_FEATURE_FACET,
   UPDATE_SELECTED_CMR_FACET,
@@ -9,6 +11,7 @@ import { prepareCMRFacetPayload } from '../util/facets'
 import actions from './index'
 
 import { updateCollectionQuery } from './search'
+import { scienceKeywordTypes } from '../util/scienceKeywordTypes'
 
 export const updateFeatureFacet = facetInfo => ({
   type: UPDATE_SELECTED_FEATURE_FACET,
@@ -40,9 +43,52 @@ export const updateCmrFacet = newParams => ({
  * @param {object} newParams - An object containing the params from the apply/remove link
  * @param {function} dispatch - A dispatch function provided by redux.
  */
-export const changeCmrFacet = (newParams, facet, applied) => (dispatch) => {
-  // if the facet is being removed (applied === false), remove an autocomplete value
+export const changeCmrFacet = (newParams, facet, applied) => (dispatch, getState) => {
+  // If the facet is being removed (applied === false), remove an autocomplete value
   if (applied === false) {
+    const { autocomplete } = getState()
+    const { selected } = autocomplete
+
+    const {
+      level,
+      type: facetType,
+      value: facetValue
+    } = facet
+
+    if (facetType === 'science_keywords' && level > 0) {
+      // Create a science keyword facet based on the facet param, up to one level below facet param
+      // If the facet param is { level: 1, type: 'science_keywords', value: 'Atmospheric Temperature' }
+      // We need to build { topic: Atmosphere } in order to delete the parent facet
+
+
+      // Find a selected autocomplete that matches the facet that was removed
+      const autocompleteItem = find(selected, (item) => {
+        const {
+          fields,
+          type: itemType
+        } = item
+        const parts = fields.split(':')
+
+        // If the autocompleteType is science_keywords, check the correct level of keyword for the autocomplete
+        return itemType === 'science_keywords' && parts[level] === facetValue
+      })
+
+      if (autocompleteItem) {
+        // Translate autocompleteItem into a science keyword facet, stopping one level below the facet level
+        const { fields } = autocompleteItem
+        const fieldParts = fields.split(':')
+
+        const facetToRemove = {}
+        fieldParts.forEach((keywordValue, i) => {
+          if (i < level) facetToRemove[scienceKeywordTypes[i]] = keywordValue
+        })
+
+        // Remove the facetToRemove from newParams, so the facet gets removed on the next getCollections
+        const { science_keywords_h: scienceKeywordsH } = newParams
+        remove(scienceKeywordsH, item => (isEqual(item, facetToRemove)))
+      }
+    }
+
     dispatch(actions.deleteAutocompleteValue(facet))
   }
 
