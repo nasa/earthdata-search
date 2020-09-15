a291d647f59e7f1f3759d357c69ee82ae31a52a5
Ryan Abbott <ryan@element84.com>
2020-09-15 12:03:00 -0400

    EDSC-2829: PR Feedback

--- serverless/src/submitHarmonyOrder/__tests__/handler.test.js
+++ serverless/src/submitHarmonyOrder/__tests__/handler.test.js
@@ -4,6 +4,7 @@ import nock from 'nock'
 import * as getDbConnection from '../../util/database/getDbConnection'
 import * as getEarthdataConfig from '../../../../sharedUtils/config'
 import * as startOrderStatusUpdateWorkflow from '../../util/startOrderStatusUpdateWorkflow'
+import * as createLimitedShapefile from '../../util/createLimitedShapefile'
 import { mockHarmonyOrder } from './mocks'
 import submitHarmonyOrder from '../handler'
 
@@ -122,6 +123,110 @@ describe('submitHarmonyOrder', () => {
     expect(queries[0].method).toEqual('first')
     expect(queries[1].method).toEqual('first')
     expect(queries[2].method).toEqual('update')
+    expect(startOrderStatusUpdateWorkflowMock).toBeCalledWith(12, 'access-token:clientId', 'Harmony')
+  })
+
+  test('creates a limited shapefile if the shapefile was limited by the user', async () => {
+    jest.spyOn(getEarthdataConfig, 'getEarthdataConfig').mockImplementation(() => ({
+      cmrHost: 'https://cmr.earthdata.nasa.gov',
+      edscHost: 'http://localhost:8080'
+    }))
+
+    const startOrderStatusUpdateWorkflowMock = jest.spyOn(startOrderStatusUpdateWorkflow, 'startOrderStatusUpdateWorkflow')
+      .mockImplementation(() => (jest.fn()))
+
+    const createLimitedShapefileMock = jest.spyOn(createLimitedShapefile, 'createLimitedShapefile')
+      .mockImplementation(() => ('limited mock shapefile'))
+
+    nock(/cmr/)
+      .get(/search\/granules/)
+      .reply(200, {
+        feed: {
+          entry: [{
+            id: 'G10000005-EDSC'
+          }]
+        }
+      })
+
+    nock('https://harmony.earthdata.nasa.gov')
+      .post('/C100000-EDSC/ogc-api-coverages/1.0.0/collections/test_var,test_var_2/coverage/rangeset')
+      .reply(201, {
+        username: 'rabbott',
+        status: 'running',
+        message: 'The job is being processed',
+        progress: 0,
+        createdAt: '2020-07-21T15:34:37.487Z',
+        updatedAt: '2020-07-21T15:34:37.487Z',
+        links: [],
+        request: 'https://harmony.earthdata.nasa.gov/C100000-EDSC/ogc-api-coverages/1.0.0/collections/test_var,test_var_2/coverage/rangeset',
+        jobID: '3e0d745b-adcf-4d33-9810-d81f38a5bd5a'
+      })
+
+    dbTracker.on('query', (query, step) => {
+      if (step === 1) {
+        query.response([{
+          user_id: 1,
+          id: '1',
+          jsondata: { source: '?sf=1&sfs[0]=1', shapefileId: 1, selectedFeatures: ['1'] },
+          collection_id: 'C100000-EDSC',
+          access_method: {
+            type: 'Harmony',
+            selectedOutputFormat: 'NetCDF-4',
+            selectedVariables: [
+              'V100000-EDSC',
+              'V100002-EDSC'
+            ],
+            variables: {
+              'V100000-EDSC': {
+                name: 'test_var',
+                longName: 'Test Variable',
+                conceptId: 'V100000-EDSC'
+              },
+              'V100001-EDSC': {
+                name: 'test_var_1',
+                longName: 'Test Variable 1',
+                conceptId: 'V100001-EDSC'
+              },
+              'V100002-EDSC': {
+                name: 'test_var_2',
+                longName: 'Test Variable 2',
+                conceptId: 'V100002-EDSC'
+              }
+            },
+            url: 'https://harmony.earthdata.nasa.gov'
+          },
+          granule_params: {}
+        }])
+      } else if (step === 2) {
+        query.response({
+          file: 'mock shapefile',
+          filename: 'MockFile.geojson'
+        })
+      } else {
+        query.response([])
+      }
+    })
+
+    const context = {}
+    await submitHarmonyOrder(mockHarmonyOrder, context)
+
+    const { queries } = dbTracker.queries
+
+    expect(queries[0].method).toEqual('first') // retrievals
+    expect(queries[1].method).toEqual('first') // shapefiles
+    expect(queries[2].method).toEqual('first') // shapefiles (limited shapefile query)
+    expect(queries[3].method).toEqual('insert') // save limited shapefile
+    expect(queries[3].bindings).toEqual([
+      'limited mock shapefile', // new file
+      '959220857ddbb3b2398ac31a58765df6', // file_hash
+      'Limited-MockFile.geojson', // filename
+      1084815579, // parent_shapefile_id
+      ['1'], // selectedFeatures
+      1 // user_id
+    ])
+    expect(queries[4].method).toEqual('update') // update retrieval orders
+
+    expect(createLimitedShapefileMock).toHaveBeenCalledTimes(1)
     expect(startOrderStatusUpdateWorkflowMock).toBeCalledWith(12, 'access-token:clientId', 'Harmony')
   })
 
--- serverless/src/submitHarmonyOrder/handler.js
+++ serverless/src/submitHarmonyOrder/handler.js
@@ -1,8 +1,10 @@
 import 'array-foreach-async'
 import axios from 'axios'
+import forge from 'node-forge'
 
 import { constructOrderPayload } from './constructOrderPayload'
 import { constructOrderUrl } from './constructOrderUrl'
+import { createLimitedShapefile } from '../util/createLimitedShapefile'
 import { deobfuscateId } from '../util/obfuscation/deobfuscateId'
 import { getDbConnection } from '../util/database/getDbConnection'
 import { getEdlConfig } from '../util/configUtil'
@@ -62,7 +64,8 @@ const submitHarmonyOrder = async (event, context) => {
       access_method: accessMethod,
       collection_id: collectionId,
       granule_params: granuleParams,
-      jsondata
+      jsondata,
+      user_id: userId
     } = retrievalRecord
 
     const {
@@ -70,11 +73,12 @@ const submitHarmonyOrder = async (event, context) => {
     } = accessMethod
 
     const {
-      shapefileId
+      shapefileId,
+      selectedFeatures
     } = jsondata
 
     // Retrieve the shapefile if one was provided
-    let shapefile
+    let file
     if (shapefileId) {
       const deobfuscatedShapefileId = deobfuscateId(
         shapefileId,
@@ -82,12 +86,43 @@ const submitHarmonyOrder = async (event, context) => {
       )
 
       const shapefileRecord = await dbConnection('shapefiles')
-        .first('file')
-        .where({ id: deobfuscatedShapefileId })
+        .first('file', 'filename')
+        .where({ id: deobfuscatedShapefileId });
 
-      const { file } = shapefileRecord
+      ({ file } = shapefileRecord)
 
-      shapefile = file
+      // If selectedFeatures exists, build a new shapefile out of those features and use the new shapefile to submit order
+      if (selectedFeatures) {
+        // Create a new shapefile
+        const newFile = createLimitedShapefile(file, selectedFeatures)
+        file = newFile
+
+        const fileHash = forge.md.md5.create()
+        fileHash.update(JSON.stringify(file))
+
+        // If the user already used this file, don't save the file again
+        const existingShapefileRecord = await dbConnection('shapefiles')
+          .first('id')
+          .where({
+            file_hash: fileHash,
+            user_id: userId
+          })
+
+        if (!existingShapefileRecord) {
+          const { filename } = shapefileRecord
+
+          // Save new shapefile into database, adding the parent_shapefile_id
+          await dbConnection('shapefiles')
+            .insert({
+              file_hash: fileHash.digest().toHex(),
+              file,
+              filename: `Limited-${filename}`,
+              parent_shapefile_id: deobfuscatedShapefileId,
+              selected_features: selectedFeatures,
+              user_id: userId
+            })
+        }
+      }
     }
 
     try {
@@ -97,16 +132,16 @@ const submitHarmonyOrder = async (event, context) => {
         accessMethod,
         granuleParams,
         accessTokenWithClient,
-        shapefile
+        shapefile: file
       })
 
       const orderResponse = await axios({
         method: 'post',
         url: constructedHarmonyUrl,
-        data: orderPayload.stream,
+        data: orderPayload.stream, // Set internal stream as request body
         headers: {
           Authorization: `Bearer ${accessToken}`,
-          ...orderPayload.headers
+          ...orderPayload.headers // Set headers (Content-Type) of the current FormData instance include the Boundary
         },
         maxRedirects: 1
       })
