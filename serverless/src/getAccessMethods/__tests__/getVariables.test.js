17f38e08f83b47ad3571ebc77a085cabe4025bd2
Ryan Abbott <ryan@element84.com>
2020-09-25 12:19:05 -0400

    EDSC-2829: Multiple fixes related to ordering Harmony orders

--- serverless/src/getAccessMethods/__tests__/getVariables.test.js
+++ serverless/src/getAccessMethods/__tests__/getVariables.test.js
@@ -6,10 +6,21 @@ beforeEach(() => {
 })
 
 describe('getVariables', () => {
-  test('correctly formats variables from graphql', () => {
-    const { keywordMappings, variables } = getVariables(variablesResponse)
+  describe('when variables exist', () => {
+    test('correctly formats variables from graphql', () => {
+      const { keywordMappings, variables } = getVariables(variablesResponse)
 
-    expect(keywordMappings).toEqual(mockKeywordMappings)
-    expect(variables).toEqual(mockVariables)
+      expect(keywordMappings).toEqual(mockKeywordMappings)
+      expect(variables).toEqual(mockVariables)
+    })
+  })
+
+  describe('when no variables exist', () => {
+    test('correctly formats variables from graphql', () => {
+      const { keywordMappings, variables } = getVariables({ count: 0, items: null })
+
+      expect(keywordMappings).toEqual([])
+      expect(variables).toEqual({})
+    })
   })
 })
