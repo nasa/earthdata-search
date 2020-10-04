b201ade0adc82ed13ca3b241847e22dd0ef704a4
Keith Charles <keithc@ouraysportswear.com>
2020-10-04 16:34:52 -0600

    EDSC-2515: add skeleton for collection load state

--- static/src/js/components/CollectionResults/skeleton.js
+++ static/src/js/components/CollectionResults/skeleton.js
@@ -8,3 +8,54 @@ export const collectionResultsTotal = [
     radius: 2
   }
 ]
+
+export const collectionResultsItemSkeleton = [
+  {
+    shape: 'rectangle',
+    top: 10,
+    left: 20,
+    height: 75,
+    width: 75,
+    radius: 3
+  },
+  {
+    shape: 'rectangle',
+    top: 10,
+    left: 110,
+    height: 14,
+    width: '60%',
+    radius: 3
+  },
+  {
+    shape: 'rectangle',
+    top: 32,
+    left: 110,
+    height: 14,
+    width: '40%',
+    radius: 3
+  },
+  {
+    shape: 'rectangle',
+    top: 56,
+    left: 110,
+    height: 18,
+    width: '100px',
+    radius: 3
+  },
+  {
+    shape: 'rectangle',
+    top: 56,
+    left: 215,
+    height: 18,
+    width: '210px',
+    radius: 3
+  },
+  {
+    shape: 'rectangle',
+    top: 10,
+    right: 25,
+    height: 40,
+    width: 27,
+    radius: 3
+  }
+]
