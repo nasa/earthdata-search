1e6eca2a5e1dbca8275aa69f770185eea21ec2ec
Matt Suhay <matthew.suhay@gmail.com>
2020-10-04 08:52:11 -0500

    Adding in test cases

---
+++ static/src/js/components/EDSCIcon/__tests__/EDSCIcon.test.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import Enzyme, { mount, shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+
+import EDSCIcon from '../EDSCIcon'
+
+Enzyme.configure({ adapter: new Adapter() })
+
+describe('EDSCIcon component', () => {
+  test('should render an icon', () => {
+    const enzymeWrapper = mount(<EDSCIcon icon="FaQuestionCircle" />)
+
+    expect(enzymeWrapper.type()).toEqual(EDSCIcon)
+    expect(enzymeWrapper.prop('icon')).toEqual('FaQuestionCircle')
+  })
+
+  describe('when classes are supplied', () => {
+    test('should add the class name', () => {
+      const enzymeWrapper = mount(<EDSCIcon icon="FaQuestionCircle" className="test-class" />)
+      expect(enzymeWrapper.prop('className')).toContain('test-class')
+    })
+  })
+
+  describe('when children are provided', () => {
+    test('should render the children', () => {
+      const enzymeWrapper = shallow(<EDSCIcon icon="FaQuestionCircle"><div className="test-child">Test</div></EDSCIcon>)
+      expect(enzymeWrapper.find('.test-child').length).toEqual(1)
+    })
+  })
+
+  describe('when an icon is not found', () => {
+    test('should not render the icon', () => {
+      const enzymeWrapper = shallow(<EDSCIcon icon="noIcon" />)
+      expect(enzymeWrapper.find('EDSCIcon').length).toEqual(0)
+    })
+  })
+})
