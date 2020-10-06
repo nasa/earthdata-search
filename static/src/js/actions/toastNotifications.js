70e203dda1057dfbc3153f8bf88c1f12d6bb49f7
Andy Amaya <aamay001@gmail.com>
2020-10-06 10:58:23 -0700

    Toast notification actions.

---
+++ static/src/js/actions/toastNotifications.js
@@ -0,0 +1,211 @@
+import { toast } from 'react-toastify'
+import uuid from 'uuid/v4'
+
+import {
+  PUSHING_NOTIFICATION,
+  NOTIFICATION_PUSHED,
+  NOTIFICATION_DIMISSED
+} from '../constants/actionTypes'
+
+export const ToastType = {
+  info: 'info',
+  error: 'error',
+  warning: 'warn',
+  success: 'success'
+}
+
+export const ToastPosition = {
+  bottomLeft: 'bottom-left',
+  bottomCenter: 'bottom-center',
+  bottomRight: 'bottom-right',
+  topLeft: 'top-left',
+  topCenter: 'top-center',
+  topRight: 'top-right'
+}
+
+const notificationDefaults = {
+  type: ToastType.info,
+  autoClose: 15000,
+  position: ToastPosition.topRight,
+  hideProgressBar: false,
+  newestOnTop: true,
+  closeOnClick: true,
+  closeButton: true,
+  rtl: false,
+  pauseOnVisibilityChange: true,
+  draggable: false,
+  pauseOnHover: true,
+  style: {
+    color: '#fcfcfc'
+  },
+  progressStyle: {
+    background: 'navy'
+  }
+}
+
+// Sets the default notification settings
+toast.configure({
+  ...notificationDefaults,
+  bodyClassName: 'root'
+})
+
+const pushingNotification = notification => ({
+  type: PUSHING_NOTIFICATION,
+  notification
+})
+
+const notificationPushed = id => ({
+  type: NOTIFICATION_PUSHED,
+  id
+})
+
+const notificationDismissed = id => ({
+  type: NOTIFICATION_DIMISSED,
+  id
+})
+
+/**
+ * @name addNotification
+ * @param {object} param0
+ * @description Call toast system render the notification.
+ */
+const addNotification = ({
+  content,
+  type,
+  autoClose,
+  position,
+  onClose
+}) => {
+  toast[type || notificationDefaults.type](content, {
+    autoClose,
+    position,
+    onClose
+  })
+}
+
+/**
+ * @name pushSuccessNotification
+ * @param {React..Node} content
+ * @description Generates a default success notification with
+ * the content value inside the toast container.
+ */
+export const pushSuccessNotification = content => (dispatch) => {
+  const id = uuid()
+  const notification = {
+    id,
+    content,
+    type: ToastType.success,
+    pushed: false,
+    dismissed: false,
+    onClose: () => dispatch(notificationDismissed(id)),
+    time: new Date()
+  }
+  dispatch(pushingNotification(notification))
+  addNotification(notification)
+  dispatch(notificationPushed(id))
+}
+
+/**
+ * @name pushWarningNotification
+ * @param {React..Node} content
+ * @description Generates a default warning notification with
+ * the content value inside the toast container.
+ */
+export const pushWarningNotification = content => (dispatch) => {
+  const id = uuid()
+  const notification = {
+    id,
+    content,
+    type: ToastType.warning,
+    pushed: false,
+    dismissed: false,
+    onClose: () => dispatch(notificationDismissed(id)),
+    time: new Date()
+  }
+  dispatch(pushingNotification(notification))
+  addNotification(notification)
+  dispatch(notificationPushed(id))
+}
+
+/**
+ * @name pushErrorNotification
+ * @param {React..Node} content
+ * @description Generates a default error notification with
+ * the content value inside the toast container.
+ */
+export const pushErrorNotification = content => (dispatch) => {
+  const id = uuid()
+  const notification = {
+    id,
+    content,
+    type: ToastType.error,
+    autoClose: false,
+    pushed: false,
+    dismissed: false,
+    onClose: () => dispatch(notificationDismissed(id)),
+    time: new Date()
+  }
+  dispatch(pushingNotification(notification))
+  addNotification(notification)
+  dispatch(notificationPushed(id))
+}
+
+/**
+ * @name pushInfoNotification
+ * @param {React..Node} content
+ * @description Generates a default info notification with
+ * the content value inside the toast container.
+ */
+export const pushInfoNotification = content => (dispatch) => {
+  const id = uuid()
+  const notification = {
+    id,
+    content,
+    type: ToastType.info,
+    autoClose: false,
+    pushed: false,
+    dismissed: false,
+    onClose: () => dispatch(notificationDismissed(id)),
+    time: new Date()
+  }
+  dispatch(pushingNotification(notification))
+  addNotification(notification)
+  dispatch(notificationPushed(id))
+}
+
+/**
+ *
+ * @param {React.Node} content
+ * @param {string} type
+ * @param {bool | number} autoClose
+ * @param {string} position
+ * @param {function} onClose
+ * @description Generates a notification based on the
+ * passed in configuration.
+ */
+export const pushNotification = (
+  content,
+  type,
+  autoClose,
+  position,
+  onClose
+) => (dispatch) => {
+  const id = uuid()
+  const notification = {
+    id,
+    content,
+    type,
+    autoClose,
+    pushed: false,
+    dimissed: false,
+    position,
+    onClose: () => {
+      onClose()
+      dispatch(notificationDismissed(id))
+    },
+    time: new Date()
+  }
+  dispatch(pushingNotification(notification))
+  addNotification(notification)
+  dispatch(notificationPushed(id))
+}
