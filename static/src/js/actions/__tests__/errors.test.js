d2e715b6e509839bed2219c9b8e67af051cd02a5
Andy Amaya <aamay001@gmail.com>
2020-10-13 11:46:39 -0700

    EDSC-2669: Updates error action tests.

--- static/src/js/actions/__tests__/errors.test.js
+++ static/src/js/actions/__tests__/errors.test.js
@@ -12,20 +12,46 @@ beforeEach(() => {
 describe('addError', () => {
   test('should create an action to update the authToken', async () => {
     const store = mockStore({})
-    const payload = {
+    const toastPayload = {
       id: 1,
       title: 'title',
-      message: 'message'
+      message: 'message',
+      notificationType: 'toast'
     }
 
-    const expectedAction = {
+    const bannerPayload = {
+      id: 1,
+      title: 'title',
+      message: 'message',
+      notificationType: 'banner'
+    }
+
+    const nonePayload = {
+      id: 1,
+      title: 'title',
+      message: 'message',
+      notificationType: 'none'
+    }
+
+    const expectedAction0 = {
       type: ADD_ERROR,
-      payload
+      payload: bannerPayload
     }
 
-    await store.dispatch(addError(payload))
-    const storeActions = store.getActions()
-    expect(storeActions[0]).toEqual(expectedAction)
+    await store.dispatch(addError(toastPayload))
+    let storeActions = store.getActions()
+    // no action should be pushed
+    expect(storeActions.length).toEqual(0)
+
+    await store.dispatch(addError(bannerPayload))
+    storeActions = store.getActions()
+    // Action does get pushed
+    expect(storeActions[0]).toEqual(expectedAction0)
+
+    await store.dispatch(addError(nonePayload))
+    storeActions = store.getActions()
+    // No new action pushed
+    expect(storeActions.length).toEqual(1)
   })
 })
 
