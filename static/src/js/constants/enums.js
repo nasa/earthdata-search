b89dc304a961fc5ae7020a8d9b8e2a6d0d2b05ef
Andy Amaya <aamay001@gmail.com>
2020-10-13 11:45:17 -0700

    EDSC-2669: Removed unnecessary enums.

--- static/src/js/constants/enums.js
+++ static/src/js/constants/enums.js
@@ -1,21 +1,5 @@
 export const displayNotificationType = {
-  none: 0,
-  banner: 1,
-  toast: 2
-}
-
-export const toastType = {
-  info: 'info',
-  error: 'error',
-  warning: 'warning',
-  success: 'success'
-}
-
-export const toastPlacement = {
-  bottomLeft: 'bottom-left',
-  bottomCenter: 'bottom-center',
-  bottomRight: 'bottom-right',
-  topLeft: 'top-left',
-  topCenter: 'top-center',
-  topRight: 'top-right'
+  none: 'none',
+  banner: 'banner',
+  toast: 'toast'
 }
