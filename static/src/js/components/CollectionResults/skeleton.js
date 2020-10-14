0f0c855f36d044323ba873a5b311c8307cf8c600
Keith Charles <keithc@ouraysportswear.com>
2020-10-14 10:06:44 -0600

    EDSC-2515: fix skeleton btn size/linter errors

--- static/src/js/components/CollectionResults/skeleton.js
+++ static/src/js/components/CollectionResults/skeleton.js
@@ -10,14 +10,15 @@ export const collectionResultsTotal = [
 ]
 
 export const collectionResultsItemSkeleton = [
+  // image box
   {
     shape: 'rectangle',
-    top: 10,
-    left: 20,
+    top: 2,
+    left: 15,
     height: 75,
-    width: 75,
-    radius: 3
+    width: 75
   },
+  // 2x header lines
   {
     shape: 'rectangle',
     top: 10,
@@ -28,34 +29,61 @@ export const collectionResultsItemSkeleton = [
   },
   {
     shape: 'rectangle',
-    top: 32,
+    top: 30,
     left: 110,
     height: 14,
     width: '40%',
     radius: 3
   },
+  // 3x description lines
   {
     shape: 'rectangle',
-    top: 56,
+    top: 54,
     left: 110,
-    height: 18,
+    height: 10,
+    width: '60%',
+    radius: 1
+  },
+  {
+    shape: 'rectangle',
+    top: 68,
+    left: 110,
+    height: 10,
+    width: '54%',
+    radius: 1
+  },
+  {
+    shape: 'rectangle',
+    top: 82,
+    left: 110,
+    height: 10,
+    width: '58%',
+    radius: 1
+  },
+  // 2x inline tags
+  {
+    shape: 'rectangle',
+    top: 99,
+    left: 110,
+    height: 22,
     width: '100px',
     radius: 3
   },
   {
     shape: 'rectangle',
-    top: 56,
+    top: 99,
     left: 215,
-    height: 18,
+    height: 22,
     width: '210px',
     radius: 3
   },
+  // side buttons
   {
     shape: 'rectangle',
-    top: 10,
-    right: 25,
-    height: 40,
-    width: 27,
+    top: 2,
+    right: 16,
+    height: 35,
+    width: 35,
     radius: 3
   }
 ]
