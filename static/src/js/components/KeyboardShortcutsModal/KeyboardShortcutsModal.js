d8fcacdf839585415c5699b0f34f45afcef97c66
Mark Reese <mreese@element84.com>
2020-10-14 08:50:15 -0500

    EDSC-2911: Remove timeline shortcut text

--- static/src/js/components/KeyboardShortcutsModal/KeyboardShortcutsModal.js
+++ static/src/js/components/KeyboardShortcutsModal/KeyboardShortcutsModal.js
@@ -44,7 +44,6 @@ export class KeyboardShortcutsModal extends Component {
       a: 'Toggle advanced search modal',
       '/': 'Focus on search input field',
       '?': 'Display all keyboard shortcuts',
-      t: 'Toggle timeline'
     }
     return (
       <EDSCModalContainer
