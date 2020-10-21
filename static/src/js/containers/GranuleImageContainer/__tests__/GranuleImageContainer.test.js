80755a73565f87ef8239fdca8d4b883008fb8f87
Ryan Abbott <ryan@element84.com>
2020-10-21 12:36:15 -0400

    EDSC-2882: Fixes test

--- static/src/js/containers/GranuleImageContainer/__tests__/GranuleImageContainer.test.js
+++ static/src/js/containers/GranuleImageContainer/__tests__/GranuleImageContainer.test.js
@@ -11,6 +11,7 @@ Enzyme.configure({ adapter: new Adapter() })
 
 function setup() {
   const props = {
+    earthdataEnvironment: 'prod',
     focusedGranuleId: 'focusedGranule',
     granuleMetadata: {
       browseFlag: true
