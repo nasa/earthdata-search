ad0343e0de6e8b68ca98c39a049d1fb444167387
Matthew Crouch <matthew@element84.com>
2020-10-23 12:29:21 -0400

    EDSC-2951: Merge portal configs successfully

--- static/src/js/util/__tests__/portals.test.js
+++ static/src/js/util/__tests__/portals.test.js
@@ -1,4 +1,4 @@
-import { isDefaultPortal } from '../portals'
+import { getPortalConfig, isDefaultPortal } from '../portals'
 import * as getApplicationConfig from '../../../../../sharedUtils/config'
 
 beforeEach(() => {
@@ -19,5 +19,60 @@ describe('isDefaultPortal', () => {
 
   test('returns false if the portalId does not match the defaultPortal', () => {
     expect(isDefaultPortal('simple')).toBeFalsy()
+  })
+})
+
+describe('getPortalConfig', () => {
+  test('builds a portal config of portal > edsc portal > default portal', () => {
+    const config = getPortalConfig('idn')
+
+    expect(config).toEqual({
+      features: {
+        advancedSearch: true,
+        authentication: true,
+        featureFacets: {
+          showMapImagery: true,
+          showNearRealTime: true,
+          showCustomizable: true
+        }
+      },
+      footer: {
+        displayVersion: true,
+        attributionText: 'NASA Official: Stephen Berrick',
+        primaryLinks: [{
+          title: 'FOIA',
+          href: 'http://www.nasa.gov/FOIA/index.html'
+        },
+        {
+          title: 'NASA Privacy Policy',
+          href: 'http://www.nasa.gov/about/highlights/HP_Privacy.html'
+        },
+        {
+          title: 'USA.gov',
+          href: 'http://www.usa.gov'
+        }],
+        secondaryLinks: [{
+          title: 'Earthdata Access: A Section 508 accessible alternative',
+          href: 'https://access.earthdata.nasa.gov/'
+        }]
+      },
+      hasScripts: false,
+      hasStyles: true,
+      logo: {
+        id: 'idn-logo',
+        link: 'https://idn.ceos.org/',
+        title: 'CEOS IDN Search'
+      },
+      org: 'IDN',
+      pageTitle: 'IDN',
+      query: { hasGranulesOrCwic: null },
+      title: 'Search',
+      ui: {
+        showOnlyGranulesCheckbox: false,
+        showNonEosdisCheckbox: false,
+        showTophat: true
+      },
+      parentConfig: 'edsc'
+    })
   })
 })
