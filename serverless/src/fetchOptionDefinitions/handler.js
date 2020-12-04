407707180a5acb51e792642f6cdc178a1af236c2
Matthew Crouch <matthew@element84.com>
2020-12-04 10:17:48 -0500

    EDSC-2977: Use 'Echo-Token' header for tagging calls

--- serverless/src/fetchOptionDefinitions/handler.js
+++ serverless/src/fetchOptionDefinitions/handler.js
@@ -60,9 +60,9 @@ const fetchOptionDefinitions = async (event, context) => {
           'catalog_item_id[]': granuleId
         }, { indices: false, arrayFormat: 'brackets' }),
         headers: {
-          Authorization: `Bearer ${cmrToken}`,
           'Client-Id': getClientId().background,
-          'Content-Type': 'application/x-www-form-urlencoded'
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Echo-Token': cmrToken
         },
         json: true,
         resolveWithFullResponse: true
