8539a02eb7177845660cf777e208a633fd37489c
Ryan Abbott <ryan@element84.com>
2021-03-04 17:12:39 -0500

    EDSC-3090: Ensures that paging methods await a response

--- serverless/src/generateSubsettingTags/__tests__/getRelevantServices.test.js
+++ serverless/src/generateSubsettingTags/__tests__/getRelevantServices.test.js
@@ -1,22 +1,30 @@
+import nock from 'nock'
+
 import { cmrServiceResponse } from './mocks'
 import { getRelevantServices } from '../getRelevantServices'
 
-import * as pageAllCmrResults from '../../util/cmr/pageAllCmrResults'
+import * as getEarthdataConfig from '../../../../sharedUtils/config'
 import * as getSystemToken from '../../util/urs/getSystemToken'
 
 beforeEach(() => {
   jest.clearAllMocks()
+
+  jest.spyOn(getEarthdataConfig, 'getEarthdataConfig').mockImplementation(() => ({ cmrHost: 'http://cmr.example.com' }))
 })
 
 describe('getRelevantServices', () => {
   test('does not iterate when uneccessary', async () => {
     jest.spyOn(getSystemToken, 'getSystemToken').mockImplementation(() => 'mocked-system-token')
 
-    const allCmrResultsMock = jest.spyOn(pageAllCmrResults, 'pageAllCmrResults').mockImplementationOnce(() => cmrServiceResponse)
+    nock(/example/)
+      .post(/services/)
+      .reply(200, {
+        items: cmrServiceResponse
+      }, {
+        'cmr-hits': '4'
+      })
 
-    const relevantServices = await getRelevantServices()
-
-    expect(allCmrResultsMock).toBeCalledTimes(1)
+    const relevantServices = await getRelevantServices('cmr-token')
 
     // Mocked data has 4 relevant services (1 OPeNDAP, 2 ESI, 1 ECHO ORDERS)
     expect(Object.keys(relevantServices).length).toBe(4)
