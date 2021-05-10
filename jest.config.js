49d8d3b0f8b1e9930627162b3f5a9ad08e4ef199
Trevor Lang <trevorlang@ymail.com>
2021-05-10 14:47:51 -0600

    EDSC-3146: Fixing jest config

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   collectCoverage: true,
-  // collectCoverageFrom: [
-  //   'serverless/src/**/*.js',
-  //   'static/src/**/*.js',
-  //   'sharedUtils/**/*.js'
-  // ],
+  collectCoverageFrom: [
+    'serverless/src/**/*.js',
+    'static/src/**/*.js',
+    'sharedUtils/**/*.js'
+  ],
   moduleNameMapper: {
     '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/static/src/js/util/mocks/fileMock.js',
     '^.+\\.(css|less|scss)$': 'babel-jest'
