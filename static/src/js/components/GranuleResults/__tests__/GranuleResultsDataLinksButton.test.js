3570deedc0abcb4f110a5f81bfff525cb1bdee92
Trevor Lang <trevorlang@ymail.com>
2021-05-11 09:39:06 -0600

    EDSC-3146: Improving tests

--- static/src/js/components/GranuleResults/__tests__/GranuleResultsDataLinksButton.test.js
+++ static/src/js/components/GranuleResults/__tests__/GranuleResultsDataLinksButton.test.js
@@ -36,7 +36,6 @@ function setup(overrideProps) {
 
 beforeEach(() => {
   jest.clearAllMocks()
-  Object.defineProperty(global.navigator, 'writeText', () => Promise.resolve())
 })
 
 describe('GranuleResultsDataLinksButton component', () => {
@@ -54,6 +53,16 @@ describe('GranuleResultsDataLinksButton component', () => {
 
       expect(enzymeWrapper.find(Button).props().disabled).toBe(true)
       expect(enzymeWrapper.type()).toBe(Button)
+    })
+
+    test('prevents default when clicked', () => {
+      const preventDefaultMock = jest.fn()
+      const { enzymeWrapper } = setup({
+        dataLinks: []
+      })
+
+      enzymeWrapper.find(Button).simulate('click', { preventDefault: preventDefaultMock })
+      expect(preventDefaultMock).toHaveBeenCalledTimes(1)
     })
   })
 
@@ -432,6 +441,272 @@ describe('GranuleResultsDataLinksButton component', () => {
         expect(addToastMock.mock.calls[0][0]).toEqual('Copied AWS S3 path for: linkhref')
         expect(addToastMock.mock.calls[0][1].appearance).toEqual('success')
         expect(addToastMock.mock.calls[0][1].autoDismiss).toEqual(true)
+      })
+    })
+
+    describe('when direct distribution information is provided', () => {
+      test('displays the region as a button', () => {
+        ReactDOM.createPortal = jest.fn(dropdown => dropdown)
+
+        const { enzymeWrapper } = setup({
+          dataLinks: [],
+          directDistributionInformation: {
+            region: 'aws-region'
+          },
+          s3Links: [
+            {
+              rel: 'http://linkrel/s3#',
+              title: 'linktitle',
+              href: 's3://linkhref'
+            }, {
+              rel: 'http://linkrel2/s3#',
+              title: 'linktitle2',
+              href: 's3://linkhref2'
+            }
+          ]
+        })
+        const distributionInformation = enzymeWrapper.find('.tab-content').props().children().props.children[0]
+        const regionRow = distributionInformation.props.children[0]
+        const value = regionRow.props.children[1]
+        expect(value.props.children).toBe('aws-region')
+      })
+
+      describe('when clicking the region', () => {
+        describe('when navigation.clipboard.writeText is not defined', () => {
+          test('displays a toast', async () => {
+            // Mocks createPortal method of ReactDOM (https://stackoverflow.com/a/60953708/8116576)
+            ReactDOM.createPortal = jest.fn(dropdown => dropdown)
+            const addToastMock = jest.spyOn(addToast, 'addToast')
+
+            Object.assign(navigator, {
+              clipboard: {}
+            })
+
+            const { enzymeWrapper } = setup({
+              dataLinks: [],
+              directDistributionInformation: {
+                region: 'aws-region'
+              },
+              s3Links: [
+                {
+                  rel: 'http://linkrel/s3#',
+                  title: 'linktitle',
+                  href: 's3://linkhref'
+                }, {
+                  rel: 'http://linkrel2/s3#',
+                  title: 'linktitle2',
+                  href: 's3://linkhref2'
+                }
+              ]
+            })
+            const distributionInformation = enzymeWrapper.find('.tab-content').props().children().props.children[0]
+            const regionRow = distributionInformation.props.children[0]
+            const value = regionRow.props.children[1]
+            await value.props.onClick()
+
+            expect(addToastMock.mock.calls.length).toBe(1)
+            expect(addToastMock.mock.calls[0][0]).toEqual('Failed to copy the AWS S3 Region')
+            expect(addToastMock.mock.calls[0][1].appearance).toEqual('error')
+            expect(addToastMock.mock.calls[0][1].autoDismiss).toEqual(true)
+          })
+        })
+
+        describe('when navigation.clipboard.writeText is not defined', () => {
+          test('displays a toast', async () => {
+            // Mocks createPortal method of ReactDOM (https://stackoverflow.com/a/60953708/8116576)
+            ReactDOM.createPortal = jest.fn(dropdown => dropdown)
+            const addToastMock = jest.spyOn(addToast, 'addToast')
+
+            Object.assign(navigator, {
+              clipboard: {
+                writeText: jest.fn()
+              }
+            })
+
+            const { enzymeWrapper } = setup({
+              dataLinks: [],
+              directDistributionInformation: {
+                region: 'aws-region'
+              },
+              s3Links: [
+                {
+                  rel: 'http://linkrel/s3#',
+                  title: 'linktitle',
+                  href: 's3://linkhref'
+                }, {
+                  rel: 'http://linkrel2/s3#',
+                  title: 'linktitle2',
+                  href: 's3://linkhref2'
+                }
+              ]
+            })
+            const distributionInformation = enzymeWrapper.find('.tab-content').props().children().props.children[0]
+            const regionRow = distributionInformation.props.children[0]
+            const value = regionRow.props.children[1]
+            await value.props.onClick()
+
+            expect(addToastMock.mock.calls.length).toBe(1)
+            expect(addToastMock.mock.calls[0][0]).toEqual('Copied the AWS S3 Region')
+            expect(addToastMock.mock.calls[0][1].appearance).toEqual('success')
+            expect(addToastMock.mock.calls[0][1].autoDismiss).toEqual(true)
+          })
+        })
+      })
+
+      test('displays the s3 bucket and object prefix as a button', () => {
+        ReactDOM.createPortal = jest.fn(dropdown => dropdown)
+
+        const { enzymeWrapper } = setup({
+          dataLinks: [],
+          directDistributionInformation: {
+            region: 'aws-region',
+            s3BucketAndObjectPrefixNames: ['TestBucketOrObjectPrefix'],
+            s3CredentialsApiEndpoint: 'https://DAACCredentialEndpoint.org',
+            s3CredentialsApiDocumentationUrl: 'https://DAACCredentialDocumentation.org'
+          },
+          s3Links: [
+            {
+              rel: 'http://linkrel/s3#',
+              title: 'linktitle',
+              href: 's3://linkhref'
+            }, {
+              rel: 'http://linkrel2/s3#',
+              title: 'linktitle2',
+              href: 's3://linkhref2'
+            }
+          ]
+        })
+        const distributionInformation = enzymeWrapper.find('.tab-content').props().children().props.children[0]
+        const bucketObjectPrefixRow = distributionInformation.props.children[1]
+        const value = bucketObjectPrefixRow.props.children[1][0]
+        const bucketObjectPrefixButton = value.props.children[0]
+        expect(bucketObjectPrefixButton.props.children).toBe('TestBucketOrObjectPrefix')
+      })
+
+      describe('when clicking the bucket object prefix button', () => {
+        describe('when navigation.clipboard.writeText is not defined', () => {
+          test('displays a toast', async () => {
+            // Mocks createPortal method of ReactDOM (https://stackoverflow.com/a/60953708/8116576)
+            ReactDOM.createPortal = jest.fn(dropdown => dropdown)
+            const addToastMock = jest.spyOn(addToast, 'addToast')
+
+            Object.assign(navigator, {
+              clipboard: {}
+            })
+
+            const { enzymeWrapper } = setup({
+              dataLinks: [],
+              directDistributionInformation: {
+                region: 'aws-region',
+                s3BucketAndObjectPrefixNames: ['TestBucketOrObjectPrefix'],
+                s3CredentialsApiEndpoint: 'https://DAACCredentialEndpoint.org',
+                s3CredentialsApiDocumentationUrl: 'https://DAACCredentialDocumentation.org'
+              },
+              s3Links: [
+                {
+                  rel: 'http://linkrel/s3#',
+                  title: 'linktitle',
+                  href: 's3://linkhref'
+                }, {
+                  rel: 'http://linkrel2/s3#',
+                  title: 'linktitle2',
+                  href: 's3://linkhref2'
+                }
+              ]
+            })
+            const distributionInformation = enzymeWrapper.find('.tab-content').props().children().props.children[0]
+            const bucketObjectPrefixRow = distributionInformation.props.children[1]
+            const value = bucketObjectPrefixRow.props.children[1][0]
+            const bucketObjectPrefixButton = value.props.children[0]
+            await bucketObjectPrefixButton.props.onClick()
+
+            expect(addToastMock.mock.calls.length).toBe(1)
+            expect(addToastMock.mock.calls[0][0]).toEqual('Failed to copy the AWS S3 Bucket/Object Prefix')
+            expect(addToastMock.mock.calls[0][1].appearance).toEqual('error')
+            expect(addToastMock.mock.calls[0][1].autoDismiss).toEqual(true)
+          })
+        })
+
+        describe('when navigation.clipboard.writeText is not defined', () => {
+          test('displays a toast', async () => {
+            // Mocks createPortal method of ReactDOM (https://stackoverflow.com/a/60953708/8116576)
+            ReactDOM.createPortal = jest.fn(dropdown => dropdown)
+            const addToastMock = jest.spyOn(addToast, 'addToast')
+
+            Object.assign(navigator, {
+              clipboard: {
+                writeText: jest.fn()
+              }
+            })
+
+            const { enzymeWrapper } = setup({
+              dataLinks: [],
+              directDistributionInformation: {
+                region: 'aws-region',
+                s3BucketAndObjectPrefixNames: ['TestBucketOrObjectPrefix'],
+                s3CredentialsApiEndpoint: 'https://DAACCredentialEndpoint.org',
+                s3CredentialsApiDocumentationUrl: 'https://DAACCredentialDocumentation.org'
+              },
+              s3Links: [
+                {
+                  rel: 'http://linkrel/s3#',
+                  title: 'linktitle',
+                  href: 's3://linkhref'
+                }, {
+                  rel: 'http://linkrel2/s3#',
+                  title: 'linktitle2',
+                  href: 's3://linkhref2'
+                }
+              ]
+            })
+            const distributionInformation = enzymeWrapper.find('.tab-content').props().children().props.children[0]
+            const bucketObjectPrefixRow = distributionInformation.props.children[1]
+            const value = bucketObjectPrefixRow.props.children[1][0]
+            const bucketObjectPrefixButton = value.props.children[0]
+            await bucketObjectPrefixButton.props.onClick()
+
+            expect(addToastMock.mock.calls.length).toBe(1)
+            expect(addToastMock.mock.calls[0][0]).toEqual('Copied the AWS S3 Bucket/Object Prefix')
+            expect(addToastMock.mock.calls[0][1].appearance).toEqual('success')
+            expect(addToastMock.mock.calls[0][1].autoDismiss).toEqual(true)
+          })
+        })
+      })
+
+      describe('when multiple bucket object prefixes are provided', () => {
+        test('displays the s3 bucket and object prefixes as buttons', () => {
+          ReactDOM.createPortal = jest.fn(dropdown => dropdown)
+
+          const { enzymeWrapper } = setup({
+            dataLinks: [],
+            directDistributionInformation: {
+              region: 'aws-region',
+              s3BucketAndObjectPrefixNames: [
+                'TestBucketOrObjectPrefix',
+                'TestBucketOrObjectPrefixTwo'
+              ],
+              s3CredentialsApiEndpoint: 'https://DAACCredentialEndpoint.org',
+              s3CredentialsApiDocumentationUrl: 'https://DAACCredentialDocumentation.org'
+            },
+            s3Links: [
+              {
+                rel: 'http://linkrel/s3#',
+                title: 'linktitle',
+                href: 's3://linkhref'
+              }, {
+                rel: 'http://linkrel2/s3#',
+                title: 'linktitle2',
+                href: 's3://linkhref2'
+              }
+            ]
+          })
+          const distributionInformation = enzymeWrapper.find('.tab-content').props().children().props.children[0]
+          const bucketObjectPrefixRow = distributionInformation.props.children[1]
+          const bucketObjectPrefixButtonOne = bucketObjectPrefixRow.props.children[1][0]
+          const bucketObjectPrefixButtonTwo = bucketObjectPrefixRow.props.children[1][1]
+          expect(bucketObjectPrefixButtonOne.props.children[0].props.children).toBe('TestBucketOrObjectPrefix')
+          expect(bucketObjectPrefixButtonTwo.props.children[0].props.children).toBe('TestBucketOrObjectPrefixTwo')
+        })
       })
     })
   })
