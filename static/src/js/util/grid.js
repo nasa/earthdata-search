77c2188dc753e2de91b1868b18782ae8acfa0121
Matthew Crouch <matthew@element84.com>
2021-05-13 10:41:30 -0400

    EDSC-3162: Adds Military Grid Reference System configuration

--- static/src/js/util/grid.js
+++ static/src/js/util/grid.js
@@ -1,12 +1,13 @@
 const config = [
-  ['CALIPSO', 'CALIPSO', 'orbit', 1, 75000, 'path', 1, 233],
-  ['MISR', 'MISR', 'path', 1, 233, 'block', 1, 180],
-  ['MODIS Tile EASE', 'MODIS EASE Grid', 'h', 0, 18, 'v', 0, 38],
-  ['MODIS Tile SIN', 'MODIS Sinusoidal', 'h', 0, 35, 'v', 0, 17],
-  ['WELD ALASKA Tile', 'WELD ALASKA Tile', 'h', 0, 16, 'v', 0, 13],
-  ['WELD CONUS Tile', 'WELD CONUS Tile', 'h', 0, 32, 'v', 0, 21],
-  ['WRS-1', 'WRS-1 (Landsat 1-3)', 'path', 1, 251, 'row', 1, 248],
-  ['WRS-2', 'WRS-2 (Landsat 4+)', 'path', 1, 233, 'row', 1, 248]
+  ['CALIPSO', 'CALIPSO', 'orbit', 'path'],
+  ['MISR', 'MISR', 'path', 'block'],
+  ['MODIS Tile EASE', 'MODIS EASE Grid', 'h', 'v'],
+  ['MODIS Tile SIN', 'MODIS Sinusoidal', 'h', 'v'],
+  ['WELD ALASKA Tile', 'WELD ALASKA Tile', 'h', 'v'],
+  ['WELD CONUS Tile', 'WELD CONUS Tile', 'h', 'v'],
+  ['WRS-1', 'WRS-1 (Landsat 1-3)', 'path', 'row'],
+  ['WRS-2', 'WRS-2 (Landsat 4+)', 'path', 'row'],
+  ['Military Grid Reference System', 'Military Grid Reference System', 'h', 'v']
 ]
 
 export const availableSystems = config.map((item) => {
@@ -14,22 +15,14 @@ export const availableSystems = config.map((item) => {
     name,
     label,
     axis0label,
-    axis0min,
-    axis0max,
-    axis1label,
-    axis1min,
-    axis1max
+    axis1label
   ] = item
 
   return {
     name,
     label,
     axis0label,
-    axis0min,
-    axis0max,
-    axis1label,
-    axis1min,
-    axis1max
+    axis1label
   }
 })
 
