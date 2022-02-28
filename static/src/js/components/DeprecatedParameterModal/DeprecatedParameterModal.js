1e07eed24c0549b1494c47bfbb3c3beeb535f583
Trevor Lang <trevorlang@ymail.com>
2022-02-28 13:03:53 -0700

    EDSC-3361: Formatting

--- static/src/js/components/DeprecatedParameterModal/DeprecatedParameterModal.js
+++ static/src/js/components/DeprecatedParameterModal/DeprecatedParameterModal.js
@@ -36,9 +36,10 @@ export class DeprecatedParameterModal extends Component {
             className="link link--external"
             target="_blank"
             rel="noreferrer"
-            href="https://wiki.earthdata.nasa.gov/display/EDSC/Earthdata+Search+URL+Parameters">
-              Earthdata Search URL Parameters
-            </a>
+            href="https://wiki.earthdata.nasa.gov/display/EDSC/Earthdata+Search+URL+Parameters"
+          >
+            Earthdata Search URL Parameters
+          </a>
           {' wiki page for more information on the supported URL parameters.'}
         </p>
       </>
