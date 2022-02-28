df37ea965e3f1e80413eb014e4774a37018d17ef
Matthew Crouch <matthew@element84.com>
2022-02-28 10:23:44 -0500

    EDSC-3211: Removes empty facet params from autocomplete suggestions

--- static/src/js/actions/__tests__/autocomplete.test.js
+++ static/src/js/actions/__tests__/autocomplete.test.js
@@ -228,6 +228,25 @@ describe('mapAutocompleteToFacets', () => {
     expect(mapAutocompleteToFacets(autocomplete)).toEqual(result)
   })
 
+  test('maps science keywords autocomplete to facets when a facet level doesn\'t exist', () => {
+    const autocomplete = {
+      suggestion: {
+        fields: 'Land Surface:Surface Radiative Properties::Laser Reflectance',
+        type: 'science_keywords'
+      }
+    }
+
+    const result = {
+      science_keywords_h: {
+        topic: 'Land Surface',
+        term: 'Surface Radiative Properties',
+        variable_level_2: 'Laser Reflectance'
+      }
+    }
+
+    expect(mapAutocompleteToFacets(autocomplete)).toEqual(result)
+  })
+
   test('maps platforms autocomplete to facets', () => {
     const autocomplete = {
       suggestion: {
@@ -241,6 +260,25 @@ describe('mapAutocompleteToFacets', () => {
         basis: 'Space-based Platforms',
         category: 'Earth Observation Satellites',
         sub_category: 'Landsat',
+        short_name: 'LANDSAT-8'
+      }
+    }
+
+    expect(mapAutocompleteToFacets(autocomplete)).toEqual(result)
+  })
+
+  test('maps platforms autocomplete to facets when a facet level doesn\'t exist', () => {
+    const autocomplete = {
+      suggestion: {
+        fields: 'Space-based Platforms:Earth Observation Satellites::LANDSAT-8',
+        type: 'platforms'
+      }
+    }
+
+    const result = {
+      platforms_h: {
+        basis: 'Space-based Platforms',
+        category: 'Earth Observation Satellites',
         short_name: 'LANDSAT-8'
       }
     }
