c900b4a54b3bfb569d5175e3537a8762c915d1c2
Matthew Crouch <matthew@element84.com>
2022-04-21 14:41:49 -0400

    EDSC-3448: Fixes jest config

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   collectCoverage: true,
   collectCoverageFrom: [
-    // 'serverless/src/**/*.js',
-    // 'static/src/**/*.js',
-    // 'sharedUtils/**/*.js'
+    'serverless/src/**/*.js',
+    'static/src/**/*.js',
+    'sharedUtils/**/*.js'
   ],
   moduleNameMapper: {
     '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/static/src/js/util/mocks/fileMock.js',
