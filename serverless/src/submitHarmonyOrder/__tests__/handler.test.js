45b2f4ea2e0617cc4766824fd00772786844491c
Matthew Crouch <matthew@element84.com>
2022-05-05 15:23:33 -0400

    EDSC-3440: Changes Harmony parameter name to skipPreview

--- serverless/src/submitHarmonyOrder/__tests__/handler.test.js
+++ serverless/src/submitHarmonyOrder/__tests__/handler.test.js
@@ -332,7 +332,7 @@ describe('submitHarmonyOrder', () => {
     expect(consoleMock.mock.calls[2]).toEqual(['forceAsync: true'])
     expect(consoleMock.mock.calls[3]).toEqual(['granuleId: G10000005-EDSC'])
     expect(consoleMock.mock.calls[4]).toEqual(['format: NetCDF-4'])
-    expect(consoleMock.mock.calls[5]).toEqual(['preview: false'])
+    expect(consoleMock.mock.calls[5]).toEqual(['skipPreview: true'])
     expect(consoleMock.mock.calls[6]).toEqual(['Error (403): Error: You are not authorized to access the requested resource'])
   })
 })
