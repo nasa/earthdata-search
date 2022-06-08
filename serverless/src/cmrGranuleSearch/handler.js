8ac25e77b68effdedc9b22ea272a7888218579e1
Trevor Lang <trevorlang@ymail.com>
2022-06-08 07:36:55 -0600

    EDSC-3422: Adding constants for granule request handler

--- serverless/src/cmrGranuleSearch/handler.js
+++ serverless/src/cmrGranuleSearch/handler.js
@@ -6,6 +6,8 @@ import { doSearchRequest } from '../util/cmr/doSearchRequest'
 import { getApplicationConfig } from '../../../sharedUtils/config'
 import { getJwtToken } from '../util/getJwtToken'
 import { parseError } from '../../../sharedUtils/parseError'
+import { granuleRequestPermittedCmrKeys } from '../../../sharedConstants/permittedCmrKeys'
+import { granuleRequestNonIndexedCmrKeys } from '../../../sharedConstants/nonIndexedCmrKeys'
 
 /**
  * Perform an authenticated CMR Granule search
@@ -23,52 +25,14 @@ const cmrGranuleSearch = async (event) => {
 
   const earthdataEnvironment = determineEarthdataEnvironment(headers)
 
-  // Whitelist parameters supplied by the request
-  const permittedCmrKeys = [
-    'concept_id',
-    'bounding_box',
-    'circle',
-    'browse_only',
-    'cloud_cover',
-    'day_night_flag',
-    'echo_collection_id',
-    'equator_crossing_date',
-    'equator_crossing_longitude',
-    'exclude',
-    'line',
-    'online_only',
-    'options',
-    'orbit_number',
-    'page_num',
-    'page_size',
-    'point',
-    'polygon',
-    'readable_granule_name',
-    'sort_key',
-    'temporal',
-    'two_d_coordinate_system'
-  ]
-
-  const nonIndexedKeys = [
-    'bounding_box',
-    'circle',
-    'concept_id',
-    'exclude',
-    'line',
-    'point',
-    'polygon',
-    'readable_granule_name',
-    'sort_key'
-  ]
-
   try {
     return doSearchRequest({
       jwtToken: getJwtToken(event),
       path: '/search/granules.json',
       params: buildParams({
         body,
-        permittedCmrKeys,
-        nonIndexedKeys
+        permittedCmrKeys: granuleRequestPermittedCmrKeys,
+        nonIndexedKeys: granuleRequestNonIndexedCmrKeys
       }),
       providedHeaders,
       requestId,
