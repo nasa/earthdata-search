3bde7a45c5717296ed9d001bd2603051d9e3391b
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2023-02-23 09:35:42 -0500

    EDSC-3612: added getSearchExportS3Endpoint

---
+++ serverless/src/util/aws/getSearchExportS3Endpoint.js
@@ -0,0 +1,14 @@
+/**
+ * Returns the search export S3 endpoint from the env variable or the hard coded offline value
+ * @return {string} url to the S3 endpoint
+ */
+export default function getSearchExportS3Endpoint () {
+  let endpoint = process.env.searchExportS3Endpoint
+
+  if (process.env.IS_OFFLINE) {
+    // if we are running locally offline, this is the local S3-compatible endpoint
+    endpoint = 'http://0.0.0.0:5000'
+  }
+
+  return endpoint
+}
