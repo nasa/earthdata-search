eb6779ea540cee95b40406054f044824279adb5b
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2023-02-23 15:45:42 -0500

    EDSC-3612: updated actions/exportSearch.js

--- static/src/js/actions/exportSearch.js
+++ static/src/js/actions/exportSearch.js
@@ -1,4 +1,5 @@
 import { EXPORT_FINISHED, EXPORT_STARTED } from '../constants/actionTypes'
+import { getApplicationConfig } from '../../../../sharedUtils/config'
 import { getEarthdataEnvironment } from '../selectors/earthdataEnvironment'
 import { buildCollectionSearchParams, prepareCollectionParams } from '../util/collections'
 import ExportSearchRequest from '../util/request/exportSearchRequest'
@@ -22,6 +23,8 @@ export const exportSearch = (format) => (dispatch, getState) => {
   dispatch(onExportStarted(format))
 
   const state = getState()
+
+  if (!state.authToken) throw new Error('you must log in before exporting search results')
 
   // Retrieve data from Redux using selectors
   const earthdataEnvironment = getEarthdataEnvironment(state)
@@ -121,6 +124,35 @@ export const exportSearch = (format) => (dispatch, getState) => {
       }
     }`
 
+  const searchExportParams = {
+    itempath: 'collections.items',
+    cursorpath: 'collections.cursor'
+  }
+
+  if (format === 'csv') {
+    searchExportParams.columns = [
+      { name: 'Data Provider', path: 'provider' },
+      { name: 'Short Name', path: 'shortName' },
+      { name: 'Version', path: 'versionId' },
+      { name: 'Entry Title', path: 'title' },
+      { name: 'Processing Level', path: 'processingLevelId' },
+      { name: 'Platform', path: 'platforms.shortName' },
+      { name: 'Start Time', path: 'timeStart' },
+      { name: 'End Time', path: 'timeEnd' }
+    ]
+  }
+
+  const handleExportSearchError = (error) => {
+    dispatch(onExportFinished(format))
+
+    dispatch(handleError({
+      error,
+      action: 'exportSearch',
+      resource: 'collections',
+      graphQlRequestObject
+    }))
+  }
+
   const response = graphQlRequestObject.search(graphQuery, {
     ...buildCollectionSearchParams(collectionParams),
     limit: 1000,
@@ -130,45 +162,67 @@ export const exportSearch = (format) => (dispatch, getState) => {
     includeTags: undefined,
     pageNum: undefined,
     pageSize: undefined
-  }, format)
-    .then((response) => {
-      const { data } = response
+  }, format, searchExportParams)
+    .then(async (response) => {
+      // key is a random uuid for this specific export request
+      // we use it to poll the status of the export later
+      const { key } = response.data
+      if (!key) throw Error('server did not response with a uuid for the export request, which we need in order to find the download')
 
-      // Create a blob with the text data from the export
-      let blob
-      if (format === 'csv') {
-        blob = new Blob([data], { type: 'text/csv' })
+      const { exportStatusRefreshTime = 5 * 1000 } = getApplicationConfig()
+
+      const [error, signedUrl] = await new Promise((resolve) => {
+        const intervalId = setInterval(async () => {
+          try {
+            const response = await graphQlRequestObject.post('collections/export-check', { key })
+
+            const { data } = response
+
+            if (typeof data !== 'object') {
+              clearInterval(intervalId) // stop polling
+              resolve(['server returned an invalid response', null])
+            }
+
+            const { state, url } = data
+
+            if (state === 'DONE') {
+              clearInterval(intervalId)
+              resolve([null, url])
+            } else if (state === 'FAILED') {
+              clearInterval(intervalId)
+              resolve(['server reported that the export has failed', null])
+            } else if ([null, ''].includes(state)) {
+              clearInterval(intervalId)
+              resolve(['server returned an invalid state', null])
+            }
+          } catch (error) {
+            clearInterval(intervalId)
+            resolve([error, null])
+          }
+        }, exportStatusRefreshTime)
+      })
+
+      if (error) {
+        handleExportSearchError(error)
       } else {
-        blob = new Blob([JSON.stringify(data)])
+        // Create a hyperlink to the export and give it a filename
+        const link = document.createElement('a')
+        link.href = signedUrl
+        link.setAttribute('download', `edsc_collection_results_export.${format}`)
+
+        // Add the link to the page
+        document.body.appendChild(link)
+
+        // Click on the link to download the export file to the user's computer
+        link.click()
+
+        // Remove the link from the page
+        link.parentNode.removeChild(link)
+
+        dispatch(onExportFinished(format))
       }
-      const url = window.URL.createObjectURL(blob)
-
-      // Create a hyperlink to the blob and give it a filename
-      const link = document.createElement('a')
-      link.href = url
-      link.setAttribute('download', `edsc_collection_results_export.${format}`)
-
-      // Add the link to the page
-      document.body.appendChild(link)
-
-      // Click on the link to download the export file to the user's computer
-      link.click()
-
-      // Remove the link from the page
-      link.parentNode.removeChild(link)
-
-      dispatch(onExportFinished(format))
     })
-    .catch((error) => {
-      dispatch(onExportFinished(format))
-
-      dispatch(handleError({
-        error,
-        action: 'exportSearch',
-        resource: 'collections',
-        graphQlRequestObject
-      }))
-    })
+    .catch(handleExportSearchError)
 
   return response
 }
