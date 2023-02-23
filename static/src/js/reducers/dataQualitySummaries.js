1d73ad5534a8fd4aad080cfc4a5d9119c9a77115
Ed Olivares <34591886+eudoroolivares2016@users.noreply.github.com>
2023-02-22 21:41:23 -0500

    EDSC-3637: Cleaning up

--- static/src/js/reducers/dataQualitySummaries.js
+++ static/src/js/reducers/dataQualitySummaries.js
@@ -1,7 +1,7 @@
 import { SET_DATA_QUALITY_SUMMARIES } from '../constants/actionTypes'
 
 const initialState = {}
-// TODO: We need to use this to put in the data quality-summary data
+
 const dataQualitySummariesReducer = (state = initialState, action = {}) => {
   switch (action.type) {
     case SET_DATA_QUALITY_SUMMARIES: {
