8b5c5058762757eb911580339902b188b8d17c7a
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2023-02-23 09:29:02 -0500

    EDSC-3612: added doesBucketExist

---
+++ serverless/src/util/aws/doesBucketExist.js
@@ -0,0 +1,13 @@
+/**
+ * @name doesBucketExist
+ * @param {Object} s3 - instance of AWS.S3 from 'aws-sdk'
+ * @param {String} bucketName
+ * @returns {Promise<Boolean>} exists - Promise that resolves to true or false
+ */
+export default async function doesBucketExist(s3, bucketName) {
+    const { Buckets = [] } = await s3.listBuckets().promise()
+
+    const bucketNames = Buckets.map(({ Name }) => Name)
+
+    return bucketNames.includes(bucketName)
+}
