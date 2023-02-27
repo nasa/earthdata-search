7f4541b8e787e773581a4deca14fce2405546a8b
Macy Huang <macyhuang2020@gmail.com>
2023-02-27 17:16:37 -0500

    EDSC-3636: Update user order notification level preferences through cmr-ordering.

---
+++ migrations/1676659585316_change-user-preferences-from-echo-to-cmr.js
@@ -0,0 +1,18 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined
+
+exports.up = (pgm) => {
+  pgm.addColumns('users', {
+    cmr_preferences: {
+      type: 'jsonb',
+      notNull: true,
+      default: '{}'
+    },
+  })
+  pgm.dropColumns('users', ['echo_preferences', 'echo_profile', 'echo_id'])
+
+}
+
+exports.down = (pgm) => {}
+
