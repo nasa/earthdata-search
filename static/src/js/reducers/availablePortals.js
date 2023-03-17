f241e1547658534233574662ea325cd750600419
Matthew Crouch <matthew@element84.com>
2023-03-17 11:07:09 -0400

    EDSC-3546: Updates portal descriptions, PR feedback

--- static/src/js/reducers/availablePortals.js
+++ static/src/js/reducers/availablePortals.js
@@ -1,6 +1,13 @@
-import mergedPortalConfigs from '../../../../portals/mergedPortalConfigs.json'
+let configs = {}
 
-const initialState = mergedPortalConfigs
+try {
+  // eslint-disable-next-line global-require, import/no-unresolved
+  configs = require('../../../../portals/mergedPortalConfigs.json')
+} catch {
+  // mergedPortalConfigs.json does't exist, using an empty object
+}
+
+const initialState = configs
 
 const availablePortalsReducer = (state = initialState, action = {}) => {
   switch (action.type) {
