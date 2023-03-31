4e1f3a75a8624771d18e25b56bff16786d756eef
Matthew Crouch <matthew@element84.com>
2023-03-30 20:53:17 -0400

    EDSC-3546: Adds cypress test for browser button functionality

--- static/src/js/containers/BackButtonContainer/__tests__/BackButtonContainer.test.js
+++ static/src/js/containers/BackButtonContainer/__tests__/BackButtonContainer.test.js
@@ -1,37 +1,6 @@
-import React from 'react'
-import { render } from '@testing-library/react'
-import '@testing-library/jest-dom'
-import { Router } from 'react-router'
-import { Provider } from 'react-redux'
-import { createMemoryHistory } from 'history'
-
-import BackButtonContainer, { mapDispatchToProps } from '../BackButtonContainer'
-
-import configureStore from '../../../store/configureStore'
+import { mapDispatchToProps } from '../BackButtonContainer'
 
 import actions from '../../../actions'
-
-const store = configureStore()
-
-const setup = () => {
-  const history = createMemoryHistory({
-    initialEntries: ['/search']
-  })
-
-  render(
-    <Provider store={store}>
-      <Router history={history}>
-        <BackButtonContainer />
-      </Router>
-    </Provider>
-  )
-
-  return { history }
-}
-
-beforeEach(() => {
-  jest.restoreAllMocks()
-})
 
 describe('mapDispatchToProps', () => {
   test('onChangePath calls actions.changePath', () => {
@@ -42,31 +11,5 @@ describe('mapDispatchToProps', () => {
 
     expect(spy).toBeCalledTimes(1)
     expect(spy).toBeCalledWith('/search')
-  })
-})
-
-// TODO this test isn't activating the history.listen that is setup in the useEffect
-describe.skip('BackButtonContainer', () => {
-  test('calls onChangePath when the history is POPed', async () => {
-    const changePathMock = jest.spyOn(actions, 'changePath').mockImplementation(() => jest.fn())
-
-    const { history } = setup()
-
-    history.push({
-      pathname: '/projects'
-    })
-
-    expect(changePathMock).toHaveBeenCalledTimes(0)
-
-    expect(history.location).toEqual(expect.objectContaining({
-      pathname: '/projects'
-    }))
-
-    history.goBack()
-    expect(changePathMock).toHaveBeenCalledTimes(1)
-
-    expect(history.location).toEqual(expect.objectContaining({
-      pathname: '/search'
-    }))
   })
 })
