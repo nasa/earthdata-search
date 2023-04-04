cd30db882a7d134fb6abe7606acdbf5d187931e7
Trevor Lang <trevorlang@ymail.com>
2023-04-04 16:54:50 -0600

    EDSC-3741: Document usePortalLogo hook

--- static/src/js/hooks/usePortalLogo.js
+++ static/src/js/hooks/usePortalLogo.js
@@ -1,24 +1,36 @@
 import { useCallback, useEffect, useState } from 'react'
 
+/**
+ * Attempts to import a portal logo, if the logo exists, the path is returned. If a logo
+ * does not exist, an empty string is returned.
+ * @prop {String} portalId - The portal id
+ * @returns {String} - The url for a portal image or an empty string
+ */
 const usePortalLogo = (portalId) => {
   const [portalLogoSrc, setPortalLogoSrc] = useState('')
-  let logo = ''
 
   const getPortalLogo = useCallback(async () => {
+    // If a portal id is not provided, do nothing. The default empty string
+    // will be returned from the hook.
     if (!portalId) return
 
     try {
-      logo = await import(`../../../../portals/${portalId}/images/logo.png`)
+      // Attempt to import a portal logo. If one exists, set the state value to
+      // to the url for the image.
+      const logo = await import(`../../../../portals/${portalId}/images/logo.png`)
       const { default: imgSrc } = logo
       setPortalLogoSrc(imgSrc)
     } catch (e) {
+      // If the import fails, set the state to an empty string.
       setPortalLogoSrc('')
     }
   }, [portalId])
 
+  // Set an effect to set the logo url any time the portalId changes.
   useEffect(() => {
     getPortalLogo()
 
+    // Cleanup any in flight imports
     return () => {}
   }, [portalId])
 
