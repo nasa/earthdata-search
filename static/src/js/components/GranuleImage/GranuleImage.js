baaaa9c19bb95056565ade2b5ad4491413229ea7
Ed Olivares <34591886+eudoroolivares2016@users.noreply.github.com>
2023-04-21 16:25:06 -0400

    EDSC-3598: Removing debugging code

--- static/src/js/components/GranuleImage/GranuleImage.js
+++ static/src/js/components/GranuleImage/GranuleImage.js
@@ -14,12 +14,9 @@ import './GranuleImage.scss'
  * @param {String} imageSrc - The image source.
  */
 
-// todo this should just inherit things from the parent container
-
 export const GranuleImage = ({
   imageSrc
 }) => {
-  console.log('🚀 ~ file: GranuleImage.js:23 ~ imageSrc:', imageSrc)
   const [isOpen, setIsOpen] = useState(true)
   const [isLoaded, setIsLoaded] = useState(false)
   const [isLoading, setIsLoading] = useState(true)
@@ -36,46 +33,21 @@ export const GranuleImage = ({
   })
 
   const handleToggleImage = (() => {
-    // todo Set state to the opposite state it is currently in
-    // const [isOpen, setIsOpen] = useState(!isOpen)
-    console.log('🚀 ~ file: GranuleImage.js:43 ~ handleToggleImage ~ isOpen:', isOpen)
-    // const toggleIsOpen = (isOpen) => !isOpen
     setIsOpen(!isOpen)
-    // todo why does this work but, not show up in console?
-    console.log('🚀 ~ file: GranuleImage.js:45 ~ handleToggleImage ~ isOpen:', isOpen)
   })
 
-  // todo set these values here
-
-  // todo functions for managing state switches
   const handleImageLoaded = (() => {
-  // this.setState({
-    //   isLoaded: true,
-    //   isLoading: false,
-    //   isErrored: false
-    // })
-    // const [isLoaded, setIsLoaded] = useState(true)
-    // const [isLoading, setIsLoading] = useState(false)
-    // const [isErrored, setIsErrored] = useState(true)
     setIsLoaded(true)
     setIsLoading(false)
     setIsErrored(false)
   })
 
   const handleImageErrored = (() => {
-    // this.setState({
-    //   isLoaded: false,
-    //   isLoading: false,
-    //   isErrored: true
-    // })
-    // const [isLoaded, setIsLoaded] = useState(false)
-    // const [isLoading, setIsLoading] = useState(false)
-    // const [isErrored, setIsErrored] = useState(true)
     setIsLoaded(false)
     setIsLoading(false)
     setIsErrored(true)
   })
-  // todo wait for the image to load
+
   const loadImage = () => {
     setIsErrored(false)
     setIsLoaded(false)
@@ -135,130 +107,6 @@ export const GranuleImage = ({
     </div>
   )
 }
-
-// class GranuleImage extends Component {
-//   constructor(props) {
-//     super(props)
-
-//     this.state = {
-//       isOpen: true,
-//       isLoaded: false,
-//       isLoading: true,
-//       isErrored: false
-//     }
-
-//     this.handleToggleImage = this.handleToggleImage.bind(this)
-//     this.handleImageLoaded = this.handleImageLoaded.bind(this)
-//     this.handleImageErrored = this.handleImageErrored.bind(this)
-//   }
-
-//   UNSAFE_componentWillReceiveProps(nextProps) {
-//     const { imageSrc } = this.props
-//     const { imageSrc: nextImageSrc } = nextProps
-//     if (nextImageSrc && imageSrc !== nextImageSrc) {
-//       this.setState({
-//         isLoaded: false,
-//         isLoading: true,
-//         isErrored: false
-//       })
-//     }
-//   }
-
-//   handleToggleImage() {
-//     const {
-//       isOpen
-//     } = this.state
-
-//     this.setState({
-//       isOpen: !isOpen
-//     })
-//   }
-
-//   handleImageLoaded() {
-//     this.setState({
-//       isLoaded: true,
-//       isLoading: false,
-//       isErrored: false
-//     })
-//   }
-
-//   handleImageErrored() {
-//     this.setState({
-//       isLoaded: false,
-//       isLoading: false,
-//       isErrored: true
-//     })
-//   }
-
-//   render() {
-//     const {
-//       isErrored,
-//       isLoaded,
-//       isLoading,
-//       isOpen
-//     } = this.state
-
-//     const { imageSrc } = this.props
-
-//     const containerClassName = classNames({
-//       'granule-image': true,
-//       'granule-image--is-open': isOpen
-//     })
-
-//     const imageClassName = classNames({
-//       'granule-image__image': true,
-//       'granule-image__image--is-loaded': isLoaded
-//     })
-
-//     if (!imageSrc) return null
-
-//     return (
-//       <div className={containerClassName}>
-//         {
-//           isOpen ? (
-//             <Button
-//               className="granule-image__button granule-image__button--close"
-//               icon={FaTimes}
-//               onClick={this.handleToggleImage}
-//               label="Close browse image"
-//             />
-//           ) : (
-//             <Button
-//               className="granule-image__button granule-image__button--open"
-//               icon={FaPlus}
-//               onClick={this.handleToggleImage}
-//               label="Open browse image"
-//             />
-//           )
-//         }
-//         <div className="granule-image__container">
-//           {/* eslint-disable-next-line jsx-a11y/img-redundant-alt */}
-//           <img
-//             className={imageClassName}
-//             src={imageSrc}
-//             alt="Browse Image"
-//             onLoad={this.handleImageLoaded}
-//             onError={this.handleImageErrored}
-//           />
-//           {
-//             isLoading && (
-//               <div className="granule-image__placeholder">
-//                 <Spinner type="dots" color="green" size="small" />
-//               </div>
-//             )
-//           }
-//           {
-//             isErrored && (
-//               <div className="granule-image__placeholder">
-//                 <span>Error loading granule image</span>
-//               </div>
-//             )
-//           }
-//         </div>
-//       </div>
-//     )
-//   }
-// }
 
 GranuleImage.propTypes = {
   imageSrc: PropTypes.string.isRequired
--- static/src/js/components/GranuleImage/__tests__/GranuleImage.test.js
+++ static/src/js/components/GranuleImage/__tests__/GranuleImage.test.js
@@ -1,32 +1,21 @@
 import React from 'react'
-import Enzyme, { shallow } from 'enzyme'
-import { Provider } from 'react-redux'
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17'
 import {
-  fireEvent,
   render,
-  screen,
-  waitFor,
+  screen
 } from '@testing-library/react'
 import { act } from 'react-dom/test-utils'
 import userEvent from '@testing-library/user-event'
 import '@testing-library/jest-dom'
 import GranuleImage from '../GranuleImage'
-import configureStore from '../../../store/configureStore'
-
-const store = configureStore()
 
 const setup = (props) => {
   act(() => {
     render(
-      <Provider store={store}>
-        <GranuleImage imageSrc={props.imageSrc} />
-      </Provider>
+      <GranuleImage imageSrc={props.imageSrc} />
     )
   })
 }
-// todo make sure this is actually covering the test case and
-// todo not just passing because it is null
+
 describe('GranuleImage component', () => {
   describe('when no image is present', () => {
     test('renders itself correctly', () => {
@@ -34,12 +23,7 @@ describe('GranuleImage component', () => {
         imageSrc: ''
       }
       setup(props)
-      // const granuleImage = screen.getByRole('GranuleImage')
-      // expect(screen.queryByText('GranuleImage')).toBeNull()
-      // expect(screen.getByRole('img')).not().toHaveClass('granule-image__image')
-      // expect(screen.getByRole('img')).toBeNull()
       expect(screen.queryByRole('button')).toBeNull()
-      // expect(setup(props)).toBeNull()
     })
   })
 
@@ -49,28 +33,20 @@ describe('GranuleImage component', () => {
         imageSrc: '/some/image/src'
       }
       setup(props)
-      // todo should we be using test-ids?
       expect(screen.queryByRole('button')).toBeTruthy()
-      // expect(screen.getByRole('button')).toHaveClass('granule-image__button granule-image__button--close')
-      // expect(screen.getByRole('img')).toHaveClass('granule-image__image')
     })
   })
 })
 
 describe('buttons', () => {
   test('when clicking the close button, closes the image', async () => {
-    const newProps = {
+    const props = {
       imageSrc: '/some/image/src'
     }
     const user = userEvent.setup()
-    setup(newProps)
+    setup(props)
     const closeButton = screen.getByRole('button')
-    // console.log('🐨 ~ file: GranuleImage.test.js:68 ~ test.only ~ closebutton1:', closebutton1)
-    // const closeButton = screen.queryByTestId('granule-image__button granule-image__button--close')
-    console.log('🚀 ~ file: GranuleImage.test.js:68 ~ test ~ closeButton:', closeButton)
-    // todo mock the handle toggleImage
     await user.click(closeButton)
-    // close button is gone and has been toggled so now the only button is the open button
     expect(screen.getByRole('button')).toHaveClass('granule-image__button granule-image__button--open')
   })
 
@@ -84,78 +60,9 @@ describe('buttons', () => {
     const closeButton = screen.getByRole('button')
     await user.click(closeButton)
 
-    // todo now only the open button is in here
     const openButton = screen.getByRole('button')
     expect(screen.getByRole('button')).toHaveClass('granule-image__button granule-image__button--open')
     await user.click(openButton)
-    console.log('🚀 ~ file: GranuleImage.test.js:89 ~ test ~ openButton:', openButton)
     expect(screen.getByRole('button')).toHaveClass('granule-image__button granule-image__button--close')
-    // expect(screen.getByRole('img')).toHaveClass('granule-image__image')
   })
 })
-
-// Enzyme.configure({ adapter: new Adapter() })
-
-// function setup(overrideProps) {
-//   const props = {
-//     imageSrc: '',
-//     ...overrideProps
-//   }
-
-//   const enzymeWrapper = shallow(<GranuleImage {...props} />)
-
-//   return {
-//     enzymeWrapper,
-//     props
-//   }
-// }
-
-// describe('GranuleImage component', () => {
-//   describe('when no image is present', () => {
-//     test('renders itself correctly', () => {
-//       const { enzymeWrapper } = setup()
-
-//       expect(enzymeWrapper.type()).toBe(null)
-//     })
-//   })
-
-//   describe('when an image is present', () => {
-//     test('renders itself correctly', () => {
-//       const { enzymeWrapper } = setup({
-//         imageSrc: '/some/image/src'
-//       })
-
-//       expect(enzymeWrapper.type()).toBe('div')
-//     })
-//   })
-
-//   describe('buttons', () => {
-//     test('when clicking the close button, closes the image', () => {
-//       const { enzymeWrapper } = setup({
-//         imageSrc: '/some/image/src'
-//       })
-
-//       expect(enzymeWrapper.state().isOpen).toEqual(true)
-
-//       enzymeWrapper.find('.granule-image__button').simulate('click')
-
-//       expect(enzymeWrapper.state().isOpen).toEqual(false)
-//     })
-
-//     test('when clicking the open button, closes the image', () => {
-//       const { enzymeWrapper } = setup({
-//         imageSrc: '/some/image/src'
-//       })
-
-//       enzymeWrapper.setState({
-//         isOpen: false
-//       })
-
-//       expect(enzymeWrapper.state().isOpen).toEqual(false)
-
-//       enzymeWrapper.find('.granule-image__button').simulate('click')
-
-//       expect(enzymeWrapper.state().isOpen).toEqual(true)
-//     })
-//   })
-// })
