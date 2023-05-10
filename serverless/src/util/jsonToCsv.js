1b784d16d96a7e0c6ad7dbfd7b7e4ec4dedbda7d
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2023-05-10 10:05:02 -0400

    EDSC-3612: fixed sentence casing in jsonToCsv.js

--- serverless/src/util/jsonToCsv.js
+++ serverless/src/util/jsonToCsv.js
@@ -2,7 +2,7 @@ import { get } from 'sendero'
 import { unparse } from 'papaparse'
 
 /**
- * converts an array of JSON objects to a CSV
+ * Converts an array of JSON objects to a CSV
  * @param {Array} jsonArray - array of JSON objects
  * @param {Array} columns - array of column objects with name and dot path to value, such as [{ name: "Platform", path: "platforms.shortName" }, ...]
  * @returns {String} csv
