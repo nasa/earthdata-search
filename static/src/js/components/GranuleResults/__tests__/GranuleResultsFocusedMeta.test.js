0c6d8651354353681ce02b7f5958deab697b7d71
Trevor Lang <trevorlang@ymail.com>
2023-06-15 17:34:42 -0400

    EDSC-3750: Cleanup

--- static/src/js/components/GranuleResults/__tests__/GranuleResultsFocusedMeta.test.js
+++ static/src/js/components/GranuleResults/__tests__/GranuleResultsFocusedMeta.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { fireEvent, render, screen } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
 
 import GranuleResultsFocusedMeta from '../GranuleResultsFocusedMeta'
