2a33e6a723e6122beca9d3768373891bc56ff864
drewpesall <andrew.t.pesall@nasa.gov>
2023-06-19 22:00:38 -0400

    EDSC-3665 getGoogleMapsApiKey v3 update

--- serverless/src/util/google/__tests__/getGoogleMapsApiKey.test.js
+++ serverless/src/util/google/__tests__/getGoogleMapsApiKey.test.js
@@ -1,29 +1,34 @@
-import AWS from 'aws-sdk'
+import { SecretsManagerClient } from '@aws-sdk/client-secrets-manager'
 
 import { getGoogleMapsApiKey } from '../getGoogleMapsApiKey'
 
+jest.mock('@aws-sdk/client-secrets-manager', () => {
+  const original = jest.requireActual('@aws-sdk/client-secrets-manager')
+  const sendMock = jest.fn().mockReturnValueOnce({
+    SecretString: '{"key":"fake-api-key"}'
+  })
+
+  return {
+    ...original,
+    SecretsManagerClient: jest.fn().mockImplementation(() => ({
+      send: sendMock
+    }))
+  }
+})
+
+const client = new SecretsManagerClient()
+
 describe('getGoogleMapsApiKey', () => {
   test('fetches urs credentials from secrets manager', async () => {
-    const secretsManagerData = jest.fn().mockReturnValue({
-      promise: jest.fn().mockResolvedValue({
-        SecretString: '{"key":"fake-api-key"}'
-      })
-    })
-
-    AWS.SecretsManager = jest.fn()
-      .mockImplementationOnce(() => ({
-        getSecretValue: secretsManagerData
-      }))
-
     const response = await getGoogleMapsApiKey('prod')
 
     expect(response).toEqual({
       key: 'fake-api-key'
     })
 
-    expect(secretsManagerData).toBeCalledTimes(1)
-    expect(secretsManagerData.mock.calls[0]).toEqual([{
-      SecretId: 'GoogleMapsApiKey'
-    }])
+    expect(client.send).toBeCalledTimes(1)
+    expect(client.send).toHaveBeenCalledWith(
+      { SecretId: 'GoogleMapsApiKey' }
+    )
   })
 })
