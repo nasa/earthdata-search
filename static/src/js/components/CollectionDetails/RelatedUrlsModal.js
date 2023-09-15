eb23104a79e482b1156f9698d8e00566c080bbc2
Ed Olivares <34591886+eudoroolivares2016@users.noreply.github.com>
2023-09-15 12:16:16 -0500

    EDSC-3831: Surface variable instance/zarr store information in EDSC (#1664)
    EDSC-3831: Surfacing variable instance information onto EDSC

--- static/src/js/components/CollectionDetails/RelatedUrlsModal.js
+++ static/src/js/components/CollectionDetails/RelatedUrlsModal.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 
 import EDSCModalContainer from '../../containers/EDSCModalContainer/EDSCModalContainer'
@@ -8,68 +8,57 @@ import { pluralize } from '../../util/pluralize'
 
 import './RelatedUrlsModal.scss'
 
-export class RelatedUrlsModal extends Component {
-  constructor(props) {
-    super(props)
-    this.onModalClose = this.onModalClose.bind(this)
-  }
+export const RelatedUrlsModal = ({
+  collectionMetadata,
+  isOpen,
+  onToggleRelatedUrlsModal
+}) => {
+  const { relatedUrls = [] } = collectionMetadata
 
-  onModalClose() {
-    const { onToggleRelatedUrlsModal } = this.props
-    onToggleRelatedUrlsModal(false)
-  }
-
-  render() {
-    const {
-      collectionMetadata,
-      isOpen
-    } = this.props
-
-    const { relatedUrls = [] } = collectionMetadata
-
-    const body = (
-      // eslint-disable-next-line react/jsx-no-useless-fragment
-      <>
-        {
-          relatedUrls && relatedUrls.map((category, i) => {
-            if (category.urls.length) {
-              const key = `modal_related_url_${i}`
-              return (
-                <div key={key} className="related-urls-modal__group">
-                  <h4 className="related-urls-modal__group-title">{pluralize(category.label, category.urls)}</h4>
-                  {
-                  category.urls.map((url, j) => {
-                    const key = `modal_related_url_${i}-${j}`
-                    return (
-                      <ul key={key} className="related-urls-modal__url">
-                        <ArrowTags tags={[url.type, url.subtype]} />
-                        {/* eslint-disable-next-line react/jsx-no-target-blank */}
-                        <a className="related-urls-modal__link" href={url.url} target="_blank">{url.url}</a>
-                      </ul>
-                    )
-                  })
-                }
-                </div>
-              )
-            }
-            return null
-          })
+  const body = (
+    // eslint-disable-next-line react/jsx-no-useless-fragment
+    <>
+      {
+        relatedUrls && relatedUrls.map((category, i) => {
+          if (category.urls.length) {
+            const key = `modal_related_url_${i}`
+            return (
+              <div key={key} className="related-urls-modal__group">
+                <h4 className="related-urls-modal__group-title">{pluralize(category.label, category.urls)}</h4>
+                {
+                category.urls.map((url, j) => {
+                  const key = `modal_related_url_${i}-${j}`
+                  return (
+                    <ul key={key} className="related-urls-modal__url">
+                      <ArrowTags tags={[url.type, url.subtype]} />
+                      {/* eslint-disable-next-line react/jsx-no-target-blank */}
+                      <a className="related-urls-modal__link" href={url.url} target="_blank">{url.url}</a>
+                    </ul>
+                  )
+                })
+              }
+              </div>
+            )
+          }
+          return null
+        })
         }
-      </>
-    )
+    </>
+  )
 
-    return (
-      <EDSCModalContainer
-        className="related-urls"
-        title="Related URLs"
-        isOpen={isOpen}
-        id="related-urls"
-        size="lg"
-        onClose={this.onModalClose}
-        body={body}
-      />
-    )
-  }
+  return (
+    <EDSCModalContainer
+      className="related-urls"
+      title="Related URLs"
+      isOpen={isOpen}
+      id="related-urls"
+      size="lg"
+      onClose={() => {
+        onToggleRelatedUrlsModal(false)
+      }}
+      body={body}
+    />
+  )
 }
 
 RelatedUrlsModal.propTypes = {
---
+++ static/src/js/components/CollectionDetails/__tests__/RelatedUrlsModal.test.js
@@ -0,0 +1,207 @@
+import React from 'react'
+import {
+  act,
+  render,
+  screen
+} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import '@testing-library/jest-dom'
+
+import RelatedUrlsModal from '../RelatedUrlsModal'
+
+const setup = (overrides) => {
+  const {
+    overrideMetadata = {},
+    overrideProps = {}
+  } = overrides || {}
+
+  const onToggleRelatedUrlsModal = jest.fn()
+
+  const props = {
+    collectionMetadata: {
+      hasAllMetadata: true,
+      dataCenters: [],
+      directDistributionInformation: {},
+      scienceKeywords: [],
+      nativeDataFormats: [],
+      relatedUrls: [
+        {
+          content_type: 'CollectionURL',
+          label: 'Collection URL',
+          urls: [
+            {
+              description: 'Data set Landing Page DOI URL',
+              urlContentType: 'CollectionURL',
+              type: 'DATA SET LANDING PAGE',
+              url: 'https://doi.org/10.3334/ORNLDAAC/830',
+              subtype: ''
+            }
+          ]
+        },
+        {
+          content_type: 'DistributionURL',
+          label: 'Distribution URL',
+          urls: [
+            {
+              description: 'This link allows direct data access via Earthdata login',
+              urlContentType: 'DistributionURL',
+              type: 'GET DATA',
+              url: 'https://daac.ornl.gov/daacdata/global_climate/global_N_deposition_maps/',
+              subtype: ''
+            },
+            {
+              description: 'Web Coverage Service for this collection.',
+              urlContentType: 'DistributionURL',
+              type: 'USE SERVICE API',
+              subtype: 'WEB COVERAGE SERVICE (WCS)',
+              url: 'https://webmap.ornl.gov/wcsdown/dataset.jsp?ds_id=830',
+              get_service: {
+                mimeType: 'application/gml+xml',
+                protocol: 'Not provided',
+                fullName: 'Not provided',
+                dataId: 'NotProvided',
+                dataType: 'Not provided'
+              }
+            }
+          ]
+        },
+        {
+          content_type: 'PublicationURL',
+          label: 'Publication URL',
+          urls: [
+            {
+              description: 'ORNL DAAC Data Set Documentation',
+              urlContentType: 'PublicationURL',
+              type: 'VIEW RELATED INFORMATION',
+              subtype: 'GENERAL DOCUMENTATION',
+              url: 'https://daac.ornl.gov/CLIMATE/guides/global_N_deposition_maps.html'
+            },
+            {
+              description: 'Data Set Documentation',
+              urlContentType: 'PublicationURL',
+              type: 'VIEW RELATED INFORMATION',
+              subtype: 'GENERAL DOCUMENTATION',
+              url: 'https://daac.ornl.gov/daacdata/global_climate/global_N_deposition_maps/comp/deposition_maps.jpg'
+            },
+            {
+              description: 'Data Set Documentation',
+              urlContentType: 'PublicationURL',
+              type: 'VIEW RELATED INFORMATION',
+              subtype: 'GENERAL DOCUMENTATION',
+              url: 'https://daac.ornl.gov/daacdata/global_climate/global_N_deposition_maps/comp/global_N_deposition_maps.pdf'
+            },
+            {
+              description: 'Data Set Documentation',
+              urlContentType: 'PublicationURL',
+              type: 'VIEW RELATED INFORMATION',
+              subtype: 'GENERAL DOCUMENTATION',
+              url: 'https://daac.ornl.gov/daacdata/global_climate/global_N_deposition_maps/comp/global_N_deposition_maps_readme.pdf'
+            }
+          ]
+        },
+        {
+          content_type: 'VisualizationURL',
+          label: 'Visualization URL',
+          urls: [
+            {
+              description: 'Browse Image',
+              urlContentType: 'VisualizationURL',
+              type: 'GET RELATED VISUALIZATION',
+              url: 'https://daac.ornl.gov/graphics/browse/sdat-tds/830_1_fit.png',
+              subtype: ''
+            }
+          ]
+        },
+        {
+          content_type: 'HighlightedURL',
+          label: 'Highlighted URL',
+          urls: [
+            {
+              description: 'Data set Landing Page DOI URL',
+              urlContentType: 'CollectionURL',
+              type: 'DATA SET LANDING PAGE',
+              url: 'https://doi.org/10.3334/ORNLDAAC/830',
+              highlightedType: 'Data Set Landing Page'
+            }
+          ]
+        }
+      ],
+      ...overrideMetadata
+    },
+    isOpen: true,
+    onToggleRelatedUrlsModal,
+    ...overrideProps
+  }
+  act(() => {
+    render(<RelatedUrlsModal {...props} />)
+  })
+  return {
+    onToggleRelatedUrlsModal
+  }
+}
+
+describe('CollectionDetailsBody component', () => {
+  describe('when the relatedUrls Modal has relatedUrls', () => {
+    test('calls leaflet to render map with correct props', () => {
+      setup()
+
+      expect(screen.getByText('Related URLs')).toBeInTheDocument()
+    })
+  })
+
+  describe('when the isOpen prop for the Modal is off', () => {
+    test('calls leaflet to render map with correct props', () => {
+      setup({
+        overrideProps: {
+          isOpen: false
+        }
+      })
+
+      expect(screen.queryByText('Related URLs')).not.toBeInTheDocument()
+
+      expect(screen.queryAllByText('Related URLs').length).toEqual(0)
+    })
+  })
+
+  describe('when the related urls is empty', () => {
+    test('calls leaflet to render map with correct props', () => {
+      setup({
+        overrideMetadata: {
+          relatedUrls: []
+        }
+      })
+
+      const collectionDetailsrelatedUrlsList = screen.queryAllByRole('list')
+
+      expect(collectionDetailsrelatedUrlsList.length).toEqual(0)
+    })
+  })
+
+  describe('when the related urls is null', () => {
+    test('calls leaflet to render map with correct props', () => {
+      setup({
+        overrideMetadata: {
+          relatedUrls: null
+        }
+      })
+
+      const collectionDetailsrelatedUrlsList = screen.queryAllByRole('list')
+
+      expect(collectionDetailsrelatedUrlsList.length).toEqual(0)
+    })
+
+    describe('when the clicking the close button on the modal', () => {
+      test('calls the `onToggleRelatedUrlsModal` function with false', async () => {
+        const { onToggleRelatedUrlsModal } = setup()
+
+        userEvent.setup()
+
+        const closeButton = screen.getByRole('button')
+        await userEvent.click(closeButton)
+
+        expect(screen.getByText('Related URLs')).toBeInTheDocument()
+        expect(onToggleRelatedUrlsModal).toHaveBeenCalledTimes(1)
+      })
+    })
+  })
+})
