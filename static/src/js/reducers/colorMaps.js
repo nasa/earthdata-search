66dab786e1f626f50c8bb41112df52bfe094cf19
Benjamin Poreh <bnp26@case.edu>
2023-09-19 10:20:46 -0400

    EDSC-3880: just fixing some eslinting

--- static/src/js/reducers/colorMaps.js
+++ static/src/js/reducers/colorMaps.js
@@ -1,54 +1,53 @@
 import {
-    ERRORED_COLOR_MAPS,
-    SET_COLOR_MAPS_LOADING,
-    SET_COLOR_MAPS_LOADED
-  } from '../constants/actionTypes'
-  
-  const initialState = {}
-  
-  const colorMapsReducer = (state = initialState, action = {}) => {
-    switch (action.type) {
-      case ERRORED_COLOR_MAPS: {
-        const { product } = action.payload
-        return {
-          ...state,
-          [product]: {
-            ...state[product],
-            isLoading: false,
-            isLoaded: false,
-            isErrored: true,
-            jsondata: {}
-          }
+  ERRORED_COLOR_MAPS,
+  SET_COLOR_MAPS_LOADING,
+  SET_COLOR_MAPS_LOADED
+} from '../constants/actionTypes'
+
+const initialState = {}
+
+const colorMapsReducer = (state = initialState, action = {}) => {
+  switch (action.type) {
+    case ERRORED_COLOR_MAPS: {
+      const { product } = action.payload
+      return {
+        ...state,
+        [product]: {
+          ...state[product],
+          isLoading: false,
+          isLoaded: false,
+          isErrored: true,
+          jsondata: {}
         }
       }
-      case SET_COLOR_MAPS_LOADING: {
-        const { product } = action.payload
-        return {
-          ...state,
-          [product]: {
-            isLoading: true,
-            isLoaded: false,
-            isErrored: false,
-            jsondata: {}
-          }
-        }
-      }
-      case SET_COLOR_MAPS_LOADED: {
-        const { product, jsondata } = action.payload
-        return {
-          ...state,
-          [product]: {
-            isLoading: false,
-            isLoaded: true,
-            isErrored: false,
-            jsondata
-          }
-        }
-      }
-      default:
-        return state
     }
+    case SET_COLOR_MAPS_LOADING: {
+      const { product } = action.payload
+      return {
+        ...state,
+        [product]: {
+          isLoading: true,
+          isLoaded: false,
+          isErrored: false,
+          jsondata: {}
+        }
+      }
+    }
+    case SET_COLOR_MAPS_LOADED: {
+      const { product, jsondata } = action.payload
+      return {
+        ...state,
+        [product]: {
+          isLoading: false,
+          isLoaded: true,
+          isErrored: false,
+          jsondata
+        }
+      }
+    }
+    default:
+      return state
   }
-  
-  export default colorMapsReducer
-  
+}
+
+export default colorMapsReducer
