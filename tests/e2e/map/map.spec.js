609adbb9d0859721cafe00a3b273c6af1c5eceec
Benjamin Poreh <bnp26@case.edu>
2023-09-21 10:11:51 -0400

    EDSC-3880: fixed up some of the tests and fixed up focusedCollection so it doesn't fail when there are no gibs tags.

--- tests/e2e/map/map.spec.js
+++ tests/e2e/map/map.spec.js
@@ -1629,7 +1629,7 @@ test.describe('Map interactions', () => {
         })
       })
 
-      await page.route(/colormaps\/GHRSST_Sea_Surface_Temperature/, async (route) => {
+      await page.route(/colormaps\/GHRSST_Sea_Ice_Concentration/, async (route) => {
         await route.fulfill({
           json: colormapOneBody
         })
