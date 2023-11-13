02865e6457f2588b5f8c790f311baaeb9c3e6f6a
Johnathan Rush <10775395+rushgeo@users.noreply.github.com>
2023-11-13 15:44:11 -0500

    EDSC-3886: reorder imports

--- static/src/js/components/CollectionResults/__tests__/CollectionResultsItem.test.js
+++ static/src/js/components/CollectionResults/__tests__/CollectionResultsItem.test.js
@@ -3,11 +3,11 @@ import Enzyme, { shallow } from 'enzyme'
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17'
 import {
   FaClock,
+  FaCubes,
   FaFileAlt,
   FaGlobe,
   FaSlidersH,
-  FaTags,
-  FaCubes
+  FaTags
 } from 'react-icons/fa'
 
 import { collectionListItemProps } from './mocks'
