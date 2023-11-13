414c59ef5aca82f5579655fb644553628981f546
Benjamin Poreh <bnp26@case.edu>
2023-11-13 12:50:14 -0500

    EDSC-3797: changed the wording on the description of the concatenation test for constructing the orderPayload

--- serverless/src/submitHarmonyOrder/__tests__/constructOrderPayload.test.js
+++ serverless/src/submitHarmonyOrder/__tests__/constructOrderPayload.test.js
@@ -694,7 +694,7 @@ describe('constructOrderPayload', () => {
     })
   })
 
-  describe('when only sending concatenate is true', () => {
+  describe('when only concatenation is supported', () => {
     test('constructs a payload containing supportsConcatenation = true and enableConcatenateDownload = true', async () => {
       nock(/cmr/)
         .matchHeader('Authorization', 'Bearer access-token')
