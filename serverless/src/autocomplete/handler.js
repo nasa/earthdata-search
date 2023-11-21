483f21602747508929aa4c1d6f9558d74cc462a6
Ed Olivares <34591886+eudoroolivares2016@users.noreply.github.com>
2023-11-21 17:19:34 -0500

    EDSC-3925: Remove the rest of the spatial-autocomplete code

--- serverless/src/autocomplete/handler.js
+++ serverless/src/autocomplete/handler.js
@@ -1,97 +1,9 @@
-import axios from 'axios'
-
 import { buildParams } from '../util/cmr/buildParams'
 import { determineEarthdataEnvironment } from '../util/determineEarthdataEnvironment'
 import { doSearchRequest } from '../util/cmr/doSearchRequest'
-import { getApplicationConfig, getEnvironmentConfig } from '../../../sharedUtils/config'
+import { getApplicationConfig } from '../../../sharedUtils/config'
 import { getJwtToken } from '../util/getJwtToken'
 import { parseError } from '../../../sharedUtils/parseError'
-
-/**
- * Search the Nominatim OpenStreetMaps API endpoint
- * @param {query} query The spatial query provided from the user
- */
-const nominatimGeocode = async (query) => {
-  // Nominatim terms require that we provide a referrer
-  const { edscHost } = getEnvironmentConfig()
-
-  const { geocodingIncludePolygons } = process.env
-
-  // Default query parameters
-  const queryParams = {
-    format: 'json',
-    extratags: 1,
-    dedupe: 1,
-    q: query
-  }
-
-  // Only request geojson if polygons are enabled
-  if (geocodingIncludePolygons === 'true') {
-    queryParams.polygon_geojson = 1
-  }
-
-  const geocodeResult = await axios.get('https://nominatim.openstreetmap.org/search.php', {
-    params: queryParams,
-    headers: {
-      Referer: edscHost
-    }
-  })
-
-  const { data } = geocodeResult
-
-  const formattedResult = data.map((place) => {
-    const {
-      boundingbox,
-      display_name: displayName,
-      geojson,
-      lat,
-      lon
-    } = place
-
-    const boundingBox = [
-      parseFloat(boundingbox[2]),
-      parseFloat(boundingbox[0]),
-      parseFloat(boundingbox[3]),
-      parseFloat(boundingbox[1])
-    ]
-
-    const spatialResponse = {
-      name: displayName,
-      point: [
-        lat,
-        lon
-      ],
-      bounding_box: boundingBox
-    }
-
-    // If geojson was returned, return it as 'polygon'
-    if (geojson) {
-      spatialResponse.polygon = geojson
-    }
-
-    return spatialResponse
-  })
-
-  return formattedResult
-}
-
-/**
- * Geocode the user query based on the configured geocoding service
- * @param {query} query The spatial query provided from the user
- */
-const geocode = (query, earthdataEnvironment) => {
-  const { geocodingService } = process.env
-
-  console.log(`Geocoding '${query}' with ${geocodingService}`)
-
-  if (geocodingService === 'nominatim') {
-    return nominatimGeocode(query, earthdataEnvironment)
-  }
-
-  // Not setting a geocoding service should result in an
-  // empty response by default
-  throw new Error(`Geocoder (${geocodingService}) not supported`)
-}
 
 /**
  * Perform a search across all endpoints for autocompletion
