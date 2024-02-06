bd889408e70ea8a48c865385432f9d2c4224166f
Benjamin Poreh <bnp26@case.edu>
2024-02-06 14:58:31 -0500

    EDSC-3607: small fix

--- static/src/js/components/SpatialDisplay/__tests__/SpatialDisplay.test.js
+++ static/src/js/components/SpatialDisplay/__tests__/SpatialDisplay.test.js
@@ -1,10 +1,6 @@
 import React from 'react'
 
-import {
-  act,
-  render,
-  screen
-} from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 
 import userEvent from '@testing-library/user-event'
 
