55e50f34258faca2d3f9bc8502d916cb7cb3ab56
Ed Olivares <34591886+eudoroolivares2016@users.noreply.github.com>
2024-02-26 15:38:07 -0500

    EDSC-3932: Adding binary content types for the API gateway

--- static/src/js/components/CollectionResults/CollectionResultsItem.js
+++ static/src/js/components/CollectionResults/CollectionResultsItem.js
@@ -67,6 +67,7 @@ export const CollectionResultsItem = forwardRef(({
     thumbnail,
     versionId
   } = collectionMetadata
+  console.log('🚀 ~ file: CollectionResultsItem.js:75 ~ thumbnail:', thumbnail)
 
   const [loadingThumbnail, setLoadingThumbnail] = useState(true)
   const { thumbnailSize } = getApplicationConfig()
