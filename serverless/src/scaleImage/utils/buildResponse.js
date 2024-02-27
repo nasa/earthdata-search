fc653b8e21a51f1cc31fd3cc50302bb86d6c2907
Ed Olivares <34591886+eudoroolivares2016@users.noreply.github.com>
2024-02-26 19:04:13 -0500

    EDSC-3932: Setting lambda function and media types

--- serverless/src/scaleImage/utils/buildResponse.js
+++ serverless/src/scaleImage/utils/buildResponse.js
@@ -4,17 +4,18 @@
  * @param {Integer>} statusCode Status code to return with the response
  * @returns {JSON} Constructed response object with image as a base64 string
  */
+// eslint-disable-next-line no-unused-vars
 export const buildResponse = (imageBuffer, statusCode = 200) => {
-  console.log('🚀 Building the response object back')
   const base64Image = imageBuffer.toString('base64')
 
+  // Client must send an Accept header who's 1st media type matches what you have set as a binary media types in API Gateway
   return {
-    isBase64Encoded: true,
     statusCode,
     headers: {
-      'Content-Type': 'image/png',
+      'Content-type': 'image/png',
       'Access-Control-Allow-Origin': '*'
     },
-    body: base64Image
+    body: base64Image,
+    isBase64Encoded: true
   }
 }
