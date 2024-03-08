3f5404ca63f216e47a04723bb048442428caec46
Benjamin Poreh <bnp26@case.edu>
2024-03-08 17:34:13 -0500

    EDSC-4002: changed up the calls to have variables in them now.

--- tests/e2e/map/map.spec.js
+++ tests/e2e/map/map.spec.js
@@ -1066,7 +1066,7 @@ test.describe('Map interactions', () => {
 
         const urlValues = {
           chromium: 'search?lat=13.999032615512775',
-          firefox: 'search?lat=13.99892766713073',
+          firefox: 'search?lat=13.999207532962274',
           webkit: 'search?lat=13.999032615512775'
         }
         await expect(page).toHaveURL(urlValues[browser])
