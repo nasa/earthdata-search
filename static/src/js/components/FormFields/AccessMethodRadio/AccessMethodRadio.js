8a5fd4a5b4a7474aed1499821d5d242a63b93669
Johnathan Rush <10775395+rushgeo@users.noreply.github.com>
2024-03-12 15:31:10 -0400

    EDSC-3966: fix icon position

--- static/src/js/components/FormFields/AccessMethodRadio/AccessMethodRadio.js
+++ static/src/js/components/FormFields/AccessMethodRadio/AccessMethodRadio.js
@@ -64,7 +64,7 @@ export const AccessMethodRadio = ({
             hasFormats,
             hasTemporalSubsetting,
             hasCombine,
-            false
+            true
           )
         )
       }
