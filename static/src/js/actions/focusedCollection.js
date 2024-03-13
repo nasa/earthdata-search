6c220cce6b93391df7b65a61d9a933425ef9a206
Johnathan Rush <10775395+rushgeo@users.noreply.github.com>
2024-03-13 16:14:18 -0400

    EDSC-3966: attempt to fix cypress test

--- static/src/js/actions/focusedCollection.js
+++ static/src/js/actions/focusedCollection.js
@@ -129,6 +129,7 @@ export const getFocusedCollection = () => async (dispatch, getState) => {
           count
           items {
             conceptId
+            description
             longName
             name
             type
