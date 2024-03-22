2591f3cacf90ee92fd7abe614fed837d09cf4ef2
Benjamin Poreh <bnp26@case.edu>
2024-03-22 14:55:44 -0400

    EDSC-4020: fixed eslint issues

--- static/src/js/actions/__tests__/colorMaps.test.js
+++ static/src/js/actions/__tests__/colorMaps.test.js
@@ -2,13 +2,10 @@ import nock from 'nock'
 import configureMockStore from 'redux-mock-store'
 import thunk from 'redux-thunk'
 
-import actions from '..'
-
 import {
   SET_COLOR_MAPS_LOADED,
   SET_COLOR_MAPS_LOADING,
-  ERRORED_COLOR_MAPS,
-  ADD_ERROR
+  ERRORED_COLOR_MAPS
 } from '../../constants/actionTypes'
 
 import {
@@ -59,8 +56,6 @@ describe('setColorMapsLoading', () => {
 
 describe('getColorMaps with error', () => {
   test('should call all the SET_COLOR_MAPS_LOADING and ERRORED_COLOR_MAPS actions when there is an error', async () => {
-    const handleErrorMock = jest.spyOn(actions, 'handleError')
-
     nock(/localhost/)
       .get(/colormaps\/AIRS_Prata_SO2_Index_Day/)
       .reply(500)
@@ -71,8 +66,6 @@ describe('getColorMaps with error', () => {
 
     const store = mockStore({})
     const product = 'AIRS_Prata_SO2_Index_Day'
-
-    const consoleMock = jest.spyOn(console, 'error').mockImplementationOnce(() => jest.fn())
 
     await store.dispatch(getColorMap({ product })).then(() => {
       const storeActions = store.getActions()
