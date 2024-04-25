8585d2e6df0348cd8d07acdc05ad94038b245506
Ed Olivares <34591886+eudoroolivares2016@users.noreply.github.com>
2024-04-25 16:40:42 -0400

    EDSC-3932: Fix loader for svg file type

--- serverless/src/scaleImage/utils/sharp/buildUnavailableImageBuffer.js
+++ serverless/src/scaleImage/utils/sharp/buildUnavailableImageBuffer.js
@@ -1,18 +1,18 @@
 import sharp from 'sharp'
 import { resizeImage } from './resizeImage'
 
+import notFoundAsset from '../../../../../static/src/assets/images/image-unavailable.svg'
+
 /**
  * No image available? This will pull the svg file and return it as a Buffer
  * @return {Buffer<Image>} This is what you show the user when an image cannot be found or resized
  */
 export const buildUnavailableImageBuffer = async (height = null, width = null) => {
-  // Const noFoundAsset = '../../../../../static/src/assets/images/image-unavailable.svg'
-  const noFoundAsset = 'static/src/assets/images/image-unavailable.svg'
   if (height || width) {
-    return resizeImage(noFoundAsset, height, width)
+    return resizeImage(notFoundAsset, height, width)
   }
 
-  const notFound = await sharp(noFoundAsset)
+  const notFound = await sharp(notFoundAsset)
     .toFormat('png')
     .toBuffer()
 
