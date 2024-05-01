704214c601624af10d8a12427039f0d9c78e778f
Ed Olivares <34591886+eudoroolivares2016@users.noreply.github.com>
2024-05-01 16:39:15 -0400

    EDSC-3932: fix system token calls

--- serverless/src/scaleImage/utils/cmr/fetchCmrConcept.js
+++ serverless/src/scaleImage/utils/cmr/fetchCmrConcept.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 import { getEarthdataConfig } from '../../../../../sharedUtils/config'
-// Import { getSystemToken } from '../../../util/urs/getSystemToken'
+import { getSystemToken } from '../../../util/urs/getSystemToken'
 import { requestTimeout } from '../../../util/requestTimeout'
+import { deployedEnvironment } from '../../../../../sharedUtils/deployedEnvironment'
 
 /**
  * Retrieve the CMR metadata for the provided concept id
@@ -11,16 +12,24 @@ import { requestTimeout } from '../../../util/requestTimeout'
  */
 export const fetchCmrConcept = async (conceptId, earthdataEnvironment) => {
   const headers = {}
+  const DeployedEnv = deployedEnvironment()
+  console.log('🚀 ~ file: fetchCmrConcept.js:16 ~ fetchCmrConcept ~ DeployedEnv:', DeployedEnv)
+
   // TODO This value with ee params is not getting updated to the correct endpoint
   // const earthdataEnvironment = determineEarthdataEnvironment()
   console.log('🚀 ~ file: fetchCmrConcept.js:15 ~ fetchCmrConcept ~ earthdataEnvironment:', earthdataEnvironment)
   const ee = getEarthdataConfig(earthdataEnvironment)
   console.log('🚀 ~ file: fetchCmrConcept.js:16 ~ fetchCmrConcept ~ ee:', ee)
 
-  // If (!process.env.IS_OFFLINE) {
-  //   const cmrToken = await getSystemToken()
-  //   headers.Authorization = `${cmrToken}`
-  // }
+  const retrieveSystemToken = earthdataEnvironment === deployedEnvironment()
+  console.log('🚀 ~ file: fetchCmrConcept.js:25 ~ fetchCmrConcept ~ retrieveSystemToken:', retrieveSystemToken)
+
+  // Do not retrieve system token if offline or using the `ee` params to query other CMR envs
+  if (!process.env.IS_OFFLINE && (retrieveSystemToken)) {
+    console.log('Retrieving System token for collection image')
+    const cmrToken = await getSystemToken()
+    headers.Authorization = `${cmrToken}`
+  }
 
   const conceptUrl = `${getEarthdataConfig(earthdataEnvironment).cmrHost}/search/concepts/${conceptId}.json`
   try {
--- serverless/src/scaleImage/utils/sharp/buildUnavailableImageBuffer.js
+++ serverless/src/scaleImage/utils/sharp/buildUnavailableImageBuffer.js
@@ -1,13 +1,13 @@
 import sharp from 'sharp'
 import { resizeImage } from './resizeImage'
 
-import notFoundAsset from '../../../../../static/src/assets/images/image-unavailable.svg'
-
 /**
  * No image available? This will pull the svg file and return it as a Buffer
  * @return {Buffer<Image>} This is what you show the user when an image cannot be found or resized
  */
 export const buildUnavailableImageBuffer = async (height = null, width = null) => {
+  console.log('unavailable is being called and its in a problem state🛑 ')
+  const notFoundAsset = 'static/src/assets/images/image-unavailable.svg'
   if (height || width) {
     return resizeImage(notFoundAsset, height, width)
   }
