ae23fb4dd2fd7e647e1266aad4dcef9bc2a0478c
Brandon Corbett <bccorb1000@gmail.com>
2024-06-03 10:17:18 -0400

    EDSC-4055: Add Performance testing to CI/CD

--- tests/e2e/performace/performance.spec.js
+++ tests/e2e/performace/performance.spec.js
@@ -1,8 +1,26 @@
-import { test } from 'playwright-test-coverage'
+import { test, expect } from 'playwright-test-coverage'
 
-test('Capture homepage load times', async ({ page }) => {
+test('Search page load time is less than 1 second', async ({ page }) => {
   const requestFinishedPromise = page.waitForEvent('requestfinished')
   await page.goto('/search')
   const request = await requestFinishedPromise
-  console.log(request.timing())
+
+  expect(request.timing().responseEnd < 1000).toBe(true)
+})
+
+test('Search page LCP start time is less than 1 second', async ({ page }) => {
+  await page.goto('/search')
+
+  const LCP = await page.evaluate(() => new Promise((resolve) => {
+    new PerformanceObserver((list) => {
+      const entries = list.getEntries()
+      const lcp = entries.at(-1)
+      resolve(lcp.startTime)
+    }).observe({
+      type: 'largest-contentful-paint',
+      buffered: true
+    })
+  }))
+
+  expect(LCP < 1000).toBe(true)
 })
