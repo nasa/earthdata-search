7d3ee7811e8966c47575276516cafe8f8c8f2b74
Brandon Corbett <bccorb1000@gmail.com>
2024-06-04 14:53:41 -0400

    EDSC-4055: Adding mocks

--- tests/e2e/performace/performance.spec.js
+++ tests/e2e/performace/performance.spec.js
@@ -1,26 +1,44 @@
 import { test, expect } from 'playwright-test-coverage'
 
-test('Search page load time is less than 1 second', async ({ page }) => {
-  const requestFinishedPromise = page.waitForEvent('requestfinished')
-  await page.goto('/search')
-  const request = await requestFinishedPromise
+import singleCollection from './__mocks__/single_collection.json'
 
-  expect(request.timing().responseEnd < 5000).toBe(true)
-})
 
-test('Search page LCP start time is less than 1 second', async ({ page }) => {
-  await page.goto('/search')
-
-  const LCP = await page.evaluate(() => new Promise((resolve) => {
-    new PerformanceObserver((list) => {
-      const entries = list.getEntries()
-      const lcp = entries.at(-1)
-      resolve(lcp.startTime)
-    }).observe({
-      type: 'largest-contentful-paint',
-      buffered: true
+test.describe('Collection List Behavior', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.route('**/*.{png,jpg,jpeg}', (route) => route.abort())
+    await page.route(/collections/, async (route) => {
+      await route.fulfill({
+        json: singleCollection.body,
+        headers: singleCollection.headers
+      })
     })
-  }))
 
-  expect(LCP < 5000).toBe(true)
+    await page.goto('/search')
+  })
+
+
+  test('Search page load time is less than 1 second', async ({ page }) => {
+    const requestFinishedPromise = page.waitForEvent('requestfinished')
+    await page.goto('/search')
+    const request = await requestFinishedPromise
+
+    expect(request.timing().responseEnd < 5000).toBe(true)
+  })
+
+  test('Search page LCP start time is less than 1 second', async ({ page }) => {
+    await page.goto('/search')
+
+    const LCP = await page.evaluate(() => new Promise((resolve) => {
+      new PerformanceObserver((list) => {
+        const entries = list.getEntries()
+        const lcp = entries.at(-1)
+        resolve(lcp.startTime)
+      }).observe({
+        type: 'largest-contentful-paint',
+        buffered: true
+      })
+    }))
+
+    expect(LCP < 5000).toBe(true)
+  })
 })
