4c1cd6b94c34d389eebe2c5d87dcace507aa05c6
Ed Olivares <34591886+eudoroolivares2016@users.noreply.github.com>
2024-06-18 13:18:25 -0400

    EDSC-3938: trying adding playwright test

---
+++ tests/e2e/preferences/preferences.spec.js
@@ -0,0 +1,84 @@
+import { test, expect } from 'playwright-test-coverage'
+
+import collectionsSearchBody from './__mocks__/collectionsSearch.body.json'
+import commonHeaders from './__mocks__/common.headers.json'
+import getCollectionsGraphQlBody from './__mocks__/getCollectionsGraphql.body.json'
+import getSubscriptionsGraphQlBody from './__mocks__/getSubscriptions.graphql.body.json'
+import granulesBody from './__mocks__/granules.body.json'
+import granulesGraphQlBody from './__mocks__/granulesGraphql.body.json'
+import graphQlHeaders from './__mocks__/graphql.headers.json'
+import timeline from './__mocks__/timeline.json'
+
+import { graphQlGetSubscriptionsQuery } from '../../support/graphQlGetSubscriptionsQuery'
+import { graphQlGetCollection } from '../../support/graphQlGetCollection'
+import { graphQlGetProjectCollections } from '../../support/graphQlProjectGetCollections'
+import { login } from '../../support/login'
+
+const conceptId = 'C1214470488-ASF'
+
+test.describe('preferences', () => {
+  test.beforeEach(async ({ page, context }) => {
+    const granuleHits = 1
+
+    await page.route(/collections/, async (route) => {
+      await route.fulfill({
+        json: collectionsSearchBody,
+        headers: {
+          ...commonHeaders,
+          'cmr-hits': '7648'
+        }
+      })
+    })
+
+    await page.route(/granules$/, async (route) => {
+      await route.fulfill({
+        json: granulesBody,
+        headers: {
+          ...commonHeaders,
+          'cmr-hits': granuleHits.toString()
+        }
+      })
+    })
+
+    await page.route(/timeline/, async (route) => {
+      await route.fulfill({
+        json: timeline.body
+      })
+    })
+
+    await page.route(/graphql/, async (route) => {
+      const { query } = JSON.parse(route.request().postData()).data
+
+      if (query === graphQlGetSubscriptionsQuery) {
+        await route.fulfill({
+          json: getSubscriptionsGraphQlBody,
+          headers: graphQlHeaders
+        })
+      }
+
+      if (query === JSON.parse(graphQlGetCollection(conceptId)).query) {
+        await route.fulfill({
+          json: granulesGraphQlBody,
+          headers: graphQlHeaders
+        })
+      }
+
+      if (query === JSON.parse(graphQlGetProjectCollections(conceptId)).query) {
+        await route.fulfill({
+          json: getCollectionsGraphQlBody,
+          headers: graphQlHeaders
+        })
+      }
+    })
+
+    await login(context)
+
+    await page.goto('/preferences')
+  })
+
+  test.describe('preferences page', () => {
+    test('loads the correct context', async ({ page }) => {
+      await expect(page.getByText('Preferences')).toBeVisible()
+    })
+  })
+})
