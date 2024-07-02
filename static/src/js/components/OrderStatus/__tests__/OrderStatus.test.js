48b35255aaf433ab6a2e20eb5812db7eb462f65b
Ed Olivares <34591886+eudoroolivares2016@users.noreply.github.com>
2024-07-02 07:07:11 -0700

    EDSC-4134: Harmony Order status not updating when harmony is progressing with errors
    * EDSC-4134: Adding `running_with_errors` state for Harmony orders
    
    * EDSC-4134: rewrite `static/src/js/components/OrderStatus/__tests__/OrderStatus.test.js` with passing RTL tests

--- static/src/js/components/OrderStatus/__tests__/OrderStatus.test.js
+++ static/src/js/components/OrderStatus/__tests__/OrderStatus.test.js
@@ -1,18 +1,21 @@
 import React from 'react'
 import { Provider } from 'react-redux'
-import Enzyme, { mount } from 'enzyme'
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17'
+import {
+  render,
+  screen,
+  waitFor,
+  within
+} from '@testing-library/react'
+import '@testing-library/jest-dom'
+import userEvent from '@testing-library/user-event'
+
 import { StaticRouter } from 'react-router'
-import Helmet from 'react-helmet'
 
 import { retrievalStatusProps } from './mocks'
 
-import { Well } from '../../Well/Well'
-import { RelatedCollection } from '../../RelatedCollection/RelatedCollection'
 import { OrderStatus } from '../OrderStatus'
 
 import configureStore from '../../../store/configureStore'
-import PortalLinkContainer from '../../../containers/PortalLinkContainer/PortalLinkContainer'
 
 import * as config from '../../../../../../sharedUtils/config'
 
@@ -23,12 +26,11 @@ beforeEach(() => {
   jest.spyOn(config, 'getEnvironmentConfig').mockImplementation(() => ({ edscHost: 'https://search.earthdata.nasa.gov' }))
 })
 
-Enzyme.configure({ adapter: new Adapter() })
-
-function setup() {
+const setup = () => {
   const props = retrievalStatusProps
 
-  const enzymeWrapper = mount(
+  const { onFetchRetrieval, onChangePath } = props
+  render(
     <Provider store={store}>
       <StaticRouter>
         <OrderStatus {...props} />
@@ -37,37 +39,39 @@ function setup() {
   )
 
   return {
-    enzymeWrapper,
-    props
+    onChangePath,
+    onFetchRetrieval
   }
 }
 
 describe('OrderStatus component', () => {
   test('renders itself correctly', () => {
-    const { enzymeWrapper } = setup()
-    const orderStatus = enzymeWrapper.find(OrderStatus)
-    expect(orderStatus).toBeDefined()
+    setup()
+    expect(screen.getByText('Download Status'))
   })
 
-  test('renders the correct Helmet meta information', () => {
+  test('renders the correct Helmet meta information', async () => {
     setup()
-    const helmet = Helmet.peek()
-    expect(helmet.title).toEqual('Download Status')
-    expect(helmet.metaTags[0].name).toEqual('title')
-    expect(helmet.metaTags[0].content).toEqual('Download Status')
-    expect(helmet.metaTags[1].name).toEqual('robots')
-    expect(helmet.metaTags[1].content).toEqual('noindex, nofollow')
-    expect(helmet.linkTags[0].rel).toEqual('canonical')
-    expect(helmet.linkTags[0].href).toEqual('https://search.earthdata.nasa.gov/downloads')
+
+    await waitFor(() => expect(document.title).toEqual('Download Status'))
+
+    const metaTitleElement = document.querySelector('meta[name="title"]')
+    const robotsMetaElement = document.querySelector('meta[name="robots"]')
+    expect(metaTitleElement.content).toEqual('Download Status')
+    expect(robotsMetaElement.content).toEqual('noindex, nofollow')
+
+    const linkTag = document.querySelector('link')
+    expect(linkTag.href).toEqual('https://search.earthdata.nasa.gov/downloads')
+    expect(linkTag.rel).toEqual('canonical')
   })
 
   test('calls onFetchRetrieval when mounted', () => {
-    const { props } = setup()
-    expect(props.onFetchRetrieval).toHaveBeenCalledTimes(1)
-    expect(props.onFetchRetrieval).toHaveBeenCalledWith('7', 'testToken')
+    const { onFetchRetrieval } = setup()
+    expect(onFetchRetrieval).toHaveBeenCalledTimes(1)
+    expect(onFetchRetrieval).toHaveBeenCalledWith('7', 'testToken')
   })
 
-  describe('introduction', () => {
+  describe('Order Status page', () => {
     beforeEach(() => {
       jest.spyOn(config, 'getEnvironmentConfig').mockImplementation(() => ({ edscHost: 'http://localhost' }))
       jest.spyOn(config, 'getApplicationConfig').mockImplementation(() => ({
@@ -77,13 +81,19 @@ describe('OrderStatus component', () => {
     })
 
     test('displays the correct text', () => {
-      const { enzymeWrapper } = setup()
-      expect(enzymeWrapper.find(Well.Introduction).text()).toEqual('This page will automatically update as your orders are processed. The Download Status page can be accessed later by visiting http://localhost/downloads/7 or the Download Status and History page.')
+      setup()
+      expect(screen.getByText(/This page will automatically update as your orders are processed. The Download Status page can be accessed later by visiting/))
+        .toBeInTheDocument()
+
+      expect(screen.getByText('http://localhost/downloads/7')).toBeInTheDocument()
+      expect(screen.getByText('Download Status and History')).toBeInTheDocument()
+      expect(screen.getByText(/or the.*page./)).toBeInTheDocument()
     })
 
     test('download status link has correct href', () => {
-      const { enzymeWrapper } = setup()
-      expect(enzymeWrapper.find(Well.Introduction).find('a').at(0).props().href).toEqual('http://localhost/downloads/7')
+      setup()
+      const link = screen.getByRole('link', { name: 'http://localhost/downloads/7' })
+      expect(link).toBeInTheDocument()
     })
 
     test('download status link has correct href when earthdataEnvironment is different than the deployed environment', () => {
@@ -92,17 +102,15 @@ describe('OrderStatus component', () => {
         env: 'uat'
       }))
 
-      const { enzymeWrapper } = setup()
-      expect(enzymeWrapper.find(Well.Introduction).find('a').at(0).props().href).toEqual('http://localhost/downloads/7?ee=prod')
+      setup()
+      const link = screen.getByRole('link', { name: 'http://localhost/downloads/7?ee=prod' })
+      expect(link).toBeInTheDocument()
     })
 
     test('status link has correct href', () => {
-      const { enzymeWrapper } = setup()
-      expect(enzymeWrapper.find(Well.Introduction)
-        .find(PortalLinkContainer).at(0).props().to).toEqual({
-        pathname: '/downloads',
-        search: ''
-      })
+      setup()
+      const link = screen.getByRole('link', { name: 'Download Status and History' })
+      expect(link.href).toEqual('http://localhost/downloads?portal=')
     })
 
     test('status link has correct href when earthdataEnvironment is different than the deployed environment', () => {
@@ -111,40 +119,50 @@ describe('OrderStatus component', () => {
         env: 'uat'
       }))
 
-      const { enzymeWrapper } = setup()
-      expect(enzymeWrapper.find(Well.Introduction)
-        .find(PortalLinkContainer).at(0).props().to).toEqual({
-        pathname: '/downloads',
-        search: '?ee=prod'
-      })
+      setup()
+      const link = screen.getByRole('link', { name: 'Download Status and History' })
+      expect(link.href).toEqual('http://localhost/downloads?ee=prod&portal=')
     })
   })
 
   describe('data links', () => {
     test('renders data links in a list', () => {
-      const { enzymeWrapper } = setup()
-      const orderStatus = enzymeWrapper.find(OrderStatus)
-      expect(orderStatus.find('.order-status__collection-links-item').length).toEqual(1)
-      expect(orderStatus.find('.order-status__collection-link').at(0).props().href).toEqual('http://linkurl.com/test')
+      setup()
+      expect(screen.getByRole('link', { name: 'http://linkurl.com/test' })).toBeInTheDocument()
     })
   })
 
   describe('related collection links', () => {
     test('renders related collections', () => {
-      const { enzymeWrapper } = setup()
-      const orderStatus = enzymeWrapper.find(OrderStatus)
-      expect(orderStatus.find(RelatedCollection).length).toEqual(3)
+      setup()
+      const relatedCollectionsHeading = screen.getByRole('heading', { name: 'You might also be interested in...' })
+      expect(relatedCollectionsHeading).toBeInTheDocument()
+      const listElements = screen.getAllByRole('list')
+      // Several other list elements on DOM get the related collections list
+      const relatedColList = listElements[3]
+
+      // Get list-items for each individual related collections
+      const relatedCollections = within(relatedColList).getAllByRole('listitem')
+
+      expect(relatedCollections.length).toEqual(3)
+      expect(within(relatedCollections[0]).getByRole('link', { key: 'related-collection-TEST_COLLECTION_3_111' })).toBeInTheDocument()
+      expect(within(relatedCollections[1]).getByRole('link', { key: 'related-collection-TEST_COLLECTION_2_111' })).toBeInTheDocument()
+      expect(within(relatedCollections[2]).getByRole('link', { key: 'related-collection-TEST_COLLECTION_1_111' })).toBeInTheDocument()
     })
   })
 
   describe('footer links', () => {
-    test('calls onChangePath when the search link is clicked', () => {
-      const { enzymeWrapper, props } = setup()
-      const orderStatus = enzymeWrapper.find(OrderStatus)
-      const backToSearchLink = orderStatus.find('.order-status__footer-link-list').find(PortalLinkContainer).at(0)
-      backToSearchLink.find('a').simulate('click')
-      expect(props.onChangePath).toHaveBeenCalledTimes(1)
-      expect(props.onChangePath).toHaveBeenCalledWith('/search?test=source_link')
+    test('calls onChangePath when the search link is clicked', async () => {
+      const user = userEvent.setup()
+      const { onChangePath } = setup()
+
+      const backToSearchLink = screen.getByRole('link', { name: 'Back to Earthdata Search Results' })
+      user.click(backToSearchLink)
+
+      await waitFor(() => {
+        expect(onChangePath).toHaveBeenCalledTimes(1)
+        expect(onChangePath).toHaveBeenCalledWith('/search?test=source_link')
+      })
     })
   })
 })
