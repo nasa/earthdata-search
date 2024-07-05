b680db0c56ade6f57171f9b30b89d01ac56adfbb
Benjamin Poreh <bnp26@case.edu>
2024-07-05 13:19:33 -0400

    EDSC-4140: trying to move stuff to the top

--- tests/e2e/download/download.spec.js
+++ tests/e2e/download/download.spec.js
@@ -29,52 +29,19 @@ test.describe('Download spec', () => {
       })
     })
 
-    await page.route(/dqs/, async (route) => {
-      await route.fulfill({
-        json: []
-      })
-    })
-
-    await page.route(/granules$/, async (route) => {
-      await route.fulfill({
-        json: granules.body,
-        headers: {
-          ...authHeaders,
-          'cmr-hits': '42'
-        }
-      })
-    })
-
-    await page.route(/saved_access_configs/, async (route) => {
-      await route.fulfill({
-        json: {}
-      })
-    })
-
-    await page.goto('/projects?p=!C1443528505-LAADS&sb=-77.15071%2C38.78817%2C-76.89801%2C38.99784&lat=37.64643450971326&long=-77.407470703125&zoom=7&qt=2020-01-06T04%3A15%3A27.310Z%2C2020-01-13T07%3A32%3A50.962Z&ff=Map%20Imagery&tl=1563377338!4!!')
-
-    await page.getByTestId('C1443528505-LAADS_access-method__direct-download').click()
-
-    // Click the done panel
-    await page.getByTestId('project-panels-done').click()
-    await expect(page.getByTestId('panels-section')).toBeVisible()
-
-    // Click the Download Data button
-    await page.getByTestId('project-download-data').click()
-
-    // Load the download data and mock the results
     await page.route(/retrievals/, async (route) => {
-      await route.fulfill({
-        json: retrievals.body,
-        headers: retrievals.headers
-      })
-    })
-
-    await page.route(/retrievals/, async (route) => {
-      await route.fulfill({
-        json: retrieval.body,
-        headers: retrieval.headers
-      })
+      console.log(route.request().url())
+      if (route.request().method() === 'GET') {
+        await route.fulfill({
+          json: retrieval.body,
+          headers: retrieval.headers
+        })
+      } else {
+        await route.fulfill({
+          json: retrievals.body,
+          headers: retrievals.headers
+        })
+      }
     })
 
     await page.route(/granule_links/, async (route) => {
@@ -112,6 +79,41 @@ test.describe('Download spec', () => {
         })
       }
     })
+
+    await page.route(/dqs/, async (route) => {
+      await route.fulfill({
+        json: []
+      })
+    })
+
+    await page.route(/granules$/, async (route) => {
+      await route.fulfill({
+        json: granules.body,
+        headers: {
+          ...authHeaders,
+          'cmr-hits': '42'
+        }
+      })
+    })
+
+    await page.route(/saved_access_configs/, async (route) => {
+      await route.fulfill({
+        json: {}
+      })
+    })
+
+    await page.goto('/projects?p=!C1443528505-LAADS&sb=-77.15071%2C38.78817%2C-76.89801%2C38.99784&lat=37.64643450971326&long=-77.407470703125&zoom=7&qt=2020-01-06T04%3A15%3A27.310Z%2C2020-01-13T07%3A32%3A50.962Z&ff=Map%20Imagery&tl=1563377338!4!!')
+
+    await page.getByTestId('C1443528505-LAADS_access-method__direct-download').click()
+
+    // Click the done panel
+    await page.getByTestId('project-panels-done').click()
+    await expect(page.getByTestId('panels-section')).toBeVisible()
+
+    // Click the Download Data button
+    await page.getByTestId('project-download-data').click()
+
+    // Load the download data and mock the results
 
     // Make sure all links that are in the download list are visible on the page
     await expect(page.getByText('https://ladsweb.modaps.eosdis.nasa.gov/archive/allData/61/MYD04_3K/2020/006/MYD04_3K.A2020006.1720.061.2020008170450.hdf').first()).toBeVisible()
