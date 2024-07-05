cbc255e80f3608f1d6e85bf4168a96d11d78bb8a
Benjamin Poreh <bnp26@case.edu>
2024-07-05 12:15:11 -0400

    EDSC-4140: came up with a way to fulfill granule_links correctly.

--- tests/e2e/download/download.spec.js
+++ tests/e2e/download/download.spec.js
@@ -78,39 +78,39 @@ test.describe('Download spec', () => {
     })
 
     await page.route(/granule_links/, async (route) => {
-      await route.fulfill({
-        json: {
-          cursor: 'mock-cursor',
-          links: {
-            browse: [],
-            download: [
-              'https://ladsweb.modaps.eosdis.nasa.gov/archive/allData/61/MYD04_3K/2020/006/MYD04_3K.A2020006.1720.061.2020008170450.hdf',
-              'https://ladsweb.modaps.eosdis.nasa.gov/archive/allData/61/MYD04_3K/2020/006/MYD04_3K.A2020006.1900.061.2020008170003.hdf',
-              'https://ladsweb.modaps.eosdis.nasa.gov/archive/allData/61/MYD04_3K/2020/007/MYD04_3K.A2020007.1805.061.2020008182434.hdf',
-              'https://ladsweb.modaps.eosdis.nasa.gov/archive/allData/61/MYD04_3K/2020/008/MYD04_3K.A2020008.1850.061.2020010183913.hdf',
-              'https://ladsweb.modaps.eosdis.nasa.gov/archive/allData/61/MYD04_3K/2020/009/MYD04_3K.A2020009.1755.061.2020010200250.hdf',
-              'https://ladsweb.modaps.eosdis.nasa.gov/archive/allData/61/MYD04_3K/2020/010/MYD04_3K.A2020010.1835.061.2020011153413.hdf',
-              'https://ladsweb.modaps.eosdis.nasa.gov/archive/allData/61/MYD04_3K/2020/011/MYD04_3K.A2020011.1740.061.2020012150910.hdf',
-              'https://ladsweb.modaps.eosdis.nasa.gov/archive/allData/61/MYD04_3K/2020/012/MYD04_3K.A2020012.1825.061.2020013152621.hdf'
-            ],
-            s3: []
+      if (route.request().url().includes('pageNum=1')) {
+        await route.fulfill({
+          json: {
+            cursor: 'mock-cursor',
+            links: {
+              browse: [],
+              download: [
+                'https://ladsweb.modaps.eosdis.nasa.gov/archive/allData/61/MYD04_3K/2020/006/MYD04_3K.A2020006.1720.061.2020008170450.hdf',
+                'https://ladsweb.modaps.eosdis.nasa.gov/archive/allData/61/MYD04_3K/2020/006/MYD04_3K.A2020006.1900.061.2020008170003.hdf',
+                'https://ladsweb.modaps.eosdis.nasa.gov/archive/allData/61/MYD04_3K/2020/007/MYD04_3K.A2020007.1805.061.2020008182434.hdf',
+                'https://ladsweb.modaps.eosdis.nasa.gov/archive/allData/61/MYD04_3K/2020/008/MYD04_3K.A2020008.1850.061.2020010183913.hdf',
+                'https://ladsweb.modaps.eosdis.nasa.gov/archive/allData/61/MYD04_3K/2020/009/MYD04_3K.A2020009.1755.061.2020010200250.hdf',
+                'https://ladsweb.modaps.eosdis.nasa.gov/archive/allData/61/MYD04_3K/2020/010/MYD04_3K.A2020010.1835.061.2020011153413.hdf',
+                'https://ladsweb.modaps.eosdis.nasa.gov/archive/allData/61/MYD04_3K/2020/011/MYD04_3K.A2020011.1740.061.2020012150910.hdf',
+                'https://ladsweb.modaps.eosdis.nasa.gov/archive/allData/61/MYD04_3K/2020/012/MYD04_3K.A2020012.1825.061.2020013152621.hdf'
+              ],
+              s3: []
+            }
+          },
+          headers: authHeaders
+        })
+      } else {
+        await route.fulfill({
+          json: {
+            cursor: null,
+            links: {
+              browse: [],
+              download: [],
+              s3: []
+            }
           }
-        },
-        headers: authHeaders
-      })
-    })
-
-    await page.route(/granule_links/, async (route) => {
-      await route.fulfill({
-        json: {
-          cursor: null,
-          links: {
-            browse: [],
-            download: [],
-            s3: []
-          }
-        }
-      })
+        })
+      }
     })
 
     // Make sure all links that are in the download list are visible on the page
