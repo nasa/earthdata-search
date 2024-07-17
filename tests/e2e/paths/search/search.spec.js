b475c3ef433c90bbee8fe2573c2b7afe5c6fa170
daniel-zamora <daniel.p.zamora@nasa.gov>
2024-07-17 10:24:05 -0400

    EDSC-4142 adjusts test,expect import

--- tests/e2e/paths/search/search.spec.js
+++ tests/e2e/paths/search/search.spec.js
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test'
+import { test, expect } from 'playwright-test-coverage'
 
 import { commafy } from '../../../../static/src/js/util/commafy'
 import { getApplicationConfig } from '../../../../sharedUtils/config'
