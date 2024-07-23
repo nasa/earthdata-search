2dfb4282a864fd10e415b11fc495c7ecb95e25ed
Brandon Corbett <Bccorb@users.noreply.github.com>
2024-07-23 13:01:12 -0400

    EDSC-4055: Add performance testing (#1771)

---
+++ tests/e2e/performance/performance.spec.js
@@ -0,0 +1,41 @@
+import { test, expect } from 'playwright-test-coverage'
+
+test.describe('Performance Benchmarking', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.route('**/*.{png,jpg,jpeg}', (route) => route.abort())
+  })
+
+  test('Search page load time is less than 2 second', async ({ page, browserName }) => {
+    if (['chromium'].includes(browserName)) {
+      await page.goto('/')
+      const requestFinishedPromise = page.waitForEvent('requestfinished')
+      const request = await requestFinishedPromise
+
+      const requestTime = request.timing().responseEnd
+      console.log('Request time:', Math.round(requestTime), 'ms')
+      expect(requestTime < 2000).toBe(true)
+    }
+  })
+
+  test('Search page LCP start time is less than 2 second', async ({ page, browserName }) => {
+    if (browserName === 'chromium') {
+      await page.goto('/')
+      const paintTimingJson = await page.evaluate(async () => new Promise((resolve) => {
+        new PerformanceObserver((entryList) => {
+          const largestPaintEntry = entryList.getEntries().find(
+            (entry) => entry.entryType === 'largest-contentful-paint'
+          )
+          resolve(largestPaintEntry.startTime)
+        }).observe({
+          type: 'largest-contentful-paint',
+          buffered: true
+        })
+      }))
+
+      const paintTiming = JSON.parse(paintTimingJson)
+      console.log('LCP:', Math.round(paintTiming), 'ms')
+
+      expect(paintTiming).toBeLessThan(2000)
+    }
+  })
+})
