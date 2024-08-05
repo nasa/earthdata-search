4e63a98e616d4fe34c36ac74d65aa609873678ce
daniel-zamora <daniel.p.zamora@nasa.gov>
2024-08-05 11:02:54 -0400

    EDSC-4153 removes cypress

--- cypress/support/getByTestId.js
+++
@@ -1,5 +0,0 @@
-/**
- * Finds an element by using the data-testid attribute
- * @param {String} id data-testid attribute in markup
- */
-export const getByTestId = (id) => cy.get(`[data-testid=${id}]`)
