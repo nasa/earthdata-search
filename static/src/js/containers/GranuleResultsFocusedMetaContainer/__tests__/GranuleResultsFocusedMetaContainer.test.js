ea8f1c63eceb8a25f93c0505c0cc356ff75c11fb
Ed Olivares <34591886+eudoroolivares2016@users.noreply.github.com>
2024-08-07 13:06:50 -0400

    EDSC-4175: PR Feedback
     Adding test for container to assert child gets correct props

--- static/src/js/containers/GranuleResultsFocusedMetaContainer/__tests__/GranuleResultsFocusedMetaContainer.test.js
+++ static/src/js/containers/GranuleResultsFocusedMetaContainer/__tests__/GranuleResultsFocusedMetaContainer.test.js
@@ -4,22 +4,38 @@ import { screen, render } from '@testing-library/react'
 
 import '@testing-library/jest-dom'
 import { GranuleResultsFocusedMetaContainer } from '../GranuleResultsFocusedMetaContainer'
+import GranuleResultsFocusedMeta from '../../../components/GranuleResults/GranuleResultsFocusedMeta'
+
+jest.mock('../../../components/GranuleResults/GranuleResultsFocusedMeta', () => jest.fn(() => <div data-testid="granule-results-focused-meta-overlay-wrapper" />))
 
 const setup = (overrideProps) => {
+  const onMetricsBrowseGranuleImage = jest.fn()
   const props = {
     focusedGranuleMetadata: { test: 'test' },
     focusedGranuleId: '1234-TEST',
-    onMetricsBrowseGranuleImage: jest.fn(),
+    onMetricsBrowseGranuleImage,
     ...overrideProps
   }
 
   render(<GranuleResultsFocusedMetaContainer {...props} />)
+
+  return { onMetricsBrowseGranuleImage }
 }
 
 describe('GranuleResultsFocusedMetaContainer component', () => {
   test('passes its props and renders a single GranuleResultsFocusedMeta component', () => {
-    setup()
-    screen.debug()
+    const { onMetricsBrowseGranuleImage } = setup()
+
     expect(screen.getByTestId('granule-results-focused-meta-overlay-wrapper')).toBeInTheDocument()
+    expect(GranuleResultsFocusedMeta).toHaveBeenCalledTimes(1)
+
+    // Using the `toHaveBeenCalledWith` assertion also have the deprecated react context object in it making it less readable
+    expect(GranuleResultsFocusedMeta.mock.calls[0][0]).toEqual(
+      {
+        focusedGranuleMetadata: { test: 'test' },
+        focusedGranuleId: '1234-TEST',
+        onMetricsBrowseGranuleImage
+      }
+    )
   })
 })
