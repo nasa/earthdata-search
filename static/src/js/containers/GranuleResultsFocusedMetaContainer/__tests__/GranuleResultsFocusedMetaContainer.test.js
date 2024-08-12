c501d90eed85a26651a103c0e3e64083d7bf06b3
Ed Olivares <34591886+eudoroolivares2016@users.noreply.github.com>
2024-08-12 13:21:31 -0400

    EDSC-4175: increase coverage with another test

--- static/src/js/containers/GranuleResultsFocusedMetaContainer/__tests__/GranuleResultsFocusedMetaContainer.test.js
+++ static/src/js/containers/GranuleResultsFocusedMetaContainer/__tests__/GranuleResultsFocusedMetaContainer.test.js
@@ -1,10 +1,14 @@
 import React from 'react'
 
 import { screen, render } from '@testing-library/react'
-
 import '@testing-library/jest-dom'
-import { GranuleResultsFocusedMetaContainer } from '../GranuleResultsFocusedMetaContainer'
+
+import * as actions from '../../../middleware/metrics/actions'
 import GranuleResultsFocusedMeta from '../../../components/GranuleResults/GranuleResultsFocusedMeta'
+import {
+  GranuleResultsFocusedMetaContainer,
+  mapDispatchToProps
+} from '../GranuleResultsFocusedMetaContainer'
 
 jest.mock('../../../components/GranuleResults/GranuleResultsFocusedMeta', () => jest.fn(() => <div data-testid="granule-results-focused-meta-overlay-wrapper" />))
 
@@ -21,6 +25,26 @@ const setup = (overrideProps) => {
 
   return { onMetricsBrowseGranuleImage }
 }
+
+describe('mapDispatchToProps', () => {
+  test('onMetricsBrowseGranuleImage calls metrics actions.metricsBrowseGranuleImage', () => {
+    const dispatch = jest.fn()
+    const spy = jest.spyOn(actions, 'metricsBrowseGranuleImage')
+
+    mapDispatchToProps(dispatch).onMetricsBrowseGranuleImage({
+      modalOpen: true,
+      granuleId: 'G-1234-TEST',
+      value: 'Test'
+    })
+
+    expect(spy).toBeCalledTimes(1)
+    expect(spy).toBeCalledWith({
+      modalOpen: true,
+      granuleId: 'G-1234-TEST',
+      value: 'Test'
+    })
+  })
+})
 
 describe('GranuleResultsFocusedMetaContainer component', () => {
   test('passes its props and renders a single GranuleResultsFocusedMeta component', () => {
