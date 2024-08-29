75a578891ea9e6ff9173cbe849d46a7b98f77c17
Ed Olivares <34591886+eudoroolivares2016@users.noreply.github.com>
2024-08-29 11:28:26 -0400

    EDSC-4063: Fix unit tests

--- static/src/js/components/GranuleResults/__tests__/GranuleResultsFocusedMeta.test.js
+++ static/src/js/components/GranuleResults/__tests__/GranuleResultsFocusedMeta.test.js
@@ -581,7 +581,7 @@ describe('GranuleResultsFocusedMeta component', () => {
             })
 
             const modal = await screen.findByTestId('granule-results-focused-meta-modal')
-            const modalCloseButton = within(modal).queryByText('Close')
+            const modalCloseButton = within(modal).getByRole('button', { name: 'Close EDSCModal' })
 
             await act(async () => {
               await user.click(modalCloseButton)
