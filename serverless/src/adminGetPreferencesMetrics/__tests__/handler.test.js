1721f3b4332f7d48baed6c28669f9b3478283e7c
Benjamin Poreh <benjaminporeh@gmail.com>
2024-09-11 16:59:26 -0400

    EDSC-4170: one test to check that only the top 5 results return

--- serverless/src/adminGetPreferencesMetrics/__tests__/handler.test.js
+++ serverless/src/adminGetPreferencesMetrics/__tests__/handler.test.js
@@ -254,6 +254,197 @@ describe('adminGetPreferencesMetrics', () => {
     expect(statusCode).toEqual(200)
   })
 
+  test('correctly retrieves only top 5 preferences metrics when there is more than 5 options', async () => {
+    dbTracker.on('query', (query, step) => {
+      if (step === 1) {
+        query.response([
+          {
+            site_preferences: {
+              mapView: {
+                zoom: 2,
+                latitude: 0,
+                baseLayer: 'blueMarble',
+                longitude: 0,
+                projection: 'epsg4326',
+                overlayLayers: ['referenceFeatures', 'referenceLabels']
+              },
+              panelState: 'open',
+              granuleSort: 'start_date',
+              collectionSort: '-score',
+              granuleListView: 'default',
+              collectionListView: 'list'
+            }
+          },
+          {
+            site_preferences: {
+              mapView: {
+                zoom: 2,
+                latitude: 1,
+                baseLayer: 'blueMarble',
+                longitude: 0,
+                projection: 'epsg4326',
+                overlayLayers: ['referenceFeatures', 'referenceLabels']
+              },
+              panelState: 'open',
+              granuleSort: '-start_date',
+              collectionSort: '-score',
+              granuleListView: 'default',
+              collectionListView: 'default'
+            }
+          },
+          {
+            site_preferences: {
+              mapView: {
+                zoom: 2,
+                latitude: 2,
+                baseLayer: 'blueMarble',
+                longitude: 0,
+                projection: 'epsg4326',
+                overlayLayers: ['referenceFeatures', 'referenceLabels']
+              },
+              panelState: 'open',
+              granuleSort: '-start_date',
+              collectionSort: '-score',
+              granuleListView: 'default',
+              collectionListView: 'default'
+            }
+          },
+          {
+            site_preferences: {
+              mapView: {
+                zoom: 2,
+                latitude: 3,
+                baseLayer: 'blueMarble',
+                longitude: 0,
+                projection: 'epsg4326',
+                overlayLayers: ['referenceFeatures', 'referenceLabels']
+              },
+              panelState: 'open',
+              granuleSort: '-start_date',
+              collectionSort: '-score',
+              granuleListView: 'default',
+              collectionListView: 'default'
+            }
+          },
+          {
+            site_preferences: {
+              mapView: {
+                zoom: 2,
+                latitude: 4,
+                baseLayer: 'blueMarble',
+                longitude: 0,
+                projection: 'epsg4326',
+                overlayLayers: ['referenceFeatures', 'referenceLabels']
+              },
+              panelState: 'open',
+              granuleSort: '-start_date',
+              collectionSort: '-score',
+              granuleListView: 'default',
+              collectionListView: 'default'
+            }
+          },
+          {
+            site_preferences: {
+              mapView: {
+                zoom: 2,
+                latitude: 5,
+                baseLayer: 'blueMarble',
+                longitude: 0,
+                projection: 'epsg4326',
+                overlayLayers: ['referenceFeatures', 'referenceLabels']
+              },
+              panelState: 'open',
+              granuleSort: '-start_date',
+              collectionSort: '-score',
+              granuleListView: 'default',
+              collectionListView: 'default'
+            }
+          },
+          {
+            site_preferences: {}
+          },
+          {
+            site_preferences: {}
+          }
+        ])
+      }
+    })
+
+    const retrievalResponse = await adminGetPreferencesMetrics({
+      queryStringParameters: null
+    }, {})
+
+    const { queries } = dbTracker.queries
+
+    // Ensure only 1 sql call is made
+    expect(queries.length === 1)
+    // Ensure sql call is correct.
+    expect(queries[0].sql).toEqual('select "users"."site_preferences" from "users" where "users"."environment" = $1')
+    expect(queries[0].method).toEqual('select')
+
+    const { body, statusCode } = retrievalResponse
+
+    const responseObj = {
+      results: {
+        preferences: {
+          panelState: {
+            open: '75% (6)',
+            'not set (open)': '25% (2)'
+          },
+          granuleSort: {
+            '-start_date': '62.5% (5)',
+            'not set (-start_date)': '25% (2)',
+            start_date: '12.5% (1)'
+          },
+          granuleListView: {
+            default: '75% (6)',
+            'not set (default)': '25% (2)'
+          },
+          collectionSort: {
+            '-score': '75% (6)',
+            'not set (-score)': '25% (2)'
+          },
+          collectionListView: {
+            default: '62.5% (5)',
+            'not set (default)': '25% (2)',
+            list: '12.5% (1)'
+          },
+          zoom: {
+            2: '75% (6)',
+            'not set (2)': '25% (2)'
+          },
+          latitude: {
+            0: '12.5% (1)',
+            1: '12.5% (1)',
+            2: '12.5% (1)',
+            3: '12.5% (1)',
+            4: '12.5% (1)',
+            'not set (0)': '25% (2)'
+          },
+          longitude: {
+            0: '75% (6)',
+            'not set (0)': '25% (2)'
+          },
+          projection: {
+            epsg4326: '75% (6)',
+            'not set (epsg4326)': '25% (2)'
+          },
+          overlayLayers: {
+            referenceFeatures: '75% (6)',
+            referenceLabels: '75% (6)',
+            'not set (referenceFeatures & referenceLabels)': '25% (2)'
+          },
+          baseLayer: {
+            blueMarble: '75% (6)',
+            'not set (blueMarble)': '25% (2)'
+          }
+        }
+      }
+    }
+    expect(body).toEqual(JSON.stringify(responseObj))
+    expect(statusCode).toEqual(200)
+  })
+
   test('correctly returns an error', async () => {
     dbTracker.on('query', (query) => {
       query.reject('Unknown Error')
