87c6497fb62d507770e9427392e8d6a219a17120
Drew Pesall <pesallandrew@gmail.com>
2024-09-12 16:52:58 -0400

    EDSC-4162 Syntax fixes

--- static/src/js/components/Tour/__tests__/SearchTour.test.js
+++ static/src/js/components/Tour/__tests__/SearchTour.test.js
@@ -1,27 +1,7 @@
 import React from 'react'
-import { render, screen, act } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
+import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import SearchTour from '../SearchTour'
-
-// Mock the react-joyride module
-jest.mock('react-joyride', () => {
-  return jest.fn().mockImplementation(({ callback }) => {
-    return (
-      <div
-        data-testid="mocked-joyride"
-        onClick={() => callback({
-          action: 'close',
-          status: 'FINISHED',
-          index: 0,
-          type: 'step:after'
-        })}
-      >
-        Mocked Joyride
-      </div>
-    )
-  })
-})
 
 // Mock the local storage
 const localStorageMock = {
@@ -40,7 +20,7 @@ describe('SearchTour', () => {
 
   describe('when rendered', () => {
     it('displays the tour component', () => {
-      render(<SearchTour runTour={true} setRunTour={mockSetRunTour} />)
+      render(<SearchTour runTour setRunTour={mockSetRunTour} />)
       expect(screen.getByTestId('mocked-joyride')).toBeInTheDocument()
     })
   })
