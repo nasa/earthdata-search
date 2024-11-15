55371d7312524c536d5189833caa0cc5b1e591c6
Drew Pesall <pesallandrew@gmail.com>
2024-11-15 14:16:35 -0500

    EDSC-4263 Updating playwright tests to use getByRole

--- tests/e2e/tour/tour.spec.js
+++ tests/e2e/tour/tour.spec.js
@@ -37,15 +37,15 @@ test.describe('When dontShowTour is set to false', () => {
   test.describe('When clicking the "Skip for now" button', () => {
     test.beforeEach(async ({ page }) => {
       // Expect the first step to show the "Take the tour" button
-      await expect(page.locator('.search-tour__welcome')).toContainText('Welcome to Earthdata Search!')
+      await expect(page.getByRole('heading', { name: 'Welcome to Earthdata Search!' })).toBeVisible()
 
       // Click the "Skip for now" button to close the tour
-      await page.click('button:has-text("Skip for now")')
+      await page.getByRole('button', { name: 'Skip for now' }).click()
     })
 
     test('should close the tour', async ({ page }) => {
       // Ensure the tour is closed by checking that the tour container is no longer visible
-      await expect(page.locator('.search-tour__container')).toBeHidden()
+      await expect(page.getByRole('dialog', { name: /search tour/i })).toBeHidden()
     })
 
     test.describe('When refreshing the page', () => {
@@ -54,7 +54,7 @@ test.describe('When dontShowTour is set to false', () => {
         await page.reload()
 
         // Expect the tour to run on page load
-        await expect(page.locator('.search-tour__welcome')).toContainText('Welcome to Earthdata Search!')
+        await expect(page.getByRole('heading', { name: 'Welcome to Earthdata Search!' })).toBeVisible()
       })
     })
   })
@@ -62,7 +62,7 @@ test.describe('When dontShowTour is set to false', () => {
   test.describe('When checking the "Don\'t show again" checkbox', () => {
     test.beforeEach(async ({ page }) => {
       // Verify the tour is open
-      await expect(page.locator('.search-tour__welcome')).toContainText('Welcome to Earthdata Search!')
+      await expect(page.getByRole('heading', { name: 'Welcome to Earthdata Search!' })).toBeVisible()
 
       // Verify the checkbox is unchecked
       const checkbox = page.getByRole('checkbox', { name: 'Don\'t show the tour next time I visit Earthdata Search' })
@@ -76,9 +76,9 @@ test.describe('When dontShowTour is set to false', () => {
     test('should not see the tour when the page reloads if the checkbox is checked', async ({ page }) => {
       await page.reload()
 
-      await page.locator('.sidebar-section__header-primary .sidebar-section__title', { hasText: 'Filter Collections' }).waitFor()
+      await page.getByRole('heading', { name: 'Filter Collections' }).waitFor()
 
-      await expect(page.getByRole('alertdialog', { value: /Welcome to Earthdata Search/ })).toBeHidden()
+      await expect(page.getByRole('alertdialog', { name: /Welcome to Earthdata Search/ })).toBeHidden()
     })
   })
 })
@@ -102,9 +102,9 @@ test.describe('When loading the page with dontShowTour preference set to true', 
   })
 
   test('should not see the tour when the page loads', async ({ page }) => {
-    await page.locator('.sidebar-section__header-primary .sidebar-section__title', { hasText: 'Filter Collections' }).waitFor()
+    await page.getByRole('heading', { name: 'Filter Collections' }).waitFor()
 
-    await expect(page.getByRole('alertdialog', { value: /Welcome to Earthdata Search/ })).toBeHidden()
+    await expect(page.getByRole('alertdialog', { name: /Welcome to Earthdata Search/ })).toBeHidden()
   })
 })
 
@@ -137,46 +137,46 @@ test.describe('When logged in', () => {
     await page.getByRole('button', { name: 'Take the tour' }).click()
 
     // Navigating through the tour steps
-    await expect(page.locator('.search-tour__content').first()).toContainText('This area contains the filters used when searching for collections')
+    await expect(page.getByRole('alertdialog', { name: 'This area contains the filters used when searching for collections' })).toBeVisible()
 
     await page.keyboard.press('ArrowRight')
-    await expect(page.locator('.search-tour__content').first()).toContainText('Search for collections')
+    await expect(page.getByRole('alertdialog', { name: 'Search for collections by topic (e.g., "Land Surface Temperature"), by collection name, or by CMR Concept ID.' })).toBeVisible()
 
     await page.keyboard.press('ArrowRight')
-    await expect(page.locator('.search-tour__content').first()).toContainText('Use the temporal filters to limit search results to a specific date')
+    await expect(page.getByRole('alertdialog', { name: 'Use the temporal filters to limit search results to a specific date and time range.' })).toBeVisible()
 
     await page.keyboard.press('ArrowRight')
-    await expect(page.locator('.search-tour__content').first()).toContainText('Use the spatial filters')
+    await expect(page.getByRole('alertdialog', { name: 'Use the spatial filters to limit search results to the specified area of interest.' })).toBeVisible()
 
     await page.keyboard.press('ArrowRight')
-    await expect(page.locator('.search-tour__content').first()).toContainText('Use Advanced Search parameters')
+    await expect(page.getByRole('alertdialog', { name: 'Use Advanced Search parameters to filter results using features like Hydrologic Unit Code (HUC) or SWORD River Reach.' })).toBeVisible()
 
     await page.keyboard.press('ArrowRight')
-    await expect(page.locator('.search-tour__content').first()).toContainText('Choose a portal to refine search')
+    await expect(page.getByRole('alertdialog', { name: 'Choose a portal to refine search results to a particular area of study, project, or organization.' })).toBeVisible()
 
     await page.keyboard.press('ArrowRight')
-    await expect(page.locator('.search-tour__content').first()).toContainText('Refine your search further')
+    await expect(page.getByRole('alertdialog', { name: 'Refine your search further using categories like Features, Keywords, Platforms, Organizations, etc.' })).toBeVisible()
 
     await page.keyboard.press('ArrowRight')
-    await expect(page.locator('.search-tour__content').first()).toContainText('A high-level description')
+    await expect(page.getByRole('alertdialog', { name: 'A high-level description is displayed for each search result to help you find the right data' })).toBeVisible()
 
     await page.keyboard.press('ArrowRight')
-    await expect(page.locator('.search-tour__content').first()).toContainText('To make more room')
+    await expect(page.getByRole('alertdialog', { name: 'To make more room to view the map, the search results can be resized by clicking or dragging the bar above.' })).toBeVisible()
 
     await page.keyboard.press('ArrowRight')
-    await expect(page.locator('.search-tour__content').first()).toContainText('Pan the map by')
+    await expect(page.getByRole('alertdialog', { name: 'Pan the map by clicking and dragging, and zoom by using the scroll wheel or map tools.' })).toBeVisible()
 
     await page.keyboard.press('ArrowRight')
-    await expect(page.locator('.search-tour__content').first()).toContainText('Use the map tools')
+    await expect(page.getByRole('alertdialog', { name: 'Use the map tools to switch map projections, draw, edit, or remove spatial bounds, zoom the map, or select the base map.' })).toBeVisible()
 
     // Logged-in specific steps
     // Step 12: Save Project Button
     await page.keyboard.press('ArrowRight')
-    await expect(page.locator('.search-tour__content').first()).toContainText('to save a project using your current search criteria')
+    await expect(page.getByRole('alertdialog', { name: 'Click to save a project using your current search criteria.' })).toBeVisible()
 
     // Step 13: User Menu Dropdown
     await page.keyboard.press('ArrowRight')
-    await expect(page.locator('.search-tour__content').first()).toContainText('Use this menu to set preferences, view saved projects')
+    await expect(page.getByRole('alertdialog', { name: 'Use this menu to set preferences, view saved projects, manage subscriptions, view download status and history, or log out.' })).toBeVisible()
   })
 })
 
@@ -200,15 +200,15 @@ test.describe('When not logged in', () => {
 
   test('should navigate through the Joyride tour highlighting the correct parts of the page', async ({ page }) => {
     // Start the tour by clicking the "Start Tour" button
-    page.getByRole('button', { name: 'Start tour' }).click()
+    await page.getByRole('button', { name: 'Start tour' }).click()
 
     // Start Tour View: Welcome to Earthdata Search
-    await expect(page.locator('.search-tour__welcome')).toContainText('Welcome to Earthdata Search!')
-    await page.click('button:has-text("Take the tour")')
+    await expect(page.getByRole('heading', { name: 'Welcome to Earthdata Search!' })).toBeVisible()
+    await page.getByRole('button', { name: 'Take the tour' }).click()
 
     // Step 1: This area contains the filters
     await page.waitForTimeout(500)
-    await expect(page.locator('.search-tour__content').first()).toContainText('This area contains the filters used when searching for collections')
+    await expect(page.getByRole('alertdialog', { name: 'This area contains the filters used when searching for collections' })).toBeVisible()
 
     // Check for the presence of the highlighted section
     const spotlight = page.locator('.react-joyride__spotlight')
@@ -236,7 +236,7 @@ test.describe('When not logged in', () => {
     await page.waitForTimeout(500)
 
     // Verify we're on the next step
-    await expect(page.locator('.search-tour__content').first()).toContainText('Search for collections by topic (e.g., "Land Surface Temperature")')
+    await expect(page.getByRole('alertdialog', { name: 'Search for collections by topic (e.g., "Land Surface Temperature")' })).toBeVisible()
 
     // Now go back to the previous step using the left arrow key
     await page.keyboard.press('ArrowLeft')
@@ -244,12 +244,12 @@ test.describe('When not logged in', () => {
 
     // Testing "Previous" button
     await page.getByRole('button', { name: 'Previous' }).click()
-    await expect(page.locator('.search-tour__content').first()).toContainText('This area contains the filters used when searching for collections')
+    await expect(page.getByRole('alertdialog', { name: 'This area contains the filters used when searching for collections' })).toBeVisible()
     await page.getByRole('button', { name: 'Next' }).click()
 
     // Step 2: Search for collections
     await page.waitForTimeout(500)
-    await expect(page.locator('.search-tour__content').first()).toContainText('Search for collections by topic (e.g., "Land Surface Temperature")')
+    await expect(page.getByRole('alertdialog', { name: 'Search for collections by topic (e.g., "Land Surface Temperature")' })).toBeVisible()
     await page.getByRole('button', { name: 'Next' }).click()
 
     // Get and verify the position and size of the highlighted section
@@ -270,7 +270,7 @@ test.describe('When not logged in', () => {
 
     // Step 3: Temporal filters
     await page.waitForTimeout(500)
-    await expect(page.locator('.search-tour__content').first()).toContainText('Use the temporal filters to limit search results to a specific date')
+    await expect(page.getByRole('alertdialog', { name: 'Use the temporal filters to limit search results to a specific date' })).toBeVisible()
     await page.getByRole('button', { name: 'Next' }).click()
 
     // Get and verify the position and size of the highlighted section
@@ -291,7 +291,7 @@ test.describe('When not logged in', () => {
 
     // Step 4: Spatial filters
     await page.waitForTimeout(500)
-    await expect(page.locator('.search-tour__content').first()).toContainText('Use the spatial filters to limit search results to the specified area of interest')
+    await expect(page.getByRole('alertdialog', { name: 'Use the spatial filters to limit search results to the specified area of interest' })).toBeVisible()
     await page.getByRole('button', { name: 'Next' }).click()
 
     // Get and verify the position and size of the highlighted section
@@ -312,7 +312,7 @@ test.describe('When not logged in', () => {
 
     // Step 5: Advanced Search
     await page.waitForTimeout(500)
-    await expect(page.locator('.search-tour__content').first()).toContainText('Use Advanced Search parameters to filter results')
+    await expect(page.getByRole('alertdialog', { name: 'Use Advanced Search parameters to filter results' })).toBeVisible()
     await page.getByRole('button', { name: 'Next' }).click()
 
     // Get and verify the position and size of the highlighted section
@@ -333,7 +333,7 @@ test.describe('When not logged in', () => {
 
     // Step 6: Browse Portals
     await page.waitForTimeout(500)
-    await expect(page.locator('.search-tour__content').first()).toContainText('Choose a portal to refine search results')
+    await expect(page.getByRole('alertdialog', { name: 'Choose a portal to refine search results' })).toBeVisible()
     await page.getByRole('button', { name: 'Next' }).click()
 
     // Get and verify the position and size of the highlighted section
@@ -354,7 +354,7 @@ test.describe('When not logged in', () => {
 
     // Step 7: Refine Search by Category
     await page.waitForTimeout(500)
-    await expect(page.locator('.search-tour__content').first()).toContainText('Refine your search further using categories like Features, Keywords')
+    await expect(page.getByRole('alertdialog', { name: 'Refine your search further using categories like Features, Keywords' })).toBeVisible()
     await page.getByRole('button', { name: 'Next' }).click()
 
     // Get and verify the position and size of the highlighted section
@@ -375,7 +375,7 @@ test.describe('When not logged in', () => {
 
     // Step 8: High-level description for each search result
     await page.waitForTimeout(500)
-    await expect(page.locator('.search-tour__content').first()).toContainText('A high-level description is displayed for each search result')
+    await expect(page.getByRole('alertdialog', { name: 'A high-level description is displayed for each search result' })).toBeVisible()
     await page.getByRole('button', { name: 'Next' }).click()
 
     // Get and verify the position and size of the highlighted section
@@ -396,7 +396,7 @@ test.describe('When not logged in', () => {
 
     // Step 9: Resize Search Results Panel
     await page.waitForTimeout(500)
-    await expect(page.locator('.search-tour__content').first()).toContainText('To make more room to view the map, the search results can be resized')
+    await expect(page.getByRole('alertdialog', { name: 'To make more room to view the map, the search results can be resized' })).toBeVisible()
     await page.getByRole('button', { name: 'Next' }).click()
 
     // Get and verify the position and size of the highlighted section
@@ -417,7 +417,7 @@ test.describe('When not logged in', () => {
 
     // Step 10: Pan and zoom the map
     await page.waitForTimeout(500)
-    await expect(page.locator('.search-tour__content').first()).toContainText('Pan the map by clicking and dragging, and zoom by using the scroll wheel')
+    await expect(page.getByRole('alertdialog', { name: 'Pan the map by clicking and dragging, and zoom by using the scroll wheel' })).toBeVisible()
     await page.getByRole('button', { name: 'Next' }).click()
 
     // Get and verify the position and size of the highlighted section
@@ -438,25 +438,25 @@ test.describe('When not logged in', () => {
 
     // Step 11: Map tools
     await page.waitForTimeout(500)
-    await expect(page.locator('.search-tour__content').first()).toContainText('Use the map tools to switch map projections, draw, edit, or remove spatial bounds')
+    await expect(page.getByRole('alertdialog', { name: 'Use the map tools to switch map projections, draw, edit, or remove spatial bounds' })).toBeVisible()
     await page.getByRole('button', { name: 'Next' }).click()
 
     // Step 12: Log in to set preferences, save projects, etc.
     await page.waitForTimeout(500)
-    await expect(page.locator('.search-tour__content').first()).toContainText('Log in with Earthdata Login to set')
+    await expect(page.getByRole('alertdialog', { name: 'Log in with Earthdata Login to set' })).toBeVisible()
     await page.getByRole('button', { name: 'Next' }).click()
 
     // Testing "Previous" button on Step 13
     await page.getByRole('button', { name: 'Previous' }).click()
-    await expect(page.locator('.search-tour__content').first()).toContainText('Log in with Earthdata Login')
+    await expect(page.getByRole('alertdialog', { name: 'Log in with Earthdata Login' })).toBeVisible()
     await page.getByRole('button', { name: 'Next' }).click()
 
     // Step 13: Replay info
     await page.waitForTimeout(500)
-    await expect(page.locator('.search-tour__content')).toContainText('You can replay this tour anytime')
-    await page.locator('.search-tour__buttons button:has-text("Finish Tour")').click()
+    await expect(page.getByRole('alertdialog', { name: 'You can replay this tour anytime' })).toBeVisible()
+    await page.getByRole('button', { name: 'Finish Tour' }).click()
 
     // Final step: Want to learn more?
-    await expect(page.locator('.search-tour__heading')).toContainText('Want to learn more?')
+    await expect(page.getByRole('heading', { name: 'Want to learn more?' })).toBeVisible()
   })
 }, 60000)
