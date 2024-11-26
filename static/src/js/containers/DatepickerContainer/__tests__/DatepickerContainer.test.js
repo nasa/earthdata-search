840b16b86faac868b39044840ca3450772394b67
Drew Pesall <pesallandrew@gmail.com>
2024-11-26 18:52:06 -0500

    EDSC-4166 Removing unused import

--- static/src/js/containers/DatepickerContainer/__tests__/DatepickerContainer.test.js
+++ static/src/js/containers/DatepickerContainer/__tests__/DatepickerContainer.test.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import MockDate from 'mockdate'
