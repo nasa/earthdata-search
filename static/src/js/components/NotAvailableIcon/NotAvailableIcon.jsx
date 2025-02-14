da38f6b5680f6ad8aa5f841221c0f245246de5a2
Matthew Crouch <mcrouch@innovim.com>
2025-02-14 11:32:10 -0500

    EDSC-4345: Adds aria-label

--- static/src/js/components/NotAvailableIcon/NotAvailableIcon.jsx
+++ static/src/js/components/NotAvailableIcon/NotAvailableIcon.jsx
@@ -10,6 +10,7 @@ const NotAvailableIcon = ({ size }) => (
     xmlns="http://www.w3.org/2000/svg"
     className="meta-icon__icon"
     role="graphics-symbol"
+    aria-label="A slash icon"
   >
     <path id="forground" fillRule="evenodd" clipRule="evenodd" d="M18.1484 20.0001L-0.000119153 1.85162L1.71094 0.140564L19.8594 18.2891L18.1484 20.0001Z" fill="currentColor" />
     <path id="background" fillRule="evenodd" clipRule="evenodd" d="M19.3584 18.7901L1.20992 0.641646L1.85156 0L20.0001 18.1485L19.3584 18.7901Z" fill="white" />
