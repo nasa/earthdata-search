605e09bd03d57583cdd3fa18bbc6707d56cd5ac6
William Valencia <william.m.valencia@nasa.gov>
2025-02-24 16:28:47 -0500

    EDSC-3560: Adding collection sort key parameter into URL

--- static/src/js/selectors/preferences.js
+++ static/src/js/selectors/preferences.js
@@ -1,4 +1,5 @@
 import { createSelector } from 'reselect'
+import { collectionSortKeys } from '../constants/collectionSortKeys'
 
 /**
  * Retrieve all preferences from Redux
@@ -46,3 +47,19 @@ export const getGranuleSortPreference = createSelector(
     return granuleSort
   }
 )
+
+/**
+ * Retrieve the collection sort key from Redux
+ * If set to the default of usage, return null
+ * This function is used for inserting the sortKey
+ * into the URL query parameters when a user selects
+ * a different collection sorting key.
+ * @param {Object} state Current state of Redux
+ */
+export const getNondefaultCollectionSortKey = (state) => {
+  const { query } = state
+  const { collection } = query
+  const { sortKey } = collection
+
+  return sortKey[0] !== collectionSortKeys.usageDescending ? sortKey[0] : null
+}
