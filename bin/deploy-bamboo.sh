7a72c485baba04dcf5c74e2b292b128bc60679e2
Ed Olivares <34591886+eudoroolivares2016@users.noreply.github.com>
2025-03-06 17:35:05 -0500

    EDSC-4433: Updating the value, script, removing the old partials which are no longer being used

--- bin/deploy-bamboo.sh
+++ bin/deploy-bamboo.sh
@@ -7,13 +7,14 @@ set -eux
 ####################################
 
 # Read in edsc portal configuration
-edscPortal="`cat portals/edsc/config.json`"
+# jq --arg bamboo_NASA_ATTRIBUTION_TEXT "$bamboo_NASA_ATTRIBUTION_TEXT" '.footer.attributionText = $bamboo_NASA_ATTRIBUTION_TEXT' portals/edsc/config.json > portals/edsc/config.tmp.json && mv portals/edsc/config.tmp.json portals/edsc/config.json
 
 # Update portal keys for deployment
-edscPortal="`jq '.footer.attributionText = "$newValue"' --arg newValue "$bamboo_NASA_ATTRIBUTION_TEXT" <<< $edscPortal`"
-
+# edscPortal="`cat portals/edsc/config.json`"
+# edscPortal="`jq '.footer.attributionText = "$newValue"' --arg newValue $bamboo_NASA_ATTRIBUTION_TEXT <<< $edscPortal`"
+edscPortal=$(jq --arg newValue "$bamboo_NASA_ATTRIBUTION_TEXT" '.footer.attributionText = $newValue' <<< "$edscPortal")
 # Overwrite edsc portal with new values
-echo $edscPortal > tmp.$$.json && mv tmp.$$.json portals/edsc/config.json
+# echo $edscPortal > tmp.$$.json && mv tmp.$$.json portals/edsc/config.json
 echo 'Value of the edsc portal after writting to it'
 echo "$(cat portals/edsc/config.json)" 
 
