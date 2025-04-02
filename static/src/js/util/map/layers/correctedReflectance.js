39c069b4ac14da11d3c1e46b2396d848e6ad3f55
Drew Pesall <pesallandrew@gmail.com>
2025-04-02 14:31:36 -0400

    EDSC-4412 PR feedback

--- static/src/js/util/map/layers/correctedReflectance.js
+++ static/src/js/util/map/layers/correctedReflectance.js
@@ -11,7 +11,7 @@ import { getTileGrid } from '../getTileGrid'
 const correctedReflectance = ({
   projectionCode
 }) => {
-  // Return near time image so that the map is completely filled 
+  // Return near time image so that the map is completely filled
   const projection = projectionCode
   const yesterday = moment().subtract(1, 'days')
   const date = yesterday.format('YYYY-MM-DD')
