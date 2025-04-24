ee145b5d3bec7641e4609272951a2d04de44c743
Ed Olivares <34591886+eudoroolivares2016@users.noreply.github.com>
2025-04-24 13:08:32 -0400

    EDSC-4771: Adding comment to database migration; rebasing

--- migrations/1745455551174_clear-access-configurations-table.js
+++ migrations/1745455551174_clear-access-configurations-table.js
@@ -1,5 +1,8 @@
 exports.shorthands = undefined
 
+// Prior to EDSC-4471, the access_configurations table not properly saving the access configuration
+// Specifically the form digest field was not being passed along therefore the access configuration could not be pulled again making the saved values in the form
+// unreachable
 exports.up = (pgm) => {
   pgm.sql('DELETE FROM access_configurations')
 }
