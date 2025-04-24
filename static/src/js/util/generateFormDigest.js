2a782446dc02bf9c88a2d1671a50765128d6813e
Ed Olivares <34591886+eudoroolivares2016@users.noreply.github.com>
2025-04-24 14:54:19 -0400

    EDSC-4771: Add access method type; fix docstring

--- static/src/js/util/generateFormDigest.js
+++ static/src/js/util/generateFormDigest.js
@@ -1,7 +1,7 @@
 import md5 from 'md5'
 
 /**
- * Generates a base64 encoding of the echoform, used to determine if the saved
+ * Generates an md5 encoding of the echoform, used to determine if the saved
  * EchoForm data is up to date with the current EchoForm
  * @param {String} form EchoForm string
  */
