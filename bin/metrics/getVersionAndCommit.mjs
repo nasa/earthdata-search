25342d65323115cc796a7820c4113e5256347f8a
Matthew Crouch <mcrouch@innovim.com>
2025-05-09 12:27:38 -0400

    EDSC-4489: Remove auth header from github api call

--- bin/metrics/getVersionAndCommit.mjs
+++ bin/metrics/getVersionAndCommit.mjs
@@ -27,12 +27,12 @@ const getVersionAndCommit = async (url) => {
 
   const version = versionMatch[1]
 
-  const { GITHUB_TOKEN } = process.env
+  // const { GITHUB_TOKEN } = process.env
   // Get the commit hash for the git tag that matches the version
   const gitTagResponse = await fetch('https://api.github.com/repos/nasa/earthdata-search/tags?per_page=50', {
-    headers: {
-      authorization: `Bearer ${GITHUB_TOKEN}`
-    }
+    // headers: {
+    //   authorization: `Bearer ${GITHUB_TOKEN}`
+    // }
   })
   if (!gitTagResponse.ok) {
     throw new Error(`Error fetching GitHub tags: ${gitTagResponse.statusText}`)
