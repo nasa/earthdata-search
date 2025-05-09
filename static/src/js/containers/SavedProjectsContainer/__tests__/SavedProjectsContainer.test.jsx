6bcbf84c52cf9a2b8beb3478a794982ce3bea0b0
Drew Pesall <pesallandrew@gmail.com>
2025-05-09 11:43:25 -0400

    EDSC-4466 - ESLint fix

--- static/src/js/containers/SavedProjectsContainer/__tests__/SavedProjectsContainer.test.jsx
+++ static/src/js/containers/SavedProjectsContainer/__tests__/SavedProjectsContainer.test.jsx
@@ -1,9 +1,5 @@
 import React from 'react'
-import {
-  screen,
-  waitFor,
-  act
-} from '@testing-library/react'
+import { screen, waitFor } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import nock from 'nock'
 
@@ -141,9 +137,7 @@ describe('SavedProjectsContainer', () => {
       setup()
 
       const removeBtn = await screen.findByRole('button', { name: /remove project/i })
-      await act(async () => {
-        await userEvent.click(removeBtn)
-      })
+      await userEvent.click(removeBtn)
 
       await waitFor(() => {
         expect(addToast).toHaveBeenCalledWith('Project removed', {
