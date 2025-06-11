4f0277d3ac387a31d916f0e097c4bcbf418583dc
Matthew Crouch <mcrouch@innovim.com>
2025-06-11 13:24:37 -0400

    EDSC-4509: Fixes logic for non-polygon shapes

--- static/src/js/util/map/getQueryFromShapefileFeature.ts
+++ static/src/js/util/map/getQueryFromShapefileFeature.ts
@@ -16,7 +16,12 @@ const getQueryFromShapefileFeature = (feature: Feature) => {
   // Shapefiles can have altitude in their coordinates. This shows up as a 3rd value in each coordinate pair (e.g., [longitude, latitude, altitude]).
   // For CMR spatial queries, we only need the longitude and latitude values.
   let geographicCoordinatesWithoutAltitude = geographicCoordinates
-  if (geographicCoordinates[0][0].length === 3) {
+  if (
+    geographicCoordinates
+    && geographicCoordinates[0] // Array of coordinates
+    && geographicCoordinates[0][0] // First coordinate
+    && geographicCoordinates[0][0].length === 3 // If the coordinate has 3 values (longitude, latitude, altitude)
+  ) {
     geographicCoordinatesWithoutAltitude = geographicCoordinates.map(
       (coords: number[][]) => coords.map(
         (coord: number[]) => coord.slice(0, 2)
