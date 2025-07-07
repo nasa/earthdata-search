3ac6e93b8b008b2183838f669bf9d91da8a89c2a
KushaalP <kpalasamudrum@gmail.com>
2025-07-07 13:58:26 -0400

    EDSC-4479: Removed Unnecesary Route Handling, improved robustness of tests (in response to comments)

--- static/src/js/components/TemporalDisplay/__tests__/TemporalSelectionDropdownMenu.test.jsx
+++ static/src/js/components/TemporalDisplay/__tests__/TemporalSelectionDropdownMenu.test.jsx
@@ -7,25 +7,14 @@ import setupTest from '../../../../../../jestConfigs/setupTest'
 import PortalLinkContainer from '../../../containers/PortalLinkContainer/PortalLinkContainer'
 import Button from '../../Button/Button'
 
-jest.mock('react-router-dom', () => ({
-  ...jest.requireActual('react-router-dom'),
-  useLocation: jest.fn().mockReturnValue({
-    pathname: '/',
-    search: '',
-    hash: '',
-    state: null,
-    key: 'testKey'
-  })
-}))
-
 jest.mock('../../../containers/PortalLinkContainer/PortalLinkContainer', () => ({
   __esModule: true,
-  default: jest.fn(() => <div />)
+  default: jest.fn(({ children }) => <div>{children}</div>)
 }))
 
 jest.mock('../../Button/Button', () => ({
   __esModule: true,
-  default: jest.fn(() => <div />)
+  default: jest.fn(({ children }) => <div>{children}</div>)
 }))
 
 // Mock react-bootstrap Dropdown
@@ -37,70 +26,87 @@ jest.mock('react-bootstrap/Dropdown', () => ({
 }))
 
 const setup = setupTest({
-  ComponentsByRoute: {
-    '/': TemporalSelectionDropdownMenu
-  },
-  defaultPropsByRoute: {
-    '/': {
-      allowRecurring: true,
-      disabled: false,
-      displayStartDate: '',
-      displayEndDate: '',
-      isHomePage: true,
-      onApplyClick: jest.fn(),
-      onChangeQuery: jest.fn(),
-      onChangeRecurring: jest.fn(),
-      onClearClick: jest.fn(),
-      onInvalid: jest.fn(),
-      onRecurringToggle: jest.fn(),
-      onSliderChange: jest.fn(),
-      onValid: jest.fn(),
-      setEndDate: jest.fn(),
-      setStartDate: jest.fn(),
-      temporal: { isRecurring: false }
-    }
-  },
-  withRedux: true,
-  withRouter: true
-})
-
-beforeAll(() => {
+  Component: TemporalSelectionDropdownMenu,
+  defaultProps: {
+    allowRecurring: true,
+    disabled: false,
+    displayStartDate: '',
+    displayEndDate: '',
+    isHomePage: true,
+    onApplyClick: jest.fn(),
+    onChangeQuery: jest.fn(),
+    onChangeRecurring: jest.fn(),
+    onClearClick: jest.fn(),
+    onInvalid: jest.fn(),
+    onRecurringToggle: jest.fn(),
+    onSliderChange: jest.fn(),
+    onValid: jest.fn(),
+    setEndDate: jest.fn(),
+    setStartDate: jest.fn(),
+    temporal: { isRecurring: false }
+  }
 })
 
 beforeEach(() => {
-  jest.clearAllMocks()
   ReactDOM.createPortal = jest.fn((dropdown) => dropdown)
-  window.console.warn = jest.fn()
-})
-
-afterEach(() => {
-  ReactDOM.createPortal.mockClear()
-  window.console.warn.mockClear()
 })
 
 describe('TemporalSelectionDropdownMenu component', () => {
-  test('renders PortalLinkContainer when isHomePage is true', () => {
-    setup({
-      overridePropsByRoute: {
-        '/': {
-          isHomePage: true
-        }
-      }
-    })
+  describe('when isHomePage is true', () => {
+    test('renders the Apply button with PortalLinkContainer', () => {
+      const { props } = setup()
 
-    expect(PortalLinkContainer).toHaveBeenCalledTimes(1)
+      expect(PortalLinkContainer).toHaveBeenCalledTimes(1)
+      expect(PortalLinkContainer).toHaveBeenCalledWith(
+        expect.objectContaining({
+          className: expect.stringContaining('temporal-selection-dropdown-menu__button--apply'),
+          type: 'button',
+          bootstrapVariant: 'primary',
+          label: 'Apply',
+          onClick: props.onApplyClick,
+          disabled: false,
+          to: '/search',
+          children: 'Apply'
+        }),
+        {}
+      )
+    })
   })
 
-  test('renders Button twice when isHomePage is false', () => {
-    setup({
-      overridePropsByRoute: {
-        '/': {
+  describe('when isHomePage is false', () => {
+    test('renders the Apply button with Button and Clear button with Button', () => {
+      const { props } = setup({
+        overrideProps: {
           isHomePage: false
         }
-      }
-    })
+      })
 
-    expect(Button).toHaveBeenCalledTimes(2)
-    expect(PortalLinkContainer).not.toHaveBeenCalled()
+      expect(Button).toHaveBeenCalledTimes(2)
+      expect(Button).toHaveBeenNthCalledWith(
+        1,
+        expect.objectContaining({
+          className: expect.stringContaining('temporal-selection-dropdown-menu__button--apply'),
+          type: 'button',
+          bootstrapVariant: 'primary',
+          label: 'Apply',
+          onClick: props.onApplyClick,
+          disabled: false,
+          children: 'Apply'
+        }),
+        {}
+      )
+
+      expect(Button).toHaveBeenNthCalledWith(
+        2,
+        expect.objectContaining({
+          className: expect.stringContaining('temporal-selection-dropdown-menu__button--cancel'),
+          bootstrapVariant: 'light',
+          label: 'Clear',
+          onClick: props.onClearClick,
+          children: 'Clear'
+        }),
+        {}
+      )
+    })
   })
 })
