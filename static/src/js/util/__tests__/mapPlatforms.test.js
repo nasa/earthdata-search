01c07de8acfdd2fb657d2197b752fd0ad1fc3849
KushaalP <kpalasamudrum@gmail.com>
2025-07-23 23:52:16 -0400

    refactored mapping tests to be contained to new util files

---
+++ static/src/js/util/__tests__/mapPlatforms.test.js
@@ -0,0 +1,76 @@
+import { mapPlatforms } from '../mapPlatforms'
+
+describe('mapPlatforms', () => {
+  test('maps full platform string with all levels', () => {
+    const input = 'Space-based Platforms:Earth Observation Satellites:Landsat:LANDSAT-8'
+
+    const result = mapPlatforms(input)
+
+    expect(result).toEqual({
+      basis: 'Space-based Platforms',
+      category: 'Earth Observation Satellites',
+      sub_category: 'Landsat',
+      short_name: 'LANDSAT-8'
+    })
+  })
+
+  test('maps partial platform string', () => {
+    const input = 'Space-based Platforms:Earth Observation Satellites'
+
+    const result = mapPlatforms(input)
+
+    expect(result).toEqual({
+      basis: 'Space-based Platforms',
+      category: 'Earth Observation Satellites'
+    })
+  })
+
+  test('handles empty values in the string', () => {
+    const input = 'Space-based Platforms::LANDSAT-8'
+
+    const result = mapPlatforms(input)
+
+    expect(result).toEqual({
+      basis: 'Space-based Platforms',
+      sub_category: 'LANDSAT-8'
+    })
+  })
+
+  test('handles single value', () => {
+    const input = 'Space-based Platforms'
+
+    const result = mapPlatforms(input)
+
+    expect(result).toEqual({
+      basis: 'Space-based Platforms'
+    })
+  })
+
+  test('handles empty string', () => {
+    const input = ''
+
+    const result = mapPlatforms(input)
+
+    expect(result).toEqual({})
+  })
+
+  test('handles string with only colons', () => {
+    const input = ':::'
+
+    const result = mapPlatforms(input)
+
+    expect(result).toEqual({})
+  })
+
+  test('maps aircraft platform', () => {
+    const input = 'Air-based Platforms:Aircraft:DC-8'
+
+    const result = mapPlatforms(input)
+
+    expect(result).toEqual({
+      basis: 'Air-based Platforms',
+      category: 'Aircraft',
+      sub_category: 'DC-8'
+    })
+  })
+})
---
+++ static/src/js/util/__tests__/mapScienceKeywords.test.js
@@ -0,0 +1,67 @@
+import { mapScienceKeywords } from '../mapScienceKeywords'
+
+describe('mapScienceKeywords', () => {
+  test('maps full science keyword string with all levels', () => {
+    const input = 'Land Surface:Surface Radiative Properties:Reflectance:Laser Reflectance:Detail Level:Extra Detail'
+
+    const result = mapScienceKeywords(input)
+
+    expect(result).toEqual({
+      topic: 'Land Surface',
+      term: 'Surface Radiative Properties',
+      variable_level_1: 'Reflectance',
+      variable_level_2: 'Laser Reflectance',
+      variable_level_3: 'Detail Level',
+      detailed_variable: 'Extra Detail'
+    })
+  })
+
+  test('maps partial science keyword string', () => {
+    const input = 'Land Surface:Surface Radiative Properties:Reflectance'
+
+    const result = mapScienceKeywords(input)
+
+    expect(result).toEqual({
+      topic: 'Land Surface',
+      term: 'Surface Radiative Properties',
+      variable_level_1: 'Reflectance'
+    })
+  })
+
+  test('handles empty values in the string', () => {
+    const input = 'Land Surface::Reflectance'
+
+    const result = mapScienceKeywords(input)
+
+    expect(result).toEqual({
+      topic: 'Land Surface',
+      variable_level_1: 'Reflectance'
+    })
+  })
+
+  test('handles single value', () => {
+    const input = 'Land Surface'
+
+    const result = mapScienceKeywords(input)
+
+    expect(result).toEqual({
+      topic: 'Land Surface'
+    })
+  })
+
+  test('handles empty string', () => {
+    const input = ''
+
+    const result = mapScienceKeywords(input)
+
+    expect(result).toEqual({})
+  })
+
+  test('handles string with only colons', () => {
+    const input = ':::'
+
+    const result = mapScienceKeywords(input)
+
+    expect(result).toEqual({})
+  })
+})
