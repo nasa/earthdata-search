8acd8b9d75e89fc8fa2400dee5cf0458b98f94f5
Drew Pesall <pesallandrew@gmail.com>
2025-09-04 14:04:26 -0400

    EDSC-4388 - Test coverage

--- static/src/js/util/request/__tests__/nlpSearchRequest.test.js
+++ static/src/js/util/request/__tests__/nlpSearchRequest.test.js
@@ -1,9 +1,13 @@
 import axios from 'axios'
+import { simplify, booleanClockwise } from '@turf/turf'
 import NlpSearchRequest from '../nlpSearchRequest'
 import { getEarthdataConfig } from '../../../../../../sharedUtils/config'
+import { convertNlpTemporalData } from '../../temporal/convertNlpTemporalData'
 
 jest.mock('axios')
 jest.mock('../../../../../../sharedUtils/config')
+jest.mock('@turf/turf')
+jest.mock('../../temporal/convertNlpTemporalData')
 
 const mockConfig = {
   cmrHost: 'https://cmr.sit.earthdata.nasa.gov'
@@ -13,6 +17,10 @@ beforeEach(() => {
   jest.restoreAllMocks()
   jest.clearAllMocks()
   getEarthdataConfig.mockReturnValue(mockConfig)
+
+  simplify.mockClear()
+  booleanClockwise.mockClear()
+  convertNlpTemporalData.mockClear()
 })
 
 describe('NlpSearchRequest#constructor', () => {
@@ -114,5 +122,692 @@ describe('NlpSearchRequest#permittedCmrKeys', () => {
     const request = new NlpSearchRequest('', 'sit')
 
     expect(request.permittedCmrKeys()).toEqual(['q', 'pageNum', 'pageSize'])
+  })
+})
+
+describe('NlpSearchRequest#simplifyNlpGeometry', () => {
+  let request
+
+  beforeEach(() => {
+    request = new NlpSearchRequest('', 'sit')
+  })
+
+  test('handles null geometry through transformResponse', () => {
+    const response = {
+      data: {
+        queryInfo: {
+          spatial: null
+        }
+      }
+    }
+
+    const result = request.transformResponse(response, 'test query')
+
+    expect(result).toEqual({
+      query: 'test query',
+      spatial: null,
+      temporal: null
+    })
+  })
+
+  test('handles geometry without type through transformResponse', () => {
+    const invalidGeometry = {
+      coordinates: [[0, 0], [1, 1]]
+    }
+
+    const response = {
+      data: {
+        queryInfo: {
+          spatial: {
+            geoJson: invalidGeometry
+          }
+        }
+      }
+    }
+
+    const result = request.transformResponse(response, 'test query')
+
+    expect(result).toEqual({
+      query: 'test query',
+      spatial: null,
+      temporal: null
+    })
+  })
+
+  test('returns Point geometry unchanged', () => {
+    const pointGeometry = {
+      type: 'Point',
+      coordinates: [0, 0]
+    }
+
+    const mockResponse = {
+      data: {
+        queryInfo: {
+          spatial: {
+            geoJson: pointGeometry
+          }
+        }
+      }
+    }
+
+    const result = request.transformResponse(mockResponse, 'test query')
+
+    expect(result.spatial.features[0].geometry).toEqual(pointGeometry)
+  })
+
+  test('returns small polygon unchanged when under MAX_POLYGON_SIZE', () => {
+    const smallPolygon = {
+      type: 'Polygon',
+      coordinates: [[[0, 0], [1, 0], [1, 1], [0, 1], [0, 0]]]
+    }
+
+    const mockResponse = {
+      data: {
+        queryInfo: {
+          spatial: {
+            geoJson: smallPolygon
+          }
+        }
+      }
+    }
+
+    const result = request.transformResponse(mockResponse, 'test query')
+
+    expect(result.spatial.features[0].geometry).toEqual(smallPolygon)
+  })
+
+  test('simplifies large polygon when over MAX_POLYGON_SIZE', () => {
+    const largeCoordinates = Array.from({ length: 60 }, (_, i) => [i, i])
+    largeCoordinates.push(largeCoordinates[0])
+
+    const largePolygon = {
+      type: 'Polygon',
+      coordinates: [largeCoordinates]
+    }
+
+    const simplifiedPolygon = {
+      type: 'Polygon',
+      coordinates: [[[0, 0], [10, 10], [20, 20], [0, 0]]]
+    }
+
+    simplify.mockReturnValue(simplifiedPolygon)
+    booleanClockwise.mockReturnValue(false)
+
+    const mockResponse = {
+      data: {
+        queryInfo: {
+          spatial: {
+            geoJson: largePolygon
+          }
+        }
+      }
+    }
+
+    const result = request.transformResponse(mockResponse, 'test query')
+
+    expect(simplify).toHaveBeenCalledWith(largePolygon, {
+      tolerance: 0.001,
+      highQuality: false
+    })
+
+    expect(booleanClockwise).toHaveBeenCalledWith(simplifiedPolygon.coordinates[0])
+
+    const expectedGeometry = {
+      ...simplifiedPolygon,
+      coordinates: [simplifiedPolygon.coordinates[0].reverse()]
+    }
+    expect(result.spatial.features[0].geometry).toEqual(expectedGeometry)
+  })
+
+  test('handles simplification with multiple attempts when still too large', () => {
+    const largeCoordinates = Array.from({ length: 60 }, (_, i) => [i, i])
+    largeCoordinates.push(largeCoordinates[0])
+
+    const largePolygon = {
+      type: 'Polygon',
+      coordinates: [largeCoordinates]
+    }
+
+    const stillLargePolygon = {
+      type: 'Polygon',
+      coordinates: [Array.from({ length: 55 }, (_, i) => [i, i]).concat([[0, 0]])]
+    }
+
+    const goodPolygon = {
+      type: 'Polygon',
+      coordinates: [Array.from({ length: 10 }, (_, i) => [i, i]).concat([[0, 0]])]
+    }
+
+    simplify
+      .mockReturnValueOnce(stillLargePolygon)
+      .mockReturnValueOnce(goodPolygon)
+
+    booleanClockwise.mockReturnValue(true)
+
+    const mockResponse = {
+      data: {
+        queryInfo: {
+          spatial: {
+            geoJson: largePolygon
+          }
+        }
+      }
+    }
+
+    const result = request.transformResponse(mockResponse, 'test query')
+
+    expect(simplify).toHaveBeenCalledTimes(2)
+    expect(simplify).toHaveBeenNthCalledWith(1, largePolygon, {
+      tolerance: 0.001,
+      highQuality: false
+    })
+
+    expect(simplify).toHaveBeenNthCalledWith(2, largePolygon, {
+      tolerance: 0.002,
+      highQuality: false
+    })
+
+    expect(result.spatial.features[0].geometry).toEqual(goodPolygon)
+  })
+
+  test('handles simplification error and returns original geometry', () => {
+    const largeCoordinates = Array.from({ length: 60 }, (_, i) => [i, i])
+    largeCoordinates.push(largeCoordinates[0])
+
+    const largePolygon = {
+      type: 'Polygon',
+      coordinates: [largeCoordinates]
+    }
+
+    simplify.mockImplementation(() => {
+      throw new Error('Simplification failed')
+    })
+
+    const consoleWarnSpy = jest.spyOn(console, 'warn').mockImplementation()
+
+    const mockResponse = {
+      data: {
+        queryInfo: {
+          spatial: {
+            geoJson: largePolygon
+          }
+        }
+      }
+    }
+
+    const result = request.transformResponse(mockResponse, 'test query')
+
+    expect(consoleWarnSpy).toHaveBeenCalledWith('Error simplifying geometry:', expect.any(Error))
+    expect(result.spatial.features[0].geometry).toEqual(largePolygon)
+
+    consoleWarnSpy.mockRestore()
+  })
+
+  test('handles maximum simplification attempts reached', () => {
+    const largeCoordinates = Array.from({ length: 60 }, (_, i) => [i, i])
+    largeCoordinates.push(largeCoordinates[0])
+
+    const largePolygon = {
+      type: 'Polygon',
+      coordinates: [largeCoordinates]
+    }
+
+    const stillTooLargePolygon = {
+      type: 'Polygon',
+      coordinates: [Array.from({ length: 55 }, (_, i) => [i, i]).concat([[0, 0]])]
+    }
+
+    simplify.mockReturnValue(stillTooLargePolygon)
+
+    const consoleWarnSpy = jest.spyOn(console, 'warn').mockImplementation()
+
+    const mockResponse = {
+      data: {
+        queryInfo: {
+          spatial: {
+            geoJson: largePolygon
+          }
+        }
+      }
+    }
+
+    const result = request.transformResponse(mockResponse, 'test query')
+
+    expect(simplify).toHaveBeenCalledTimes(10)
+    expect(consoleWarnSpy).toHaveBeenCalledWith('Could not simplify geometry below max polygon size')
+    expect(result.spatial.features[0].geometry).toEqual(largePolygon)
+
+    consoleWarnSpy.mockRestore()
+  })
+})
+
+describe('NlpSearchRequest#transformResponse', () => {
+  let request
+
+  beforeEach(() => {
+    request = new NlpSearchRequest('', 'sit')
+  })
+
+  test('returns default structure when response has no data', () => {
+    const response = {}
+    const query = 'test query'
+
+    const result = request.transformResponse(response, query)
+
+    expect(result).toEqual({
+      query: 'test query',
+      spatial: null,
+      temporal: null
+    })
+  })
+
+  test('returns default structure when response has no queryInfo', () => {
+    const response = {
+      data: {}
+    }
+    const query = 'climate data'
+
+    const result = request.transformResponse(response, query)
+
+    expect(result).toEqual({
+      query: 'climate data',
+      spatial: null,
+      temporal: null
+    })
+  })
+
+  test('processes spatial data with geoJson property', () => {
+    const spatialGeometry = {
+      type: 'Point',
+      coordinates: [10, 20]
+    }
+
+    const response = {
+      data: {
+        queryInfo: {
+          spatial: {
+            geoJson: spatialGeometry
+          }
+        }
+      }
+    }
+
+    const result = request.transformResponse(response, 'test query')
+
+    expect(result).toEqual({
+      query: 'test query',
+      spatial: {
+        type: 'FeatureCollection',
+        name: 'NLP Extracted Spatial Area',
+        features: [{
+          type: 'Feature',
+          properties: {
+            source: 'nlp',
+            query: 'test query',
+            edscId: '0'
+          },
+          geometry: spatialGeometry
+        }]
+      },
+      temporal: null
+    })
+  })
+
+  test('processes spatial data as direct geometry', () => {
+    const spatialGeometry = {
+      type: 'Polygon',
+      coordinates: [[[0, 0], [1, 0], [1, 1], [0, 1], [0, 0]]]
+    }
+
+    const response = {
+      data: {
+        queryInfo: {
+          spatial: spatialGeometry
+        }
+      }
+    }
+
+    const result = request.transformResponse(response, 'ocean data')
+
+    expect(result.spatial).toEqual({
+      type: 'FeatureCollection',
+      name: 'NLP Extracted Spatial Area',
+      features: [{
+        type: 'Feature',
+        properties: {
+          source: 'nlp',
+          query: 'ocean data',
+          edscId: '0'
+        },
+        geometry: spatialGeometry
+      }]
+    })
+
+    expect(result.temporal).toBeNull()
+  })
+
+  test('processes temporal data', () => {
+    const temporalData = {
+      startDate: '2020-01-01',
+      endDate: '2020-12-31'
+    }
+
+    const convertedTemporal = {
+      startDate: '2020-01-01T00:00:00.000Z',
+      endDate: '2020-12-31T23:59:59.999Z'
+    }
+
+    convertNlpTemporalData.mockReturnValue(convertedTemporal)
+
+    const response = {
+      data: {
+        queryInfo: {
+          temporal: temporalData
+        }
+      }
+    }
+
+    const result = request.transformResponse(response, 'temperature data')
+
+    expect(convertNlpTemporalData).toHaveBeenCalledWith(temporalData)
+    expect(result).toEqual({
+      query: 'temperature data',
+      spatial: null,
+      temporal: {
+        startDate: '2020-01-01T00:00:00.000Z',
+        endDate: '2020-12-31T23:59:59.999Z'
+      }
+    })
+  })
+
+  test('processes both spatial and temporal data', () => {
+    const spatialGeometry = {
+      type: 'Point',
+      coordinates: [-100, 40]
+    }
+
+    const temporalData = {
+      startDate: '2023-01-01',
+      endDate: '2023-06-30'
+    }
+
+    const convertedTemporal = {
+      startDate: '2023-01-01T00:00:00.000Z',
+      endDate: '2023-06-30T23:59:59.999Z'
+    }
+
+    convertNlpTemporalData.mockReturnValue(convertedTemporal)
+
+    const response = {
+      data: {
+        queryInfo: {
+          spatial: {
+            geoJson: spatialGeometry
+          },
+          temporal: temporalData
+        }
+      }
+    }
+
+    const result = request.transformResponse(response, 'landsat data')
+
+    expect(result).toEqual({
+      query: 'landsat data',
+      spatial: {
+        type: 'FeatureCollection',
+        name: 'NLP Extracted Spatial Area',
+        features: [{
+          type: 'Feature',
+          properties: {
+            source: 'nlp',
+            query: 'landsat data',
+            edscId: '0'
+          },
+          geometry: spatialGeometry
+        }]
+      },
+      temporal: {
+        startDate: '2023-01-01T00:00:00.000Z',
+        endDate: '2023-06-30T23:59:59.999Z'
+      }
+    })
+  })
+
+  test('handles empty temporal conversion result', () => {
+    const temporalData = {
+      invalidData: 'not-a-date'
+    }
+
+    convertNlpTemporalData.mockReturnValue({})
+
+    const response = {
+      data: {
+        queryInfo: {
+          temporal: temporalData
+        }
+      }
+    }
+
+    const result = request.transformResponse(response, 'test query')
+
+    expect(result.temporal).toBeNull()
+  })
+
+  test('handles null temporal conversion result', () => {
+    const temporalData = {
+      malformedData: 'invalid'
+    }
+
+    convertNlpTemporalData.mockReturnValue(null)
+
+    const response = {
+      data: {
+        queryInfo: {
+          temporal: temporalData
+        }
+      }
+    }
+
+    const result = request.transformResponse(response, 'test query')
+
+    expect(result.temporal).toBeNull()
+  })
+
+  test('handles simplified geometry returning null', () => {
+    const badGeometry = {
+      type: 'Polygon',
+      coordinates: []
+    }
+
+    const response = {
+      data: {
+        queryInfo: {
+          spatial: {
+            geoJson: badGeometry
+          }
+        }
+      }
+    }
+
+    const originalTransformResponse = request.transformResponse
+    request.transformResponse = function mockTransformResponse(mockResponse, mockQuery) {
+      const { data } = mockResponse
+
+      if (!data || !data.queryInfo) {
+        return {
+          query: mockQuery,
+          spatial: null,
+          temporal: null
+        }
+      }
+
+      let spatialData = null
+
+      if (data.queryInfo.spatial) {
+        const simplifiedGeometry = null
+
+        if (simplifiedGeometry) {
+          spatialData = {
+            type: 'FeatureCollection',
+            name: 'NLP Extracted Spatial Area',
+            features: [{
+              type: 'Feature',
+              properties: {
+                source: 'nlp',
+                query: mockQuery,
+                edscId: '0'
+              },
+              geometry: simplifiedGeometry
+            }]
+          }
+        }
+      }
+
+      return {
+        query: mockQuery,
+        spatial: spatialData,
+        temporal: null
+      }
+    }
+
+    const result = request.transformResponse(response, 'test query')
+
+    expect(result).toEqual({
+      query: 'test query',
+      spatial: null,
+      temporal: null
+    })
+
+    request.transformResponse = originalTransformResponse
+  })
+
+  test('handles LineString geometry type', () => {
+    const lineGeometry = {
+      type: 'LineString',
+      coordinates: [[0, 0], [1, 1], [2, 2]]
+    }
+
+    const response = {
+      data: {
+        queryInfo: {
+          spatial: {
+            geoJson: lineGeometry
+          }
+        }
+      }
+    }
+
+    const result = request.transformResponse(response, 'flight path')
+
+    expect(result.spatial.features[0].geometry).toEqual(lineGeometry)
+  })
+
+  test('handles geometry with missing coordinates', () => {
+    const badGeometry = {
+      type: 'Polygon'
+    }
+
+    const response = {
+      data: {
+        queryInfo: {
+          spatial: {
+            geoJson: badGeometry
+          }
+        }
+      }
+    }
+
+    const result = request.transformResponse(response, 'test query')
+
+    expect(result.spatial.features[0].geometry).toEqual(badGeometry)
+  })
+
+  test('handles polygon winding correction when already clockwise', () => {
+    const largeCoordinates = Array.from({ length: 60 }, (_, i) => [i, i])
+    largeCoordinates.push(largeCoordinates[0])
+
+    const largePolygon = {
+      type: 'Polygon',
+      coordinates: [largeCoordinates]
+    }
+
+    const simplifiedPolygon = {
+      type: 'Polygon',
+      coordinates: [[[0, 0], [10, 10], [20, 20], [0, 0]]]
+    }
+
+    simplify.mockReturnValue(simplifiedPolygon)
+    booleanClockwise.mockReturnValue(true)
+
+    const mockResponse = {
+      data: {
+        queryInfo: {
+          spatial: {
+            geoJson: largePolygon
+          }
+        }
+      }
+    }
+
+    const result = request.transformResponse(mockResponse, 'test query')
+
+    expect(booleanClockwise).toHaveBeenCalledWith(simplifiedPolygon.coordinates[0])
+    expect(result.spatial.features[0].geometry).toEqual(simplifiedPolygon)
+  })
+
+  test('handles temporal data with missing startDate', () => {
+    const temporalData = {
+      endDate: '2020-12-31'
+    }
+
+    const convertedTemporal = {
+      startDate: '',
+      endDate: '2020-12-31T23:59:59.999Z'
+    }
+
+    convertNlpTemporalData.mockReturnValue(convertedTemporal)
+
+    const response = {
+      data: {
+        queryInfo: {
+          temporal: temporalData
+        }
+      }
+    }
+
+    const result = request.transformResponse(response, 'partial temporal data')
+
+    expect(result.temporal).toEqual({
+      startDate: '',
+      endDate: '2020-12-31T23:59:59.999Z'
+    })
+  })
+
+  test('handles temporal data with missing endDate', () => {
+    const temporalData = {
+      startDate: '2020-01-01'
+    }
+
+    const convertedTemporal = {
+      startDate: '2020-01-01T00:00:00.000Z',
+      endDate: ''
+    }
+
+    convertNlpTemporalData.mockReturnValue(convertedTemporal)
+
+    const response = {
+      data: {
+        queryInfo: {
+          temporal: temporalData
+        }
+      }
+    }
+
+    const result = request.transformResponse(response, 'partial temporal data')
+
+    expect(result.temporal).toEqual({
+      startDate: '2020-01-01T00:00:00.000Z',
+      endDate: ''
+    })
   })
 })
