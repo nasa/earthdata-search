af59ec7c1f8fc69263efb7244f5e169d1c32bcb4
Matthew Crouch <mcrouch@innovim.com>
2025-09-11 13:38:11 -0400

    EDSC-4501: Fixes flaky playwright map test

--- static/src/js/containers/MapContainer/MapContainer.tsx
+++ static/src/js/containers/MapContainer/MapContainer.tsx
@@ -424,15 +424,6 @@ export const MapContainer: React.FC<MapContainerProps> = (props) => {
     allRemovedGranuleIds.push(...removedGranuleIds)
   }
 
-  // Generate a key based on the nonExcludedGranules, addedGranuleIds, gibsTagProduct, and removedGranuleIds,
-  // and gibs tags. `granulesKey` is used to prevent unnecessary rerenders in the Map component.
-  const granulesKey = Buffer.from(JSON.stringify({
-    gibsTagProduct: gibsTag?.product || '',
-    nonExcludedGranuleIds: Object.keys(nonExcludedGranules),
-    addedGranuleIds: allAddedGranuleIds,
-    removedGranuleIds: allRemovedGranuleIds
-  })).toString('base64')
-
   // Generate the granulesToDraw based on the nonExcludedGranules and the addedGranuleIds and removedGranuleIds
   const granulesToDraw: MapGranule[] = []
   const granuleIds = Object.keys(nonExcludedGranules)
@@ -536,6 +527,13 @@ export const MapContainer: React.FC<MapContainerProps> = (props) => {
 
   // If the panels or sidebar are not visible, don't render the map
   if (panelsWidth === 0 || sidebarWidth === 0) return null
+
+  // Generate a key based on the granules that need to be drawn on the map, and the gibsTagProduct.
+  // `granulesKey` is used to prevent unnecessary rerenders in the Map component.
+  const granulesKey = Buffer.from(JSON.stringify({
+    gibsTagProduct: gibsTag?.product || '',
+    granulesToDraw: granulesToDraw.map((granule) => granule.granuleId)
+  })).toString('base64')
 
   return (
     <Map
