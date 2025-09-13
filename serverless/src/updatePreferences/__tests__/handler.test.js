37ca938b3cd52de5be6d25472270a8ba5e23ecc8
Min Htet Myet (Mattral) <88831350+Mattral@users.noreply.github.com>
2025-09-13 08:00:03 +0100

    Update handler.test.js clean code

--- serverless/src/updatePreferences/__tests__/handler.test.js
+++ serverless/src/updatePreferences/__tests__/handler.test.js
@@ -1,86 +1,86 @@
-import knex from 'knex'
-import mockKnex from 'mock-knex'
+import knex from 'knex';
+import mockKnex from 'mock-knex';
 
-import updatePreferences from '../handler'
-import * as getJwtToken from '../../util/getJwtToken'
-import * as getVerifiedJwtToken from '../../util/getVerifiedJwtToken'
-import * as getDbConnection from '../../util/database/getDbConnection'
+import updatePreferences from '../handler';
+import * as getJwtToken from '../../util/getJwtToken';
+import * as getVerifiedJwtToken from '../../util/getVerifiedJwtToken';
+import * as getDbConnection from '../../util/database/getDbConnection';
 
-let dbConnectionToMock
-let dbTracker
+let dbConnectionToMock;
+let dbTracker;
 
 beforeEach(() => {
-  jest.clearAllMocks()
-  jest.spyOn(getJwtToken, 'getJwtToken').mockImplementation(() => 'mockJwt')
-  jest.spyOn(getVerifiedJwtToken, 'getVerifiedJwtToken').mockImplementation(() => ({
-    id: 1,
-    username: 'testuser'
-  }))
+  jest.clearAllMocks();
 
-  jest.spyOn(getDbConnection, 'getDbConnection').mockImplementationOnce(() => {
-    dbConnectionToMock = knex({
-      client: 'pg',
-      debug: false
-    })
+  jest.spyOn(getJwtToken, 'getJwtToken')
+    .mockImplementation(() => 'mockJwt');
 
-    // Mock the db connection
-    mockKnex.mock(dbConnectionToMock)
+  jest.spyOn(getVerifiedJwtToken, 'getVerifiedJwtToken')
+    .mockImplementation(() => ({
+      id: 1,
+      username: 'testuser'
+    }));
 
-    return dbConnectionToMock
-  })
+  jest.spyOn(getDbConnection, 'getDbConnection')
+    .mockImplementationOnce(() => {
+      dbConnectionToMock = knex({
+        client: 'pg',
+        debug: false
+      });
 
-  dbTracker = mockKnex.getTracker()
-  dbTracker.install()
-})
+      mockKnex.mock(dbConnectionToMock);
+      return dbConnectionToMock;
+    });
+
+  dbTracker = mockKnex.getTracker();
+  dbTracker.install();
+});
 
 afterEach(() => {
-  dbTracker.uninstall()
-})
+  dbTracker.uninstall();
+});
 
 describe('updatePreferences', () => {
   test('updates the users table with new site_preferences', async () => {
     dbTracker.on('query', (query, step) => {
       if (step === 1) {
-        query.response([{ id: 1 }])
+        query.response([{ id: 1 }]);
       } else {
-        query.response(undefined)
+        query.response(undefined);
       }
-    })
+    });
 
     const preferences = {
       panelState: 'default',
       collectionListView: 'default',
       granuleListView: 'default'
-    }
+    };
+
     const event = {
       body: JSON.stringify({
-        params: {
-          preferences
-        }
+        params: { preferences }
       })
-    }
+    };
 
-    const result = await updatePreferences(event, {})
+    const result = await updatePreferences(event, {});
+    const expectedBody = JSON.stringify({ preferences });
 
-    const expectedBody = JSON.stringify({ preferences })
+    const { queries } = dbTracker.queries;
 
-    const { queries } = dbTracker.queries
-    expect(queries[0].method).toEqual('update')
+    expect(queries[0].method).toEqual('update');
+    expect(result.body).toEqual(expectedBody);
+  });
 
-    expect(result.body).toEqual(expectedBody)
-  })
+  test('does not update if the preferences don\'t validate against the JSON schema', async () => {
+    const preferences = { badKey: 'default' };
 
-  test('does not update if the preferences don\'t validate against the json schema', async () => {
-    const preferences = { badKey: 'default' }
     const event = {
       body: JSON.stringify({
-        params: {
-          preferences
-        }
+        params: { preferences }
       })
-    }
+    };
 
-    const result = await updatePreferences(event, {})
+    const result = await updatePreferences(event, {});
 
     expect(result.body).toEqual(JSON.stringify({
       statusCode: 500,
@@ -88,36 +88,35 @@ describe('updatePreferences', () => {
         keyword: 'additionalProperties',
         dataPath: '',
         schemaPath: '#/additionalProperties',
-        params: { additionalProperty: 'badKey' },
+        params: {
+          additionalProperty: 'badKey'
+        },
         message: 'should NOT have additional properties'
       }])]
-    }))
-  })
+    }));
+  });
 
-  test('correctly returns an error', async () => {
+  test('correctly returns an error on query failure', async () => {
     dbTracker.on('query', (query) => {
-      query.reject('Unknown Error')
-    })
+      query.reject('Unknown Error');
+    });
 
     const preferences = {
       panelState: 'default',
       collectionListView: 'default',
       granuleListView: 'default'
-    }
+    };
+
     const event = {
       body: JSON.stringify({
-        params: {
-          preferences
-        }
+        params: { preferences }
       })
-    }
+    };
 
-    const response = await updatePreferences(event, {})
+    const response = await updatePreferences(event, {});
+    const { queries } = dbTracker.queries;
 
-    const { queries } = dbTracker.queries
-
-    expect(queries[0].method).toEqual('update')
-
-    expect(response.statusCode).toEqual(500)
-  })
-})
+    expect(queries[0].method).toEqual('update');
+    expect(response.statusCode).toEqual(500);
+  });
+});
