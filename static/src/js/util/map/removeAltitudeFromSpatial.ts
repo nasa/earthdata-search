436d5858e921a73169dee90b80e3645cae3c3a28
Matthew Crouch <mcrouch@innovim.com>
2025-09-15 13:57:17 -0400

    EDSC-4521: Simplify code

--- static/src/js/util/map/removeAltitudeFromSpatial.ts
+++ static/src/js/util/map/removeAltitudeFromSpatial.ts
@@ -1,36 +1,23 @@
 import type { Point, SpatialCoordinates } from '../../types/sharedTypes'
 
 /**
- * Recursively removes altitude information from spatial coordinates.
- * @param coordinates The spatial coordinates which may include altitude information
- * @returns The spatial coordinates with altitude information removed
- */
-const removeAltitudeFromCoordinates = (coordinates: SpatialCoordinates): SpatialCoordinates => {
-  // If the first value in coordinates is a number, return only the first two elements (longitude and latitude)
-  if (typeof coordinates[0] === 'number') {
-    const [lon, lat] = coordinates as Point
-
-    return [lon ?? 0, lat ?? 0]
-  }
-
-  // Recursively remove altitude from nested coordinates
-  return coordinates.map(
-    (coordinate) => removeAltitudeFromCoordinates(coordinate as SpatialCoordinates)
-  ) as SpatialCoordinates
-}
-
-/**
- * Removes the altitude (3rd value) from spatial coordinates. Input can be a Point, MultiPoint,
+ * Recursively removes altitude (3rd value) from spatial coordinates. Input can be a Point, MultiPoint,
  * Line, MultiLine, Polygon, or MultiPolygon.
  */
 const removeAltitudeFromSpatial = (spatial: SpatialCoordinates): SpatialCoordinates => {
   if (!spatial) return spatial
 
-  if (Array.isArray(spatial)) {
-    return removeAltitudeFromCoordinates(spatial)
+  // If the first value in coordinates is a number, return only the first two elements (longitude and latitude)
+  if (typeof spatial[0] === 'number') {
+    const [lon, lat] = spatial as Point
+
+    return [lon ?? 0, lat ?? 0]
   }
 
-  return spatial
+  // Recursively remove altitude from nested coordinates
+  return spatial.map(
+    (coordinate) => removeAltitudeFromSpatial(coordinate as SpatialCoordinates)
+  ) as SpatialCoordinates
 }
 
 export default removeAltitudeFromSpatial
