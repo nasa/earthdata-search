f804e31a0e2c1423fa63fc2c16f0808483a54c1a
Ed Olivares <34591886+eudoroolivares2016@users.noreply.github.com>
2025-09-16 22:29:05 -0400

    EDSC-4543: Cleanup tests and typing

--- static/src/js/components/Legend/__tests__/Legend.test.tsx
+++ static/src/js/components/Legend/__tests__/Legend.test.tsx
@@ -1,21 +1,68 @@
+import React from 'react'
+
 import { screen } from '@testing-library/react'
 
 import Legend from '../Legend'
 import setupTest from '../../../../../../jestConfigs/setupTest'
+import { ImageryLayers } from '../../../types/sharedTypes'
 
-const mockGranuleImageryLayerGroup = {
-  getLayers: jest.fn(() => ({
-    getArray: jest.fn(() => [])
-  }))
+// Mock the LayerPicker component
+jest.mock('../../LayerPicker/LayerPicker', () => jest.fn(() => <div data-testid="layer-picker" />))
+
+const mockCollectionId = 'test-collection'
+
+// Create mock functions
+const mockToggleLayerVisibility = jest.fn()
+const mockSetMapLayersOrder = jest.fn()
+const mockUpdateLayerOpacity = jest.fn()
+
+const mockImageryLayersWithLayers: ImageryLayers = {
+  layerData: [
+    {
+      product: 'IMERG_Precipitation_Rate',
+      title: 'Precipitation Rate',
+      colormap: {
+        scale: {
+          colors: [],
+          labels: []
+        }
+      },
+      isVisible: true,
+      opacity: 1
+    },
+    {
+      product: 'IMERG_Precipitation_Rate_30min',
+      title: 'Precipitation Rate (30-min)',
+      colormap: {
+        scale: {
+          colors: [],
+          labels: []
+        }
+      },
+      isVisible: false,
+      opacity: 0.5
+    }
+  ],
+  toggleLayerVisibility: mockToggleLayerVisibility,
+  setMapLayersOrder: mockSetMapLayersOrder,
+  updateLayerOpacity: mockUpdateLayerOpacity
+}
+
+const mockImageryLayersEmpty: ImageryLayers = {
+  layerData: [],
+  toggleLayerVisibility: mockToggleLayerVisibility,
+  setMapLayersOrder: mockSetMapLayersOrder,
+  updateLayerOpacity: mockUpdateLayerOpacity
 }
 
 const setup = setupTest({
   Component: Legend,
   defaultProps: {
-    collectionId: 'test-collection',
-    colorMap: {}
+    collectionId: mockCollectionId,
+    imageryLayers: mockImageryLayersEmpty
   }
 })
+
 describe('Legend', () => {
   test('renders the legend container', () => {
     setup()
@@ -26,25 +73,27 @@ describe('Legend', () => {
     expect(legend).toHaveClass('legend')
   })
 
-  test('renders LayerPicker when granuleImageryLayerGroup is provided', () => {
+  test('renders LayerPicker when imageryLayers has layers', () => {
     setup({
       overrideProps: {
-        granuleImageryLayerGroup: mockGranuleImageryLayerGroup
+        imageryLayers: mockImageryLayersWithLayers
       }
     })
 
     const legend = screen.getByTestId('legend')
-
     expect(legend).toBeInTheDocument()
-    // LayerPicker should be rendered (we can't easily test its content without mocking it)
+
+    // LayerPicker should be rendered with the layer data
+    expect(screen.getByTestId('layer-picker')).toBeInTheDocument()
   })
 
-  test('does not render LayerPicker when granuleImageryLayerGroup is not provided', () => {
+  test('does not render LayerPicker when imageryLayers has no layers', () => {
     setup()
 
     const legend = screen.getByTestId('legend')
 
     expect(legend).toBeInTheDocument()
-    expect(legend.innerHTML).toBe('')
+    expect(legend).toBeEmptyDOMElement()
+    expect(screen.queryByTestId('layer-picker')).not.toBeInTheDocument()
   })
 })
