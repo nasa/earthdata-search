6f3f642571e8b68800e64b335b0122982646d46f
Trevor Lang <trevorlang@ymail.com>
2025-09-17 20:13:23 -0600

    EDSC-4529: Adding definition list components

---
+++ static/src/js/components/DefinitionList/DefinitionList.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { Row } from 'react-bootstrap'
+
+import DefinitionListItem, {
+  DefinitionListItemProps
+} from '../DefinitionListItem/DefinitionListItem'
+
+/**
+ * Props for a definition list component
+ */
+export interface DefinitionListProps {
+  /** The items to display in the definition list. Nested arrays of items will be displayed in separate rows. */
+  items: DefinitionListItemProps[] | DefinitionListItemProps[][]
+}
+
+/**
+ * A definition list component to display a list of definitions as a groups with a label and value in a grid
+ */
+const DefinitionList = ({ items }: DefinitionListProps) => (
+  <dl>
+    {
+      items.map((item) => {
+        if (Array.isArray(item)) {
+          return (
+            <Row key={item.map((subItem) => subItem.label).join('-')} className="mb-4">
+              {
+                item.map((subItem) => (
+                  <DefinitionListItem key={subItem.label} {...subItem} />
+                ))
+              }
+            </Row>
+          )
+        }
+
+        return (
+          <Row key={item.label} className="mb-4">
+            <DefinitionListItem {...item} />
+          </Row>
+        )
+      })
+    }
+  </dl>
+)
+
+export default DefinitionList
---
+++ static/src/js/components/DefinitionListItem/DefinitionListItem.stories.tsx
@@ -0,0 +1,34 @@
+import DefinitionListItem from './DefinitionListItem'
+
+export default {
+  title: 'Components/DefinitionListItem',
+  component: DefinitionListItem,
+  parameters: {
+    layout: 'padded'
+  },
+  tags: ['autodocs']
+}
+
+export const Basic = {
+  args: {
+    label: 'Term',
+    value: 'Definition'
+  }
+}
+
+export const WithColProps = {
+  args: {
+    label: 'Wide Term',
+    value: 'This column is wider',
+    colProps: {
+      xs: 8
+    }
+  }
+}
+
+export const WithReactNodeValue = {
+  args: {
+    label: 'Custom Content',
+    value: <span style={{ color: 'red' }}>This value is a ReactNode</span>
+  }
+}
