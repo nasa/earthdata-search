fea93847ed6a7815b21c6dfc66e9b302180e64b3
William Valencia <william.m.valencia@nasa.gov>
2025-10-14 23:38:38 -0400

    EDSC-4354: Fixing tests

--- serverless/src/scaleImage/utils/cache/__tests__/generateCacheKey.test.js
+++ serverless/src/scaleImage/utils/cache/__tests__/generateCacheKey.test.js
@@ -2,42 +2,33 @@ import { generateCacheKey } from '../generateCacheKey'
 
 describe('generateCacheKey', () => {
   test('when provided a height and a width', () => {
-    const cacheKey = generateCacheKey('C100000-EDSC', 'datasets', undefined, {
+    const cacheKey = generateCacheKey('http://test.com/test.jpg', {
       height: 200,
       width: 200
     })
 
-    expect(cacheKey).toEqual('C100000-EDSC-datasets-200-200')
+    expect(cacheKey).toEqual('http://test.com/test.jpg-200-200')
   })
 
   test('when provided only a height', () => {
-    const cacheKey = generateCacheKey('C100000-EDSC', 'datasets', undefined, {
+    const cacheKey = generateCacheKey('http://test.com/test.jpg', {
       height: 200
     })
 
-    expect(cacheKey).toEqual('C100000-EDSC-datasets-200-w')
+    expect(cacheKey).toEqual('http://test.com/test.jpg-200-w')
   })
 
   test('when provided only a width', () => {
-    const cacheKey = generateCacheKey('C100000-EDSC', 'datasets', undefined, {
+    const cacheKey = generateCacheKey('http://test.com/test.jpg', {
       width: 200
     })
 
-    expect(cacheKey).toEqual('C100000-EDSC-datasets-h-200')
+    expect(cacheKey).toEqual('http://test.com/test.jpg-h-200')
   })
 
   test('when provided no dimensions', () => {
-    const cacheKey = generateCacheKey('C100000-EDSC', 'datasets')
+    const cacheKey = generateCacheKey('http://test.com/test.jpg')
 
-    expect(cacheKey).toEqual('C100000-EDSC-datasets-h-w')
-  })
-
-  test('when `imageSrc` is being passed', () => {
-    const cacheKey = generateCacheKey('C100000-EDSC', 'datasets', 'https://example.com', {
-      height: 20,
-      width: 20
-    })
-
-    expect(cacheKey).toEqual('C100000-EDSC-datasets-20-20-https://example.com')
+    expect(cacheKey).toEqual('http://test.com/test.jpg-h-w')
   })
 })
