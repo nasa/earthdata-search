43871a58514539946c98ff05905038ac6928f29b
Mandy Parson <amanda.parson@nasa.gov>
2025-10-21 10:14:58 -0600

    EDSC-4575: Improve granule gibs imagery drawing

--- static/src/js/types/sharedTypes.ts
+++ static/src/js/types/sharedTypes.ts
@@ -181,6 +181,18 @@ export type GranulesMetadata = {
   [key: string]: GranuleMetadata
 }
 
+export type ProjectionCode = keyof typeof crsProjections
+
+export type GibsDataByCollection = {
+  /** The collection id */
+  [collectionId: string]: {
+    /** The map layers for the collection */
+    layers: MapLayer[]
+    /** The projection for the collection */
+    projection: ProjectionCode
+  }
+}
+
 export type GibsData = {
   /** The format of the GIBS product */
   format?: string
@@ -209,8 +221,6 @@ export type MapGranule = {
   collectionId: string
   /** The temporal value formatted for display */
   formattedTemporal: string
-  /** The GIBS metadata */
-  gibsData: GibsData[]
   /** The granule id */
   granuleId: string
   /** The granule style */
@@ -219,6 +229,8 @@ export type MapGranule = {
   highlightedStyle: Style
   /** The spatial value for the granule */
   spatial: GeoJsonObject
+  /** The time value for the granule */
+  time: string
 }
 export type ColormapScale = {
   /** The scale object contains colors and labels */
@@ -332,8 +344,6 @@ export type PortalConfig = {
     showTophat: boolean
   }
 }
-
-export type ProjectionCode = keyof typeof crsProjections
 
 /** CMR specific headers */
 export interface CmrHeaders {
