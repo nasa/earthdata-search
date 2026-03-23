37118c4071bea52cdc81f2a6ec54643a4afafc79
Ben Sultzer <benjaminsultzer84@gmail.com>
2026-03-23 09:55:18 -0400

    EDSC-4635: Have basic script/logic finished. Requires debugging

--- static/src/js/util/map/heatmap/spatialToHeatmap.ts
+++ static/src/js/util/map/heatmap/spatialToHeatmap.ts
@@ -1,3 +1,4 @@
+import { GranuleMetadata } from '../../../types/sharedTypes'
 import useEdscStore from '../../../zustand/useEdscStore'
 
 const COLOR_PARTITIONS = new Array(6)
@@ -7,7 +8,26 @@ const COLOR_PARTITIONS = new Array(6)
  * frequency scale for holding granules that belong to that color's
  * category
  */
-const HEATMAP = Array.from({ length: 5 }, () => [])
+const HEATMAP: GranuleMetadata[][] = Array.from({ length: 5 }, () => new Array(0))
+
+/**
+ * Returns the corrct position in the heatmap Arrays to place a granule
+ */
+function placeGranule(granule: GranuleMetadata) {
+  const granuleStart = Date.parse(granule.timeStart)
+  let insertLocation = 0
+  for (let i = 0; i < COLOR_PARTITIONS.length; i += 1) {
+    if (granuleStart < COLOR_PARTITIONS[i]) {
+      insertLocation = i - 1
+    }
+  }
+
+  if (insertLocation < 0) {
+    return 0
+  } else {
+    return insertLocation
+  }
+}
 
 /**
  * Create an array that stores all granules within the index that
@@ -22,17 +42,7 @@ function constructHeatmap() {
 
   // Loop through the list of granules and insert them into their proper categories
   for (let i = 0; i < granules.length; i += 1) {
-    for (let j = 0; j < COLOR_PARTITIONS.length; j += 1) {
-      if (Date.parse(granules[i].timeStart) < COLOR_PARTITIONS[j]) {
-        HEATMAP[j - 1].push(granules[i])
-        console.log(`
-          i: ${i}\n
-          j: ${j}\n
-          granules[i]: ${granules[i]}\n
-          HEATMAP: ${HEATMAP.toString()}
-        `)
-      }
-    }
+    HEATMAP[placeGranule(granules[i])].push(granules[i])
   }
 }
 
@@ -47,7 +57,7 @@ function constructHeatmap() {
  */
 function createFrequencyScale() {
   const state = useEdscStore.getState()
-  const collection = state.collection.collectionMetadata['C1327985645-ASF']
+  const collection = state.collection.collectionMetadata['C1327985661-ASF']
 
   // Convert the start and end times to a numeric representation
   const numericStartTime = Date.parse(collection.timeStart)
@@ -75,13 +85,13 @@ async function spatialToHeatmap() {
   const setCollectionId = state.collection.setCollectionId
   const getCollectionMetadata = state.collection.getCollectionMetadata
 
-  await setCollectionId('C1327985645-ASF')
+  await setCollectionId('C1327985661-ASF')
   await getCollectionMetadata()
 
   // Get the updated state
   const updatedState = useEdscStore.getState()
 
-  // Get the granules of the C1327985645-ASF collection
+  // Get the granules of the C1327985661-ASF collection
   updatedState.granules.getGranules()
 
   createFrequencyScale()
