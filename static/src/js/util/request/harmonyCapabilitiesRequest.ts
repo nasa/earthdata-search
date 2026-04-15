efea6af1d6f494c2aee452aad20abcc2e58622fb
Mandy Parson <amanda.parson@nasa.gov>
2026-04-15 09:00:17 -0600

    EDSC-4642: Editing static.config

--- static/src/js/util/request/harmonyCapabilitiesRequest.ts
+++ static/src/js/util/request/harmonyCapabilitiesRequest.ts
@@ -1,10 +1,10 @@
 import Request from './request'
 // @ts-expect-error This file does not have types
-import { getEnvironmentConfig } from '../../../../../sharedUtils/config'
+import { getEarthdataConfig } from '../../../../../sharedUtils/config'
 
 export default class HarmonyCapabilitiesRequest extends Request {
   constructor(edlToken: string, earthdataEnvironment: string) {
-    super(getEnvironmentConfig().harmonyHost, earthdataEnvironment)
+    super(getEarthdataConfig(earthdataEnvironment).harmonyHost, earthdataEnvironment)
 
     this.authenticated = true
     this.edlToken = edlToken
