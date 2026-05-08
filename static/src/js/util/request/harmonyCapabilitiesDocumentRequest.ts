9ba29f0f10754d5da5203ec2a3eb59811a31faf6
Mandy Parson <135638667+mandyparson@users.noreply.github.com>
2026-05-08 13:33:11 -0600

    EDSC-4642: As a user, I would like to set temporal, spatial, or output format capabilities and know which parameters are still valid (#2042)
    * EDSC-4642: Initial Commit
    
    * EDSC-4642: Editing static.config
    
    * EDSC-4642: Edits to static.config
    
    * EDSC-4642: Updating based on community feedback
    
    * EDSC-4642: Fixing tests for all but AccessMethod
    
    * EDSC-4642: Fixing tests
    
    * EDSC-4642: Adding version to url params
    
    * EDSC-4642: Structural changes to store
    
    * EDSC-4642: A bit of clean up
    
    * EDSC-4642: Cleaning up as per feedback
    
    * EDSC-4642: PR feedback

---
+++ static/src/js/util/request/harmonyCapabilitiesDocumentRequest.ts
@@ -0,0 +1,18 @@
+import Request from './request'
+// @ts-expect-error This file does not have types
+import { getEarthdataConfig } from '../../../../../sharedUtils/config'
+import { HarmonyRequestParams } from '../../types/sharedTypes'
+
+export default class HarmonyCapabilitiesDocumentRequest extends Request {
+  constructor(edlToken: string, earthdataEnvironment: string) {
+    super(getEarthdataConfig(earthdataEnvironment).harmonyHost, earthdataEnvironment)
+
+    this.authenticated = true
+    this.edlToken = edlToken
+    this.searchPath = 'capabilities'
+  }
+
+  search(params: HarmonyRequestParams) {
+    return this.getSearch(params)
+  }
+}
