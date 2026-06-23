328c64d650c52a97337fe9b988279ea5016aeff5
Matthew Crouch <mcrouch@innovim.com>
2026-06-23 11:48:11 -0400

    EDSC-4688: Updates @edsc/cdk-utils, sets allowMajorVersionUpgrade

--- cdk/earthdata-search-infrastructure/lib/earthdata-search-infrastructure-stack.ts
+++ cdk/earthdata-search-infrastructure/lib/earthdata-search-infrastructure-stack.ts
@@ -164,6 +164,7 @@ export class EarthdataSearchInfrastructureStack extends cdk.Stack {
     // eslint-disable-next-line no-new
     new infrastructure.Database(this, 'EDSCDatabase', {
       allocatedStorage: DB_ALLOCATED_STORAGE,
+      allowMajorVersionUpgrade: true,
       appName: 'EDSC',
       databaseUsername: 'edsc',
       dbInstanceClass: DB_INSTANCE_CLASS,
