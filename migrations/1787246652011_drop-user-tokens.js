92e9dbea085fd61fdd3ef2d075a3e922a74203ff
Matthew Crouch <mcrouch@innovim.com>
2026-08-20 13:28:52 -0400

    EDSC-4695: Drops user_tokens table

---
+++ migrations/1787246652011_drop-user-tokens.js
@@ -0,0 +1,43 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined
+
+exports.up = (pgm) => {
+  pgm.dropTable('user_tokens')
+}
+
+exports.down = (pgm) => {
+  pgm.createTable('user_tokens', {
+    id: 'id',
+    user_id: {
+      type: 'integer',
+      references: 'users'
+    },
+    environment: {
+      type: 'varchar(100)',
+      notNull: true
+    },
+    access_token: {
+      type: 'varchar(100)',
+      notNull: true
+    },
+    refresh_token: {
+      type: 'varchar(100)',
+      notNull: true
+    },
+    expires_at: {
+      type: 'timestamp',
+      notNull: true
+    },
+    updated_at: {
+      type: 'timestamp',
+      notNull: true,
+      default: pgm.func('current_timestamp')
+    },
+    created_at: {
+      type: 'timestamp',
+      notNull: true,
+      default: pgm.func('current_timestamp')
+    }
+  })
+}
